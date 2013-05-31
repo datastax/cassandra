ce66b8779afbb8b0781df74476730e639d782126
beobal <sam@beobal.com>
2013-05-31 15:55:57 +0100

    DSP-1957 Add check to ensure we don't allow CredentialsMessage when we shouldn't

--- src/java/org/apache/cassandra/transport/messages/CredentialsMessage.java
+++ src/java/org/apache/cassandra/transport/messages/CredentialsMessage.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.cassandra.auth.AuthenticatedUser;
+import org.apache.cassandra.auth.PasswordAuthenticator;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.transport.ProtocolException;
 import org.jboss.netty.buffer.ChannelBuffer;
@@ -79,6 +80,34 @@ public class CredentialsMessage extends Message.Request
 
     public Message.Response execute(QueryState state)
     {
+        // C* 1.2 for DSE 3.1 includes a backport of CASSANDRA-5545
+        // to allow us to plugin in Kerberos authentication. However,
+        // because we want to stay as compatible as possible with
+        // Apache C* and the community tools/drivers etc, we don't
+        // bump the protocol version, which means that a DSE server
+        // can be configured with KerberosAuthenticator, but clients
+        // may still send v1 CREDENTIALS messages. In such a case, the
+        // Kerberos authentication would be bypassed & users could log
+        // in with any credentials.
+        // To prevent that, here we verify the IAuthenticator class &
+        // reject the request if it is anything other than
+        // PasswordAuthenticator.
+
+        // An alternative would be to bump the protocol version to 2,
+        // but
+        // a) version 2 isn't complete yet - the SASL part should be
+        // b) it would mean pulling in more feature that we don't want to
+        // support yet & may cause issues for clients who don't need
+        // kerberos and want to use the open source drivers.
+
+        // This will cease to be an issue when we eventually catch up
+        // to the protocol in C* - i.e. when DSE moves to C* 2.0
+        Class c = DatabaseDescriptor.getAuthenticator().getClass();
+        if (! PasswordAuthenticator.class.isAssignableFrom(c))
+        {
+            return ErrorMessage.fromException(new AuthenticationException("Unsupported Authenticator " + c.getName()));
+        }
+
         try
         {
             AuthenticatedUser user = DatabaseDescriptor.getAuthenticator().authenticate(credentials);
