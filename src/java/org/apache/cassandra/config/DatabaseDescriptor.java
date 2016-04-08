b84acceb3165fafe61a86336e78505132d5c38fa
Josh McKenzie <josh.mckenzie@datastax.com>
2016-04-08 16:42:44 -0400

    make CDC designation DC-aware on write path

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -2013,6 +2013,12 @@ public class DatabaseDescriptor
         return localDC;
     }
 
+    @VisibleForTesting
+    public static void setLocalDataCenter(String input)
+    {
+        localDC = input;
+    }
+
     public static Comparator<InetAddress> getLocalComparator()
     {
         return localComparator;
