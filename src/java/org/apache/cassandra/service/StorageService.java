133581cd1087dbf66bb2d68583134b77376fe3d1
Joel Knighton <joel.knighton@datastax.com>
2017-05-16 10:23:06 -0500

    Revert "Fix isJoined return true only after becoming cluster member"
    This reverts commit 8408c76aff1a5fbb7ea8984aaeced55c076b00a9.

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -1095,7 +1095,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public boolean isJoined()
     {
-        return tokenMetadata.isMember(FBUtilities.getBroadcastAddress()) && !isSurveyMode;
+        return joined && !isSurveyMode;
     }
 
     public void rebuild(String sourceDc)
