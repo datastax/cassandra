ab49dcc3b49b8744524c00918793007c2517542d
Brandon Williams <brandon@datastax.com>
2017-05-18 12:18:28 -0500

    Increase logging

--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -994,7 +994,7 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
         localState.markDead();
 
         MessageOut<EchoMessage> echoMessage = new MessageOut<EchoMessage>(MessagingService.Verb.ECHO, EchoMessage.instance, EchoMessage.serializer);
-        logger.trace("Sending a EchoMessage to {}", addr);
+        logger.debug("Sending a EchoMessage to {}", addr);
         pendingEcho++;
         IAsyncCallbackWithFailure echoHandler = new IAsyncCallbackWithFailure()
         {
