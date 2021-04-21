179bbb3bd7de0398bcfc175949444a1071be9784
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2021-04-21 14:46:36 +0200

    STAR-452: Add EverywhereStrategy (#111)

--- src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
+++ src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
@@ -273,6 +273,14 @@ public abstract class AbstractReplicationStrategy
         return getAddressReplicas(tokenMetadata.cloneOnlyTokenMap(), endpoint);
     }
 
+    /**
+     * Returns the number of token-owning nodes.
+     */
+    protected int getSizeOfRingMemebers()
+    {
+        return tokenMetadata.getAllRingMembers().size();
+    }
+
     public RangesAtEndpoint getPendingAddressRanges(TokenMetadata metadata, Token pendingToken, InetAddressAndPort pendingAddress)
     {
         return getPendingAddressRanges(metadata, Collections.singleton(pendingToken), pendingAddress);
@@ -356,6 +364,14 @@ public abstract class AbstractReplicationStrategy
 
         strategy.validateOptions();
         return strategy;
+    }
+
+    /**
+     * Whether this strategy partitions data across the ring
+     */
+    public boolean isPartitioned()
+    {
+        return true;
     }
 
     /**
