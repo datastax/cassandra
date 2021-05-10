9fb053ec2bfe798bec17190c9156c3e7dcf40dca
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2021-05-10 17:07:24 +0200

    STAR-530 SchemaKeyspace is compatible with riptano/6.8-cndb (#133)
    * STAR-530 SchemaKeyspace is compatible with riptano/6.8-cndb
    
    Add missing columns in SchemaKeyspace, which results in sstable
    compatibility with riptano/6.8-cndb (used by CNDB). This makes
    full-stop upgrade from Astra serverless to dse-db possible.
    
    Additionally fix reading commit log coming from riptano/6.8-cndb.
    Commit log version 680 is read with MessagingService.VERSION_40
    messaging version but in truth it doesn't matter as the version
    is never used in deserialization.
    
    * STAR-530 add UCS mock
    
    Add temp UCS mock to support full-stop upgrades from CNDB to
    dse-db. The mock should be removed by UCS feature PR.
    
    (cherry picked from commit 03ad8bab34840501cacf1bcee2028ab2a62cbcdf)
    (cherry picked from commit a4598b5346c9fbf3f46a1ffe96e004df412f3310)
    (cherry picked from commit 2492b498d13076fd78bc96d4d09295e359b982bd)
    (cherry picked from commit 805ab0551420c4cbf0ef14e5f8c06a224f7c84aa)
    (cherry picked from commit a4eec2f4ee5ea6b4522572fa8f76d6e07a19d2e9)
    (cherry picked from commit 126aabc5666a5ffed7b88e555ccadf89acaf1904)
    (cherry picked from commit 16620ed3ed18522fda1659fbcf08f6fa54954ac5)
    (cherry picked from commit 206504fd0a108ed6f05c7af9c42751fbe3358ed5)
    (cherry picked from commit 7d4879ba614592acec1fd77cfe2e21640f2e7051)
    
     (Rebase of commit e15919743ae713c506f3289cad06d48f817ebf0f)
    
     (Rebase of commit 6e87b81f1605478bd7cbd3e15b4b2fcb88803748)

--- src/java/org/apache/cassandra/schema/SchemaKeyspace.java
+++ src/java/org/apache/cassandra/schema/SchemaKeyspace.java
@@ -94,6 +94,7 @@ public final class SchemaKeyspace
               + "keyspace_name text,"
               + "durable_writes boolean,"
               + "replication frozen<map<text, text>>,"
+              + "graph_engine text,"
               + "PRIMARY KEY ((keyspace_name)))");
 
     // auto_repair column is only included if AUTOREPAIR_ENABLE is true to avoid schema disagreement
@@ -122,6 +123,7 @@ public final class SchemaKeyspace
               + "max_index_interval int,"
               + "memtable_flush_period_in_ms int,"
               + "min_index_interval int,"
+              + "nodesync frozen<map<text, text>>,"
               + "read_repair_chance double," // no longer used, left for drivers' sake
               + "speculative_retry text,"
               + "additional_write_policy text,"
@@ -142,6 +144,7 @@ public final class SchemaKeyspace
               + "kind text,"
               + "position int,"
               + "type text,"
+              + "required_for_liveness boolean,"
               + "PRIMARY KEY ((keyspace_name), table_name, column_name))");
 
     private static final TableMetadata ColumnMasks =
@@ -209,10 +212,12 @@ public final class SchemaKeyspace
               + "max_index_interval int,"
               + "memtable_flush_period_in_ms int,"
               + "min_index_interval int,"
+              + "nodesync frozen<map<text, text>>,"
               + "read_repair_chance double," // no longer used, left for drivers' sake
               + "speculative_retry text,"
               + "additional_write_policy text,"
               + "cdc boolean,"
+              + "version int,"
               + "read_repair text,"
               + (CassandraRelevantProperties.AUTOREPAIR_ENABLE.getBoolean() ? "auto_repair frozen<map<text, text>>," : "")
               + "PRIMARY KEY ((keyspace_name), view_name))");
@@ -250,6 +255,9 @@ public final class SchemaKeyspace
               + "language text,"
               + "return_type text,"
               + "called_on_null_input boolean,"
+              + "deterministic boolean,"
+              + "monotonic boolean,"
+              + "monotonic_on frozen<list<text>>,"
               + "PRIMARY KEY ((keyspace_name), function_name, argument_types))");
 
     private static final TableMetadata Aggregates =
@@ -264,6 +272,7 @@ public final class SchemaKeyspace
               + "return_type text,"
               + "state_func text,"
               + "state_type text,"
+              + "deterministic boolean,"
               + "PRIMARY KEY ((keyspace_name), aggregate_name, argument_types))");
 
     private static final List<TableMetadata> ALL_TABLE_METADATA = ImmutableList.of(Keyspaces,
