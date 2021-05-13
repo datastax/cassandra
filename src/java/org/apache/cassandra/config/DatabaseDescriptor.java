b33812780de85dfef7d631e29636180f83cbb185
dan jatnieks <jatnieks@pobox.com>
2021-05-13 13:18:08 -0700

    STAR-515 Add guardrail for SASI and STAR-512 Add guardrails for total and per-table indexes for SAI (#140)
    * STAR-512 Add guardrails for total and per-table indexes for SAI
    
    Co-authored-by: Paulo Ricardo Motta Gomes <pauloricardomg@users.noreply.github.com>
    
    * STAR-515 Add guardrail for SASI
    
    Co-authored-by: Zhao Yang <jasonstack.zhao@gmail.com>
    
    Co-authored-by: Paulo Ricardo Motta Gomes <pauloricardomg@users.noreply.github.com>
    Co-authored-by: Zhao Yang <jasonstack.zhao@gmail.com>
    (cherry picked from commit e2ae18ead10d7186a76bb1a8299bfcd4ddf3820e)
    (cherry picked from commit eaf8c59b3820a575742370aa6b4936d3783bc49b)
    (cherry picked from commit 753fb6916ba32673a479b8c8e39cec447aa97012)
    (cherry picked from commit 0fafd7b053fc2520219ab8a0243362539696baf8)
    (cherry picked from commit 41d26d3e2dc86f8be5e1639ac68d684eb3e57772)
    (cherry picked from commit 3a40eae385b31cac03b61cb81fd5042c0f93964c)
    (cherry picked from commit 203e112c8ac22fb9141913b15e3b9d9b5b36f30a)
    (cherry picked from commit d508237f522f941c33c00c5ad97beb9d7ab09aab)
    (cherry picked from commit 5f8f130ba6d9db65441a334c36ae3d29c66cca79)

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -4933,6 +4933,12 @@ public class DatabaseDescriptor
         conf.sai_options.zerocopy_used_threshold = threshold;
     }
 
+    @VisibleForTesting
+    public static boolean setApplyDbaasDefaults(boolean dbaasDefaults)
+    {
+        return conf.apply_dbaas_defaults = dbaasDefaults;
+    }
+
     public static boolean isApplyDbaasDefaults()
     {
         return conf.apply_dbaas_defaults;
--- src/java/org/apache/cassandra/cql3/statements/schema/CreateIndexStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/schema/CreateIndexStatement.java
@@ -18,6 +18,7 @@
 package org.apache.cassandra.cql3.statements.schema;
 
 import java.util.*;
+import java.util.stream.StreamSupport;
 
 import com.google.common.base.Strings;
 import com.google.common.annotations.VisibleForTesting;
@@ -204,6 +205,27 @@ public final class CreateIndexStatement extends AlterSchemaStatement
 
         IndexMetadata index = IndexMetadata.fromIndexTargets(indexTargets, name, kind, options);
 
+        String className = index.getIndexClassName();
+        IndexGuardrails guardRails = IndexGuardrails.forClassName(className);
+        String indexDescription = indexName == null ? String.format("on table %s", table.name) : String.format("%s on table %s", indexName, table.name);
+
+        // Guardrail to limit number of secondary indexes (per table)
+        if (guardRails.hasPerTableThreshold())
+        {
+            long indexesOnSameTable = table.indexes.stream().filter(other -> className.equals(other.getIndexClassName())).count();
+            guardRails.perTableThreshold.guard(indexesOnSameTable + 1, indexDescription,false, state);
+        }
+
+        // Guardrail to limit number of secondary indexes (total)
+        if (guardRails.hasTotalThreshold())
+        {
+            long indexesOnAllTables = StreamSupport.stream(Keyspace.all().spliterator(), false).flatMap(ks -> ks.getColumnFamilyStores().stream())
+                                                   .flatMap(ks -> ks.indexManager.listIndexes().stream())
+                                                   .map(i -> i.getIndexMetadata().getIndexClassName())
+                                                   .filter(otherClassName -> className.equals(otherClassName)).count();
+            guardRails.totalThreshold.guard(indexesOnAllTables + 1, indexDescription, false, state);
+        }
+
         // check to disallow creation of an index which duplicates an existing one in all but name
         IndexMetadata equalIndex = tryFind(table.indexes, i -> i.equalsWithoutName(index)).orNull();
         if (null != equalIndex)
@@ -378,5 +400,48 @@ public final class CreateIndexStatement extends AlterSchemaStatement
 
             return new CreateIndexStatement(keyspaceName, tableName.getName(), indexName.getName(), rawIndexTargets, attrs, ifNotExists);
         }
+    }
+
+    enum IndexGuardrails
+    {
+        LEGACY(Guardrails.secondaryIndexesPerTable, null),
+        SAI(Guardrails.indexesPerTableSai, Guardrails.indexesTotalSai),
+        SASI(Guardrails.indexesPerTableSasi, null),
+        UNKNOWN(null, null);
+
+        final Guardrail.Threshold perTableThreshold;
+        final Guardrail.Threshold totalThreshold;
+
+        IndexGuardrails(Guardrail.Threshold perTableThreshold, Guardrail.Threshold totalThreshold)
+        {
+            this.perTableThreshold = perTableThreshold;
+            this.totalThreshold = totalThreshold;
+        }
+
+        boolean hasPerTableThreshold()
+        {
+            return perTableThreshold != null;
+        }
+
+        boolean hasTotalThreshold()
+        {
+            return totalThreshold != null;
+        }
+
+        static IndexGuardrails forClassName(String className)
+        {
+            switch (className)
+            {
+                case "org.apache.cassandra.index.internal.CassandraIndex":
+                    return IndexGuardrails.LEGACY;
+                case "org.apache.cassandra.index.sasi.SASIIndex":
+                    return IndexGuardrails.SASI;
+                case "org.apache.cassandra.index.sai.StorageAttachedIndex":
+                    return IndexGuardrails.SAI;
+                default:
+                    return IndexGuardrails.UNKNOWN;
+            }
+        }
+
     }
 }
