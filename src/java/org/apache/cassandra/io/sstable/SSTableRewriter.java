9021c0a663a3bee0598670bf5d15df8d916f1242
Branimir Lambov <branimir.lambov@datastax.com>
2021-06-15 18:53:40 +0300

    Add comments explaining the canonical sstable view logic.

--- src/java/org/apache/cassandra/io/sstable/SSTableRewriter.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableRewriter.java
@@ -260,10 +260,10 @@ public class SSTableRewriter extends Transactional.AbstractTransactional impleme
 
             if (!transaction.isObsolete(latest))
             {
-                DecoratedKey newStart = latest.firstKeyBeyond(lowerbound);
-                assert newStart != null;
-                SSTableReader replacement = latest.cloneWithNewStart(newStart, runOnClose);
-                transaction.update(replacement, true);
+            DecoratedKey newStart = latest.firstKeyBeyond(lowerbound);
+            assert newStart != null;
+            SSTableReader replacement = latest.cloneWithNewStart(newStart, runOnClose);
+            transaction.update(replacement, true);
             }
         }
     }
@@ -316,6 +316,8 @@ public class SSTableRewriter extends Transactional.AbstractTransactional impleme
             return;
         }
 
+        // Open fully completed sstables early. This is also required for the final sstable in a set (where newWriter
+        // is null) to permit the compilation of a canonical set of sstables (see View.select).
         if (preemptiveOpenInterval != Long.MAX_VALUE)
         {
             // we leave it as a tmp file, but we open it and add it to the Tracker
