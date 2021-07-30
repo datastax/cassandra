dd1055d3be0875cb2f30037b87689470b100f90a
Jacek Lewandowski <6516951+jacek-lewandowski@users.noreply.github.com>
2021-07-30 16:04:24 +0200

    STAR-762: Implement paging in bytes (#223)
    Add page size in bytes flag to protocol
    Introduce PageSize object
    Protocol version changes
    No support for describe statement yet
    Simplify SecondaryIndexManager page calculation
    Add page size in bytes to DataLimits
    Refactor pagers
    Add / pull some tests
    Add some toString implementations
    Add PageSize to expected classes in DatabaseDescriptorRefTest
    
    Fix AggregationPartitionIterator
    So far we were passing the main page size to the AggregationPartitionIterator, which:
    - was pointless because there is no paging when we aggregate everything
    - it was actually harmful because AggregationPartitionIterator is a subclass of GroupByPartitionIterator and the later updates the subPager's limits with the minimum count of main page size and the number of remaining. It is correct if we use grouping aware limits, where count applies to the whole groups. But when we do aggregate everything, simply CQL limits are used and count limit applies to rows. Concluding, without fixing that we would limit the number of aggregated rows to the main page size which is not what we want
    
    (cherry picked from commit e11d71626e5a8db0a957e708855ffc8f18e1c0f5)
    (cherry picked from commit 13d45692b66304c2249e3f8241e80169b75ecb96)
    (cherry picked from commit 4f65564eb53c0f676040208dd76ba79e30a1fb10)
    (cherry picked from commit 9d084ea9ae4e83654c372f26b0855a20a8ffeb9d)
    (cherry picked from commit b0f05f2afcccfe9c734c2b18f77afe688d176a20)
    (cherry picked from commit 8dd49b0b9d110f2db14e1a9b425e4ba670b047f8)
    (cherry picked from commit 223d849b87d06e0242b72d691c0a39f397081e40)
    
    STAR-762 Fix PagingQueryTest with updated internal row size.
    
    STAR-762 Fix GuardrailPageWeightTest
    Align Guardrails and GuardrailsMBean page weight get/set warn/fail threshold methods with others that use DataStorageSpec values;
    
    STAR-762 Fix GuardrailPageWeightTest by removing code added during rebase that alters the data limit given by the user (e.g. LIMIT 100) to instead be the page size limit. Since that was different from existing CC or C* and broke the GuardrailPageWeightTest, is seems best to remove that for now.
    
     (Rebase of commit f19c9218a73a344f1d4b560f2b4404f59238c5b8)

--- src/java/org/apache/cassandra/cql3/QueryProcessor.java
+++ src/java/org/apache/cassandra/cql3/QueryProcessor.java
@@ -570,7 +570,7 @@ public class QueryProcessor implements QueryHandler
         }
     }
 
-    public static UntypedResultSet executeInternalWithPaging(String query, int pageSize, Object... values)
+    public static UntypedResultSet executeInternalWithPaging(String query, PageSize pageSize, Object... values)
     {
         Prepared prepared = prepareInternal(query);
 
