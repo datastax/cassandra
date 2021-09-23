b185b8b5704fcae78dcfe17580b1871ec724cb17
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2021-09-23 15:25:17 +0200

    STAR-844 Read/Write hooks (#250)
    This is a port of
    - https://github.com/riptano/bdp/commit/b6f0a18cb832c62f05cdcbd9cdcc2923f2fa727f
    - https://github.com/riptano/bdp/pull/19468
    
    The first change set introduces QueryInfoTracker (QIT) interface and
    hooks it to StorageProxy. The second adds ClientState to the interface.
    
    The original QIT utilizes ReadReconciliationObserver in the ReadTracker
    paths. Only onRow, onPartition and queried callbacks are utilized by
    CNDB and thus only these methods are ported to Converged Cassandra
    (CC). The callbacks are a bit different tho:
    - The callback methods are added directly to ReadTracker as CC doesn't
    have ReadReconciliationObserver. The class was added as a part of
    NodeSync effort and it is rather superfluous. Porting the whole class would
    add unnecessary complexity. Adding the required methods directly to the
    ReadTracker makes the interface cleaner and easier to understand.
    - CC operates on ReplicaPlans instead of plain host lists, that is why queried
    was changed to onReplicaPlan.
    
    (cherry picked from commit c32e91ff0fcb06bc4b7637670627c65dfa27e21c)
    (cherry picked from commit 093bc63c61e98ce68095f0eda368bbe21eca506c)
    (cherry picked from commit efdfa1f49cfe9610211964409c2771b30f9059d9)
    (cherry picked from commit 2ab888d2ad41267710cc35d0154d2b5d4cf1b51c)
    (cherry picked from commit 546bc4a7b5603e8de6c621f047055f0713e61d82)
    
    STAR-844 Fix rebase conflict in ParamType with CASSANDRA-17981
    
    (cherry picked from commit 12cd994fbecc2066352cd95f59f56a515f3a7a8e)
    (cherry picked from commit 5ce23cf15e9a00dfc69d61de055fc905d2cc4ca5)
    
     (Rebase of commit 99dbcaa8ee246cd9215f58190483ffa7e1fc7480)

--- src/java/org/apache/cassandra/service/reads/ReplicaFilteringProtection.java
+++ src/java/org/apache/cassandra/service/reads/ReplicaFilteringProtection.java
@@ -70,6 +70,7 @@ import org.apache.cassandra.net.MessagingService;
 import org.apache.cassandra.schema.ColumnMetadata;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.service.ClientWarn;
+import org.apache.cassandra.service.QueryInfoTracker;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.service.reads.repair.NoopReadRepair;
 import org.apache.cassandra.service.reads.repair.PartitionIteratorMergeListener;
@@ -163,7 +164,11 @@ public class ReplicaFilteringProtection<E extends Endpoints<E>>
     {
         @SuppressWarnings("unchecked")
         DataResolver<EndpointsForToken, ReplicaPlan.ForTokenRead> resolver =
-            new DataResolver<>(cmd, replicaPlan, (NoopReadRepair<EndpointsForToken, ReplicaPlan.ForTokenRead>) NoopReadRepair.instance, requestTime);
+            new DataResolver<>(cmd,
+                               replicaPlan,
+                               (NoopReadRepair<EndpointsForToken, ReplicaPlan.ForTokenRead>) NoopReadRepair.instance,
+                               requestTime,
+                               QueryInfoTracker.ReadTracker.NOOP);
 
         ReadCallback<EndpointsForToken, ReplicaPlan.ForTokenRead> handler = new ReadCallback<>(resolver, cmd, replicaPlan, requestTime);
 
