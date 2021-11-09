e5fd3e5bc114a22e8d2afb31d786205e61bc08c9
Zhao Yang <zhaoyangsingapore@gmail.com>
2021-11-09 17:17:03 +0800

    CNDB-1707: Port remote file system abstractions to Stargazer (#273)
    * LogTransaction: add ILogTransactionsFactory to provide custom log
    transaction
    
    * UCS: Port CNDB-2134 to disable shards on UCS L0
    
    * UCS: add CompactionAggregatePrioritizer to prioritize sstables based on remote file cache
    
    * NativeLibrary: Add INativeLibrary interface to provide custom implementation
    
    * SSTableWatcher: to discover custom component before opening sstables
    
    * StorageProvider: support custom file system and change Descriptor to use URI
    
    * StorageFeatureFlags: disable features that are not supported by custom file system
    
    * StorageHandler: to reload sstable from custom file system
    
    (cherry picked from commit e27ee69bb404ba4c21b65d2aef157ac54425f2ef)
    (cherry picked from commit e98d05a294ad2d62d1b46dad081b5f60a16b7064)
    (cherry picked from commit 493bf4cb1236f6cdf819e8acbdbcf1f04d0573c5)
    (cherry picked from commit 2932216864ade578d487420efa88ee17ac01a0ec)
    (cherry picked from commit da98fddb2eccf91b389dd4a21976e9f81ed7b764)
    (cherry picked from commit ad3ee02e41cab1089f6e4ad088fff814ca95556c)
    
    CNDB-1707 Fix rebase compile issues
    
    CNDB-1707 Add baseFileURI field to Descriptor for CNDB
    
    CNDB-1707 Fix VirtualTableFromInternodeTest; cannot open virtual keyspaces in ReadCommandVerbHandler.doVerb when checking if reads can be serviced.
    
     (Rebase of commit 2fd31c4c8ffab2e049ffa655c7c7c1a05ca01fa5)

--- src/java/org/apache/cassandra/db/lifecycle/LogRecord.java
+++ src/java/org/apache/cassandra/db/lifecycle/LogRecord.java
@@ -56,6 +56,8 @@ import static org.apache.cassandra.io.sstable.Descriptor.TMP_EXT;
 /**
  * A decoded line in a transaction log file replica.
  *
+ * Note: this is used by {@link LogTransaction}
+ *
  * @see LogReplica and LogFile.
  */
 final class LogRecord
@@ -175,8 +177,8 @@ final class LogRecord
 
     public static LogRecord make(Type type, SSTable table)
     {
-        String absoluteTablePath = absolutePath(table.descriptor.baseFile());
-        return make(type, getExistingFiles(absoluteTablePath), table.getAllFilePaths().size(), absoluteTablePath);
+        String absoluteTablePath = table.descriptor.baseFileUri() + Component.separator;
+        return make(type, getExistingFiles(absoluteTablePath), table.getComponentSize(), absoluteTablePath);
     }
 
     public static Map<SSTable, LogRecord> make(Type type, Iterable<SSTableReader> tables)
@@ -184,7 +186,7 @@ final class LogRecord
         // contains a mapping from sstable absolute path (everything up until the 'Data'/'Index'/etc part of the filename) to the sstable
         Map<String, SSTable> absolutePaths = new HashMap<>();
         for (SSTableReader table : tables)
-            absolutePaths.put(absolutePath(table.descriptor.baseFile()), table);
+            absolutePaths.put(table.descriptor.baseFileUri() + Component.separator, table);
 
         // maps sstable base file name to the actual files on disk
         Map<String, List<File>> existingFiles = getExistingFiles(absolutePaths.keySet());
@@ -194,14 +196,9 @@ final class LogRecord
             List<File> filesOnDisk = entry.getValue();
             String baseFileName = entry.getKey();
             SSTable sstable = absolutePaths.get(baseFileName);
-            records.put(sstable, make(type, filesOnDisk, sstable.getAllFilePaths().size(), baseFileName));
+            records.put(sstable, make(type, filesOnDisk, sstable.getComponentSize(), baseFileName));
         }
         return records;
-    }
-
-    private static String absolutePath(File baseFile)
-    {
-        return baseFile.withSuffix(String.valueOf(Component.separator)).canonicalPath();
     }
 
     public LogRecord withExistingFiles(List<File> existingFiles)
@@ -355,7 +352,7 @@ final class LogRecord
 
     public static List<File> getExistingFiles(String absoluteFilePath)
     {
-        File file = new File(absoluteFilePath);
+        File file = new File(PathUtils.getPath(absoluteFilePath));
         File[] files = file.parent().tryList((dir, name) -> name.startsWith(file.name()));
         // files may be null if the directory does not exist yet, e.g. when tracking new files
         return files == null ? Collections.emptyList() : Arrays.asList(files);
@@ -374,10 +371,10 @@ final class LogRecord
         Map<File, TreeSet<String>> dirToFileNamePrefix = new HashMap<>();
         for (String absolutePath : absoluteFilePaths)
         {
-            Path fullPath = new File(absolutePath).toPath();
-            Path path = fullPath.getParent();
-            if (path != null)
-                dirToFileNamePrefix.computeIfAbsent(new File(path), (k) -> new TreeSet<>()).add(fullPath.getFileName().toString());
+            File file = new File(PathUtils.getPath(absolutePath));
+            File parent = file.parent();
+            if (parent != null)
+                dirToFileNamePrefix.computeIfAbsent(parent, (k) -> new TreeSet<>()).add(file.name());
         }
 
         BiPredicate<File, String> ff = (dir, name) -> {
@@ -389,8 +386,8 @@ final class LogRecord
             String baseName = dirSet.floor(name);
             if (baseName != null && name.startsWith(baseName))
             {
-                String absolutePath = new File(dir, baseName).path();
-                fileMap.computeIfAbsent(absolutePath, k -> new ArrayList<>()).add(new File(dir, name));
+                String absolutePath = dir.resolve(baseName).toUri().toString();
+                fileMap.computeIfAbsent(absolutePath, k -> new ArrayList<>()).add(dir.resolve(name));
             }
             return false;
         };
@@ -415,7 +412,7 @@ final class LogRecord
 
     boolean isInFolder(Path folder)
     {
-        return absolutePath.isPresent() && PathUtils.isContained(folder, new File(absolutePath.get()).toPath());
+        return absolutePath.isPresent() && PathUtils.isContained(folder, new File(PathUtils.getPath(absolutePath.get())).toPath());
     }
 
     String absolutePath()
