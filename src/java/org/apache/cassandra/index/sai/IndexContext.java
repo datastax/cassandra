a0fd49638bd418052efbd4a32b88f88baac2da7f
Mike Adamson <madamson@datastax.com>
2021-12-16 15:46:36 +0000

    STAR-840: Sharded TrieMemoryIndex to reduce write contention

--- src/java/org/apache/cassandra/index/sai/IndexContext.java
+++ src/java/org/apache/cassandra/index/sai/IndexContext.java
@@ -55,6 +55,7 @@ import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.dht.AbstractBounds;
 import org.apache.cassandra.index.TargetParser;
 import org.apache.cassandra.index.sai.analyzer.AbstractAnalyzer;
+import org.apache.cassandra.index.sai.analyzer.NoOpAnalyzer;
 import org.apache.cassandra.index.sai.disk.format.IndexFeatureSet;
 import org.apache.cassandra.index.sai.disk.format.Version;
 import org.apache.cassandra.index.sai.disk.v1.IndexWriterConfig;
@@ -74,6 +75,7 @@ import org.apache.cassandra.schema.IndexMetadata;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.cassandra.utils.Pair;
+import org.apache.cassandra.utils.concurrent.OpOrder;
 
 /**
  * Manage metadata for each column index.
@@ -94,6 +96,7 @@ public class IndexContext
     private final String table;
     private final Pair<ColumnMetadata, IndexTarget.Type> target;
     private final AbstractType<?> validator;
+    private final Memtable.Owner owner;
 
     // Config can be null if the column context is "fake" (i.e. created for a filtering expression).
     private final IndexMetadata config;
@@ -110,7 +113,7 @@ public class IndexContext
 
     private final boolean segmentCompactionEnabled;
 
-    public IndexContext(TableMetadata tableMeta, IndexMetadata config)
+    public IndexContext(TableMetadata tableMeta, IndexMetadata config, Memtable.Owner owner)
     {
         assert config != null;
 
@@ -123,6 +126,7 @@ public class IndexContext
         this.viewManager = new IndexViewManager(this);
         this.indexMetrics = new IndexMetrics(this, tableMeta);
         this.validator = TypeUtil.cellValueType(target);
+        this.owner = owner;
 
         String fullIndexName = String.format("%s.%s.%s", this.keyspace, this.table, this.config.name);
         this.indexWriterConfig = IndexWriterConfig.fromOptions(fullIndexName, validator, config.options);
@@ -173,6 +177,7 @@ public class IndexContext
                                     ? AbstractAnalyzer.fromOptionsQueryAnalyzer(getValidator(), options)
                                     : this.analyzerFactory;
         this.primaryKeyFactory = Version.LATEST.onDiskFormat().primaryKeyFactory(clusteringComparator);
+        this.owner = null;
         this.segmentCompactionEnabled = segmentCompactionEnabled;
     }
 
@@ -189,13 +194,11 @@ public class IndexContext
         this.indexMetrics = null;
         this.columnQueryMetrics = null;
         this.indexWriterConfig = IndexWriterConfig.emptyConfig();
-        Map<String, String> options = Collections.emptyMap();
-        this.analyzerFactory = AbstractAnalyzer.fromOptions(getValidator(), options);
-        this.queryAnalyzerFactory = AbstractAnalyzer.hasQueryAnalyzer(options)
-                                    ? AbstractAnalyzer.fromOptionsQueryAnalyzer(getValidator(), options)
-                                    : this.analyzerFactory;
+        this.analyzerFactory = AbstractAnalyzer.fromOptions(getValidator(), Collections.emptyMap());
+        this.queryAnalyzerFactory = this.analyzerFactory;
         this.primaryKeyFactory = Version.LATEST.onDiskFormat().primaryKeyFactory(clusteringComparator);
         this.segmentCompactionEnabled = true;
+        this.owner = null;
     }
 
     public AbstractType<?> keyValidator()
@@ -228,15 +231,21 @@ public class IndexContext
         return table;
     }
 
-    public long index(DecoratedKey key, Row row, Memtable mt)
+    public Memtable.Owner owner()
     {
-        MemtableIndex current = liveMemtables.get(mt);
+        assert owner != null : "Attempt to access null owner on index context";
+        return owner;
+    }
+
+    public void index(DecoratedKey key, Row row, Memtable memtable, OpOrder.Group opGroup)
+    {
+        MemtableIndex current = liveMemtables.get(memtable);
 
         // We expect the relevant IndexMemtable to be present most of the time, so only make the
         // call to computeIfAbsent() if it's not. (see https://bugs.openjdk.java.net/browse/JDK-8161372)
         MemtableIndex target = (current != null)
                                ? current
-                               : liveMemtables.computeIfAbsent(mt, memtable -> new MemtableIndex(this));
+                               : liveMemtables.computeIfAbsent(memtable, mt -> new MemtableIndex(this));
 
         long start = System.nanoTime();
 
@@ -250,17 +259,16 @@ public class IndexContext
                 while (bufferIterator.hasNext())
                 {
                     ByteBuffer value = bufferIterator.next();
-                    bytes += target.index(key, row.clustering(), value);
+                    target.index(key, row.clustering(), value, memtable, opGroup);
                 }
             }
         }
         else
         {
             ByteBuffer value = getValueOf(key, row, FBUtilities.nowInSeconds());
-            target.index(key, row.clustering(), value);
+            target.index(key, row.clustering(), value, memtable, opGroup);
         }
         indexMetrics.memtableIndexWriteLatency.update(System.nanoTime() - start, TimeUnit.NANOSECONDS);
-        return bytes;
     }
 
     public void renewMemtable(Memtable renewed)
@@ -313,9 +321,14 @@ public class IndexContext
         return liveMemtables.values().stream().mapToLong(MemtableIndex::writeCount).sum();
     }
 
-    public long estimatedMemIndexMemoryUsed()
+    public long estimatedOnHeapMemIndexMemoryUsed()
     {
-        return liveMemtables.values().stream().mapToLong(MemtableIndex::estimatedMemoryUsed).sum();
+        return liveMemtables.values().stream().mapToLong(MemtableIndex::estimatedOnHeapMemoryUsed).sum();
+    }
+
+    public long estimatedOffHeapMemIndexMemoryUsed()
+    {
+        return liveMemtables.values().stream().mapToLong(MemtableIndex::estimatedOffHeapMemoryUsed).sum();
     }
 
     /**
