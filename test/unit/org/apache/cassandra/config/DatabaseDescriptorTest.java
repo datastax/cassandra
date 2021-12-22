7aff844958d5044eb9b4e11465a0f4f47fc5e662
dan jatnieks <jatnieks@pobox.com>
2021-12-21 22:15:53 -0800

    STAR-946 Make methods to configure and reset DatabaseDescriptor public (#294)
    * Make setConfig public
    * Port CNDB-2562 - add method to reset DatabaseDescriptor from CNDB tests. This is an unsafe method that allows CNDB tests to share the same JVM.
    
    Co-authored-by: Stefania Alborghetti <stef1927@users.noreply.github.com>
    (cherry picked from commit a058a79070abfc5cf4d1afb69c8fdd3671e85161)
    (cherry picked from commit ee578f9b2a5a28ba7b0b29e3249bd801ce4f0e7a)
    (cherry picked from commit 723b266b8f6f7e4a236e3bef99ec907856c06884)
    (cherry picked from commit fa2266295cf08e534b159fc76cc05e4f45e68d5d)
    (cherry picked from commit a8fb7bd0109ec0ce2560ce7e63c742c642e38b57)
    (cherry picked from commit 302aa4002dcc45b68301ef33eb781e37fc3b4999)
    
     (Rebase of commit 52c54a3b79c96364f87fd16d22232d1de3e9f8fa)
    
     (Rebase of commit 93696cb53485b02647f7eb6a73f9a2901441046b)

--- test/unit/org/apache/cassandra/config/DatabaseDescriptorTest.java
+++ test/unit/org/apache/cassandra/config/DatabaseDescriptorTest.java
@@ -53,6 +53,7 @@ import org.apache.cassandra.utils.ClassLoadingTestSupport;
 import org.assertj.core.api.Assertions;
 import org.mockito.MockedStatic;
 import org.apache.cassandra.io.util.File;
+import org.apache.cassandra.utils.MBeanWrapper;
 import org.mockito.Mockito;
 
 import static org.apache.cassandra.config.CassandraRelevantProperties.ALLOW_UNLIMITED_CONCURRENT_VALIDATIONS;
@@ -63,6 +64,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
@@ -114,6 +118,8 @@ public class DatabaseDescriptorTest
 
         config = DatabaseDescriptor.loadConfig();
         assertEquals("ConfigurationLoader Test", config.cluster_name);
+
+        System.clearProperty("cassandra.config.loader");
     }
 
     public static class TestLoader implements ConfigurationLoader
@@ -994,5 +1000,30 @@ public class DatabaseDescriptorTest
         when(mockSmallFileStore.getTotalSpace()).thenReturn(1L << 29); // 512 MB
         fileStoreMultiset.add(mockSmallFileStore);
         assertEquals(0L, DatabaseDescriptor.getDataFileDirectoriesMinTotalSpaceInGB(fileStoreMultiset));
+    }
+
+    @Test
+    public void testResetUnsafe()
+    {
+        assertTrue(DatabaseDescriptor.isDaemonInitialized());
+        assertFalse(DatabaseDescriptor.isClientOrToolInitialized());
+        assertNotNull(DatabaseDescriptor.getPartitioner());
+        assertNotNull(DatabaseDescriptor.getEndpointSnitch());
+        assertTrue(MBeanWrapper.instance.isRegistered("org.apache.cassandra.db:type=EndpointSnitchInfo"));
+
+        try
+        {
+            DatabaseDescriptor.resetUnsafe();
+
+            assertFalse(DatabaseDescriptor.isDaemonInitialized());
+            assertFalse(DatabaseDescriptor.isClientOrToolInitialized());
+            assertNull(DatabaseDescriptor.getPartitioner());
+            assertNull(DatabaseDescriptor.getEndpointSnitch());
+            assertFalse(MBeanWrapper.instance.isRegistered("org.apache.cassandra.db:type=EndpointSnitchInfo"));
+        }
+        finally
+        {
+            DatabaseDescriptor.daemonInitialization();
+        }
     }
 }
