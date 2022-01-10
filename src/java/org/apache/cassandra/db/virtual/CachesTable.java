msmygit/vscassandra

STAR-1010: port CNDB-3312 -  add micrometer metrics for key cache and counter cache; add error log when failling to acquire counter lock

src/java/org/apache/cassandra/db/virtual/CachesTable.java
@@ -58,14 +58,14 @@ final class CachesTable extends AbstractVirtualTable
     private void addRow(SimpleDataSet result, String name, CacheMetrics metrics)
     {
         result.row(name)
-              .column(CAPACITY_BYTES, metrics.capacity.getValue())
-              .column(SIZE_BYTES, metrics.size.getValue())
-              .column(ENTRY_COUNT, metrics.entries.getValue())
-              .column(REQUEST_COUNT, metrics.requests.getCount())
-              .column(HIT_COUNT, metrics.hits.getCount())
-              .column(HIT_RATIO, metrics.hitRate.getValue())
-              .column(RECENT_REQUEST_RATE_PER_SECOND, (long) metrics.requests.getFifteenMinuteRate())
-              .column(RECENT_HIT_RATE_PER_SECOND, (long) metrics.hits.getFifteenMinuteRate());
+              .column(CAPACITY_BYTES, metrics.capacity())
+              .column(SIZE_BYTES, metrics.size())
+              .column(ENTRY_COUNT, metrics.entries())
+              .column(REQUEST_COUNT, metrics.requests())
+              .column(HIT_COUNT, metrics.hits())
+              .column(HIT_RATIO, metrics.hitRate())
+              .column(RECENT_REQUEST_RATE_PER_SECOND, (long) metrics.fifteenMinuteRequestRate())
+              .column(RECENT_HIT_RATE_PER_SECOND, (long) metrics.fifteenMinuteHitRate());
     }
 
     private void addRow(SimpleDataSet result, String name, ChunkCacheMetrics metrics)

src/java/org/apache/cassandra/metrics/CacheMetrics.java
@@ -17,106 +17,44 @@
  */
 package org.apache.cassandra.metrics;
 
-import java.util.function.DoubleSupplier;
-
-import com.google.common.annotations.VisibleForTesting;
-
-import com.codahale.metrics.*;
 import org.apache.cassandra.cache.CacheSize;
+import org.apache.cassandra.service.CacheService;
 
-import static org.apache.cassandra.metrics.CassandraMetricsRegistry.Metrics;
+import static org.apache.cassandra.config.CassandraRelevantProperties.USE_MICROMETER;
 
 /**
  * Metrics for {@code ICache}.
  */
-public class CacheMetrics
+public interface CacheMetrics
 {
-    /** Cache capacity in bytes */
-    public final Gauge<Long> capacity;
-    /** Total size of cache, in bytes */
-    public final Gauge<Long> size;
-    /** Total number of cache entries */
-    public final Gauge<Integer> entries;
-
-    /** Total number of cache hits */
-    public final Meter hits;
-    /** Total number of cache misses */
-    public final Meter misses;
-    /** Total number of cache requests */
-    public final Meter requests;
-
-    /** all time cache hit rate */
-    public final Gauge<Double> hitRate;
-    /** 1m hit rate */
-    public final Gauge<Double> oneMinuteHitRate;
-    /** 5m hit rate */
-    public final Gauge<Double> fiveMinuteHitRate;
-    /** 15m hit rate */
-    public final Gauge<Double> fifteenMinuteHitRate;
-
-    protected final MetricNameFactory factory;
-
-    /**
-     * Create metrics for given cache.
-     *
-     * @param type Type of Cache to identify metrics.
-     * @param cache Cache to measure metrics
-     */
-    public CacheMetrics(String type, CacheSize cache)
+    static CacheMetrics create(CacheService.CacheType cacheType, CacheSize cache)
     {
-        factory = new DefaultNameFactory("Cache", type);
-
-        capacity = registerGauge("Capacity", cache::capacity);
-        size = registerGauge("Size", cache::weightedSize);
-        entries = registerGauge("Entries", cache::size);
-
-        requests = registerMeter("Requests");
-
-        hits = registerMeter("Hits");
-        misses = registerMeter("Misses");
-
-        hitRate = registerGauge("HitRate", ratioGauge(hits::getCount, requests::getCount));
-        oneMinuteHitRate = registerGauge("OneMinuteHitRate", ratioGauge(hits::getOneMinuteRate, requests::getOneMinuteRate));
-        fiveMinuteHitRate = registerGauge("FiveMinuteHitRate", ratioGauge(hits::getFiveMinuteRate, requests::getFiveMinuteRate));
-        fifteenMinuteHitRate = registerGauge("FifteenMinuteHitRate", ratioGauge(hits::getFifteenMinuteRate, requests::getFifteenMinuteRate));
+        return USE_MICROMETER.getBoolean()
+               ? new MicrometerCacheMetrics(cacheType.micrometerMetricsPrefix(), cache)
+               : new CodahaleCacheMetrics(cacheType.toString(), cache);
     }
 
-    protected final <T> Gauge<T> registerGauge(String name, Gauge<T> gauge)
-    {
-        return Metrics.register(factory.createMetricName(name), gauge);
-    }
+    long requests();
 
-    protected final Meter registerMeter(String name)
-    {
-        return Metrics.meter(factory.createMetricName(name));
-    }
+    long capacity();
 
-    protected final Timer registerTimer(String name)
-    {
-        return Metrics.timer(factory.createMetricName(name));
-    }
+    long size();
 
-    @VisibleForTesting
-    public void reset()
-    {
-        // No actual reset happens. The Meter counter is put to zero but will not reset the moving averages
-        // It rather injects a weird value into them.
-        // This method is being only used by CacheMetricsTest and CachingBench so fixing this issue was acknowledged
-        // but not considered mandatory to be fixed now (CASSANDRA-16228)
-        hits.mark(-hits.getCount());
-        misses.mark(-misses.getCount());
-        requests.mark(-requests.getCount());
-    }
+    long entries();
 
-    private static RatioGauge ratioGauge(DoubleSupplier numeratorSupplier, DoubleSupplier denominatorSupplier)
-    {
-        return new RatioGauge()
-        {
-            @Override
-            public Ratio getRatio()
-            {
-                return Ratio.of(numeratorSupplier.getAsDouble(), denominatorSupplier.getAsDouble());
-            }
-        };
-    }
+    long hits();
+
+    long misses();
+
+    double hitRate();
+
+    double oneMinuteHitRate();
+    double fiveMinuteHitRate();
+    double fifteenMinuteHitRate();
+
+    double fifteenMinuteRequestRate();
+
+    void recordHits(int count);
+
+    void recordMisses(int count);
 }

src/java/org/apache/cassandra/metrics/MicrometerCacheMetrics.java
@@ -1,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.metrics;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import org.apache.cassandra.cache.CacheSize;
+import org.apache.cassandra.utils.ApproximateTime;
+import org.apache.cassandra.utils.ExpMovingAverage;
+import org.apache.cassandra.utils.MovingAverage;
+
+public class MicrometerCacheMetrics extends MicrometerMetrics implements CacheMetrics
+{
+    @VisibleForTesting
+    final static long hitRateUpdateInterval = TimeUnit.MILLISECONDS.toNanos(100);
+
+    private final String metricsPrefix;
+    private final CacheSize cache;
+    private final MovingAverage hitRate;
+    private final AtomicLong hitRateUpdateTime;
+
+    private volatile Counter misses;
+    private volatile Counter hits;
+    private volatile Counter requests;
+
+    public MicrometerCacheMetrics(String metricsPrefix, CacheSize cache)
+    {
+        this.metricsPrefix = metricsPrefix;
+        this.cache = cache;
+        this.hitRate = ExpMovingAverage.decayBy1000();
+        this.hitRateUpdateTime = new AtomicLong(ApproximateTime.nanoTime());
+
+        this.misses = counter(metricsPrefix + "_misses");
+        this.hits = counter(metricsPrefix + "_hits");
+        this.requests = counter(metricsPrefix + "_requests");
+    }
+
+    @Override
+    public synchronized void register(MeterRegistry newRegistry, Tags newTags)
+    {
+        super.register(newRegistry, newTags);
+
+        gauge(metricsPrefix + "_capacity", cache, CacheSize::capacity);
+        gauge(metricsPrefix + "_size", cache, CacheSize::weightedSize);
+        gauge(metricsPrefix + "_num_entries", cache, CacheSize::size);
+        gauge(metricsPrefix + "_hit_rate", hitRate, MovingAverage::get);
+
+        this.misses = counter(metricsPrefix + "_misses");
+        this.hits = counter(metricsPrefix + "_hits");
+        this.requests = counter(metricsPrefix + "_requests");
+    }
+
+    @Override
+    public long requests()
+    {
+        return (long) requests.count();
+    }
+
+    @Override
+    public long capacity()
+    {
+        return cache.capacity();
+    }
+
+    @Override
+    public long size()
+    {
+        return cache.weightedSize();
+    }
+
+    @Override
+    public long entries()
+    {
+        return cache.size();
+    }
+
+    @Override
+    public long hits()
+    {
+        return (long) hits.count();
+    }
+
+    @Override
+    public long misses()
+    {
+        return (long) misses.count();
+    }
+
+    @Override
+    public double hitRate()
+    {
+        return hitRate.get();
+    }
+
+    @Override
+    public double oneMinuteHitRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public double fiveMinuteHitRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public double fifteenMinuteHitRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public double fifteenMinuteRequestRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public void recordHits(int count)
+    {
+        hits.increment(count);
+        requests.increment(count);
+        updateHitRate();
+    }
+
+    @Override
+    public void recordMisses(int count)
+    {
+        misses.increment(count);
+        requests.increment(count);
+        updateHitRate();
+    }
+
+    private void updateHitRate()
+    {
+        long lastUpdate = hitRateUpdateTime.get();
+        if (ApproximateTime.nanoTime() - lastUpdate > hitRateUpdateInterval)
+        {
+            if (hitRateUpdateTime.compareAndSet(lastUpdate, ApproximateTime.nanoTime()))
+            {
+                double numRequests = requests.count();
+                if (numRequests > 0)
+                    hitRate.update(hits.count() / numRequests);
+                else
+                    hitRate.update(0);
+            }
+        }
+    }
+}

test/unit/org/apache/cassandra/io/sstable/LargePartitionsTest.java
@@ -134,11 +134,11 @@ public class LargePartitionsTest extends CQLTester
     private static void keyCacheMetrics(String title)
     {
         CacheMetrics metrics = CacheService.instance.keyCache.getMetrics();
-        System.out.println("Key cache metrics " + title + ": capacity:" + metrics.capacity.getValue() +
-                           " size:"+metrics.size.getValue()+
-                           " entries:" + metrics.entries.getValue() +
-                           " hit-rate:"+metrics.hitRate.getValue() +
-                           " one-min-rate:"+metrics.oneMinuteHitRate.getValue());
+        System.out.println("Key cache metrics " + title + ": capacity:" + metrics.capacity() +
+                           " size:"+metrics.size()+
+                           " entries:" + metrics.entries() +
+                           " hit-rate:"+metrics.hitRate() +
+                           " one-min-rate:"+metrics.oneMinuteHitRate());
     }
 
     @Test

datastax/cassandra

src/java/org/apache/cassandra/db/virtual/CachesTable.java
@@ -58,14 +58,14 @@ final class CachesTable extends AbstractVirtualTable
     private void addRow(SimpleDataSet result, String name, CacheMetrics metrics)
     {
         result.row(name)
-              .column(CAPACITY_BYTES, metrics.capacity.getValue())
-              .column(SIZE_BYTES, metrics.size.getValue())
-              .column(ENTRY_COUNT, metrics.entries.getValue())
-              .column(REQUEST_COUNT, metrics.requests.getCount())
-              .column(HIT_COUNT, metrics.hits.getCount())
-              .column(HIT_RATIO, metrics.hitRate.getValue())
-              .column(RECENT_REQUEST_RATE_PER_SECOND, (long) metrics.requests.getFifteenMinuteRate())
-              .column(RECENT_HIT_RATE_PER_SECOND, (long) metrics.hits.getFifteenMinuteRate());
+              .column(CAPACITY_BYTES, metrics.capacity())
+              .column(SIZE_BYTES, metrics.size())
+              .column(ENTRY_COUNT, metrics.entries())
+              .column(REQUEST_COUNT, metrics.requests())
+              .column(HIT_COUNT, metrics.hits())
+              .column(HIT_RATIO, metrics.hitRate())
+              .column(RECENT_REQUEST_RATE_PER_SECOND, (long) metrics.requestsFifteenMinuteRate())
+              .column(RECENT_HIT_RATE_PER_SECOND, (long) metrics.hitFifteenMinuteRate());
     }
 
     private void addRow(SimpleDataSet result, String name, ChunkCacheMetrics metrics)
@@ -78,7 +78,7 @@ final class CachesTable extends AbstractVirtualTable
               .column(HIT_COUNT, metrics.hits())
               .column(HIT_RATIO, metrics.hitRate())
               .column(RECENT_REQUEST_RATE_PER_SECOND, metrics.requestsFifteenMinuteRate())
-              .column(RECENT_HIT_RATE_PER_SECOND, metrics.hitsFifteenMinuteRate());
+              .column(RECENT_HIT_RATE_PER_SECOND, metrics.hitFifteenMinuteRate());
     }
 
     public DataSet data()

src/java/org/apache/cassandra/metrics/CacheMetrics.java
@@ -17,106 +17,44 @@
  */
 package org.apache.cassandra.metrics;
 
-import java.util.function.DoubleSupplier;
-
-import com.google.common.annotations.VisibleForTesting;
-
-import com.codahale.metrics.*;
 import org.apache.cassandra.cache.CacheSize;
+import org.apache.cassandra.service.CacheService;
 
-import static org.apache.cassandra.metrics.CassandraMetricsRegistry.Metrics;
+import static org.apache.cassandra.config.CassandraRelevantProperties.USE_MICROMETER;
 
 /**
  * Metrics for {@code ICache}.
  */
-public class CacheMetrics
+public interface CacheMetrics
 {
-    /** Cache capacity in bytes */
-    public final Gauge<Long> capacity;
-    /** Total size of cache, in bytes */
-    public final Gauge<Long> size;
-    /** Total number of cache entries */
-    public final Gauge<Integer> entries;
-
-    /** Total number of cache hits */
-    public final Meter hits;
-    /** Total number of cache misses */
-    public final Meter misses;
-    /** Total number of cache requests */
-    public final Meter requests;
-
-    /** all time cache hit rate */
-    public final Gauge<Double> hitRate;
-    /** 1m hit rate */
-    public final Gauge<Double> oneMinuteHitRate;
-    /** 5m hit rate */
-    public final Gauge<Double> fiveMinuteHitRate;
-    /** 15m hit rate */
-    public final Gauge<Double> fifteenMinuteHitRate;
-
-    protected final MetricNameFactory factory;
-
-    /**
-     * Create metrics for given cache.
-     *
-     * @param type Type of Cache to identify metrics.
-     * @param cache Cache to measure metrics
-     */
-    public CacheMetrics(String type, CacheSize cache)
+    static CacheMetrics create(CacheService.CacheType cacheType, CacheSize cache)
     {
-        factory = new DefaultNameFactory("Cache", type);
-
-        capacity = registerGauge("Capacity", cache::capacity);
-        size = registerGauge("Size", cache::weightedSize);
-        entries = registerGauge("Entries", cache::size);
-
-        requests = registerMeter("Requests");
-
-        hits = registerMeter("Hits");
-        misses = registerMeter("Misses");
-
-        hitRate = registerGauge("HitRate", ratioGauge(hits::getCount, requests::getCount));
-        oneMinuteHitRate = registerGauge("OneMinuteHitRate", ratioGauge(hits::getOneMinuteRate, requests::getOneMinuteRate));
-        fiveMinuteHitRate = registerGauge("FiveMinuteHitRate", ratioGauge(hits::getFiveMinuteRate, requests::getFiveMinuteRate));
-        fifteenMinuteHitRate = registerGauge("FifteenMinuteHitRate", ratioGauge(hits::getFifteenMinuteRate, requests::getFifteenMinuteRate));
+        return USE_MICROMETER.getBoolean()
+               ? new MicrometerCacheMetrics(cacheType.micrometerMetricsPrefix(), cache)
+               : new CodahaleCacheMetrics(cacheType.toString(), cache);
     }
 
-    protected final <T> Gauge<T> registerGauge(String name, Gauge<T> gauge)
-    {
-        return Metrics.register(factory.createMetricName(name), gauge);
-    }
+    long requests();
 
-    protected final Meter registerMeter(String name)
-    {
-        return Metrics.meter(factory.createMetricName(name));
-    }
+    long capacity();
 
-    protected final Timer registerTimer(String name)
-    {
-        return Metrics.timer(factory.createMetricName(name));
-    }
+    long size();
 
-    @VisibleForTesting
-    public void reset()
-    {
-        // No actual reset happens. The Meter counter is put to zero but will not reset the moving averages
-        // It rather injects a weird value into them.
-        // This method is being only used by CacheMetricsTest and CachingBench so fixing this issue was acknowledged
-        // but not considered mandatory to be fixed now (CASSANDRA-16228)
-        hits.mark(-hits.getCount());
-        misses.mark(-misses.getCount());
-        requests.mark(-requests.getCount());
-    }
+    long entries();
 
-    private static RatioGauge ratioGauge(DoubleSupplier numeratorSupplier, DoubleSupplier denominatorSupplier)
-    {
-        return new RatioGauge()
-        {
-            @Override
-            public Ratio getRatio()
-            {
-                return Ratio.of(numeratorSupplier.getAsDouble(), denominatorSupplier.getAsDouble());
-            }
-        };
-    }
+    long hits();
+
+    long misses();
+
+    double hitRate();
+
+    double hitOneMinuteRate();
+    double hitFiveMinuteRate();
+    double hitFifteenMinuteRate();
+
+    double requestsFifteenMinuteRate();
+
+    void recordHits(int count);
+
+    void recordMisses(int count);
 }

src/java/org/apache/cassandra/metrics/MicrometerCacheMetrics.java
@@ -1,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.metrics;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import org.apache.cassandra.cache.CacheSize;
+import org.apache.cassandra.utils.ApproximateTime;
+import org.apache.cassandra.utils.ExpMovingAverage;
+import org.apache.cassandra.utils.MovingAverage;
+
+public class MicrometerCacheMetrics extends MicrometerMetrics implements CacheMetrics
+{
+    @VisibleForTesting
+    final static long hitRateUpdateIntervalNanos = TimeUnit.MILLISECONDS.toNanos(100);
+
+    private final String metricsPrefix;
+    private final CacheSize cache;
+    private final MovingAverage hitRate;
+    private final AtomicLong hitRateUpdateTime;
+
+    private volatile Counter misses;
+    private volatile Counter hits;
+    private volatile Counter requests;
+
+    public MicrometerCacheMetrics(String metricsPrefix, CacheSize cache)
+    {
+        this.metricsPrefix = metricsPrefix;
+        this.cache = cache;
+        this.hitRate = ExpMovingAverage.decayBy1000();
+        this.hitRateUpdateTime = new AtomicLong(ApproximateTime.nanoTime());
+
+        this.misses = counter(metricsPrefix + "_misses");
+        this.hits = counter(metricsPrefix + "_hits");
+        this.requests = counter(metricsPrefix + "_requests");
+    }
+
+    @Override
+    public synchronized void register(MeterRegistry newRegistry, Tags newTags)
+    {
+        super.register(newRegistry, newTags);
+
+        gauge(metricsPrefix + "_capacity", cache, CacheSize::capacity);
+        gauge(metricsPrefix + "_size", cache, CacheSize::weightedSize);
+        gauge(metricsPrefix + "_num_entries", cache, CacheSize::size);
+        gauge(metricsPrefix + "_hit_rate", hitRate, MovingAverage::get);
+
+        this.misses = counter(metricsPrefix + "_misses");
+        this.hits = counter(metricsPrefix + "_hits");
+        this.requests = counter(metricsPrefix + "_requests");
+    }
+
+    @Override
+    public long requests()
+    {
+        return (long) requests.count();
+    }
+
+    @Override
+    public long capacity()
+    {
+        return cache.capacity();
+    }
+
+    @Override
+    public long size()
+    {
+        return cache.weightedSize();
+    }
+
+    @Override
+    public long entries()
+    {
+        return cache.size();
+    }
+
+    @Override
+    public long hits()
+    {
+        return (long) hits.count();
+    }
+
+    @Override
+    public long misses()
+    {
+        return (long) misses.count();
+    }
+
+    @Override
+    public double hitRate()
+    {
+        return hitRate.get();
+    }
+
+    @Override
+    public double hitOneMinuteRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public double hitFiveMinuteRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public double hitFifteenMinuteRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public double requestsFifteenMinuteRate()
+    {
+        return Double.NaN;
+    }
+
+    @Override
+    public void recordHits(int count)
+    {
+        hits.increment(count);
+        requests.increment(count);
+        updateHitRate();
+    }
+
+    @Override
+    public void recordMisses(int count)
+    {
+        misses.increment(count);
+        requests.increment(count);
+        updateHitRate();
+    }
+
+    private void updateHitRate()
+    {
+        long lastUpdate = hitRateUpdateTime.get();
+        if (ApproximateTime.nanoTime() - lastUpdate > hitRateUpdateIntervalNanos)
+        {
+            if (hitRateUpdateTime.compareAndSet(lastUpdate, ApproximateTime.nanoTime()))
+            {
+                double numRequests = requests.count();
+                if (numRequests > 0)
+                    hitRate.update(hits.count() / numRequests);
+                else
+                    hitRate.update(0);
+            }
+        }
+    }
+}

test/unit/org/apache/cassandra/io/sstable/LargePartitionsTest.java
@@ -134,11 +134,11 @@ public class LargePartitionsTest extends CQLTester
     private static void keyCacheMetrics(String title)
     {
         CacheMetrics metrics = CacheService.instance.keyCache.getMetrics();
-        System.out.println("Key cache metrics " + title + ": capacity:" + metrics.capacity.getValue() +
-                           " size:"+metrics.size.getValue()+
-                           " entries:" + metrics.entries.getValue() +
-                           " hit-rate:"+metrics.hitRate.getValue() +
-                           " one-min-rate:"+metrics.oneMinuteHitRate.getValue());
+        System.out.println("Key cache metrics " + title + ": capacity:" + metrics.capacity() +
+                           " size:"+metrics.size()+
+                           " entries:" + metrics.entries() +
+                           " hit-rate:"+metrics.hitRate() +
+                           " one-min-rate:"+metrics.hitOneMinuteRate());
     }
 
     @Test