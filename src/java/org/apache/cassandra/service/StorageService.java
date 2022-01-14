6eae1bbd412486b20203e2bfef7d617d2d4cdb46
Jacek Lewandowski <6516951+jacek-lewandowski@users.noreply.github.com>
2022-01-14 12:40:07 +0100

    STAR-893: apply some performance improvements to Nodes impl (#292)
    * STAR-893: Use read-only interface instead of preventive duplication
    
    * STAR-893: Simplify access to commonly used fields
    
    (cherry picked from commit be31ce7d8050c140b48c4d573f48d4e742a9c40a)
    (cherry picked from commit 2b352645b61f74194bd1e226cd963e3d66ac905f)
    (cherry picked from commit 81c5ee87f4bdd878774d29f8225f15d11d56664d)
    (cherry picked from commit 101f4158d2548aced6e4130762fd956625ba5538)
    (cherry picked from commit 61fe029e69f687d6f1c39a9c5e5d6c77bfe9793e)
    (cherry picked from commit 3fbef30aa296249a5d3daadf02ed6c42cc5fcb18)

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -178,7 +178,6 @@ import org.apache.cassandra.net.AsyncOneResponse;
 import org.apache.cassandra.net.Message;
 import org.apache.cassandra.net.MessagingService;
 import org.apache.cassandra.repair.RepairCoordinator;
-import org.apache.cassandra.nodes.NodeInfo;
 import org.apache.cassandra.nodes.Nodes;
 import org.apache.cassandra.repair.messages.RepairOption;
 import org.apache.cassandra.schema.CompactionParams.TombstoneOption;
@@ -2290,6 +2289,10 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public InetAddressAndPort getNativeAddressAndPort(InetAddressAndPort endpoint)
     {
+        InetAddressAndPort addr = Nodes.getNativeTransportAddressAndPort(endpoint, null);
+        if (addr != null)
+            return addr;
+
         if (endpoint.equals(FBUtilities.getBroadcastAddressAndPort()))
             return FBUtilities.getBroadcastNativeAddressAndPort();
         else if (Gossiper.instance.getEndpointStateForEndpoint(endpoint).getApplicationState(ApplicationState.NATIVE_ADDRESS_AND_PORT) != null)
@@ -2856,11 +2859,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     private Collection<Token> getTokensFor(InetAddressAndPort endpoint)
     {
-        NodeInfo<?> info = Nodes.localOrPeerInfo(endpoint);
-        if (info == null)
-            return Collections.emptyList();
-
-        return info.getTokens();
+        return Nodes.getTokens(endpoint, Collections.emptyList());
     }
 
     /**
@@ -3025,7 +3024,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         {
             return isReplacingSameAddress() &&
                     Gossiper.instance.getEndpointStateForEndpoint(DatabaseDescriptor.getReplaceAddress()) != null
-                    && Objects.equals(hostId, Nodes.localOrPeerInfoOpt(DatabaseDescriptor.getReplaceAddress()).map(NodeInfo::getHostId).orElse(null));
+                    && Objects.equals(hostId, Nodes.getHostId(DatabaseDescriptor.getReplaceAddress(), null));
         }
         catch (RuntimeException ex)
         {
@@ -3092,7 +3091,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         }
 
         // Order Matters, TM.updateHostID() should be called before TM.updateNormalToken(), (see CASSANDRA-4300).
-        UUID hostId = Objects.requireNonNull(Nodes.localOrPeerInfo(endpoint)).getHostId();
+        UUID hostId = Nodes.getHostId(endpoint, null);
         InetAddressAndPort existing = getTokenMetadata().getEndpointForHostId(hostId);
         if (replacing && isReplacingSameHostAddressAndHostId(hostId))
         {
