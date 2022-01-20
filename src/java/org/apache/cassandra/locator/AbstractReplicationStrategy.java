8698c56bb0bbcf781c81d793628a6aed365a0b70
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2022-01-20 10:42:38 +0100

    STAR-1032 Add hook to allow token metadata per keyspace (#312)
    This commit extends TokenMetadataProvider interface to allow
    storing TokenMetadata objects per keyspace. This is needed for
    and used in CNDB.
    
    (cherry picked from commit 7a3ad018aa2b9d4ae855f1b31d2eae5b67ad0598)
    (cherry picked from commit 139c2ff22b3006067287cdff4e19b621ab7041d6)
    (cherry picked from commit 350b71fe85b1e33a6b52ecfb0a8a099857eb43fc)
    (cherry picked from commit c50c1b34c26bff775bd115fea7e3becab8bea0de)
    (cherry picked from commit 7bd24a92b40ab0b8d8f14763b321355a9e28b776)

--- src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
+++ src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
@@ -28,7 +28,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 import com.google.common.base.Preconditions;
-
+import org.cliffc.high_scale_lib.NonBlockingHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,7 +48,6 @@ import org.apache.cassandra.service.DatacenterWriteResponseHandler;
 import org.apache.cassandra.service.WriteResponseHandler;
 import org.apache.cassandra.transport.Dispatcher;
 import org.apache.cassandra.utils.FBUtilities;
-import org.cliffc.high_scale_lib.NonBlockingHashMap;
 
 /**
  * A abstract parent for all replication strategies.
@@ -71,6 +70,11 @@ public abstract class AbstractReplicationStrategy
         this.snitch = snitch;
         this.configOptions = configOptions == null ? Collections.<String, String>emptyMap() : configOptions;
         this.keyspaceName = keyspaceName;
+    }
+
+    public TokenMetadata getTokenMetadata()
+    {
+        return tokenMetadata;
     }
 
     public EndpointsForRange getCachedReplicas(long ringVersion, Token t)
