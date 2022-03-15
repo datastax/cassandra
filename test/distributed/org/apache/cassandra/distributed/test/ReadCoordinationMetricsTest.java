f261b3d07ed9b7283e7f532b7f516951aaaa39fc
Daniel Jatnieks <jatnieks@pobox.com>
2022-03-14 19:13:24 -0700

    Add read path test for ReadCoordinatorMetrics preferredOtherReplicas metrics

--- test/distributed/org/apache/cassandra/distributed/test/ReadCoordinationMetricsTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/ReadCoordinationMetricsTest.java
@@ -18,14 +18,21 @@
 
 package org.apache.cassandra.distributed.test;
 
+import java.net.InetAddress;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import org.apache.cassandra.distributed.Cluster;
 import org.apache.cassandra.distributed.api.IInvokableInstance;
+import org.apache.cassandra.distributed.api.IIsolatedExecutor;
+import org.apache.cassandra.distributed.impl.DistributedTestSnitch;
+import org.apache.cassandra.locator.InetAddressAndPort;
 import org.apache.cassandra.metrics.ReadCoordinationMetrics;
+import org.apache.cassandra.service.reads.AbstractReadExecutor;
 
 import static org.apache.cassandra.distributed.api.ConsistencyLevel.ALL;
+import static org.apache.cassandra.distributed.api.ConsistencyLevel.ONE;
 
 public class ReadCoordinationMetricsTest extends TestBaseImpl
 {
@@ -34,10 +41,21 @@ public class ReadCoordinationMetricsTest extends TestBaseImpl
         return node.callOnInstance(() -> ReadCoordinationMetrics.nonreplicaRequests.getCount());
     }
 
+    private static long countPreferredOtherReplicas(IInvokableInstance node)
+    {
+        return node.callOnInstance(() -> ReadCoordinationMetrics.preferredOtherReplicas.getCount());
+    }
+
+    /**
+     * Two nodes with RF=1 so half the data will be owned by each node and the coordinator for queries is not
+     * always a replica in the list of candidates in {@link AbstractReadExecutor#getReadExecutor} where
+     * {@link ReadCoordinationMetrics#nonreplicaRequests} will be incremented.
+     *
+     * @throws Throwable
+     */
     @Test
     public void testNonReplicaRequests() throws Throwable
     {
-        // RF=1 so the coordinator is not always a replica
         try (Cluster cluster = init(Cluster.create(2), 1))
         {
             cluster.schemaChange(withKeyspace("CREATE TABLE %s.tbl (pk int, ck int, v int, PRIMARY KEY (pk, ck))"));
@@ -49,7 +67,8 @@ public class ReadCoordinationMetricsTest extends TestBaseImpl
 
             for (int i = 0; i < 100; i++)
             {
-                // Query from either node as coordinator will result in the non-replica count incremented about half the time
+                // When the coordinator is not a candidate replica, which will be half the time due to RF=1,
+                // the non-replica count metric will be incremented.
                 cluster.coordinator(1).execute(withKeyspace("SELECT * FROM %s.tbl WHERE pk = ? and ck = ?"), ALL, i, i);
                 cluster.coordinator(2).execute(withKeyspace("SELECT * FROM %s.tbl WHERE pk = ? and ck = ?"), ALL, i, i);
             }
@@ -57,6 +76,49 @@ public class ReadCoordinationMetricsTest extends TestBaseImpl
             nonReplicaRequests1 = countNonreplicaRequests(cluster.get(1)) - nonReplicaRequests1;
             nonReplicaRequests2 = countNonreplicaRequests(cluster.get(2)) - nonReplicaRequests2;
             Assert.assertEquals(100, nonReplicaRequests1 + nonReplicaRequests2);
+        }
+    }
+
+    /**
+     * Two nodes with RF=2 so that both nodes are replicas for all data; this ensures that the coordinator node for
+     * queries will be a replica in the list of candidates in {@link AbstractReadExecutor#getReadExecutor}.
+     * <p>
+     * When the candidates collection is created, the sort order is changed so that the coordinator node is last. Then,
+     * using with CL=1 in the query, the resulting contacts collection will not contain the coordinator node, causing
+     * {@link ReadCoordinationMetrics#preferredOtherReplicas} to be incremented.
+     *
+     * @throws Throwable
+     */
+    @Test
+    public void testPreferredOtherReplicas() throws Throwable
+    {
+        try (Cluster cluster = init(Cluster.create(2), 2))
+        {
+            cluster.schemaChange(withKeyspace("CREATE TABLE %s.tbl (pk int, ck int, v int, PRIMARY KEY (pk, ck))"));
+            for (int i = 0; i < 100; i++)
+                cluster.coordinator(1).execute(withKeyspace("INSERT INTO %s.tbl (pk, ck, v) VALUES (?,?,?)"), ALL, i, i, i);
+
+            long preferredOtherReplicas1 = countPreferredOtherReplicas(cluster.get(1));
+
+            // Replica nodes are normally sorted by distance from the coordinator; override the test snitch to
+            // sort with respect to another node so that the coordinator node is last in the list of replicas.
+            // This will be used together with CL=1 to drop the coordinator node from the list of contacts, while
+            // remaining in the list of candidates.
+            InetAddress address2 = cluster.get(2).broadcastAddress().getAddress();
+            cluster.get(1).acceptsOnInstance((IIsolatedExecutor.SerializableConsumer<InetAddress>) (ks) -> {
+                DistributedTestSnitch.sortByProximityAddressOverride = InetAddressAndPort.getByAddress(ks);
+            }).accept(address2);
+
+            for (int i = 0; i < 100; i++)
+            {
+                // Query using CL=1 so that the subset of "candidate" replcas selected for the "contacts" collection
+                // will have just one node; since the "candidate" list was sorted with respect to the non-coordinator
+                // node, this will cause the preferredOtherReplicas count to be incremented.
+                cluster.coordinator(1).execute(withKeyspace("SELECT * FROM %s.tbl WHERE pk = ? and ck = ?"), ONE, i, i);
+            }
+
+            preferredOtherReplicas1 = countPreferredOtherReplicas(cluster.get(1)) - preferredOtherReplicas1;
+            Assert.assertEquals(100, preferredOtherReplicas1);
         }
     }
 }
