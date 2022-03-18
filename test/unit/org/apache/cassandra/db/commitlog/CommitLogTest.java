ac87edefbe1c1d3f4de4e6517b906ffcb49d8f1f
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2022-03-18 08:26:55 +0100

    Remove non-existing property clear

--- test/unit/org/apache/cassandra/db/commitlog/CommitLogTest.java
+++ test/unit/org/apache/cassandra/db/commitlog/CommitLogTest.java
@@ -210,7 +210,6 @@ public abstract class CommitLogTest
     public void afterTest()
     {
         System.clearProperty("cassandra.replayList");
-        System.clearProperty("cassandra.custom_replay_filter_class");
         testKiller.reset();
     }
 
