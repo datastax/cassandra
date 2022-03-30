ddc31f3d960b6925ab57cbd2daa1732c22b5545a
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2022-03-30 07:48:23 +0200

    STAR-790: Adjust schema updates
    (cherry picked from commit e0982c6a84c3cc07b70f06e78d2cb0e77b777ae3)
    (cherry picked from commit 6171c0da0040d2325f365df5b248b03af773b2a0)
    (cherry picked from commit c8299f2cdd4c1dedadc5f24dfa6c7ab353926f83)
    (cherry picked from commit 2149776827f04b35f54080a99a76057f874c6c72)
    (cherry picked from commit 662f1812e72bebbc08252a30b1276b549459c134)
    
     (Rebase of commit 7e1873d12a38a6f4b4caf867597bc7230effa193)
    
     (Rebase of commit b8edee15fc0cb8fa338dc23a33cc63a2cf355d12)

--- test/unit/org/apache/cassandra/service/StorageServiceServerTest.java
+++ test/unit/org/apache/cassandra/service/StorageServiceServerTest.java
@@ -33,6 +33,7 @@ import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.SystemKeyspace;
 
 import org.junit.Assert;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -103,6 +104,16 @@ public class StorageServiceServerTest
     {
         table1.truncateBlocking();
         table2.truncateBlocking();
+    }
+
+    @AfterClass
+    public static void tearDown()
+    {
+        // a proper test would be to call decommission here, but decommission() mixes both shutdown and datatransfer
+        // calls.  This test is only interested in the shutdown-related items which a properly handled by just
+        // stopping the client.
+        //StorageService.instance.decommission();
+        StorageService.instance.stopClient();
     }
 
     @Test
