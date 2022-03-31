d2f89eb1e57ab1ac279db433c3279defe146f1b4
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2022-03-31 08:53:48 +0200

    STAR-1219: Fix race conditions during initialization (CASSANDRA-17505)
    (cherry picked from commit 6f828abaa295877712ed6b25f5cd4c60fd0958fc)
    (cherry picked from commit d20afb01812b0b78fbd77b68a6d7ac1e1221f8e0)
    (cherry picked from commit 37fdae75806c2c5bfeb523629565d467e3fb06eb)
    (cherry picked from commit 148be1293947b2d7fa5c7d98851870cb2fbcd579)
    (cherry picked from commit 32851cc0a88a60e8da16e965649220a1a1e17255)

--- src/java/org/apache/cassandra/utils/FBUtilities.java
+++ src/java/org/apache/cassandra/utils/FBUtilities.java
@@ -201,9 +201,9 @@ public class FBUtilities
     {
         if (localInetAddressAndPort == null)
         {
-            if(DatabaseDescriptor.getRawConfig() == null)
+            if (DatabaseDescriptor.getRawConfig() == null)
             {
-                localInetAddressAndPort = InetAddressAndPort.getByAddress(getJustLocalAddress());
+                throw new AssertionError("Local address and port should never be accessed before initializing DatabaseDescriptor");
             }
             else
             {
@@ -238,7 +238,7 @@ public class FBUtilities
         {
             if(DatabaseDescriptor.getRawConfig() == null)
             {
-                broadcastInetAddressAndPort = InetAddressAndPort.getByAddress(getJustBroadcastAddress());
+                throw new AssertionError("Broadcast address and port should never be accessed before initializing DatabaseDescriptor");
             }
             else
             {
