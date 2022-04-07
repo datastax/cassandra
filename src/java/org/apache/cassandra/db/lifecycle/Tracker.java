7059b6a3b7fbb276e4627b8fcc1160ccadf78b84
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2022-04-07 19:08:08 +0200

    STAR-1242 Add support for compaction pick txn ids
    This patch allows to set the transaction UUID directly when a
    compaction pick is created, which means that in CNDB we can use it as a
    key for compaction tasks, making debugging of compactions much easier
    (since we can grep the same UUID). Currently, we're using the name of
    one of the sstables as the unique key of compaction tasks, but this is
    problematic when analyzing the logs, especially after a restart.
    
    Also, the transaction UUID was already a property of the compaction
    pick, but it was nullable and volatile. It is now final, which is an
    improvement, and a boolean now signals whether the pick has been
    submitted or not.
    
    Port of CNDB-2291 and of minor part from CNDB-3376
    
    Co-authored-by: Stefania Alborghetti

--- src/java/org/apache/cassandra/db/lifecycle/Tracker.java
+++ src/java/org/apache/cassandra/db/lifecycle/Tracker.java
@@ -110,19 +110,21 @@ public class Tracker
 
     public LifecycleTransaction tryModify(SSTableReader sstable, OperationType operationType)
     {
-        return tryModify(singleton(sstable), operationType);
+        return tryModify(singleton(sstable), operationType, LifecycleTransaction.newId());
     }
 
     /**
      * @return a Transaction over the provided sstables if we are able to mark the given @param sstables as compacted, before anyone else
      */
-    public LifecycleTransaction tryModify(Iterable<? extends SSTableReader> sstables, OperationType operationType)
+    public LifecycleTransaction tryModify(Iterable<? extends SSTableReader> sstables,
+                                          OperationType operationType,
+                                          UUID uuid)
     {
         if (Iterables.isEmpty(sstables))
-            return new LifecycleTransaction(this, operationType, sstables);
+            return new LifecycleTransaction(this, operationType, sstables, uuid);
         if (null == apply(permitCompacting(sstables), updateCompacting(emptySet(), sstables)))
             return null;
-        return new LifecycleTransaction(this, operationType, sstables);
+        return new LifecycleTransaction(this, operationType, sstables, uuid);
     }
 
 
@@ -283,7 +285,9 @@ public class Tracker
      */
     public Throwable dropSSTables(final Predicate<SSTableReader> remove, OperationType operationType, Throwable accumulate)
     {
-        try (AbstractLogTransaction txnLogs = ILogTransactionsFactory.instance.createLogTransaction(operationType, metadata))
+        try (AbstractLogTransaction txnLogs = ILogTransactionsFactory.instance.createLogTransaction(operationType,
+                                                                                                    LifecycleTransaction.newId(),
+                                                                                                    metadata))
         {
             Pair<View, View> result = apply(view -> {
                 Set<SSTableReader> toremove = copyOf(filter(view.sstables, and(remove, notIn(view.compacting))));
