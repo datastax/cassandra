00a9fb99efcb22a77c5f0a2f83e975a0f9f32803
dan jatnieks <jatnieks@pobox.com>
2022-04-10 18:05:21 -0700

    STAR-1252 Add totalCompactionsFailed metric (#414)
    (cherry picked from commit 323855bec0a5d4095fb404aacc3b085bcb5c22d8)
    (cherry picked from commit a724da481cc208af1f2d9513b825d17f75a7a26e)
    (cherry picked from commit af8a6566cc536e4ad6212d094c8b8937d481b6a0)
    (cherry picked from commit 026c9395cee09863ff40e1243164f3b3319c8278)
    (cherry picked from commit bf56e846f1aa180fc6410b0d7a20891e1ab69cbf)
    
     (Rebase of commit 995e8e8877fdf0628e0a58472b75b8c100d577ce)

--- src/java/org/apache/cassandra/db/compaction/CompactionManager.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionManager.java
@@ -2043,6 +2043,11 @@ public class CompactionManager implements CompactionManagerMBean, ICompactionMan
         metrics.compactionsAborted.inc();
     }
 
+    public void incrementFailed()
+    {
+        metrics.totalCompactionsFailed.inc();
+    }
+
     public void incrementCompactionsReduced()
     {
         metrics.compactionsReduced.inc();
