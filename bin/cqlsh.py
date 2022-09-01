f6410907b5157434bbdbc1eaf9dcfa3debc18b75
Stefan Miklosovic <smiklosovic@apache.org>
2022-09-02 00:21:54 +0200

    HCD-110 remove pure_sasl from the repository
    This is follow-up for CASSANDRA-16456. The dependency
    should be downloaded and put into the lib/ folder.
    
    patch by Stefan Miklosovic; reviewed by Mick Semb Wever for CASSANDRA-16456

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -117,7 +117,7 @@ if cql_zip:
     sys.path.insert(0, os.path.join(cql_zip, 'cassandra-driver-' + ver))
 
 # the driver needs dependencies
-third_parties = ('futures-', 'six-', 'geomet-', 'puresasl-')
+third_parties = ('futures-', 'six-', 'geomet-', 'pure_sasl-')
 
 for lib in third_parties:
     lib_zip = find_zip(lib)
