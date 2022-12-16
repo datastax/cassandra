89ef1834e9c6538683c08382e02e597ffe28b563
Zhao Yang <zhaoyangsingapore@gmail.com>
2022-12-16 21:22:22 +0800

    CNDB-5791: add EACH_QUORUM support (#580)
    * make fields in AbstractWriteResponseHandler accessible
    * Add Mutator#mutateCounterOnLeader for counter leader
    * Moved mutateAtomically() into the Mutator interface.
    
    (cherry picked from commit 12d9e96f29ef4d81b3b96f30b2457a21b96b4838)
    (cherry picked from commit 7caba0cd219a1e4162cff8b5d7a9be1efb1de7da)

--- src/java/org/apache/cassandra/service/Mutator.java
+++ src/java/org/apache/cassandra/service/Mutator.java
@@ -18,6 +18,7 @@
 
 package org.apache.cassandra.service;
 
+import java.util.Collection;
 import javax.annotation.Nullable;
 
 import org.apache.cassandra.db.ConsistencyLevel;
@@ -25,6 +26,10 @@ import org.apache.cassandra.db.CounterMutation;
 import org.apache.cassandra.db.IMutation;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.WriteType;
+import org.apache.cassandra.exceptions.OverloadedException;
+import org.apache.cassandra.exceptions.UnavailableException;
+import org.apache.cassandra.exceptions.WriteTimeoutException;
+import org.apache.cassandra.metrics.ClientRequestsMetrics;
 import org.apache.cassandra.service.paxos.Commit;
 import org.apache.cassandra.transport.Dispatcher;
 
@@ -38,10 +43,33 @@ import org.apache.cassandra.transport.Dispatcher;
  */
 public interface Mutator
 {
+
     /**
-     * Used for handling counter mutations on the coordinator level.
+     * Used for handling the given {@code mutations} as a logged batch.
+     */
+    void mutateAtomically(Collection<Mutation> mutations,
+                          ConsistencyLevel consistencyLevel,
+                          boolean requireQuorumForRemove,
+                          Dispatcher.RequestTime requestTime,
+                          ClientRequestsMetrics metrics,
+                          ClientState clientState)
+    throws UnavailableException, OverloadedException, WriteTimeoutException;
+
+    /**
+     * Used for handling counter mutations on the coordinator level:
+     * - if coordinator is a replica, it will apply the counter mutation locally and forward the applied mutation to other counter replica
+     * - if coordinator is not a replica, it will forward the counter mutation to a counter leader which is a replica
      */
     AbstractWriteResponseHandler<IMutation> mutateCounter(CounterMutation cm, String localDataCenter, Dispatcher.RequestTime requestTime);
+
+    /**
+     * Used for handling counter mutations on the counter leader level
+     */
+    AbstractWriteResponseHandler<IMutation> mutateCounterOnLeader(CounterMutation mutation,
+                                                                  String localDataCenter,
+                                                                  StorageProxy.WritePerformer performer,
+                                                                  Runnable callback,
+                                                                  Dispatcher.RequestTime requestTime);
 
     /**
      * Used for standard inserts and unlogged batchs.
@@ -71,7 +99,7 @@ public interface Mutator
     /**
      * Callback invoked when the given {@code counter} is localy applied.
      */
-    default void onAppliedCounter(IMutation counter)
+    default void onAppliedCounter(IMutation counter, AbstractWriteResponseHandler<IMutation> handler)
     {
         // no-op
     }
