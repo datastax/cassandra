1847a833c493c5a845131e02dcdaa0c100b69fa9
Zhao Yang <zhaoyangsingapore@gmail.com>
2022-12-16 21:22:22 +0800

    CNDB-5791: add EACH_QUORUM support (#580)
    * make fields in AbstractWriteResponseHandler accessible
    * Add Mutator#mutateCounterOnLeader for counter leader
    * Moved mutateAtomically() into the Mutator interface.
    
    (cherry picked from commit 12d9e96f29ef4d81b3b96f30b2457a21b96b4838)
    (cherry picked from commit 7caba0cd219a1e4162cff8b5d7a9be1efb1de7da)
    
    # Conflicts:
    #	src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
    #	src/java/org/apache/cassandra/db/virtual/VirtualMutation.java
    #	src/java/org/apache/cassandra/service/AbstractWriteResponseHandler.java
    #	src/java/org/apache/cassandra/service/StorageProxy.java
    #	test/unit/org/apache/cassandra/service/StorageProxyTest.java
    #	test/unit/org/apache/cassandra/service/WriteResponseHandlerTest.java

--- src/java/org/apache/cassandra/db/Mutation.java
+++ src/java/org/apache/cassandra/db/Mutation.java
@@ -138,6 +138,11 @@ public class Mutation implements IMutation, Supplier<Mutation>
         return keyspaceName;
     }
 
+    public Keyspace getKeyspace()
+    {
+        return Keyspace.open(keyspaceName);
+    }
+
     public Collection<TableId> getTableIds()
     {
         return modifications.keySet();
