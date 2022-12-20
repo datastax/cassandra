044a485fb603a029393eb96bc56b8d653892658e
Zhao Yang <zhaoyangsingapore@gmail.com>
2022-12-20 19:05:13 +0800

    CNDB-5791: Add Cross-region LWT support (#587)
    (cherry picked from commit 351ea78f66456f81181bc9d25c1314b203d1317e)
    (cherry picked from commit b83d44ebca18aa434d9bd7d63ae2ef7371e97313)

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -571,7 +571,8 @@ public class StorageProxy implements StorageProxyMBean
                            clientState,
                            requestTime,
                            metrics.casWriteMetrics,
-                           updateProposer);
+                           updateProposer,
+                           false);
 
         }
         catch (CasWriteUnknownResultException e)
@@ -662,6 +663,7 @@ public class StorageProxy implements StorageProxyMBean
      *     this operation and 2) the result that the whole method should return. This can return {@code null} in the
      *     special where, after having "prepared" (and thus potentially replayed in-progress upgdates), we don't want
      *     to propose anything (the whole method then return {@code null}).
+     * @param skipCommitConsistencyValidation whether to skip {@link ConsistencyLevel#validateForCasCommit} for commit consistency
      * @return the second element of the pair returned by {@code createUpdateProposal} (for the last call of that method
      *     if that method is called multiple times due to retries).
      */
@@ -673,7 +675,8 @@ public class StorageProxy implements StorageProxyMBean
                                        ClientState clientState,
                                        Dispatcher.RequestTime requestTime,
                                        CASClientRequestMetrics casMetrics,
-                                       Function<Ballot, Pair<PartitionUpdate, RowIterator>> createUpdateProposal)
+                                       Function<Ballot, Pair<PartitionUpdate, RowIterator>> createUpdateProposal,
+                                       boolean skipCommitConsistencyValidation)
     throws UnavailableException, IsBootstrappingException, RequestFailureException, RequestTimeoutException, InvalidRequestException
     {
         int contentions = 0;
@@ -683,7 +686,8 @@ public class StorageProxy implements StorageProxyMBean
         {
             consistencyForPaxos.validateForCas(metadata.keyspace, clientState);
             consistencyForReplayCommits.validateForCasCommit(latestRs, metadata.keyspace, clientState);
-            consistencyForCommit.validateForCasCommit(latestRs, metadata.keyspace, clientState);
+            if (!skipCommitConsistencyValidation)
+                consistencyForCommit.validateForCasCommit(latestRs, metadata.keyspace, clientState);
 
             long timeoutNanos = DatabaseDescriptor.getCasContentionTimeout(NANOSECONDS);
             long deadline = requestTime.computeDeadline(timeoutNanos);
@@ -2109,7 +2113,8 @@ public class StorageProxy implements StorageProxyMBean
                         clientState,
                         requestTime,
                         metrics.casReadMetrics,
-                        updateProposer);
+                        updateProposer,
+                        true); // skip guardrail for ANY which is blocked by CNDB
             }
             catch (WriteTimeoutException e)
             {
--- test/unit/org/apache/cassandra/cql3/CQLTester.java
+++ test/unit/org/apache/cassandra/cql3/CQLTester.java
@@ -1626,6 +1626,11 @@ public abstract class CQLTester
         return sessionNet(protocolVersion).execute(statement);
     }
 
+    protected com.datastax.driver.core.ResultSet executeNet(Statement statement)
+    {
+        return sessionNet().execute(statement);
+    }
+
     protected com.datastax.driver.core.ResultSet executeNetWithPaging(ProtocolVersion version, String query, int pageSize, Object... values)
     {
         return sessionNet(version).execute(new SimpleStatement(formatQuery(query), values).setFetchSize(pageSize));
