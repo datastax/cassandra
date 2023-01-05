4ac6c5bbf4c46ce9772d86b1e127c2c07feb768a
Branimir Lambov <branimir.lambov@datastax.com>
2023-01-05 13:21:23 +0000

    STAR-1826: Fix invalid reuse of metadata collector during flushing
    This was causing the cardinality information of an sstable to include
    more keys than it actually has, resulting in grossly overestimated
    key counts when picking a bloom filter size; the latter resulted in
    4-5 times larger bloom filters on L1 than they should be.
    
    (cherry picked from commit b036f2d6210c5ba9f2e0d4651cee209b8e690dd7)
    (cherry picked from commit dade7d4227e4233107635aef63dae43bff20930b)
    (cherry picked from commit c09a5f6303cf894cda31f41592bc6e9ad30ca3e3)

--- src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
@@ -46,6 +46,8 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.db.DiskBoundaries;
 import org.apache.cassandra.db.SerializationHeader;
+import org.apache.cassandra.db.commitlog.CommitLogPosition;
+import org.apache.cassandra.db.commitlog.IntervalSet;
 import org.apache.cassandra.db.compaction.AbstractStrategyHolder.TasksSupplier;
 import org.apache.cassandra.db.lifecycle.LifecycleNewTracker;
 import org.apache.cassandra.db.lifecycle.LifecycleTransaction;
@@ -59,7 +61,6 @@ import org.apache.cassandra.io.sstable.SSTable;
 import org.apache.cassandra.io.sstable.SSTableMultiWriter;
 import org.apache.cassandra.io.sstable.ScannerList;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
-import org.apache.cassandra.io.sstable.metadata.MetadataCollector;
 import org.apache.cassandra.notifications.INotification;
 import org.apache.cassandra.notifications.SSTableAddedNotification;
 import org.apache.cassandra.notifications.SSTableDeletingNotification;
@@ -1207,7 +1208,8 @@ public class CompactionStrategyManager implements CompactionStrategyContainer
                                                        long repairedAt,
                                                        TimeUUID pendingRepair,
                                                        boolean isTransient,
-                                                       MetadataCollector collector,
+                                                       IntervalSet<CommitLogPosition> commitLogPositions,
+                                                       int sstableLevel,
                                                        SerializationHeader header,
                                                        Collection<Index.Group> indexGroups,
                                                        LifecycleNewTracker lifecycleNewTracker)
@@ -1222,7 +1224,8 @@ public class CompactionStrategyManager implements CompactionStrategyContainer
                                                                                               repairedAt,
                                                                                               pendingRepair,
                                                                                               isTransient,
-                                                                                              collector,
+                                                                                              commitLogPositions,
+                                                                                              sstableLevel,
                                                                                               header,
                                                                                               indexGroups,
                                                                                               lifecycleNewTracker);
