6318c0216f11869d753d9d321f62c0b99ddcfe68
Ethan Brown <ethan.brown@datastax.com>
2023-03-20 15:13:54 -0700

    renamed W to scalingParameter in Controller.java and fixed Java Language Specification issue in TableMetrics.java

--- src/java/org/apache/cassandra/db/compaction/unified/Controller.java
+++ src/java/org/apache/cassandra/db/compaction/unified/Controller.java
@@ -240,23 +240,23 @@ public abstract class Controller
     public abstract int getMaxAdaptiveCompactions();
 
     public int getFanout(int index) {
-        int W = getScalingParameter(index);
-        return W < 0 ? 2 - W : 2 + W; // see formula in design doc
+        int scalingParameter = getScalingParameter(index);
+        return scalingParameter < 0 ? 2 - scalingParameter : 2 + scalingParameter; // see formula in design doc
     }
 
     public int getPreviousFanout(int index) {
-        int W = getPreviousScalingParameter(index);
-        return W < 0 ? 2 - W : 2 + W; // see formula in design doc
+        int scalingParameter = getPreviousScalingParameter(index);
+        return scalingParameter < 0 ? 2 - scalingParameter : 2 + scalingParameter; // see formula in design doc
     }
 
     public int getThreshold(int index) {
-        int W = getScalingParameter(index);
-        return W < 0 ? 2 : getFanout(index); // see formula in design doc
+        int scalingParameter = getScalingParameter(index);
+        return scalingParameter < 0 ? 2 : getFanout(index); // see formula in design doc
     }
 
     public int getPreviousThreshold(int index) {
-        int W = getPreviousScalingParameter(index);
-        return W < 0 ? 2 : getPreviousFanout(index); // see formula in design doc
+        int scalingParameter = getPreviousScalingParameter(index);
+        return scalingParameter < 0 ? 2 : getPreviousFanout(index); // see formula in design doc
     }
 
     /**
@@ -537,9 +537,9 @@ public abstract class Controller
         if (calculator == null)
             return 0;
 
-        int W = getScalingParameter(0);
+        int scalingParameter = getScalingParameter(0);
         long length = (long) Math.ceil(calculator.spaceUsed());
-        return calculator.getReadCostForQueries(readAmplification(length, W));
+        return calculator.getReadCostForQueries(readAmplification(length, scalingParameter));
     }
 
     private double getWriteIOCost()
@@ -547,9 +547,9 @@ public abstract class Controller
         if (calculator == null)
             return 0;
 
-        int W = getScalingParameter(0);
+        int scalingParameter = getScalingParameter(0);
         long length = (long) Math.ceil(calculator.spaceUsed());
-        return calculator.getWriteCostForQueries(writeAmplification(length, W));
+        return calculator.getWriteCostForQueries(writeAmplification(length, scalingParameter));
     }
 
     public static Controller fromOptions(CompactionRealm realm, Map<String, String> options)
