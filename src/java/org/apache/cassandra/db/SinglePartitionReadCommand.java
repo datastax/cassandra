25f352fd3822bf2e3b58d2edc6d00892a6bc131d
ethan-brown2022 <107228208+ethan-brown2022@users.noreply.github.com>
2023-04-05 12:20:05 -0700

    Star 1864 (#601)
    - Changes to W are applied to level 0 first, and applied to higher levels on future calls if the choice remains the same.
    
    - Added max_adaptive_compactions option that sets a limit on the number of concurrent "adaptive compactions" (compactions triggered by changing W).
    
    - Updated read costs to include all sstables checked regardless of hit.
    
    - Made readMultiplier and writeMultiplier configurable options (costs_read_multiplier and costs_write_multiplier) and changed default readMultiplier to 0.5.
    
    - Compaction costs are only updated if inputDiskSize > 0.
    
    (cherry picked from commit 7f34b7c48df3aa7dca14229f1aac7797b63ea234)
    (cherry picked from commit 3b26eeb6ebf9db9537cca904e5724c50c66a6e43)
    
    # Conflicts:
    #	src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
    #	src/java/org/apache/cassandra/db/compaction/CompactionStrategyOptions.java
    #	src/java/org/apache/cassandra/db/compaction/unified/AdaptiveController.java
    #	src/java/org/apache/cassandra/db/compaction/unified/CostsCalculator.java
    #	src/java/org/apache/cassandra/db/memtable/Flushing.java
    #	test/unit/org/apache/cassandra/db/compaction/CompactionSimulationTest.java
    #	test/unit/org/apache/cassandra/db/compaction/unified/CostsCalculatorTest.java
    
     (Rebase of commit 8216f4f6a75a6aa8347a4a1e9c2eb69867dea1e3)

--- src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
+++ src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
@@ -534,7 +534,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
                 cfs.metric.rowCacheHit.inc();
                 Tracing.trace("Row cache hit");
                 UnfilteredRowIterator unfilteredRowIterator = clusteringIndexFilter().getUnfilteredRowIterator(columnFilter(), cachedPartition);
-                cfs.metric.updateSSTableIterated(0, 0);
+                cfs.metric.updateSSTableIterated(0, 0, 0);
                 return unfilteredRowIterator;
             }
 
@@ -817,7 +817,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
             StorageHook.instance.reportRead(cfs.metadata().id, partitionKey());
 
             List<UnfilteredRowIterator> iterators = inputCollector.finalizeIterators(cfs, nowInSec(), controller.oldestUnrepairedTombstone());
-            return withSSTablesIterated(iterators, cfs.metric, metricsCollector, startTimeNanos);
+            return withSSTablesIterated(iterators, view.sstables.size(), cfs.metric, metricsCollector, startTimeNanos);
         }
         catch (RuntimeException | Error e)
         {
@@ -885,6 +885,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
      * would cause all iterators to be initialized and hence all sstables to be accessed.
      */
     private UnfilteredRowIterator withSSTablesIterated(List<UnfilteredRowIterator> iterators,
+                                                       int totalIntersectingSSTables,
                                                        TableMetrics metrics,
                                                        SSTableReadMetricsCollector metricsCollector,
                                                        long startTimeNanos)
@@ -903,7 +904,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
            public void onPartitionClose()
            {
                int mergedSSTablesIterated = metricsCollector.getMergedSSTables();
-               metrics.updateSSTableIterated(mergedSSTablesIterated, Clock.Global.nanoTime() - startTimeNanos);
+               metrics.updateSSTableIterated(mergedSSTablesIterated, totalIntersectingSSTables, Clock.Global.nanoTime() - startTimeNanos);
                Tracing.trace("Merged data from memtables and {} sstables", mergedSSTablesIterated);
            }
         }
@@ -1024,7 +1025,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
             }
         }
 
-        cfs.metric.updateSSTableIterated(metricsCollector.getMergedSSTables(), Clock.Global.nanoTime() - startTimeNanos);
+        cfs.metric.updateSSTableIterated(metricsCollector.getMergedSSTables(), view.sstables.size(), Clock.Global.nanoTime() - startTimeNanos);
 
         if (result == null || result.isEmpty())
             return EmptyIterators.unfilteredRow(metadata(), partitionKey(), false);
