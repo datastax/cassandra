3db61089e4a366cb724bef3d5345f133081d1582
Mike Adamson <madamson@datastax.com>
2023-07-11 16:12:31 +0100

    VECTOR-55: Review REVIEWME comments: (#680)
    * VECTOR-55: Review REVIEWME comments:
     - Refactor search methods to only return long variant
     - Remove SSTableQueryContext in favour of QueryContext
     - Add checking to SegmentMetadata.toSegmentRowId method
    
     (Rebase of commit 52deb4adb2ff518d2b2e0e0b665698bcdbbde3b0)
    
     (Rebase of commit 2bf616ca99e04fc9857dfa32b4fbe98d1072f5fc)

--- src/java/org/apache/cassandra/index/sai/QueryContext.java
+++ src/java/org/apache/cassandra/index/sai/QueryContext.java
@@ -21,14 +21,11 @@ package org.apache.cassandra.index.sai;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 import javax.annotation.concurrent.NotThreadSafe;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -42,7 +39,6 @@ import org.apache.cassandra.index.sai.disk.hnsw.OnDiskOrdinalsMap;
 import org.apache.cassandra.index.sai.disk.v1.SegmentMetadata;
 import org.apache.cassandra.index.sai.utils.AbortedOperationException;
 import org.apache.cassandra.index.sai.utils.PrimaryKey;
-import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.lucene.util.Bits;
 
 import static org.apache.cassandra.utils.Clock.Global.nanoTime;
@@ -82,7 +78,6 @@ public class QueryContext
 
     public long queryTimeouts = 0;
 
-    private final Map<SSTableReader, SSTableQueryContext> sstableQueryContexts = new HashMap<>();
     public int hnswVectorsAccessed;
     public int hnswVectorCacheHits;
 
@@ -110,11 +105,6 @@ public class QueryContext
         sstablesHit++;
     }
 
-    public SSTableQueryContext getSSTableQueryContext(SSTableReader reader)
-    {
-        return sstableQueryContexts.computeIfAbsent(reader, k -> new SSTableQueryContext(this));
-    }
-
     public void checkpoint()
     {
         if (totalQueryTimeNs() >= executionQuotaNano && !DISABLE_TIMEOUT)
@@ -131,9 +121,10 @@ public class QueryContext
         shadowedPrimaryKeys.add(primaryKey);
     }
 
-    public boolean containsShadowedPrimaryKey(Supplier<PrimaryKey> supplier)
+    // Returns true if the row ID will be included or false if the row ID will be shadowed
+    public boolean shouldInclude(long sstableRowId, PrimaryKeyMap primaryKeyMap)
     {
-        return shadowedPrimaryKeys != null && shadowedPrimaryKeys.contains(supplier.get());
+        return shadowedPrimaryKeys == null || !shadowedPrimaryKeys.contains(primaryKeyMap.primaryKeyFromRowId(sstableRowId));
     }
 
     public boolean containsShadowedPrimaryKey(PrimaryKey primaryKey)
@@ -174,7 +165,7 @@ public class QueryContext
                 if (sstableRowId == Long.MAX_VALUE) // not found
                     continue;
 
-                int segmentRowId = metadata.segmentedRowId(sstableRowId);
+                int segmentRowId = metadata.toSegmentRowId(sstableRowId);
                 // not in segment yet
                 if (segmentRowId < 0)
                     continue;
@@ -208,7 +199,7 @@ public class QueryContext
         {
             this.graph = graph;
             this.ignoredOrdinals = ignoredOrdinals;
-            this.length = 1 + metadata.segmentedRowId(metadata.maxSSTableRowId);
+            this.length = 1 + metadata.toSegmentRowId(metadata.maxSSTableRowId);
         }
 
         @Override
@@ -248,5 +239,4 @@ public class QueryContext
             return graph.size();
         }
     }
-
 }
