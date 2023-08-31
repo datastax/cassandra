ea9ea3a8f56ab0be193e370e95ed6d595f0d757d
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2023-08-31 13:48:39 +0200

    expose SSTableReader getLiveSSTable(String filename) for CNDB
    (cherry picked from commit db01e9ddd0eaeff5ddd3450ec1d027c1d378c394)
    (cherry picked from commit f21eb46819094dcfe5657316c7c700e5d9153d66)
    
     (Rebase of commit bdc2d68f4dd54292859069f82f3ab4bbee67ed8f)

--- src/java/org/apache/cassandra/db/lifecycle/Tracker.java
+++ src/java/org/apache/cassandra/db/lifecycle/Tracker.java
@@ -551,6 +551,13 @@ public class Tracker
         return view.liveSSTables();
     }
 
+    // used by CNDB
+    @Nullable
+    public SSTableReader getLiveSSTable(String filename)
+    {
+        return view.get().getLiveSSTable(filename);
+    }
+
     public void maybeIncrementallyBackup(final Iterable<SSTableReader> sstables)
     {
         if (!cfstore.isTableIncrementalBackupsEnabled())
