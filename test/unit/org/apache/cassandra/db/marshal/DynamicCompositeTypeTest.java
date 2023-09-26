0ff12db3620bb4cb805213953e03c6c419f2c96c
Andrés de la Peña <a.penya.garcia@gmail.com>
2023-09-26 18:28:01 +0100

    Fix merge of AbstractCompositeType to CC #25

--- test/unit/org/apache/cassandra/db/marshal/DynamicCompositeTypeTest.java
+++ test/unit/org/apache/cassandra/db/marshal/DynamicCompositeTypeTest.java
@@ -41,7 +41,9 @@ import org.apache.cassandra.db.partitions.ImmutableBTreePartition;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.schema.KeyspaceParams;
 import org.apache.cassandra.serializers.MarshalException;
+import org.apache.cassandra.transport.ProtocolVersion;
 import org.apache.cassandra.utils.*;
+import org.assertj.core.api.Assertions;
 
 public class DynamicCompositeTypeTest
 {
@@ -365,5 +367,19 @@ public class DynamicCompositeTypeTest
         }
         bb.rewind();
         return bb;
+    }
+
+    @Test
+    public void testEmptyValue()
+    {
+        DynamicCompositeType type = DynamicCompositeType.getInstance(ImmutableMap.of((byte) 'V', BytesType.instance));
+
+        String cqlLiteral = "0x8056000000";
+        ByteBuffer bb = type.asCQL3Type().fromCQLLiteral(cqlLiteral);
+        type.validate(bb);
+
+        String str = type.getString(bb);
+        ByteBuffer read = type.fromString(str);
+        Assertions.assertThat(read).isEqualTo(bb);
     }
 }
