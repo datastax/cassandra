71ad25877af806392ee91e2dd1b072fa9922a40c
Michael Marshall <mmarshall@apache.org>
2023-10-11 13:18:42 -0500

    Remove defunct reference to primaryKeyMap.isNotFound

--- src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
+++ src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
@@ -278,8 +278,8 @@ public class V2VectorIndexSearcher extends IndexSearcher implements SegmentOrder
                     // VSTODO this is O(log n), is this a hotspot?
                     long sstableRowId = primaryKeyMap.exactRowIdForPrimaryKey(primaryKey);
                     // skip rows that are not in our segment (or more preciesely, have no vectors that were indexed)
-                    // or are not in this segment
-                    if (sstableRowId < metadata.minSSTableRowId || primaryKeyMap.isNotFound(sstableRowId))
+                    // or are not in this segment (exactRowIdForPrimaryKey returns a negative value for not found)
+                    if (sstableRowId < metadata.minSSTableRowId)
                         continue;
 
                     // if sstable row id has exceeded current ANN segment, stop
