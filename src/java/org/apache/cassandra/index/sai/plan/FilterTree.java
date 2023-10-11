5d264d369cb68c0865f3a98814ae3736dbadc36c
Piotr Kołaczkowski <pkolaczk@gmail.com>
2023-10-11 13:22:09 +0200

    SAI acceleration of NOT CONTAINS / NOT CONTAINS KEY
    NOT CONTAINS, NOT CONTAINS KEY and map entry inequality
    can be now used on SAI indexed columns without need for
    ALLOW FILTERING.
    
    The result sets are evaluated by taking an iterator
    to all primary keys (in all active memtables and sstables)
    and anti-joining them with the primary keys
    matching the CONTAINS predicate, read from the index.
    This way there was no need to change the index structure.
    Although a full scan over all primary keys is needed,
    the scan involves scanning of primary keys only, without
    deserializing the whole rows.
    For sstables, the primary key trie component is used,
    which is likely much smaller than the whole sstable.
    Therefore, this method is likely faster than filtering, as long
    as rows are big enough.
    
    Includes by cherry-pick c2d362ff3edeab824c14055b249d4e4490dec1cd
     CC5 fix compile errors following cherry-pick f8a9eb0..8c11326 and git cherry-pick 777bd1f
    
     (Rebase of commit e79bebe9ad167f3e8e4b6d67966864de2074df72)
    
     (Rebase of commit 9cce778d72802a798ec70bcf6b7b14d5167f043d)

--- src/java/org/apache/cassandra/index/sai/plan/FilterTree.java
+++ src/java/org/apache/cassandra/index/sai/plan/FilterTree.java
@@ -28,7 +28,6 @@ import com.google.common.collect.ListMultimap;
 import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.db.rows.Unfiltered;
-import org.apache.cassandra.index.sai.plan.Expression.Op;
 import org.apache.cassandra.index.sai.utils.TypeUtil;
 import org.apache.cassandra.schema.ColumnMetadata;
 import org.apache.cassandra.schema.ColumnMetadata.Kind;
@@ -107,12 +106,12 @@ public class FilterTree
                 if (TypeUtil.isNonFrozenCollection(column.type))
                 {
                     Iterator<ByteBuffer> valueIterator = filter.context.getValuesOf(row, now);
-                    result = op.apply(result, collectionMatch(valueIterator, filter));
+                    result = op.apply(result, filter.isSatisfiedBy(valueIterator));
                 }
                 else
                 {
                     ByteBuffer value = filter.context.getValueOf(key, row, now);
-                    result = op.apply(result, singletonMatch(value, filter));
+                    result = op.apply(result, filter.isSatisfiedBy(value));
                 }
 
                 if (shouldReturnNow(result))
@@ -130,31 +129,5 @@ public class FilterTree
      */
     private boolean shouldReturnNow(boolean result) {
         return (op == OperationType.AND && !result) || (op == OperationType.OR && result);
-    }
-
-    private boolean singletonMatch(ByteBuffer value, Expression filter)
-    {
-        boolean match = value != null && filter.isSatisfiedBy(value);
-        // If this is NOT_EQ operation we have to
-        // inverse match flag (to check against other expressions),
-        if (filter.getOp() == Op.NOT_EQ)
-            match = !match;
-        return match;
-    }
-
-    private boolean collectionMatch(Iterator<ByteBuffer> valueIterator, Expression filter)
-    {
-        if (valueIterator == null)
-            return false;
-
-        while (valueIterator.hasNext())
-        {
-            ByteBuffer value = valueIterator.next();
-            if (value == null)
-                continue;
-            if (filter.isSatisfiedBy(value))
-                return true;
-        }
-        return false;
     }
 }
