7115178163bba273ff657044cdd7b4a9c591c922
Andrey Yegorov <8622884+dlg99@users.noreply.github.com>
2023-10-13 11:07:49 -0700

    VECTOR-SEARCH-27: Summarize sstable merging results for index queries (#800)
    * VECTOR-SEARCH-27: Summarize sstable merging results for index queries

--- src/java/org/apache/cassandra/db/ReadExecutionController.java
+++ src/java/org/apache/cassandra/db/ReadExecutionController.java
@@ -22,8 +22,11 @@ import java.util.concurrent.TimeUnit;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Snapshot;
 import org.apache.cassandra.db.filter.DataLimits;
 import org.apache.cassandra.index.Index;
+import org.apache.cassandra.metrics.DecayingEstimatedHistogramReservoir;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.MonotonicClock;
@@ -50,6 +53,8 @@ public class ReadExecutionController implements AutoCloseable
     private final RepairedDataInfo repairedDataInfo;
     private long oldestUnrepairedTombstone = Long.MAX_VALUE;
 
+    public final Histogram sstablesScannedPerRowRead;
+
     ReadExecutionController(ReadCommand command,
                             OpOrder.Group baseOp,
                             TableMetadata baseMetadata,
@@ -67,6 +72,8 @@ public class ReadExecutionController implements AutoCloseable
         this.writeContext = writeContext;
         this.command = command;
         this.createdAtNanos = createdAtNanos;
+
+        this.sstablesScannedPerRowRead = new Histogram(new DecayingEstimatedHistogramReservoir(true));
 
         if (trackRepairedStatus)
         {
@@ -213,7 +220,17 @@ public class ReadExecutionController implements AutoCloseable
 
         if (createdAtNanos != NO_SAMPLING)
             addSample();
-    }
+
+        if (Tracing.traceSinglePartitions())
+        {
+            Snapshot sstablesHistogram = sstablesScannedPerRowRead.getSnapshot();
+            Tracing.trace("Scanned {} rows; average {} sstables scanned per row with stdev {} and max {}",
+                          sstablesScannedPerRowRead.getCount(),
+                          sstablesHistogram.getMean(),
+                          sstablesHistogram.getStdDev(),
+                          sstablesHistogram.getMax());
+        }
+}
 
     public boolean isTrackingRepairedStatus()
     {
@@ -244,5 +261,10 @@ public class ReadExecutionController implements AutoCloseable
         ColumnFamilyStore cfs = ColumnFamilyStore.getIfExists(baseMetadata.id);
         if (cfs != null)
             cfs.metric.topLocalReadQueryTime.addSample(cql, timeMicros);
+    }
+
+    public void updateSstablesIteratedPerRow(int mergedSSTablesIterated)
+    {
+        sstablesScannedPerRowRead.update(mergedSSTablesIterated);
     }
 }
