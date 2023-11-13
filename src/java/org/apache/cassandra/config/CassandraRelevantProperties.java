afb5c254555e18299117a089765616ae3cef624e
Michael Marshall <michael.marshall@datastax.com>
2023-11-13 13:47:14 -0600

    Override lucene's default for min/max gram size on ngram tokenizer (#768)
    * Updates the default ngram tokenizer configuration so that the default for `minGramSize` is 3 and for `maxGramSize` is 7.
    * Adds a generic limit on the number of bytes we will index for a given term. I set the default to 5 kb, which is the same value we have for frozen collections right now.
    * Adds logic to reject writes at the coordinator when one of their columns exceed the configured max for an SAI index
    
     (Rebase of commit bfc2d54d5a24d0b8fc2f34d3cb6cd543feb796a2)

--- src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
+++ src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
@@ -547,13 +547,18 @@ public enum CassandraRelevantProperties
     /** Latest version to be used for SAI index writing */
     SAI_LATEST_VERSION("cassandra.sai.latest_version", "ba"),
 
-    SAI_MAX_FROZEN_TERM_SIZE("cassandra.sai.max_frozen_term_size_kb", "8"),
-    SAI_MAX_STRING_TERM_SIZE("cassandra.sai.max_string_term_size_kb", "8"),
+    SAI_MAX_ANALYZED_SIZE("cassandra.sai.max_analyzed_size_kb", "5"),
+    SAI_MAX_FROZEN_TERM_SIZE("cassandra.sai.max_frozen_term_size_kb", "5"),
+    SAI_MAX_STRING_TERM_SIZE("cassandra.sai.max_string_term_size_kb", "1"),
+    SAI_MAX_VECTOR_TERM_SIZE("cassandra.sai.max_vector_term_size_kb", "16"),
 
     SAI_NUMERIC_VALUES_BLOCK_SIZE("dse.sai.numeric_values.block_size", "128"),
     SAI_NUMERIC_VALUES_MONOTONIC_BLOCK_SIZE("dse.sai.numeric_values.monotonic_block_size", "16384"),
     SAI_TEST_DISABLE_TIMEOUT("cassandra.sai.test.timeout_disabled", "false"),
     SAI_TEST_SEGMENT_BUILD_MEMORY_LIMIT("cassandra.test.sai.segment_build_memory_limit"),
+
+    /** Whether to validate terms that will be SAI indexed at the coordinator */
+    SAI_VALIDATE_TERMS_AT_COORDINATOR("cassandra.sai.validate_terms_at_coordinator", "true"),
 
     /**
      * The maximum number of primary keys that a WHERE clause may materialize before the query planner switches
@@ -775,7 +780,6 @@ public enum CassandraRelevantProperties
     USE_RANDOM_ALLOCATION_IF_NOT_SUPPORTED("cassandra.token_allocation.use_random_if_not_supported"),
 
     /** Gossiper compute expiration timeout. Default value 3 days. */
-    VALIDATE_MAX_TERM_SIZE_AT_COORDINATOR("cassandra.sai.validate_max_term_size_at_coordinator"),
 
     /** Whether vector type only allows float vectors. True by default. **/
     VECTOR_FLOAT_ONLY("cassandra.float_only_vectors", "true"),
