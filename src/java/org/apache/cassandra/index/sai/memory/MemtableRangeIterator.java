msmygit/vscassandra

SAI acceleration of NOT CONTAINS / NOT CONTAINS KEY
NOT CONTAINS, NOT CONTAINS KEY and map entry inequality
can be now used on SAI indexed columns without need for
ALLOW FILTERING.

The result sets are evaluated by taking an iterator
to all primary keys (in all active memtables and sstables)
and anti-joining them with the primary keys
matching the CONTAINS predicate, read from the index.
This way there was no need to change the index structure.
Although a full scan over all primary keys is needed,
the scan involves scanning of primary keys only, without
deserializing the whole rows.
For sstables, the primary key trie component is used,
which is likely much smaller than the whole sstable.
Therefore, this method is likely faster than filtering, as long
as rows are big enough.

src/java/org/apache/cassandra/index/sai/memory/MemtableRangeIterator.java
@@ -1,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.memory;
+
+import java.io.IOException;
+
+import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.db.DataRange;
+import org.apache.cassandra.db.PartitionPosition;
+import org.apache.cassandra.db.Slice;
+import org.apache.cassandra.db.Slices;
+import org.apache.cassandra.db.filter.ClusteringIndexSliceFilter;
+import org.apache.cassandra.db.filter.ColumnFilter;
+import org.apache.cassandra.db.memtable.Memtable;
+import org.apache.cassandra.db.partitions.UnfilteredPartitionIterator;
+import org.apache.cassandra.db.rows.Row;
+import org.apache.cassandra.db.rows.Unfiltered;
+import org.apache.cassandra.db.rows.UnfilteredRowIterator;
+import org.apache.cassandra.dht.AbstractBounds;
+import org.apache.cassandra.dht.IPartitioner;
+import org.apache.cassandra.dht.Token;
+import org.apache.cassandra.index.sai.utils.PrimaryKey;
+import org.apache.cassandra.index.sai.utils.RangeIterator;
+import org.apache.cassandra.schema.TableMetadata;
+
+/**
+ * Iterates over primary keys in a memtable
+ */
+public class MemtableRangeIterator extends RangeIterator
+{
+    private final Memtable memtable;
+    private final PrimaryKey.Factory pkFactory;
+    private final AbstractBounds<PartitionPosition> keyRange;
+    private final ColumnFilter columns;
+    private UnfilteredPartitionIterator partitionIterator;
+    private UnfilteredRowIterator rowIterator;
+
+
+    public MemtableRangeIterator(Memtable memtable,
+                                 PrimaryKey.Factory pkFactory,
+                                 AbstractBounds<PartitionPosition> keyRange)
+    {
+        super(pkFactory.createTokenOnly(keyRange.left.getToken()),
+              pkFactory.createTokenOnly(maxToken(keyRange, memtable.metadata().partitioner)),
+              memtable.getOperations());
+
+        TableMetadata metadata = memtable.metadata();
+        this.memtable = memtable;
+        this.pkFactory = pkFactory;
+        this.keyRange = keyRange;
+        this.columns = ColumnFilter.selectionBuilder()
+                                           .addAll(metadata.partitionKeyColumns())
+                                           .addAll(metadata.clusteringColumns())
+                                           .build();
+
+        DataRange dataRange = new DataRange(keyRange, new ClusteringIndexSliceFilter(Slices.ALL, false));
+        this.partitionIterator = memtable.makePartitionIterator(columns, dataRange);
+        this.rowIterator = null;
+    }
+
+    private static Token maxToken(AbstractBounds<PartitionPosition> keyRange, IPartitioner partitioner)
+    {
+        return keyRange.right.getToken().isMinimum() ? partitioner.getMaximumToken() : keyRange.right.getToken();
+    }
+
+    @Override
+    protected void performSkipTo(PrimaryKey nextKey)
+    {
+        AbstractBounds<PartitionPosition> keyRange = AbstractBounds.bounds(nextKey.partitionKey(),
+                                                                true,
+                                                                           this.keyRange.right,
+                                                                           this.keyRange.inclusiveRight());
+        DataRange dataRange = new DataRange(keyRange, new ClusteringIndexSliceFilter(Slices.ALL, false));
+        this.partitionIterator = memtable.makePartitionIterator(columns, dataRange);
+        if (partitionIterator.hasNext())
+        {
+            this.rowIterator = partitionIterator.next();
+            if (!nextKey.hasEmptyClustering() && rowIterator.partitionKey().equals(nextKey.partitionKey()))
+            {
+                Slice slice = Slice.make(nextKey.clustering(), Clustering.EMPTY);
+                Slices slices = Slices.with(this.memtable.metadata().comparator, slice);
+                this.rowIterator = memtable.getPartition(nextKey.partitionKey()).unfilteredIterator(this.columns, slices, false);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        partitionIterator.close();
+        if (rowIterator != null)
+            rowIterator.close();
+    }
+
+    @Override
+    protected PrimaryKey computeNext()
+    {
+        while (hasNextRow(rowIterator) || partitionIterator.hasNext())
+        {
+            if (!hasNextRow(rowIterator))
+            {
+                rowIterator = partitionIterator.next();
+                continue;
+            }
+
+            Unfiltered unfiltered = rowIterator.next();
+            if (!unfiltered.isRow())
+                continue;
+
+            Row row = (Row) unfiltered;
+            return pkFactory.create(rowIterator.partitionKey(), row.clustering());
+        }
+        return endOfData();
+    }
+
+    private static boolean hasNextRow(UnfilteredRowIterator rowIterator)
+    {
+        return rowIterator != null && rowIterator.hasNext();
+    }
+}

src/java/org/apache/cassandra/index/sai/utils/RangeAntiJoinIterator.java
@@ -1,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.utils;
+
+import java.io.IOException;
+
+import org.apache.cassandra.io.util.FileUtils;
+
+/**
+ * An iterator wrapper that wraps two iterators (left and right) and returns the primary keys from the left iterator
+ * that do not match the primary keys from the right iterator. The keys returned by the wrapped iterators must
+ * follow token-clustering order.
+ */
+public class RangeAntiJoinIterator extends RangeIterator
+{
+    final RangeIterator left;
+    final RangeIterator right;
+
+    private PrimaryKey nextKeyToSkip = null;
+
+    private RangeAntiJoinIterator(RangeIterator left, RangeIterator right)
+    {
+        super(left.getMinimum(), left.getMaximum(), left.getCount());
+        this.left = left;
+        this.right = right;
+    }
+
+    public static RangeAntiJoinIterator create(RangeIterator left, RangeIterator right)
+    {
+        return new RangeAntiJoinIterator(left, right);
+    }
+
+    protected void performSkipTo(PrimaryKey nextKey)
+    {
+        left.skipTo(nextKey);
+        right.skipTo(nextKey);
+    }
+
+    public void close() throws IOException
+    {
+        FileUtils.closeQuietly(left);
+        FileUtils.closeQuietly(right);
+    }
+
+
+    protected PrimaryKey computeNext()
+    {
+        if (nextKeyToSkip == null)
+            nextKeyToSkip = right.nextOrNull();
+
+        PrimaryKey key = left.nextOrNull();
+        int cmp = compare(key, nextKeyToSkip);
+
+        while (key != null && cmp >= 0)
+        {
+            if (cmp == 0)
+            {
+                key = left.nextOrNull();
+                nextKeyToSkip = right.nextOrNull();
+            }
+            else
+            {
+                nextKeyToSkip = right.skipTo(key);
+            }
+            cmp = compare(key, nextKeyToSkip);
+        }
+
+        return key != null ? key : endOfData();
+    }
+
+    private int compare(PrimaryKey key1, PrimaryKey key2)
+    {
+        return (key1 == null || key2 == null) ? -1 : key1.compareTo(key2);
+    }
+}

datastax/cassandra

src/java/org/apache/cassandra/index/sai/memory/MemtableRangeIterator.java
@@ -1,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.memory;
+
+import java.io.IOException;
+
+import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.db.DataRange;
+import org.apache.cassandra.db.PartitionPosition;
+import org.apache.cassandra.db.Slice;
+import org.apache.cassandra.db.Slices;
+import org.apache.cassandra.db.filter.ClusteringIndexSliceFilter;
+import org.apache.cassandra.db.filter.ColumnFilter;
+import org.apache.cassandra.db.memtable.Memtable;
+import org.apache.cassandra.db.partitions.UnfilteredPartitionIterator;
+import org.apache.cassandra.db.rows.Row;
+import org.apache.cassandra.db.rows.Unfiltered;
+import org.apache.cassandra.db.rows.UnfilteredRowIterator;
+import org.apache.cassandra.dht.AbstractBounds;
+import org.apache.cassandra.dht.IPartitioner;
+import org.apache.cassandra.dht.Token;
+import org.apache.cassandra.index.sai.utils.PrimaryKey;
+import org.apache.cassandra.index.sai.utils.RangeIterator;
+import org.apache.cassandra.schema.TableMetadata;
+
+/**
+ * Iterates over primary keys in a memtable
+ */
+public class MemtableRangeIterator extends RangeIterator
+{
+    private final Memtable memtable;
+    private final PrimaryKey.Factory pkFactory;
+    private final AbstractBounds<PartitionPosition> keyRange;
+    private final ColumnFilter columns;
+    private UnfilteredPartitionIterator partitionIterator;
+    private UnfilteredRowIterator rowIterator;
+
+
+    public MemtableRangeIterator(Memtable memtable,
+                                 PrimaryKey.Factory pkFactory,
+                                 AbstractBounds<PartitionPosition> keyRange)
+    {
+        super(pkFactory.createTokenOnly(keyRange.left.getToken()),
+              pkFactory.createTokenOnly(maxToken(keyRange, memtable.metadata().partitioner)),
+              memtable.getOperations());
+
+        TableMetadata metadata = memtable.metadata();
+        this.memtable = memtable;
+        this.pkFactory = pkFactory;
+        this.keyRange = keyRange;
+        this.columns = ColumnFilter.selectionBuilder()
+                                           .addAll(metadata.partitionKeyColumns())
+                                           .addAll(metadata.clusteringColumns())
+                                           .build();
+
+        DataRange dataRange = new DataRange(keyRange, new ClusteringIndexSliceFilter(Slices.ALL, false));
+        this.partitionIterator = memtable.makePartitionIterator(columns, dataRange);
+        this.rowIterator = null;
+    }
+
+    private static Token maxToken(AbstractBounds<PartitionPosition> keyRange, IPartitioner partitioner)
+    {
+        return keyRange.right.getToken().isMinimum() ? partitioner.getMaximumToken() : keyRange.right.getToken();
+    }
+
+    @Override
+    protected void performSkipTo(PrimaryKey nextKey)
+    {
+        AbstractBounds<PartitionPosition> partitionBounds = AbstractBounds.bounds(nextKey.partitionKey(),
+                                                                                  true,
+                                                                                  this.keyRange.right,
+                                                                                  this.keyRange.inclusiveRight());
+        DataRange dataRange = new DataRange(partitionBounds, new ClusteringIndexSliceFilter(Slices.ALL, false));
+        this.partitionIterator = memtable.makePartitionIterator(columns, dataRange);
+        if (partitionIterator.hasNext())
+        {
+            this.rowIterator = partitionIterator.next();
+            if (!nextKey.hasEmptyClustering() && rowIterator.partitionKey().equals(nextKey.partitionKey()))
+            {
+                Slice slice = Slice.make(nextKey.clustering(), Clustering.EMPTY);
+                Slices slices = Slices.with(this.memtable.metadata().comparator, slice);
+                this.rowIterator = memtable.getPartition(nextKey.partitionKey()).unfilteredIterator(this.columns, slices, false);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        partitionIterator.close();
+        if (rowIterator != null)
+            rowIterator.close();
+    }
+
+    @Override
+    protected PrimaryKey computeNext()
+    {
+        while (hasNextRow(rowIterator) || partitionIterator.hasNext())
+        {
+            if (!hasNextRow(rowIterator))
+            {
+                rowIterator = partitionIterator.next();
+                continue;
+            }
+
+            Unfiltered unfiltered = rowIterator.next();
+            if (unfiltered.isRow())
+            {
+                Row row = (Row) unfiltered;
+                return pkFactory.create(rowIterator.partitionKey(), row.clustering());
+            }
+        }
+        return endOfData();
+    }
+
+    private static boolean hasNextRow(UnfilteredRowIterator rowIterator)
+    {
+        return rowIterator != null && rowIterator.hasNext();
+    }
+}

src/java/org/apache/cassandra/index/sai/utils/RangeAntiJoinIterator.java
@@ -1,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.utils;
+
+import java.io.IOException;
+
+import org.apache.cassandra.io.util.FileUtils;
+import org.mortbay.util.IO;
+
+/**
+ * An iterator wrapper that wraps two iterators (left and right) and returns the primary keys from the left iterator
+ * that do not match the primary keys from the right iterator. The keys returned by the wrapped iterators must
+ * follow token-clustering order.
+ */
+public class RangeAntiJoinIterator extends RangeIterator
+{
+    final RangeIterator left;
+    final RangeIterator right;
+
+    private PrimaryKey nextKeyToSkip = null;
+
+    private RangeAntiJoinIterator(RangeIterator left, RangeIterator right)
+    {
+        super(left.getMinimum(), left.getMaximum(), left.getMaxKeys());
+        this.left = left;
+        this.right = right;
+    }
+
+    public static RangeAntiJoinIterator create(RangeIterator left, RangeIterator right)
+    {
+        return new RangeAntiJoinIterator(left, right);
+    }
+
+    protected void performSkipTo(PrimaryKey nextKey)
+    {
+        left.skipTo(nextKey);
+        right.skipTo(nextKey);
+    }
+
+    public void close() throws IOException
+    {
+        FileUtils.close(left, right);
+    }
+
+    protected PrimaryKey computeNext()
+    {
+        if (nextKeyToSkip == null)
+            nextKeyToSkip = right.nextOrNull();
+
+        PrimaryKey key = left.nextOrNull();
+        int cmp = compare(key, nextKeyToSkip);
+
+        while (key != null && cmp >= 0)
+        {
+            if (cmp == 0)
+            {
+                key = left.nextOrNull();
+            }
+            else
+            {
+                right.skipTo(key);
+            }
+            nextKeyToSkip = right.nextOrNull();
+            cmp = compare(key, nextKeyToSkip);
+        }
+
+        return key != null ? key : endOfData();
+    }
+
+    private int compare(PrimaryKey key1, PrimaryKey key2)
+    {
+        return (key1 == null || key2 == null) ? -1 : key1.compareTo(key2);
+    }
+}