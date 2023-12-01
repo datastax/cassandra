msmygit/vscassandra

Now that we implemented NOT IN, both equals and non-equals can be rewritten as slices.
column != value ---> column NOT IN (value)
column  = value ---> column >= value AND column <= value

And because slices can be freely merged, this opens room for
many interesting queries that were previously
impossible, e.g.:

SELECT ... WHERE c1 != ?
SELECT ... WHERE c1 != ? AND c1 != ? AND ...
SELECT ... WHERE c1 = ? AND (c1, c2) != (v1, v2)
SELECT ... WHERE c1 IN ? AND (c1, c2) != (v1, v2)

src/java/org/apache/cassandra/cql3/restrictions/MultiColumnRestriction.java
@@ -211,8 +211,20 @@ public abstract class MultiColumnRestriction implements SingleRestriction
         @Override
         public SingleRestriction doMergeWith(SingleRestriction otherRestriction)
         {
+            if (otherRestriction instanceof SliceRestriction)
+            {
+                SingleRestriction thisAsSlice = this.toSliceRestriction();
+                return thisAsSlice.mergeWith(otherRestriction);
+            }
             throw invalidRequest("%s cannot be restricted by more than one relation if it includes an Equal",
                                  getColumnsInCommons(otherRestriction));
+        }
+
+        private SingleRestriction toSliceRestriction()
+        {
+            SliceRestriction start = SliceRestriction.fromBound(columnDefs, Bound.START, true, this.value);
+            SliceRestriction end = SliceRestriction.fromBound(columnDefs, Bound.END, true, this.value);
+            return start.mergeWith(end);
         }
 
         @Override

datastax/cassandra

src/java/org/apache/cassandra/cql3/restrictions/MultiColumnRestriction.java
@@ -210,8 +210,20 @@ public abstract class MultiColumnRestriction implements SingleRestriction
         @Override
         public SingleRestriction doMergeWith(SingleRestriction otherRestriction)
         {
+            if (otherRestriction instanceof SliceRestriction)
+            {
+                SingleRestriction thisAsSlice = this.toSliceRestriction();
+                return thisAsSlice.mergeWith(otherRestriction);
+            }
             throw invalidRequest("%s cannot be restricted by more than one relation if it includes an Equal",
                                  getColumnsInCommons(otherRestriction));
+        }
+
+        private SingleRestriction toSliceRestriction()
+        {
+            SliceRestriction start = SliceRestriction.fromBound(columnDefs, Bound.START, true, this.term);
+            SliceRestriction end = SliceRestriction.fromBound(columnDefs, Bound.END, true, this.term);
+            return start.mergeWith(end);
         }
 
         @Override