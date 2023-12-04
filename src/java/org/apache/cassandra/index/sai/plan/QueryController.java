112dc5a20573c4b4aef565a84c75bbceec1fc7c4
Michael Marshall <mmarshall@apache.org>
2023-12-04 10:41:55 -0600

    Use anti-join to filter out shadowed PKs

--- src/java/org/apache/cassandra/index/sai/plan/QueryController.java
+++ src/java/org/apache/cassandra/index/sai/plan/QueryController.java
@@ -60,10 +60,12 @@ import org.apache.cassandra.index.sai.QueryContext;
 import org.apache.cassandra.index.sai.SSTableIndex;
 import org.apache.cassandra.index.sai.StorageAttachedIndex;
 import org.apache.cassandra.index.sai.disk.format.IndexFeatureSet;
+import org.apache.cassandra.index.sai.memory.KeyRangeIterator;
 import org.apache.cassandra.index.sai.metrics.TableQueryMetrics;
 import org.apache.cassandra.index.sai.utils.AbortedOperationException;
 import org.apache.cassandra.index.sai.utils.OrderingFilterRangeIterator;
 import org.apache.cassandra.index.sai.utils.PrimaryKey;
+import org.apache.cassandra.index.sai.utils.RangeAntiJoinIterator;
 import org.apache.cassandra.index.sai.utils.RangeIntersectionIterator;
 import org.apache.cassandra.index.sai.utils.RangeIterator;
 import org.apache.cassandra.index.sai.utils.RangeUnionIterator;
@@ -283,8 +285,8 @@ public class QueryController
                                                                                  .stream()
                                                                                  .map(e -> createRowIdIterator(e, true, limit))
                                                                                  .collect(Collectors.toList());
-            // TODO do an anti-joint of known shadow keys
-            return TermIterator.build(sstableIntersections, memtableResults, queryView.referencedIndexes, queryContext);
+            var result = TermIterator.build(sstableIntersections, memtableResults, queryView.referencedIndexes, queryContext);
+            return filterShadowedPrimaryKeys(result);
         }
         catch (Throwable t)
         {
@@ -297,10 +299,8 @@ public class QueryController
     // This is a hybrid query. We apply all other predicates before ordering and limiting.
     public RangeIterator getTopKRows(RangeIterator source, RowFilter.Expression expression)
     {
-        // TODO do an anti-joint of known shadow keys
-        return new OrderingFilterRangeIterator(source,
-                                               ORDER_CHUNK_SIZE,
-                                               list -> this.getTopKRows(list, expression));
+        var result = new OrderingFilterRangeIterator(source, ORDER_CHUNK_SIZE, list -> this.getTopKRows(list, expression));
+        return filterShadowedPrimaryKeys(result);
     }
 
     private RangeIterator getTopKRows(List<PrimaryKey> rawSourceKeys, RowFilter.Expression expression)
@@ -374,6 +374,21 @@ public class QueryController
                                     }).collect(Collectors.toList());
 
         return RangeUnionIterator.builder(subIterators.size()).add(subIterators).build();
+    }
+
+    /**
+     * Filter out shadowed {@link PrimaryKey} from the source iterator. This is only necessary when a query is ordering
+     * and limiting results.
+     * @param source the source iterator
+     * @return a filtered iterator
+     */
+    private RangeIterator filterShadowedPrimaryKeys(RangeIterator source)
+    {
+        if (queryContext.getShadowedPrimaryKeys().isEmpty())
+            return source;
+        // This logic used to be managed at the vector index level. However, that led to a lot of complexity,
+        // especially when multiple rows shared the same value. For now, we just filter the results here.
+        return RangeAntiJoinIterator.create(source, new KeyRangeIterator(queryContext.getShadowedPrimaryKeys()));
     }
 
     public int getExactLimit()
