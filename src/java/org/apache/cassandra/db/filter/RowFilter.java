3252a800bafa6ef6c04921b03d0cc8b0a069d003
Michael Marshall <michael.marshall@datastax.com>
2023-12-05 08:31:10 -0600

    Simplify GEO_DISTANCE row satisfaction check: just use haversine distance (#864)
    The current GEO_DISTANCE logic to check if an expression `isSatisfiedBy` uses a two phase approach to first check the square distance and then, if the square distance is near the boundary, check the haversine distance. This logic was based on the assumption that we needed to prevent unnecessary execution of the `SloppyMath.haversinMeters` method.
    
    There were two problems.
    
    First, the value for the meters per degree latitude value was slightly wrong and that led to false positive results. The first commit removes those false positives by increasing `DISTANCE_PER_DEGREE_LATITUDE`. 
    
    However, I added a benchmark in the first commit only to find that the square distance is about 50 nanoseconds, on average, and the `SloppyMath.haversinMeters` is between 100 and 110 nanoseconds. Given that the value of the square distance check decreases as the search moves away from the equator, and given the added logical complexity, I think it is a premature optimization to use the square distance check at this time. Instead, we can just rely on the `SloppyMath.haversinMeters` to verify our results. As a consequence, our false positive ratio decreased.

--- src/java/org/apache/cassandra/db/filter/RowFilter.java
+++ src/java/org/apache/cassandra/db/filter/RowFilter.java
@@ -1413,7 +1413,6 @@ public class RowFilter implements Iterable<RowFilter.Expression>
         private final ByteBuffer distance;
         private final Operator distanceOperator;
         private final float searchRadiusMeters;
-        private final float searchRadiusDegreesSquared;
         private final float searchLat;
         private final float searchLon;
 
@@ -1424,7 +1423,6 @@ public class RowFilter implements Iterable<RowFilter.Expression>
             this.distanceOperator = operator;
             this.distance = distance;
             searchRadiusMeters = FloatType.instance.compose(distance);
-            searchRadiusDegreesSquared = GeoUtil.maximumSquareDistanceForCorrectLatLongSimilarity(searchRadiusMeters);
             var pointVector = TypeUtil.decomposeVector(column.type, point);
             // This is validated earlier in the parser because the column requires size 2, so only assert on it
             assert pointVector.length == 2 : "GEO_DISTANCE requires search vector to have 2 dimensions.";
@@ -1464,11 +1462,6 @@ public class RowFilter implements Iterable<RowFilter.Expression>
             ByteBuffer foundValue = getValue(metadata, partitionKey, row);
             if (foundValue == null)
                 return false;
-            double squareDistance = VectorUtil.squareDistance(foundValue.array(), value.array());
-            // If we are within the search radius degrees, then we are within the search radius meters.
-            // This relies on the fact that lat/long distort distance by making close points further apart.
-            if (squareDistance <= searchRadiusDegreesSquared)
-                return true;
             var foundVector = TypeUtil.decomposeVector(column.type, foundValue);
             double haversineDistance = SloppyMath.haversinMeters(foundVector[0], foundVector[1], searchLat, searchLon);
             switch (distanceOperator)
