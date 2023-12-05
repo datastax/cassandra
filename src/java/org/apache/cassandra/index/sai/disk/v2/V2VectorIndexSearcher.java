413c3a959b8dd4d7ce61522b0c0c6e0af995c0e4
Michael Marshall <michael.marshall@datastax.com>
2023-12-05 14:59:42 -0600

    Remove assumption that PK is PrimaryKeyWithSource in limitToTopResults (#895)
    * Add failing test and update two more
    
    I modified two tests because I recently added them
    and still have enough context to know that we should
    cover both before and after the flush on those tests.
    
    * Remove invalid PrimaryKeyWithSource type assertion
    
    * Remove unnecessary update to testTwoPredicates
    
    The test was modified, but it wasn't covering
    the code that I thought it was.
    
     (Rebase of commit 0812b3d669ca844490b20ddd8846269ed3e40ebc)

--- src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
+++ src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
@@ -376,11 +376,10 @@ public class V2VectorIndexSearcher extends IndexSearcher implements SegmentOrder
             {
                 // turn the pk back into a row id, with a fast path for the case where the pk is from this sstable
                 PrimaryKey primaryKey = keysInRange.get(i);
-                assert primaryKey instanceof PrimaryKeyWithSource : "Expected PrimaryKeyWithSource, got " + primaryKey;
-                PrimaryKeyWithSource pkws = (PrimaryKeyWithSource) primaryKey;
                 long sstableRowId;
-                if (pkws.getSourceSstableId().equals(primaryKeyMap.getSSTableId()))
-                    sstableRowId = pkws.getSourceRowId();
+                if (primaryKey instanceof PrimaryKeyWithSource
+                    && ((PrimaryKeyWithSource) primaryKey).getSourceSstableId().equals(primaryKeyMap.getSSTableId()))
+                    sstableRowId = ((PrimaryKeyWithSource) primaryKey).getSourceRowId();
                 else
                     sstableRowId = primaryKeyMap.exactRowIdOrInvertedCeiling(primaryKey);
 
