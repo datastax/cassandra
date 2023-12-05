28811e8977d3a43333bc33e688dd5a87321db029
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2023-12-05 21:38:41 +0100

    CNDB-8074 port DSE tickets created since the last merge (#875)
    * cndb-7691: [PORT] ignore unindexable value in memtable index (#20911)
    
    - do validation and send client warning on coordinator side if `dse.sai.validate_max_term_size_at_coordinator` is true
    
    - skip analyzer and validation if original value is less than max term size
    
    * CNDB-7739: [PORT] change SAI getInitializationTask() to flush memtable and set canFlushFromMemtableIndex to true when IndexBuildDecider#onIntialBuild is to skip index build
    
    Add Index#shouldSkipInitialization() to be invoked in SIM and allow to skip initialization for non-SAI indexes.
    
    * cndb-4310: [PORT] add ReadObserver and ReadObserverFactory to track what local data has been read
    
    * add getMemtablesLiveSize
    
    * port Unfiltered.[min|max]Timestamp
    
    * fix maxSSTablesToCompact calculation
    
    When minSSTableSize is set to MIN_SSTABLE_SIZE_AUTO
    (v1 compatibility mode), the maxSSTablesToCompact
    becomes negative.
    
    To fix this the maxSSTablesToCompact is now evaluated
    from getMinSstableSizeBytes, not the raw minSSTableSize
    parameter.
    
    * port split method from DSE
    
    Splitter correctly splits range like (x, min].
    Without the fix the splitter wouldn't include
    min value in the result. With this change the min
    is included in one of the resulting ranges.
    
    (cherry picked from commit e8ad64dab3d180a3b79705f37c0196870fb0d0b2)
    
    Fix a couple of tests in NativeIndexDDLTest

--- src/java/org/apache/cassandra/db/lifecycle/Tracker.java
+++ src/java/org/apache/cassandra/db/lifecycle/Tracker.java
@@ -55,6 +55,7 @@ import org.apache.cassandra.notifications.MemtableDiscardedNotification;
 import org.apache.cassandra.notifications.MemtableRenewedNotification;
 import org.apache.cassandra.notifications.MemtableSwitchedNotification;
 import org.apache.cassandra.notifications.SSTableAddedNotification;
+import org.apache.cassandra.notifications.SSTableAddingNotification;
 import org.apache.cassandra.notifications.SSTableDeletingNotification;
 import org.apache.cassandra.notifications.SSTableListChangedNotification;
 import org.apache.cassandra.notifications.SSTableRepairStatusChanged;
@@ -288,6 +289,7 @@ public class Tracker
                                      boolean maybeIncrementallyBackup,
                                      boolean updateSize)
     {
+        notifyAdding(sstables, operationType);
         if (!isDummy())
             setupOnline(sstables);
         apply(updateLiveSet(emptySet(), sstables));
@@ -511,10 +513,12 @@ public class Tracker
         // back up before creating a new Snapshot (which makes the new one eligible for compaction)
         maybeIncrementallyBackup(sstables);
 
+        Throwable fail;
+        fail = notifyAdding(sstables, memtable, null, OperationType.FLUSH, operationId);
+
         apply(View.replaceFlushed(memtable, sstables));
 
-        Throwable fail;
-        fail = updateSizeTracking(emptySet(), sstables, null);
+        fail = updateSizeTracking(emptySet(), sstables, fail);
 
         // TODO: if we're invalidated, should we notifyadded AND removed, or just skip both?
         fail = notifyAdded(sstables, OperationType.FLUSH, operationId, false, memtable, fail);
@@ -555,7 +559,7 @@ public class Tracker
     @Nullable
     public SSTableReader getLiveSSTable(String filename)
     {
-        return view.get().getLiveSSTable(filename);
+        return view.getLiveSSTable(filename);
     }
 
     public void maybeIncrementallyBackup(final Iterable<SSTableReader> sstables)
@@ -609,6 +613,28 @@ public class Tracker
             }
         }
         return accumulate;
+    }
+
+    Throwable notifyAdding(Iterable<SSTableReader> added, @Nullable Memtable memtable, Throwable accumulate, OperationType type, Optional<TimeUUID> operationId)
+    {
+        INotification notification = new SSTableAddingNotification(added, memtable, type, operationId);
+        for (INotificationConsumer subscriber : subscribers)
+        {
+            try
+            {
+                subscriber.handleNotification(notification, this);
+            }
+            catch (Throwable t)
+            {
+                accumulate = merge(accumulate, t);
+            }
+        }
+        return accumulate;
+    }
+
+    public void notifyAdding(Iterable<SSTableReader> added, OperationType operationType)
+    {
+        maybeFail(notifyAdding(added, null, null, operationType, Optional.empty()));
     }
 
     @VisibleForTesting
