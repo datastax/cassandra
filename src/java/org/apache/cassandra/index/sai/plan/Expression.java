526ea41918c059fcfbefc51d247fd5feb1c459b6
Michael Marshall <michael.marshall@datastax.com>
2023-12-05 08:31:10 -0600

    Simplify GEO_DISTANCE row satisfaction check: just use haversine distance (#864)
    The current GEO_DISTANCE logic to check if an expression `isSatisfiedBy` uses a two phase approach to first check the square distance and then, if the square distance is near the boundary, check the haversine distance. This logic was based on the assumption that we needed to prevent unnecessary execution of the `SloppyMath.haversinMeters` method.
    
    There were two problems.
    
    First, the value for the meters per degree latitude value was slightly wrong and that led to false positive results. The first commit removes those false positives by increasing `DISTANCE_PER_DEGREE_LATITUDE`.
    
    However, I added a benchmark in the first commit only to find that the square distance is about 50 nanoseconds, on average, and the `SloppyMath.haversinMeters` is between 100 and 110 nanoseconds. Given that the value of the square distance check decreases as the search moves away from the equator, and given the added logical complexity, I think it is a premature optimization to use the square distance check at this time. Instead, we can just rely on the `SloppyMath.haversinMeters` to verify our results. As a consequence, our false positive ratio decreased.
    
     (Rebase of commit 7d75b46ce4fb3e45de5525e3735cd87e45ee72f0)

--- src/java/org/apache/cassandra/index/sai/plan/Expression.java
+++ src/java/org/apache/cassandra/index/sai/plan/Expression.java
@@ -37,7 +37,6 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.github.jbellis.jvector.vector.VectorUtil;
 import org.apache.cassandra.cql3.Operator;
 import org.apache.cassandra.db.marshal.AbstractType;
 import org.apache.cassandra.db.marshal.CompositeType;
@@ -241,7 +240,6 @@ public class Expression
                 lower = new Bound(value, validator, true);
                 assert upper != null;
                 searchRadiusMeters = FloatType.instance.compose(upper.value.raw);
-                searchRadiusDegreesSquared = GeoUtil.maximumSquareDistanceForCorrectLatLongSimilarity(searchRadiusMeters);
                 boundedAnnEuclideanDistanceThreshold = GeoUtil.amplifiedEuclideanSimilarityThreshold(lower.value.vector, searchRadiusMeters);
                 break;
         }
@@ -271,13 +269,6 @@ public class Expression
 
         if (operation == Op.BOUNDED_ANN)
         {
-            double squareDistance = VectorUtil.squareDistance(lower.value.vector, value.vector);
-            // If we are within the search radius degrees, then we are within the search radius meters.
-            // This relies on the fact that lat/long distort distance by making close points further apart.
-            if (squareDistance <= searchRadiusDegreesSquared)
-                return true;
-            // Otherwise, we need to compute the more expensive haversine distance to determine if we are within the
-            // search radius meters.
             double haversineDistance = SloppyMath.haversinMeters(lower.value.vector[0], lower.value.vector[1], value.vector[0], value.vector[1]);
             return upperInclusive ? haversineDistance <= searchRadiusMeters : haversineDistance < searchRadiusMeters;
         }
