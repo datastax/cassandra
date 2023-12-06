e81387ada303e0b4f2da6d34af1c980ef7cacd12
Michael Marshall <mmarshall@apache.org>
2023-12-06 16:47:02 -0600

    Return findTopApproximatePostings in row id order

--- src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
+++ src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.index.sai.disk.v2;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import javax.annotation.Nullable;
@@ -280,6 +281,7 @@ public class V2VectorIndexSearcher extends IndexSearcher implements SegmentOrder
         // top K ascending
         for (int i = end; i >= 0; i--)
             postings[end - i] = pairs.get(i).node;
+        Arrays.sort(postings);
         return postings;
     }
 
