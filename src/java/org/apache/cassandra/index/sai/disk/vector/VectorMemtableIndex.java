ae3d287527fc7f26849a3a4d1b785403af27128a
Michael Marshall <mmarshall@apache.org>
2023-12-07 11:02:43 -0600

    Remove unused import

--- src/java/org/apache/cassandra/index/sai/disk/vector/VectorMemtableIndex.java
+++ src/java/org/apache/cassandra/index/sai/disk/vector/VectorMemtableIndex.java
@@ -21,7 +21,6 @@ package org.apache.cassandra.index.sai.disk.vector;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NavigableSet;
