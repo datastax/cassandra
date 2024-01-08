d516feb42b2f6e194b7fabf139c283365fd00882
Piotr Kołaczkowski <pkolaczk@gmail.com>
2024-01-08 18:57:01 +0100

    Post-filtering alternate query path for vector indexes (#915)
    * Use science to obtain a better value for softLimit
    * Select best query plan based on cost estimates
    
     (Rebase of commit addcdbbdded4216e53e9d1765cd0d64c1ffe577c)

--- src/java/org/apache/cassandra/db/memtable/Memtable.java
+++ src/java/org/apache/cassandra/db/memtable/Memtable.java
@@ -219,6 +219,9 @@ public interface Memtable extends Comparable<Memtable>, UnfilteredSource, CellSo
     /** Number of partitions stored in the memtable */
     long partitionCount();
 
+    /** Number of rows stored in the memtable */
+    long rowCount();
+
     /** Size of the data not accounting for any metadata / mapping overheads */
     long getLiveDataSize();
 
--- src/java/org/apache/cassandra/db/memtable/TrieMemtable.java
+++ src/java/org/apache/cassandra/db/memtable/TrieMemtable.java
@@ -242,6 +242,14 @@ public class TrieMemtable extends AbstractShardedMemtable
         return shards.length;
     }
 
+    @Override
+    public long rowCount()
+    {
+        DataRange range = DataRange.allData(metadata().partitioner);
+        ColumnFilter columnFilter = ColumnFilter.allRegularColumnsBuilder(metadata(), true).build();
+        return rowCount(columnFilter, range);
+    }
+
     public long rowCount(final ColumnFilter columnFilter, final DataRange dataRange)
     {
         int total = 0;
