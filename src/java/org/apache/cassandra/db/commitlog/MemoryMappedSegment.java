8d8bebd3cde6a9df87499e6dfdb75c451c4c29b7
Szymon Miężał <szymon.miezal@datastax.com>
2024-01-11 11:58:49 +0100

    STAR-1902: Option to disable call to NativeLibrary.trySkipCache
    Introduce system property "cassandra.commitlog.skip_file_advice" that allows
    to skip the native call to "fadvise" with "FADV_DONTNEED" argument.
    The native call is not skipped by default.
    
    This patch was originally implemented in DSE 5.1 under DSP-22315.

--- src/java/org/apache/cassandra/db/commitlog/MemoryMappedSegment.java
+++ src/java/org/apache/cassandra/db/commitlog/MemoryMappedSegment.java
@@ -22,8 +22,11 @@ import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.io.FSWriteError;
+import org.apache.cassandra.io.util.File;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.utils.INativeLibrary;
 import org.apache.cassandra.utils.SyncUtil;
@@ -33,12 +36,16 @@ import org.apache.cassandra.utils.SyncUtil;
  * mutation threads write. On sync forces the buffer to disk.
  * If possible, recycles used segment files to avoid reallocating large chunks of disk.
  */
-public class MemoryMappedSegment extends CommitLogSegment
+class MemoryMappedSegment extends CommitLogSegment
 {
+    @VisibleForTesting
+    static boolean skipFileAdviseToFreePageCache = Boolean.getBoolean("cassandra.commitlog.skip_file_advice");
+
     /**
      * Constructs a new segment file.
      *
      * @param commitLog the commit log it will be used with.
+     * @param manager the commit log segment manager that is linked with {@code commitLog}.
      */
     MemoryMappedSegment(CommitLog commitLog, AbstractCommitLogSegmentManager manager)
     {
@@ -90,6 +97,15 @@ public class MemoryMappedSegment extends CommitLogSegment
         {
             throw new FSWriteError(e, getPath());
         }
+
+        if (!skipFileAdviseToFreePageCache)
+        {
+            adviceOnFileToFreePageCache(fd, startMarker, nextMarker, logFile);
+        }
+    }
+
+    void adviceOnFileToFreePageCache(int fd, int startMarker, int nextMarker, File logFile)
+    {
         INativeLibrary.instance.trySkipCache(fd, startMarker, nextMarker, logFile.absolutePath());
     }
 
