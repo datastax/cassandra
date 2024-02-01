bfbde08e4009670d21a17a7ea20bff73dd48ad0f
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2024-02-01 15:24:43 +0100

    CNDB-8425 merge master to converged-cassandra (#959)
    * CNDB-8425 port: CNDB-7916 add isMarkedCompacted(Descriptor)
    
    The method allows to check whether the sstable is marked as compacted (obsolete)
    without obtaining a reference to sstable reader.
    
    Extract cleanup task to an inner class to be able
    to alter it with byteman in CNDB tests
    
    * CNDB-8425 port: CNDB-7381 Added more logging around keyspaces and column family stores creation/removal
    
    (cherry picked from commit e9d02f9ddb2767847714937c2094562af5c1a7c4)

--- src/java/org/apache/cassandra/schema/Schema.java
+++ src/java/org/apache/cassandra/schema/Schema.java
@@ -740,6 +740,7 @@ public class Schema implements SchemaProvider
             Keyspace unloadedKeyspace = maybeRemoveKeyspaceInstance(keyspaceMetadata.name, ks -> {
                 ks.unload(dropData);
                 unload(keyspaceMetadata);
+                logger.debug("Instance removed for keyspace {}", ks.getName());
             });
             assert unloadedKeyspace == keyspace;
 
