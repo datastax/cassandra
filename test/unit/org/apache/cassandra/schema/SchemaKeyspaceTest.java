msmygit/vscassandra

STAR-1205 Add everywhere() to KeyspaceParams API (#383)
To allow creating Keyspace with Everywhere strategy the correct
approach is to extend its API with everywhere() method. This is then
tested in a very simple test.

Needed for tests in CNDB.

test/unit/org/apache/cassandra/schema/SchemaKeyspaceTest.java
@@ -249,4 +249,11 @@ public class SchemaKeyspaceTest
         assertTrue(SchemaManager.isKeyspaceWithLocalStrategy(SchemaManager.instance.getKeyspaceMetadata("local_ks")));
         assertFalse(SchemaManager.isKeyspaceWithLocalStrategy("simple_ks"));
     }
+
+    @Test
+    public void testEverywhere()
+    {
+        SchemaLoader.createKeyspace("everywhereKeyspace", KeyspaceParams.everywhere());
+        assertFalse(SchemaManager.isKeyspaceWithLocalStrategy("everywhereKeyspace"));
+    }
 }

datastax/cassandra

(cherry picked from commit 8d2210c375f5e62da9e6e443ba8b908dc14881b8)
(cherry picked from commit d390b4e6026b386ef8068f4a3c9cbeedb930920f)
(cherry picked from commit 27eb798a96053cd13845d428e7a68cfe8308d581)
(cherry picked from commit dbe37a61d057426d9a7a8342b24695e3a99a7ba5)
(cherry picked from commit 73c70b33a1d239dd55a07d7a09a8e607605923b2)

test/unit/org/apache/cassandra/schema/SchemaKeyspaceTest.java
@@ -301,4 +301,11 @@ public class SchemaKeyspaceTest
         assertTrue(Schema.isKeyspaceWithLocalStrategy(Schema.instance.getKeyspaceMetadata("local_ks")));
         assertFalse(Schema.isKeyspaceWithLocalStrategy("simple_ks"));
     }
+
+    @Test
+    public void testEverywhere()
+    {
+        SchemaLoader.createKeyspace("everywhereKeyspace", KeyspaceParams.everywhere());
+        assertFalse(Schema.isKeyspaceWithLocalStrategy("everywhereKeyspace"));
+    }
 }