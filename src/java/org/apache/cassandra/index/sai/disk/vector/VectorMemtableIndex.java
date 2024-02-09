fef1341725c5c822e362afa5e766f433c4dea617
Michael Marshall <michael.marshall@datastax.com>
2024-02-08 22:23:46 -0600

    Fix VectorMemtableIndex#update logic to compare vectors (#987)
     (Rebase of commit e4ee2d8a1a0b371087f3e430e0263f44fa56e221)

--- src/java/org/apache/cassandra/index/sai/disk/vector/VectorMemtableIndex.java
+++ src/java/org/apache/cassandra/index/sai/disk/vector/VectorMemtableIndex.java
@@ -33,7 +33,6 @@ import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import javax.annotation.Nullable;
 
 import org.slf4j.Logger;
@@ -127,7 +126,7 @@ public class VectorMemtableIndex implements MemtableIndex
         }
         else
         {
-            different = IntStream.range(0, oldRemaining).anyMatch(i -> oldValue.get(i) != newValue.get(i));
+            different = indexContext.getValidator().compare(oldValue, newValue) != 0;
         }
 
         if (different)
