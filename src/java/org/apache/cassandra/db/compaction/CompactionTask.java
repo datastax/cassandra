a0678227b2de6304b9257c531ba9a496d01504f0
Andrés de la Peña <adelapena@users.noreply.github.com>
2024-02-27 10:20:21 +0000

    DSP-23946: Improve logging of compaction interruptions (#989)
    Extend TableOperation.StopTrigger and add it to CompactionInterruptedException, so those exceptions can provide information about why compaction has been interrupted.
    
    (cherry picked from commit dc34b40be36ec0d4a972e2388bfd25ff13fd0d52)
    
    DSP-23946 Fix RepairErrorsTest.testNoSuchRepairSessionAnticompaction due to missing @Shared annotation
    
     (Rebase of commit 91f9ae47fead29bb2fa574623f1eef8b699f9c5f)

--- src/java/org/apache/cassandra/db/compaction/CompactionTask.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionTask.java
@@ -352,7 +352,7 @@ public class CompactionTask extends AbstractCompactionTask
                 }
 
                 if (!controller.realm.isCompactionActive())
-                    throw new CompactionInterruptedException(op.getProgress());
+                    throw new CompactionInterruptedException(op.getProgress(), op.trigger());
 
                 estimatedKeys = writer.estimatedKeys();
 
@@ -391,8 +391,7 @@ public class CompactionTask extends AbstractCompactionTask
 
         void maybeStopOrUpdateState()
         {
-            if (op.isStopRequested())
-                throw new CompactionInterruptedException(op.getProgress());
+            op.throwIfStopRequested();
 
             long now = Clock.Global.nanoTime();
             if (now - lastCheckObsoletion > TimeUnit.MINUTES.toNanos(1L))
@@ -725,8 +724,8 @@ public class CompactionTask extends AbstractCompactionTask
                 writeLoop:
                 while (true)
                 {
-                    if (op.isStopRequested())
-                        throw new CompactionInterruptedException(op.getProgress());
+                    op.throwIfStopRequested();
+
                     switch (compactionCursor.copyOne(writer))
                     {
                         case EXHAUSTED:
