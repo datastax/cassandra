69e0e5883fe62e37cb1f4df3c7d1d72624a717eb
Andrés de la Peña <adelapena@users.noreply.github.com>
2024-02-27 10:20:21 +0000

    DSP-23946: Improve logging of compaction interruptions (#989)
    Extend TableOperation.StopTrigger and add it to CompactionInterruptedException, so those exceptions can provide information about why compaction has been interrupted.
    
    (cherry picked from commit dc34b40be36ec0d4a972e2388bfd25ff13fd0d52)
    
    DSP-23946 Fix RepairErrorsTest.testNoSuchRepairSessionAnticompaction due to missing @Shared annotation

--- src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
@@ -1031,7 +1031,7 @@ public class CompactionStrategyManager implements CompactionStrategyContainer
                 readLock.unlock();
             }
             return CompactionTasks.create(tasks);
-        }, operationType, false, false);
+        }, operationType, false, false, TableOperation.StopTrigger.COMPACTION);
     }
 
     /**
