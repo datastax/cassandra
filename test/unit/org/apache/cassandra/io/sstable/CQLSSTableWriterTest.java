4e93f13246773206caee8aa8937cc2924630ced1
Jonathan Ellis <jbellis@datastax.com>
2024-03-06 10:03:19 -0600

    Recognize that different column indexes may exist at different versions (#911)
    IndexDescriptor changes:
    * Add per-index versions
    * Combine per-sstable and per-index components into the same collections
    * mark the lazy-loaded Collections as private
    * replace Pair<IndexComponent, String> with AttachedIndexComponent
    * move sizeOfPerSSTableComponents into IndexDescriptor for consistency
    * rename IndexDescriptor.create methods to createNew and createFrom
    * we don't need per-format isFooComplete methods
    * rename componentName -> componentFileName
    
     (Rebase of commit 04df10aec43bc9e7d6d888aefb9efd88b7275b83)

--- test/unit/org/apache/cassandra/io/sstable/CQLSSTableWriterTest.java
+++ test/unit/org/apache/cassandra/io/sstable/CQLSSTableWriterTest.java
@@ -1505,7 +1505,7 @@ public abstract class CQLSSTableWriterTest
         File[] dataFiles = dataDir.list(f -> f.name().endsWith('-' + BigFormat.Components.DATA.type.repr));
         assertNotNull(dataFiles);
 
-        IndexDescriptor indexDescriptor = IndexDescriptor.create(Descriptor.fromFile(dataFiles[0]),
+        IndexDescriptor indexDescriptor = IndexDescriptor.createNew(Descriptor.fromFile(dataFiles[0]),
                                                                  Murmur3Partitioner.instance,
                                                                  Schema.instance.getTableMetadata(keyspace, table).comparator);
 
@@ -1549,7 +1549,7 @@ public abstract class CQLSSTableWriterTest
         assertNotNull(dataFiles);
 
         // no indexes built due to withBuildIndexes set to false
-        IndexDescriptor indexDescriptor = IndexDescriptor.create(Descriptor.fromFile(dataFiles[0]),
+        IndexDescriptor indexDescriptor = IndexDescriptor.createNew(Descriptor.fromFile(dataFiles[0]),
                                                                  Murmur3Partitioner.instance,
                                                                  Schema.instance.getTableMetadata(keyspace, table).comparator);
 
