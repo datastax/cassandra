609d2c27ad6fad800ba3a2b028393da2aa0e3e1e
Ayman Khalil <13960949+aymkhalil@users.noreply.github.com>
2024-03-20 09:24:38 -0700

    Track Sensor WRITE_BYTES for Counter Mutation on leader replica

--- src/java/org/apache/cassandra/db/CounterMutationVerbHandler.java
+++ src/java/org/apache/cassandra/db/CounterMutationVerbHandler.java
@@ -17,13 +17,26 @@
  */
 package org.apache.cassandra.db;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.cassandra.concurrent.ExecutorLocals;
 import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.locator.InetAddressAndPort;
 import org.apache.cassandra.net.IVerbHandler;
 import org.apache.cassandra.net.Message;
 import org.apache.cassandra.net.MessagingService;
+import org.apache.cassandra.net.NoPayload;
+import org.apache.cassandra.net.SensorsCustomParams;
+import org.apache.cassandra.sensors.RequestSensors;
+import org.apache.cassandra.sensors.RequestTracker;
+import org.apache.cassandra.sensors.Sensor;
+import org.apache.cassandra.sensors.SensorsRegistry;
+import org.apache.cassandra.sensors.Type;
 import org.apache.cassandra.service.StorageProxy;
 
 public class CounterMutationVerbHandler implements IVerbHandler<CounterMutation>
@@ -38,6 +51,11 @@ public class CounterMutationVerbHandler implements IVerbHandler<CounterMutation>
         final CounterMutation cm = message.payload;
         logger.trace("Applying forwarded {}", cm);
 
+        // Initialize the sensor and set ExecutorLocals
+        RequestSensors sensors = new RequestSensors();
+        ExecutorLocals locals = ExecutorLocals.create(sensors);
+        ExecutorLocals.set(locals);
+
         String localDataCenter = DatabaseDescriptor.getEndpointSnitch().getLocalDatacenter();
         // We should not wait for the result of the write in this thread,
         // otherwise we could have a distributed deadlock between replicas
@@ -48,7 +66,44 @@ public class CounterMutationVerbHandler implements IVerbHandler<CounterMutation>
         // it's own in that case.
         StorageProxy.applyCounterMutationOnLeader(cm,
                                                   localDataCenter,
-                                                  () -> MessagingService.instance().send(message.emptyResponse(), message.from()),
+                                                  () -> respond(message, message.from()),
                                                   queryStartNanoTime);
+    }
+
+    private void respond(Message<?> respondTo, InetAddressAndPort respondToAddress)
+    {
+        Message.Builder<NoPayload> response = respondTo.emptyResponseBuilder();
+        addSensorsToResponse(response);
+
+        MessagingService.instance().send(response.build(), respondToAddress);
+    }
+
+    private void addSensorsToResponse(Message.Builder<NoPayload> response) {
+        // Add write bytes sensors to the response
+        Function<String, String> requestParam = SensorsCustomParams::encodeTableInWriteByteRequestParam;
+        Function<String, String> tableParam = SensorsCustomParams::encodeTableInWriteByteTableParam;
+        RequestSensors requestSensors = RequestTracker.instance.get();
+        if (requestSensors != null)
+        {
+            Collection<Sensor> sensors = requestSensors.getSensors(Type.WRITE_BYTES);
+            addSensorsToResponse(sensors, requestParam, tableParam, response);
+        }
+    }
+
+    private void addSensorsToResponse(Collection<Sensor> sensors, Function<String, String> requestParamSupplier, Function<String, String> tableParamSupplier, Message.Builder<NoPayload> response) {
+        for (Sensor requestSensor : sensors)
+        {
+            String requestBytesParam = requestParamSupplier.apply(requestSensor.getContext().getTable());
+            byte[] requestBytes = SensorsCustomParams.sensorValueAsBytes(requestSensor.getValue());
+            response.withCustomParam(requestBytesParam, requestBytes);
+
+            // for each table in the mutation, send the global per table counter write bytes as recorded by the registry
+            Optional<Sensor> registrySensor = SensorsRegistry.instance.getSensor(requestSensor.getContext(), requestSensor.getType());
+            registrySensor.ifPresent(sensor -> {
+                String tableBytesParam = tableParamSupplier.apply(sensor.getContext().getTable());
+                byte[] tableBytes = SensorsCustomParams.sensorValueAsBytes(sensor.getValue());
+                response.withCustomParam(tableBytesParam, tableBytes);
+            });
+        }
     }
 }
