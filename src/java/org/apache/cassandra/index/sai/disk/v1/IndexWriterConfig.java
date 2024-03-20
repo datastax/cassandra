ff49d5f4e49b4938914e602e3af1e8a75aef11bb
Jonathan Ellis <jbellis@datastax.com>
2024-03-20 09:31:27 -0700

    validSourceModels doesn't need to be a static field

--- src/java/org/apache/cassandra/index/sai/disk/v1/IndexWriterConfig.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/IndexWriterConfig.java
@@ -60,9 +60,6 @@ public class IndexWriterConfig
                                                                 .collect(Collectors.joining(", "));
 
     private static final VectorSourceModel DEFAULT_SOURCE_MODEL = VectorSourceModel.OTHER;
-    private static final String validSourceModels = Arrays.stream(VectorSourceModel.values())
-                                                          .map(Enum::name)
-                                                          .collect(Collectors.joining(", "));
 
     private static final IndexWriterConfig EMPTY_CONFIG = new IndexWriterConfig(null, -1, -1, -1, -1, null, DEFAULT_SOURCE_MODEL);
 
@@ -252,6 +249,9 @@ public class IndexWriterConfig
                 }
                 catch (IllegalArgumentException e)
                 {
+                    var validSourceModels = Arrays.stream(VectorSourceModel.values())
+                                                  .map(Enum::name)
+                                                  .collect(Collectors.joining(", "));
                     throw new InvalidRequestException(String.format("source_model '%s' was not recognized for index %s. Valid values are: %s",
                                                                     option, indexName, validSourceModels));
                 }
