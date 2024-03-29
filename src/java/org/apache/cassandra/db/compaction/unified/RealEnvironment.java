msmygit/vscassandra

STAR-1198 Porting CNDB-3254 to support shared multi-tenant compaction (#384)
CNDB-3254 - changes for better compaction fairness in CNDB (shared compactors)

This patch performs small changes to CompactionManager and the UCS environment
so that CNDB can override some limits, and use the compaction manager to just
submit tasks, without any automated rescheduling.

Further, the aggregate prioritizer has been removed and combine with the UCS
environment.

Co-authored-by: Stefania Alborghetti <stef1927@users.noreply.github.com>

src/java/org/apache/cassandra/db/compaction/unified/RealEnvironment.java
@@ -16,9 +16,15 @@
 
 package org.apache.cassandra.db.compaction.unified;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.agrona.collections.IntArrayList;
 import org.apache.cassandra.cache.ChunkCache;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.db.compaction.CompactionAggregate;
 import org.apache.cassandra.db.compaction.CompactionRealm;
 import org.apache.cassandra.metrics.TableMetrics;
 import org.apache.cassandra.schema.CompressionParams;
@@ -118,6 +124,40 @@ class RealEnvironment implements Environment
     public double flushSize()
     {
         return metrics().flushSizeOnDisk.get();
+    }
+
+    /**
+     * Maybe sort the provided pending compaction aggregates
+     */
+    @Override
+    public List<CompactionAggregate.UnifiedAggregate> maybeSort(List<CompactionAggregate.UnifiedAggregate> pending)
+    {
+        return pending;
+    }
+
+    /**
+     * Maybe reshuffle the provided aggregate indexes
+     */
+    @Override
+    public IntArrayList maybeRandomize(IntArrayList aggregateIndexes, Random random)
+    {
+        Collections.shuffle(aggregateIndexes, random);
+        return aggregateIndexes;
+    }
+
+    @Override
+    public int maxConcurrentCompactions()
+    {
+        return DatabaseDescriptor.getConcurrentCompactors();
+    }
+
+    @Override
+    public double maxThroughput()
+    {
+        final int compactionThroughputMbPerSec = DatabaseDescriptor.getCompactionThroughputMbPerSec();
+        if (compactionThroughputMbPerSec <= 0)
+            return Double.MAX_VALUE;
+        return compactionThroughputMbPerSec * 1024.0 * 1024.0;
     }
 
     @Override

datastax/cassandra

(cherry picked from commit 153ffb82b4a85d089046be84107106a9f40fa305)
(cherry picked from commit b2f0cc280a766b068feb7f881e85c0090ba09340)
(cherry picked from commit 75a7548394697c7e441a424020acf63acb9970b9)
(cherry picked from commit 6efcd47892ef7b2ddcf92f89523038357df77771)
(cherry picked from commit 2ad417369f5194e0cd59ab46fc0205c9414fe425)

src/java/org/apache/cassandra/db/compaction/unified/RealEnvironment.java
@@ -16,9 +16,15 @@
 
 package org.apache.cassandra.db.compaction.unified;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.agrona.collections.IntArrayList;
 import org.apache.cassandra.cache.ChunkCache;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.db.compaction.CompactionAggregate;
 import org.apache.cassandra.db.compaction.CompactionRealm;
 import org.apache.cassandra.io.util.PageAware;
 import org.apache.cassandra.metrics.TableMetrics;
@@ -117,6 +123,40 @@ class RealEnvironment implements Environment
     public double flushSize()
     {
         return metrics().flushSizeOnDisk.get();
+    }
+
+    /**
+     * Maybe sort the provided pending compaction aggregates
+     */
+    @Override
+    public List<CompactionAggregate.UnifiedAggregate> maybeSort(List<CompactionAggregate.UnifiedAggregate> pending)
+    {
+        return pending;
+    }
+
+    /**
+     * Maybe reshuffle the provided aggregate indexes
+     */
+    @Override
+    public IntArrayList maybeRandomize(IntArrayList aggregateIndexes, Random random)
+    {
+        Collections.shuffle(aggregateIndexes, random);
+        return aggregateIndexes;
+    }
+
+    @Override
+    public int maxConcurrentCompactions()
+    {
+        return DatabaseDescriptor.getConcurrentCompactors();
+    }
+
+    @Override
+    public double maxThroughput()
+    {
+        final int compactionThroughputMbPerSec = DatabaseDescriptor.getCompactionThroughputMebibytesPerSecAsInt();
+        if (compactionThroughputMbPerSec <= 0)
+            return Double.MAX_VALUE;
+        return compactionThroughputMbPerSec * 1024.0 * 1024.0;
     }
 
     @Override