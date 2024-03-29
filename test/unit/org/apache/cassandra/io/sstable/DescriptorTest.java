msmygit/vscassandra

STAR-1691: Port more bits of CNDB-4090 (#548)
In STAR-1335 we ported most of CNDB-4090 but we missed out a call to
StorageProvider.invalidateFileSystemCache() which is required to
invalidate the remote storage cache in CNDB whenever we encounter
corruption.

This was discovered because the RemoteFileCacheCorruptedPageTest unit
test is failing.

(cherry picked from commit 116f5d55d5a35ecb66613712da29d2840dd8f974)
(cherry picked from commit 3feca122aa72298f0fcbf6fb1b83985b4a1255e2) (+1 squashed commit)
Squashed commits:
[c4c6df9debf] STAR-1335: port CNDB-4508 - add abstraction to StorageProvider for clearing file system cache

(cherry picked from commit e1525b268673eeabbd113d59a1c428a502f0d740)
(cherry picked from commit 2c271f06c1b3c23cf52438f9c6e09db74b2121a9) (+2 squashed commits)
Squashed commits:
[627e49a5b25] STAR-1335: make Component::repr public

(cherry picked from commit a3494f008a97b3a9a716c428a6578b0d1f4b7841)
(cherry picked from commit 5a39a3eeba9f29f9c6023c127cda5be25748eb6b)
[4723529bc74] STAR-1335: add legacy DSE API to FileUtils, Descriptor, CorruptSSTableException so that rebasing CNDB is easier

(cherry picked from commit 448204a647105b928b633adc5bcde6c140264fcb)
(cherry picked from commit 7ecb47d58dd3d41d6ea925fce0894f3439a153af)

test/unit/org/apache/cassandra/io/sstable/DescriptorTest.java
@@ -17,6 +17,7 @@
  */
 package org.apache.cassandra.io.sstable;
 
+import java.nio.file.Path;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
@@ -30,6 +31,7 @@ import org.apache.cassandra.io.sstable.format.SSTableFormat;
 import org.apache.cassandra.io.util.File;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.DseLegacy;
 import org.apache.cassandra.utils.Pair;
 
 import static org.junit.Assert.assertEquals;
@@ -106,6 +108,7 @@ public class DescriptorTest
         assertEquals(original.cfname, desc.cfname);
         assertEquals(original.version, desc.version);
         assertEquals(original.id, desc.id);
+        assertEquals(original.fileFor(Component.DATA).toPath(), desc.pathFor(Component.DATA));
         assertEquals(Component.DATA, pair.right);
 
         assertEquals(Component.DATA, Descriptor.validFilenameWithComponent(file.name()));
@@ -326,5 +329,16 @@ public class DescriptorTest
             Assert.assertEquals(expectedKeyspace, descriptor.ksname);
             Assert.assertEquals(expectedTable, descriptor.cfname);
         }
+    }
+
+    @Test
+    public void testLegacyDSEAPI()
+    {
+        File dir = new File(".");
+        Descriptor desc = new Descriptor(dir, "ks", "cf", new SequenceBasedSSTableId(1), SSTableFormat.Type.BIG);
+
+        assertEquals(dir.toCanonical().toPath(), desc.getDirectory());
+        assertEquals(desc.fileFor(Component.DATA).toPath(), desc.pathFor(Component.DATA));
+        assertEquals(desc.baseFileUri(), desc.baseFileURI());
     }
 }

datastax/cassandra

(cherry picked from commit 85710897cc346b1a655f8a560d1f26200ff581b3)
(cherry picked from commit d5b43ca329165ca914795098bb565aef98395cd5)

test/unit/org/apache/cassandra/io/sstable/DescriptorTest.java
@@ -19,6 +19,7 @@ package org.apache.cassandra.io.sstable;
 
 import java.util.UUID;
 
+import org.apache.cassandra.io.sstable.format.big.BigFormat;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -107,6 +108,7 @@ public class DescriptorTest
         assertEquals(original.cfname, desc.cfname);
         assertEquals(original.version, desc.version);
         assertEquals(original.id, desc.id);
+        assertEquals(original.fileFor(Components.DATA).toPath(), desc.pathFor(Components.DATA));
         assertEquals(Components.DATA, pair.right);
 
         assertEquals(Components.DATA, Descriptor.validFilenameWithComponent(file.name()));
@@ -332,5 +334,16 @@ public class DescriptorTest
             Assert.assertEquals(String.format("Expected keyspace not found for %s", filePath), expectedKeyspace, descriptor.ksname);
             Assert.assertEquals(String.format("Expected table not found for %s", filePath), expectedTable, descriptor.cfname);
         }
+    }
+
+    @Test
+    public void testLegacyDSEAPI()
+    {
+        File dir = new File(".");
+        Descriptor desc = new Descriptor(dir, "ks", "cf", new SequenceBasedSSTableId(1), BigFormat.getInstance());
+
+        assertEquals(dir.toCanonical().toPath(), desc.getDirectory());
+        assertEquals(desc.fileFor(Components.DATA).toPath(), desc.pathFor(Components.DATA));
+        assertEquals(desc.baseFileUri(), desc.baseFileURI());
     }
 }