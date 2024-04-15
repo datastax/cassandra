msmygit/vscassandra

STAR-1210 Port CNDB-1338 Pass to CompactionObserver#onCompleted if the task finished without exceptions (#388)
* Port CNDB-1338 Pass to CompactionObserver#onCompleted if the task finished without exceptions

Co-authored-by: Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
Co-authored-by: Ruslan Fomkin <Ruslan.Fomkin@gmail.com>

src/java/org/apache/cassandra/db/compaction/AbstractCompactionStrategy.java
@@ -113,7 +113,7 @@ abstract class AbstractCompactionStrategy implements CompactionStrategy
     }
 
     @Override
-    public void onCompleted(UUID id)
+    public void onCompleted(UUID id, boolean isSuccess)
     {
         backgroundCompactions.onCompleted(this, id);
     }

src/java/org/apache/cassandra/db/compaction/CompactionObserver.java
@@ -40,6 +40,7 @@ public interface CompactionObserver
      * Indicates that a compaction with the given id has completed.
      * <p/>
      * @param id  the id of the compaction
+     * @param isSuccess true if compaction finished without any exceptions
      */
-    void onCompleted(UUID id);
+    void onCompleted(UUID id, boolean isSuccess);
 }

src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
@@ -1138,7 +1138,7 @@ public class CompactionStrategyManager implements CompactionStrategyContainer
     }
 
     @Override
-    public void onCompleted(UUID id)
+    public void onCompleted(UUID id, boolean isSuccess)
     {
 
     }

src/java/org/apache/cassandra/db/compaction/UnifiedCompactionContainer.java
@@ -351,9 +351,9 @@ public class UnifiedCompactionContainer implements CompactionStrategyContainer
     }
 
     @Override
-    public void onCompleted(UUID id)
+    public void onCompleted(UUID id, boolean isSuccess)
     {
-        strategy.onCompleted(id);
+        strategy.onCompleted(id, isSuccess);
     }
 
     @Override

test/unit/org/apache/cassandra/db/compaction/CompactionTaskTest.java
@@ -279,9 +279,22 @@ public class CompactionTaskTest
 
         verify(operationObserver, times(1)).onOperationStart(tableOpCaptor.capture());
         verify(compObserver, times(1)).onInProgress(compactionCaptor.capture());
-        verify(compObserver, times(1)).onCompleted(eq(txn.opId()));
+        verify(compObserver, times(1)).onCompleted(eq(txn.opId()), eq(true));
     }
 
+    @Test
+    public void testFailCompactionTask()
+    {
+        Set<SSTableReader> sstables = generateData(2, 2);
+        LifecycleTransaction txn = cfs.getTracker().tryModify(sstables, OperationType.COMPACTION);
+        AbstractCompactionTask task = new CompactionTask(cfs, txn, 0, false, mockStrategy);
+        AbstractCompactionTask taskMock = Mockito.spy(task);
+        CompactionObserver compObserver = Mockito.mock(CompactionObserver.class);
+        taskMock.addObserver(compObserver);
+        Mockito.doThrow(new RuntimeException("Test throw")).when(taskMock).executeInternal();
+        Assert.assertThrows(RuntimeException.class, () ->  taskMock.execute());
+        Mockito.verify(compObserver, times(1)).onCompleted(any(UUID.class), eq(false));
+    }
 
     private Set<SSTableReader> generateData(int numSSTables, int numKeys)
     {

datastax/cassandra

(cherry picked from commit 24e65ac318c31235ace38017410f24aed4219bae)
(cherry picked from commit 8e1f7634e1b125301ab74e7bde34db19f828c383)
(cherry picked from commit 61c89cc033642fdb8b3761f25c619cd19b6be553)
(cherry picked from commit 5fb6587f9bd8f65bdc66f64ecfe65e809854c7db)
(cherry picked from commit f9d5132245d77015f32460270e92fb6a8b7337ed)

src/java/org/apache/cassandra/db/compaction/AbstractCompactionStrategy.java
@@ -112,7 +112,7 @@ abstract class AbstractCompactionStrategy implements CompactionStrategy
     }
 
     @Override
-    public void onCompleted(TimeUUID id)
+    public void onCompleted(TimeUUID id, boolean isSuccess)
     {
         backgroundCompactions.onCompleted(this, id);
     }

src/java/org/apache/cassandra/db/compaction/CompactionObserver.java
@@ -36,7 +36,7 @@ public interface CompactionObserver
         public void onInProgress(CompactionProgress progress) { }
 
         @Override
-        public void onCompleted(TimeUUID id) { }
+        public void onCompleted(TimeUUID id, boolean isSuccess) { }
     };
 
     /**
@@ -50,6 +50,7 @@ public interface CompactionObserver
      * Indicates that a compaction with the given id has completed.
      * <p/>
      * @param id  the id of the compaction
+     * @param isSuccess true if compaction finished without any exceptions
      */
-    void onCompleted(TimeUUID id);
+    void onCompleted(TimeUUID id, boolean isSuccess);
 }

src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
@@ -1273,7 +1273,7 @@ public class CompactionStrategyManager implements CompactionStrategyContainer
     }
 
     @Override
-    public void onCompleted(TimeUUID id)
+    public void onCompleted(TimeUUID id, boolean isSuccess)
     {
 
     }

src/java/org/apache/cassandra/db/compaction/UnifiedCompactionContainer.java
@@ -375,9 +375,9 @@ public class UnifiedCompactionContainer implements CompactionStrategyContainer
     }
 
     @Override
-    public void onCompleted(TimeUUID id)
+    public void onCompleted(TimeUUID id, boolean isSuccess)
     {
-        strategy.onCompleted(id);
+        strategy.onCompleted(id, isSuccess);
     }
 
     @Override

test/unit/org/apache/cassandra/db/compaction/CompactionTaskTest.java
@@ -325,9 +325,22 @@ public class CompactionTaskTest
 
         verify(operationObserver, times(1)).onOperationStart(tableOpCaptor.capture());
         verify(compObserver, times(1)).onInProgress(compactionCaptor.capture());
-        verify(compObserver, times(1)).onCompleted(eq(txn.opId()));
+        verify(compObserver, times(1)).onCompleted(eq(txn.opId()), eq(true));
     }
 
+    @Test
+    public void testFailCompactionTask()
+    {
+        Set<SSTableReader> sstables = generateData(2, 2);
+        LifecycleTransaction txn = cfs.getTracker().tryModify(sstables, OperationType.COMPACTION);
+        AbstractCompactionTask task = new CompactionTask(cfs, txn, 0, false, mockStrategy);
+        AbstractCompactionTask taskMock = Mockito.spy(task);
+        CompactionObserver compObserver = Mockito.mock(CompactionObserver.class);
+        taskMock.addObserver(compObserver);
+        Mockito.doThrow(new RuntimeException("Test throw")).when(taskMock).executeInternal();
+        Assert.assertThrows(RuntimeException.class, () ->  taskMock.execute());
+        Mockito.verify(compObserver, times(1)).onCompleted(any(TimeUUID.class), eq(false));
+    }
 
     private Set<SSTableReader> generateData(int numSSTables, int numKeys)
     {