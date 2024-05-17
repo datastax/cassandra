609202beac384df2d085818ffa7962938612314c
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2024-05-17 07:30:07 +0200

    CNDB-10090: Extract MultiCellCapableType with nameComparator method

--- src/java/org/apache/cassandra/db/marshal/CollectionType.java
+++ src/java/org/apache/cassandra/db/marshal/CollectionType.java
@@ -17,11 +17,11 @@
  */
 package org.apache.cassandra.db.marshal;
 
-import java.nio.ByteBuffer;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.Locale;
@@ -52,7 +52,7 @@ import org.apache.cassandra.utils.bytecomparable.ByteSourceInverse;
  * Please note that this comparator shouldn't be used "manually" (as a custom
  * type for instance).
  */
-public abstract class CollectionType<T> extends AbstractType<T>
+public abstract class CollectionType<T> extends MultiCellCapableType<T>
 {
     public static CellPath.Serializer cellPathSerializer = new CollectionPathSerializer();
 
@@ -97,7 +97,6 @@ public abstract class CollectionType<T> extends AbstractType<T>
         this.kind = kind;
     }
 
-    public abstract AbstractType<?> nameComparator();
     public abstract AbstractType<?> valueComparator();
 
     protected abstract List<ByteBuffer> serializedValues(Iterator<Cell<?>> cells);
