77a690dfe80b917ba74ea3a8217b3ddbe255e87a
Daniel Jatnieks <jatnieks@pobox.com>
2024-05-22 10:01:43 -0700

    CC5 fix compile errors following cherry-pick 49a51690caca180fc33bd49c7b95cfc8ee0627f3..400b556877fafa6d6abad95a91b3939b0008d8d7
     (Rebase of commit cefe5bfa06d0eb1874fbc2b8f8d17b0bed142f07)

--- test/unit/org/apache/cassandra/cql3/functions/masking/MaskingFunctionTester.java
+++ test/unit/org/apache/cassandra/cql3/functions/masking/MaskingFunctionTester.java
@@ -302,10 +302,4 @@ public abstract class MaskingFunctionTester extends CQLTester
 
         return false;
     }
-
-    @SafeVarargs
-    protected final <T> Vector<T> vector(T... values)
-    {
-        return new Vector<>(values);
-    }
 }
