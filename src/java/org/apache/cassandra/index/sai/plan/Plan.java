fdc9e934ab35b6a73d77df39fe93319ff6535209
Piotr Kołaczkowski <pkolaczk@datastax.com>
2024-06-11 13:30:49 +0200

    SAI global sorting (ORDER BY)
    Add support for ORDER BY via SAI.
    This commit adds support for ordering all existing BA and CA indexes
    without writing any new index files. AA indexes cannot be sorted
    because the primary key map is based on partitions, not primary keys.
    
    New Abstractions
    
    * PrimaryKeyWithSortKey - this is a primary key that has some metadata
      that can be used for sorting. The two implementations at the moment
      are a float used for vector similarity score and a ByteComparable
      object used from numeric and text indexes.
    * RowWithSourceTable and CellWithSourceTable - this abstraction gives
      us a way to validate the source of a given cell. That source is
      then used to confirm whether an index's ranking of the data is valid.
    
    Notable design details
    
    * ORDER BY on SAI requires CL=1.
    * Updates on same memtable: unlike the VectorMemtableIndex, the
      TrieMemoryIndex does not override MemtableIndex#update. Instead, an
      update is handled as an insertion. That presents two problems.
      First, the memtable has both values, and second, the resulting
      sstable index has both values. This PR addresses that edge case by
      comparing the index's view of the value to memtable/sstable's live
      view of the value for the row. If implement MemtableIndex#update,
      we can remove the PrimaryKeyWithSortKey#isIndexDataEqualToLiveData
      guard.
    * The RowWithSourceTable and CellWithSourceTable are used by vector
      ordering to prevent  re-calculating the score in the TopKProcessor.
    * In order for the "source table" logic to work, we must acquire a
      single view of the sstables and memtables and their associated
      indexes.  Otherwise, we risk classifying a valid row as "updated".
      In the StorageAttachedIndexSearcher, we get this view and hold it for
      the duration of the query. Because hyrbid queries do not need to have
      the source table validated, they are not included in the view logic,
      which simplifies determining which indexes need to be locked.
    * Hybrid queries are only weakly index based. Because we do not have
      an easy way to map from PrimaryKey to value in our numeric and text
      indexes, the quickest solution to implement was to read the value
      from the sstable and then sort in the index.
    
    Limitations:
    
    Currently, there are 3 types that are not sortable via SAI:
    * InetAddressType because we need to add decoding logic based on SAI's
      TypeUtil.encode method
    * DecimalType because SAI truncates the value to 24 bytes
    * IntegerType because SAI truncates to 20 bytes

--- src/java/org/apache/cassandra/index/sai/plan/Plan.java
+++ src/java/org/apache/cassandra/index/sai/plan/Plan.java
@@ -982,10 +982,10 @@ abstract public class Plan
     static class AnnSort extends KeysIteration
     {
         private final KeysIteration source;
-        final RowFilter.Expression ordering;
+        final Orderer ordering;
 
 
-        protected AnnSort(Factory factory, int id, KeysIteration source, RowFilter.Expression ordering)
+        protected AnnSort(Factory factory, int id, KeysIteration source, Orderer ordering)
         {
             super(factory, id);
             this.source = source;
@@ -1022,7 +1022,7 @@ abstract public class Plan
         @Override
         protected Iterator<? extends PrimaryKey> execute(Executor executor)
         {
-            return executor.getTopKRows((RangeIterator) source.execute(executor), ordering);
+            return executor.getTopKRows((RangeIterator) source.execute(executor));
         }
 
     }
@@ -1033,9 +1033,9 @@ abstract public class Plan
      */
     static class AnnScan extends Leaf
     {
-        final RowFilter.Expression ordering;
+        final Orderer ordering;
 
-        protected AnnScan(Factory factory, int id, RowFilter.Expression ordering)
+        protected AnnScan(Factory factory, int id, Orderer ordering)
         {
             super(factory, id);
             this.ordering = ordering;
@@ -1059,7 +1059,7 @@ abstract public class Plan
         @Override
         protected Iterator<? extends PrimaryKey> execute(Executor executor)
         {
-            return executor.getTopKRows(ordering);
+            return executor.getTopKRows();
         }
     }
 
@@ -1350,12 +1350,13 @@ abstract public class Plan
         /**
          * Constructs a node that sorts keys using DiskANN index
          */
-        public KeysIteration annSort(KeysIteration source, RowFilter.Expression ordering)
+        public KeysIteration sort(KeysIteration source, Orderer ordering)
         {
+            // VSTODO use different calculations depending on the column type being ordered.
             return annSort(source, ordering, nextId++);
         }
 
-        private KeysIteration annSort(@Nonnull KeysIteration source, @Nonnull RowFilter.Expression ordering, int id)
+        private KeysIteration annSort(@Nonnull KeysIteration source, @Nonnull Orderer ordering, int id)
         {
             return (source instanceof Everything)
                 ? new AnnScan(this, id, ordering)
@@ -1365,7 +1366,7 @@ abstract public class Plan
         /**
          * Constructs a node that scans the DiskANN index and returns key in ANN order
          */
-        public KeysIteration annScan(@Nonnull RowFilter.Expression ordering)
+        public KeysIteration annScan(@Nonnull Orderer ordering)
         {
             return new AnnScan(this, nextId++, ordering);
         }
@@ -1445,8 +1446,8 @@ abstract public class Plan
     public interface Executor
     {
         Iterator<? extends PrimaryKey> getKeysFromIndex(Expression predicate);
-        Iterator<? extends PrimaryKey> getTopKRows(RowFilter.Expression ordering);
-        Iterator<? extends PrimaryKey> getTopKRows(RangeIterator keys, RowFilter.Expression ordering);
+        Iterator<? extends PrimaryKey> getTopKRows();
+        Iterator<? extends PrimaryKey> getTopKRows(RangeIterator keys);
     }
 
     /**
