aec69e540f4ef06fe9ac9af111227314c24ceaf2
Andrés de la Peña <a.penya.garcia@gmail.com>
2024-06-26 15:53:13 +0100

    Fix license headers

--- test/unit/org/apache/cassandra/cql3/functions/IndexFctsTest.java
+++ test/unit/org/apache/cassandra/cql3/functions/IndexFctsTest.java
@@ -1,13 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright DataStax, Inc.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,6 +31,7 @@ public class IndexFctsTest extends SAITester
 
         assertRows(execute("SELECT k, sai_analyze(v, ?) FROM %s",
                            "{\n" +
+                           "\t\"tokenizer\":{\"name\":\"whitespace\"},\n" +
                            "\t\"tokenizer\":{\"name\":\"whitespace\"},\n" +
                            "\t\"filters\":[{\"name\":\"porterstem\"}]\n" +
                            '}'),
