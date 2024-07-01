2174250761c277af55deebebd7a99e99d3b6121f
Jonathan Ellis <jbellis@datastax.com>
2024-07-01 10:27:13 -0500

    mark mmap regions with FADV_DONTNEED

--- src/java/org/apache/cassandra/io/util/MmappedRegions.java
+++ src/java/org/apache/cassandra/io/util/MmappedRegions.java
@@ -360,6 +360,7 @@ public class MmappedRegions extends SharedCloseableImpl
         private void add(long pos, long size)
         {
             ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, pos - onDiskSliceOffset, size);
+            channel.trySkipCache(pos - onDiskSliceOffset, size);
 
             ++last;
 
