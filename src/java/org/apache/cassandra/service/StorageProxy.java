msmygit/vscassandra

STAR-1489 (#524)
* STAR-1489 make verb decorator efficient

* STAR-1489 Add Mutator callbacks invoked when locally applying mutations.

* STAR-1489 Allow to override attributes in create/alter keyspace.

* STAR-1489 Added tests.

Co-authored-by: Jake Luciani <jake@datastax.com>

src/java/org/apache/cassandra/service/StorageProxy.java
@@ -226,6 +226,7 @@ public class StorageProxy implements StorageProxyMBean
         {
             assert mutation instanceof Mutation;
             sendToHintedReplicas((Mutation) mutation, targets, responseHandler, localDataCenter, Stage.MUTATION);
+            mutator.onAppliedMutation(mutation);
         };
 
         /*
@@ -842,7 +843,7 @@ public class StorageProxy implements StorageProxyMBean
             {
                 try
                 {
-                    PaxosState.commit(message.payload);
+                    PaxosState.commit(message.payload, p -> mutator.onAppliedProposal(p));
                     if (responseHandler != null)
                         responseHandler.onResponse(null);
                 }
@@ -1795,6 +1796,7 @@ public class StorageProxy implements StorageProxyMBean
 
                 Mutation result = ((CounterMutation) mutation).applyCounterMutation();
                 responseHandler.onResponse(null);
+                mutator.onAppliedCounter(result);
                 sendToHintedReplicas(result, replicaPlan, responseHandler, localDataCenter, Stage.COUNTER_MUTATION);
             }
         };

datastax/cassandra

(cherry picked from commit 8fc3807cbd0ab0de2d662d9ec9d40f71ad01dc0d)
(cherry picked from commit 0f8abbcf7cde4fccdd17f5377a532b778494bc58)
(cherry picked from commit 7211eb627a202e8a30179bc1b75cec52eb2d0431)
(cherry picked from commit 5201992db8841d699627f592c440842fdb966046)

src/java/org/apache/cassandra/service/StorageProxy.java
@@ -259,6 +259,7 @@ public class StorageProxy implements StorageProxyMBean
         {
             assert mutation instanceof Mutation;
             sendToHintedReplicas((Mutation) mutation, targets, responseHandler, localDataCenter, Stage.MUTATION);
+            mutator.onAppliedMutation(mutation);
         };
 
         /*
@@ -915,7 +916,7 @@ public class StorageProxy implements StorageProxyMBean
             {
                 try
                 {
-                    PaxosState.commitDirect(message.payload);
+                    PaxosState.commitDirect(message.payload, p -> mutator.onAppliedProposal(p));
                     if (responseHandler != null)
                         responseHandler.onResponse(null);
                 }
@@ -1941,6 +1942,7 @@ public class StorageProxy implements StorageProxyMBean
 
                 Mutation result = ((CounterMutation) mutation).applyCounterMutation();
                 responseHandler.onResponse(null);
+                mutator.onAppliedCounter(result);
                 sendToHintedReplicas(result, replicaPlan, responseHandler, localDataCenter, Stage.COUNTER_MUTATION);
             }
         };