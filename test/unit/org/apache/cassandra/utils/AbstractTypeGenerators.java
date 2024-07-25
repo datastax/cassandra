95986defea8fdb58392cf5cf6fe146184fa58fe5
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2024-07-25 07:57:00 +0200

    CNDB-10090: Provide tests for types compatibility

--- test/unit/org/apache/cassandra/utils/AbstractTypeGenerators.java
+++ test/unit/org/apache/cassandra/utils/AbstractTypeGenerators.java
@@ -80,6 +80,7 @@ import org.apache.cassandra.db.marshal.LineStringType;
 import org.apache.cassandra.db.marshal.ListType;
 import org.apache.cassandra.db.marshal.LongType;
 import org.apache.cassandra.db.marshal.MapType;
+import org.apache.cassandra.db.marshal.NumberType;
 import org.apache.cassandra.db.marshal.PartitionerDefinedOrder;
 import org.apache.cassandra.db.marshal.PointType;
 import org.apache.cassandra.db.marshal.PolygonType;
@@ -147,7 +148,7 @@ public final class AbstractTypeGenerators
     }
 
 
-    private static final Map<AbstractType<?>, TypeSupport<?>> PRIMITIVE_TYPE_DATA_GENS =
+    public static final Map<AbstractType<?>, TypeSupport<?>> PRIMITIVE_TYPE_DATA_GENS =
     Stream.of(TypeSupport.of(BooleanType.instance, BOOLEAN_GEN),
               TypeSupport.of(ByteType.instance, SourceDSL.integers().between(0, Byte.MAX_VALUE * 2 + 1).map(Integer::byteValue)),
               TypeSupport.of(ShortType.instance, SourceDSL.integers().between(0, Short.MAX_VALUE * 2 + 1).map(Integer::shortValue)),
@@ -171,7 +172,11 @@ public final class AbstractTypeGenerators
                                                                                               .thenComparingInt(Duration::getDays)
                                                                                               .thenComparingLong(Duration::getNanoseconds)),
               TypeSupport.of(IntegerType.instance, Generators.bigInt()),
-              TypeSupport.of(DecimalType.instance, Generators.bigDecimal())
+              TypeSupport.of(DecimalType.instance, Generators.bigDecimal()),
+              TypeSupport.of(DateRangeType.instance, Generators.DATE_RANGE_GEN, Comparator.comparing(DateRangeType.instance::decompose)),
+              TypeSupport.of(LineStringType.instance, Generators.LINE_STRING_GEN, Comparator.comparing(LineStringType.instance::decompose)),
+              TypeSupport.of(PolygonType.instance, Generators.POLYGON_GEN, Comparator.comparing(PolygonType.instance::decompose)),
+              TypeSupport.of(PointType.instance, Generators.POINT_GEN, Comparator.comparing(PointType.instance::decompose))
     ).collect(Collectors.toMap(t -> t.type, t -> t));
     // NOTE not supporting reversed as CQL doesn't allow nested reversed types
     // when generating part of the clustering key, it would be good to allow reversed types as the top level
@@ -286,6 +291,7 @@ public final class AbstractTypeGenerators
         private Function<Integer, Gen<AbstractType<?>>> defaultSetKeyFunc;
         private Predicate<AbstractType<?>> typeFilter = null;
         private Gen<String> udtName = null;
+        private Gen<Boolean> multiCellGen = BOOLEAN_GEN;
 
         public TypeGenBuilder()
         {
@@ -468,7 +474,7 @@ public final class AbstractTypeGenerators
             }
             else
                 kindGen = SourceDSL.arbitrary().enumValues(TypeKind.class);
-            return buildRecursive(maxDepth, maxDepth, kindGen, BOOLEAN_GEN);
+            return buildRecursive(maxDepth, maxDepth, kindGen, multiCellGen);
         }
 
         private Gen<AbstractType<?>> buildRecursive(int maxDepth, int level, Gen<TypeKind> typeKindGen, Gen<Boolean> multiCellGen)
@@ -507,7 +513,7 @@ public final class AbstractTypeGenerators
                     case VECTOR:
                     {
                         Gen<Integer> sizeGen = vectorSizeGen != null ? vectorSizeGen : defaultSizeGen;
-                        return vectorTypeGen(next.get().map(AbstractType::freeze), sizeGen).generate(rnd);
+                        return vectorTypeGen(filter(primitiveGen, NumberType.class::isInstance), sizeGen).generate(rnd);
                     }
                     case COMPOSITE:
                         return compositeTypeGen(compositeElementGen != null ? compositeElementGen : next.get(), compositeSizeGen != null ? compositeSizeGen : defaultSizeGen).generate(rnd);
@@ -813,7 +819,7 @@ public final class AbstractTypeGenerators
         TypeSupport<T> support;
         if (gen != null)
         {
-            support = gen;
+            support = gen.withValueDomain(valueDomainGen);
         }
         // might be... complex...
         else if (type instanceof SetType)
@@ -868,9 +874,9 @@ public final class AbstractTypeGenerators
             // T = Map<A, B> so can not use T here
             MapType<Object, Object> mapType = (MapType<Object, Object>) type;
             // do not use valueDomainGen as map doesn't allow null/empty
-            TypeSupport<Object> keySupport = getTypeSupport(mapType.getKeysType(), sizeGen, null);
+            TypeSupport<Object> keySupport = getTypeSupport(mapType.getKeysType(), sizeGen, valueDomainGen);
             Comparator<Object> keyType = keySupport.valueComparator;
-            TypeSupport<Object> valueSupport = getTypeSupport(mapType.getValuesType(), sizeGen, null);
+            TypeSupport<Object> valueSupport = getTypeSupport(mapType.getValuesType(), sizeGen, valueDomainGen);
             Comparator<Object> valueType = valueSupport.valueComparator;
             Comparator<Map<Object, Object>> comparator = (Map<Object, Object> a, Map<Object, Object> b) -> {
                 List<Object> ak = new ArrayList<>(a.keySet());
