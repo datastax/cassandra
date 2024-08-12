5835e4fa7096a33be7509eb93386e03b57050763
Ayman Khalil <13960949+aymkhalil@users.noreply.github.com>
2024-08-12 07:06:33 -0700

    CNDB-9707 Implement RequestSensorsFactory and refactored RequestSensors as an interface to control sensors tracking per keyspace and either enable/disable it.
    Co-authored-by: Sergio Bossa <sergio.bossa@gmail.com>
    
     (Rebase of commit fad30b3384f5d2be545b8357316a9dde8cfb3a63)
    
     (Rebase of commit 728c022bc0f840fe9ba35cddc42d7bd56b14f27c)

--- src/java/org/apache/cassandra/db/CounterMutationVerbHandler.java
+++ src/java/org/apache/cassandra/db/CounterMutationVerbHandler.java
@@ -32,6 +32,7 @@ import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.sensors.Context;
 import org.apache.cassandra.sensors.RequestSensors;
 import org.apache.cassandra.sensors.RequestTracker;
+import org.apache.cassandra.sensors.RequestSensorsFactory;
 import org.apache.cassandra.sensors.Type;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.transport.Dispatcher;
@@ -49,16 +50,17 @@ public class CounterMutationVerbHandler extends AbstractMutationVerbHandler<Coun
         logger.trace("Applying forwarded {}", cm);
 
         // Initialize the sensor and set ExecutorLocals
+        RequestSensors requestSensors = RequestSensorsFactory.instance.create(message.payload.getKeyspaceName());
         Collection<TableMetadata> tables = message.payload.getPartitionUpdates().stream().map(PartitionUpdate::metadata).collect(Collectors.toSet());
-        RequestSensors requestSensors = new RequestSensors();
         RequestTracker.instance.set(requestSensors);
 
         // Initialize internode bytes with the inbound message size:
-        tables.forEach(tm -> {
+        for (TableMetadata tm : tables)
+        {
             Context context = Context.from(tm);
             requestSensors.registerSensor(context, Type.INTERNODE_BYTES);
             requestSensors.incrementSensor(context, Type.INTERNODE_BYTES, message.payloadSize(MessagingService.current_version) / tables.size());
-        });
+        }
 
         String localDataCenter = DatabaseDescriptor.getEndpointSnitch().getLocalDatacenter();
         // We should not wait for the result of the write in this thread,
@@ -70,7 +72,7 @@ public class CounterMutationVerbHandler extends AbstractMutationVerbHandler<Coun
         // it's own in that case.
         StorageProxy.applyCounterMutationOnLeader(cm,
                                                   localDataCenter,
-                                                  new CounterMutationCallback(message, message.from()),
+                                                  new CounterMutationCallback(message, message.from(), requestSensors),
                                                   Dispatcher.RequestTime.forImmediateExecution());
     }
 }
