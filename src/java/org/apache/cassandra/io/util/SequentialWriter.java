cfbbbc108ff32ad40c91115cfc634562ba3129e2
Piotr Kołaczkowski <pkolaczk@datastax.com>
2024-09-11 22:13:46 +0200

    CNDB-10783: Reuse CRC when appending index components
    This commit improves performance of appending SAI components
    by avoiding unnecessary computation of CRC from the beginning
    of the file each time it is opened for appending.
    
    Fixes https://github.com/riptano/cndb/issues/10783
    
     (Rebase of commit dad6717ebc3d5fe9a9abb939dbfc30ba6693cd0e)

--- src/java/org/apache/cassandra/io/util/SequentialWriter.java
+++ src/java/org/apache/cassandra/io/util/SequentialWriter.java
@@ -436,7 +436,7 @@ public class SequentialWriter extends BufferedDataOutputStreamPlus implements Tr
     }
 
     @Override
-    public final void close()
+    public void close()
     {
         if (option.finishOnClose())
             txnProxy.finish();
