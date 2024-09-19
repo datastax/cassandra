cfb5dc698889fffafc95a10902fa00daedc47a43
Michael Marshall <michael.marshall@datastax.com>
2024-09-19 11:01:47 -0500

    Include checksums in CorruptBlockException when relevant (#1291)
    This additional metadata is somewhat valuable in the context of troubleshooting. Recently, we had an issue where the checksum itself was not (over)written and so it was stored as 0. In many cases, this won't be helpful, but since it is cheap and could be helpful, I propose adding some additional metadata when checksums don't match.

--- src/java/org/apache/cassandra/io/util/CompressedChunkReader.java
+++ src/java/org/apache/cassandra/io/util/CompressedChunkReader.java
@@ -143,8 +143,9 @@ public abstract class CompressedChunkReader extends AbstractReaderFileProxy impl
                         int checksum = (int) ChecksumType.CRC32.of(compressed);
 
                         compressed.limit(length);
-                        if (compressed.getInt() != checksum)
-                            throw new CorruptBlockException(channel.filePath(), chunk);
+                        int storedChecksum = compressed.getInt();
+                        if (storedChecksum != checksum)
+                            throw new CorruptBlockException(channel.filePath(), chunk, storedChecksum, checksum);
 
                         compressed.position(0).limit(chunk.length);
                     }
@@ -171,9 +172,11 @@ public abstract class CompressedChunkReader extends AbstractReaderFileProxy impl
 
                         ByteBuffer scratch = bufferHolder.getBuffer(Integer.BYTES);
 
-                        if (channel.read(scratch, chunkOffset + chunk.length) != Integer.BYTES
-                                || scratch.getInt(0) != checksum)
+                        if (channel.read(scratch, chunkOffset + chunk.length) != Integer.BYTES)
                             throw new CorruptBlockException(channel.filePath(), chunk);
+                        int storedChecksum = scratch.getInt(0);
+                        if (storedChecksum != checksum)
+                            throw new CorruptBlockException(channel.filePath(), chunk, storedChecksum, checksum);
                     }
                 }
                 uncompressed.flip();
@@ -236,8 +239,9 @@ public abstract class CompressedChunkReader extends AbstractReaderFileProxy impl
                         int checksum = (int) ChecksumType.CRC32.of(compressedChunk);
 
                         compressedChunk.limit(compressedChunk.capacity());
-                        if (compressedChunk.getInt() != checksum)
-                            throw new CorruptBlockException(channel.filePath(), chunk);
+                        int storedChecksum = compressedChunk.getInt();
+                        if (storedChecksum != checksum)
+                            throw new CorruptBlockException(channel.filePath(), chunk, storedChecksum, checksum);
 
                         compressedChunk.position(chunkOffsetInSegment).limit(chunkOffsetInSegment + chunk.length);
                     }
