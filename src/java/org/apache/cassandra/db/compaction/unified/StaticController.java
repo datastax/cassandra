34dd1b6b763e7f7f1bc554b29b09951189320637
ethan-brown2022 <107228208+ethan-brown2022@users.noreply.github.com>
2024-09-25 07:59:14 -0700

    CNDB-10455: Automatically tune compaction for vector tables (#1265)
    * Added unified_compaction.override_ucs_config_for_vector_tables option.  When enabled, the Controller will use the preferred vector settings for vector tables.
    - Added additional options for vector specific configuration.
    
     (Rebase of commit b528d7024f7621b76307e351dac5dea84864d6e2)

--- src/java/org/apache/cassandra/db/compaction/unified/StaticController.java
+++ src/java/org/apache/cassandra/db/compaction/unified/StaticController.java
@@ -34,6 +34,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import static org.apache.cassandra.config.CassandraRelevantProperties.UCS_STATIC_SCALING_PARAMETERS;
+import static org.apache.cassandra.config.CassandraRelevantProperties.UCS_VECTOR_SCALING_PARAMETERS;
 
 /**
  * The static compaction controller periodically checks the IO costs
@@ -48,6 +49,7 @@ public class StaticController extends Controller
     static final String STATIC_SCALING_FACTORS_OPTION = "static_scaling_factors";
     private final static String DEFAULT_STATIC_SCALING_PARAMETERS = UCS_STATIC_SCALING_PARAMETERS.getStringWithLegacyFallback();
 
+    private static final String VECTOR_DEFAULT_STATIC_SCALING_PARAMETERS = UCS_VECTOR_SCALING_PARAMETERS.getString();
     private final int[] scalingParameters;
 
     @VisibleForTesting // comp. simulation
@@ -68,6 +70,7 @@ public class StaticController extends Controller
                             int reservedThreadsPerLevel,
                             Reservations.Type reservationsType,
                             Overlaps.InclusionMethod overlapInclusionMethod,
+                            boolean hasVectorType,
                             String keyspaceName,
                             String tableName)
     {
@@ -87,7 +90,8 @@ public class StaticController extends Controller
               sstableGrowthModifier,
               reservedThreadsPerLevel,
               reservationsType,
-              overlapInclusionMethod);
+              overlapInclusionMethod,
+              hasVectorType);
         this.scalingParameters = scalingParameters;
         this.keyspaceName = keyspaceName;
         this.tableName = tableName;
@@ -108,15 +112,20 @@ public class StaticController extends Controller
                                   int reservedThreadsPerLevel,
                                   Reservations.Type reservationsType,
                                   Overlaps.InclusionMethod overlapInclusionMethod,
+                                  boolean hasVectorType,
                                   String keyspaceName,
                                   String tableName,
-                                  Map<String, String> options)
+                                  Map<String, String> options,
+                                  boolean useVectorOptions)
     {
         int[] scalingParameters;
         if (options.containsKey(STATIC_SCALING_FACTORS_OPTION))
             scalingParameters = parseScalingParameters(options.get(STATIC_SCALING_FACTORS_OPTION));
         else
             scalingParameters = parseScalingParameters(options.getOrDefault(SCALING_PARAMETERS_OPTION, DEFAULT_STATIC_SCALING_PARAMETERS));
+
+        int[] vectorScalingParameters = parseScalingParameters(options.getOrDefault(VECTOR_SCALING_PARAMETERS_OPTION, VECTOR_DEFAULT_STATIC_SCALING_PARAMETERS));
+
         long currentFlushSize = flushSizeOverride;
 
         File f = getControllerConfigPath(keyspaceName, tableName);
@@ -139,7 +148,7 @@ public class StaticController extends Controller
             logger.warn("Unable to parse saved flush size. Using starting value instead:", e);
         }
         return new StaticController(env,
-                                    scalingParameters,
+                                    useVectorOptions ? vectorScalingParameters : scalingParameters,
                                     survivalFactors,
                                     dataSetSize,
                                     minSSTableSize,
@@ -155,6 +164,7 @@ public class StaticController extends Controller
                                     reservedThreadsPerLevel,
                                     reservationsType,
                                     overlapInclusionMethod,
+                                    hasVectorType,
                                     keyspaceName,
                                     tableName);
     }
@@ -169,6 +179,9 @@ public class StaticController extends Controller
             parseScalingParameters(factors);
         if (parameters != null && factors != null)
             throw new ConfigurationException(String.format("Either '%s' or '%s' should be used, not both", SCALING_PARAMETERS_OPTION, STATIC_SCALING_FACTORS_OPTION));
+        String vectorParameters = options.remove(VECTOR_SCALING_PARAMETERS_OPTION);
+        if (vectorParameters != null)
+            parseScalingParameters(vectorParameters);
         return options;
     }
 
@@ -209,6 +222,9 @@ public class StaticController extends Controller
     @Override
     public String toString()
     {
-        return String.format("Static controller, m: %d, o: %s, scalingParameters: %s, cost: %s", minSSTableSize, Arrays.toString(survivalFactors), printScalingParameters(scalingParameters), calculator);
+        return String.format("Static controller, m: %d, o: %s, scalingParameters: %s, cost: %s", minSSTableSize,
+                             Arrays.toString(survivalFactors),
+                             printScalingParameters(scalingParameters),
+                             calculator);
     }
 }
