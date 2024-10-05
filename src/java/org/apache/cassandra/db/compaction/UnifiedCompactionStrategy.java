a8d92e4bda981197cea8fd42a308df15426df9fc
Zhao Yang <zhaoyangsingapore@gmail.com>
2024-10-05 14:37:26 +0800

    CNDB-10990: include archive size when constructing Bucket#constructAggregate and use picked sstables size as maxOverlap for aggregate (#1309)
    CNDB-10990: include archive size in Level.avg and use maxOverlap for unified aggregate
    
    update getOverheadSizeInBytes to include non-data components size
    
    Add config to disable new behavior, default enabled. add tests

--- src/java/org/apache/cassandra/db/compaction/UnifiedCompactionStrategy.java
+++ src/java/org/apache/cassandra/db/compaction/UnifiedCompactionStrategy.java
@@ -39,6 +39,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.concurrent.ScheduledExecutors;
+import org.apache.cassandra.config.CassandraRelevantProperties;
 import org.apache.cassandra.db.DiskBoundaries;
 import org.apache.cassandra.db.SerializationHeader;
 import org.apache.cassandra.db.commitlog.CommitLogPosition;
@@ -1058,7 +1059,10 @@ public class UnifiedCompactionStrategy extends AbstractCompactionStrategy
         void add(CompactionSSTable sstable)
         {
             this.sstables.add(sstable);
-            this.avg += (sstable.onDiskLength() - avg) / sstables.size();
+            // consider size of all components to reduce chance of out-of-disk
+            long size = CassandraRelevantProperties.UCS_COMPACTION_INCLUDE_NON_DATA_FILES_SIZE.getBoolean()
+                        ? sstable.onDiskComponentsSize() : sstable.onDiskLength();
+            this.avg += (size - avg) / sstables.size();
         }
 
         void complete()
@@ -1208,12 +1212,12 @@ public class UnifiedCompactionStrategy extends AbstractCompactionStrategy
             {
                 /**
                  * Happy path. We are not late or (for levelled) we are only so late that a compaction now will
-                 * have the  same effect as doing levelled compactions one by one. Compact all. We do not cap
+                 * have the same effect as doing levelled compactions one by one. Compact all. We do not cap
                  * this pick at maxSSTablesToCompact due to an assumption that maxSSTablesToCompact is much
                  * greater than F. See {@link Controller#MAX_SSTABLES_TO_COMPACT_OPTION} for more details.
                  */
                 return CompactionAggregate.createUnified(allSSTablesSorted,
-                                                         count,
+                                                         maxOverlap,
                                                          CompactionPick.create(index, allSSTablesSorted),
                                                          Collections.emptySet(),
                                                          arena,
@@ -1229,7 +1233,7 @@ public class UnifiedCompactionStrategy extends AbstractCompactionStrategy
                 // case to cap compaction pick at maxSSTablesToCompact.
                 if (count <= maxSSTablesToCompact)
                     return CompactionAggregate.createUnified(allSSTablesSorted,
-                                                             count,
+                                                             maxOverlap,
                                                              CompactionPick.create(index, allSSTablesSorted),
                                                              Collections.emptySet(),
                                                              arena,
@@ -1244,7 +1248,7 @@ public class UnifiedCompactionStrategy extends AbstractCompactionStrategy
                     count -= maxSSTablesToCompact;
                 }
 
-                return CompactionAggregate.createUnified(allSSTablesSorted, count, pick, pending, arena, level);
+                return CompactionAggregate.createUnified(allSSTablesSorted, maxOverlap, pick, pending, arena, level);
             }
             // We may, however, have accumulated a lot more than T if compaction is very late, or a set of small
             // tables was dumped on us (e.g. when converting from legacy LCS or for tests).
@@ -1262,7 +1266,7 @@ public class UnifiedCompactionStrategy extends AbstractCompactionStrategy
                 // dump.
                 assert !picks.isEmpty();  // we only enter this if count > F: layoutCompactions must have selected something to run
                 CompactionPick selected = picks.remove(controller.random().nextInt(picks.size()));
-                return CompactionAggregate.createUnified(allSSTablesSorted, count, selected, picks, arena, level);
+                return CompactionAggregate.createUnified(allSSTablesSorted, maxOverlap, selected, picks, arena, level);
             }
         }
 
