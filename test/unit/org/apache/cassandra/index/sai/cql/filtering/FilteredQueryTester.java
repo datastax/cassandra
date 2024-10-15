ac2a4d7836bada08b3f622164fa4bcf89586d25d
Andrés de la Peña <adelapena@users.noreply.github.com>
2024-10-15 16:14:12 +0100

    CNDB-11283: Unify CQLTester#waitForIndex and SAITester#waitForIndexQueryable (#1351)
    Port of CASSANDRA-18521. Fixes the flakiness of LiteralOrderTest.testTextPrefixes and tries to prevent future similar issues by including the waiting for the index into CQLTester#createIndex. There is a new CQLTester#createIndexAsync for special cases where we don't want to wait for the index.
    
     (Rebase of commit e0471055f5f425820517820f0d8243056d7ea37a)

--- test/unit/org/apache/cassandra/index/sai/cql/filtering/FilteredQueryTester.java
+++ test/unit/org/apache/cassandra/index/sai/cql/filtering/FilteredQueryTester.java
@@ -65,7 +65,6 @@ public abstract class FilteredQueryTester extends SAITester
         indexNames = indexes.stream().map(i -> i.name).collect(Collectors.toSet());
         createTable();
         indexes.forEach(i -> i.create(this));
-        waitForTableIndexesQueryable();
         populateTable();
     }
 
