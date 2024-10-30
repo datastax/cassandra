152c9d74a043d4cf05b2979a4843dc844b90653a
ekaterinadimitrova2 <ekaterina.dimitrova@datastax.com>
2024-10-30 13:15:14 -0400

    CNDB-11434: Refactor tests to mitigate https://openjdk.org/jeps/416 (#1386)
    * CNDB-11434: Refactor TrieMemtableIndexTest
    
    * CNDB-11434: Refactor TinySegmentFlushingFailureTest
    
    * CNDB-11434: Make all tests in TrieMemtableIndexTest to be run with both offheap and onheap to improve test coverage

--- test/unit/org/apache/cassandra/cql3/CQLTester.java
+++ test/unit/org/apache/cassandra/cql3/CQLTester.java
@@ -77,6 +77,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.junit.rules.TestWatcher;
@@ -2869,6 +2870,7 @@ public abstract class CQLTester
                                                                             : "";
     }
 
+    @Ignore // Check TinySegmentFlushingFailureTest for details why this annotation is needed here despite this is not a test
     public static class Vector<T> extends AbstractList<T>
     {
         private final T[] values;
@@ -3154,6 +3156,8 @@ public abstract class CQLTester
             fs.clearListeners();
         }
     }
+
+    @Ignore // Check TinySegmentFlushingFailureTest for details why this annotation is needed here despite this is not a test
     public static class Randomization
     {
         private long seed;
@@ -3250,6 +3254,7 @@ public abstract class CQLTester
         }
     }
 
+    @Ignore // Check TinySegmentFlushingFailureTest for details why this annotation is needed here despite this is not a test
     public static class FailureWatcher extends TestWatcher
     {
         @Override
