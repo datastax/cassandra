4f5e31046119f07cd0e8dcd950adea24cbee33b6
Sylvain Lebresne <lebresne@gmail.com>
2024-11-14 09:43:12 +0100

    Allow custom SAI components discovery (#1338)
    Refactor the code that "discover" which SAI components files an SSTable
    has to allow making that code customizable. The default behavior remains
    the existing one, that is the TOC file is consulted, with a fallback to
    scanning disk if the TOC file is missing or corrupted.
    
    The way to customize this behavior is similar to what `SSTableWatcher`
    does: a new system property,
    `-Dcassandra.sai.custom_components_discovery_class` allows to load a
    specific class (that extends the `IndexCoomponentDiscovery` class)
    instead of the default.
    
    As part of the refactor to make this new customisability easier, this
    introduces a new `ComponentsBuildId` class that is the pair of the
    version and generation of a group of components, since this is
    whatultimately defines a given component "group" build.
    
    This PR is used by https://github.com/riptano/cndb/pull/11361 on the
    CNDB side, in the context of
    https://github.com/riptano/cndb/issues/10889.
    
     (Rebase of commit 54143fdc12263fb962c44b0641d6af9ac8441315)
    
     (Rebase of commit feddba8d7ca91c40c391c72e4af3adf065bae260)

--- src/java/org/apache/cassandra/index/sasi/SASIIndex.java
+++ src/java/org/apache/cassandra/index/sasi/SASIIndex.java
@@ -112,7 +112,7 @@ public class SASIIndex implements Index, INotificationConsumer
                                         });
                    });
 
-            return new SASIIndexBuilder(cfs, sstables);
+            return new SASIIndexBuilder(cfs, sstables, indexes);
         }
     }
 
@@ -144,7 +144,7 @@ public class SASIIndex implements Index, INotificationConsumer
             perSSTable.put(index.getDefinition(), index);
         }
 
-        CompactionManager.instance.submitIndexBuild(new SASIIndexBuilder(baseCfs, toRebuild));
+        CompactionManager.instance.submitIndexBuild(new SASIIndexBuilder(baseCfs, toRebuild, Collections.singleton(this)));
     }
 
     /**
