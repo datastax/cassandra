8059f8427e91903c9549889e28913d585b610d00
Michael Marshall <mmarshall@apache.org>
2024-11-13 19:10:12 -0600

    Fix NPE

--- src/java/org/apache/cassandra/index/sai/disk/vector/CassandraOnHeapGraph.java
+++ src/java/org/apache/cassandra/index/sai/disk/vector/CassandraOnHeapGraph.java
@@ -133,7 +133,9 @@ public class CassandraOnHeapGraph<T> implements Accountable
      */
     public CassandraOnHeapGraph(IndexContext context, boolean forSearching, Memtable memtable)
     {
-        this.source = memtable.getClass().getSimpleName() + '@' + Integer.toHexString(memtable.hashCode());
+        this.source = memtable == null
+                      ? "null"
+                      : memtable.getClass().getSimpleName() + '@' + Integer.toHexString(memtable.hashCode());
         var indexConfig = context.getIndexWriterConfig();
         var termComparator = context.getValidator();
         serializer = (VectorType.VectorSerializer) termComparator.getSerializer();
