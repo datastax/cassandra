a24e98790a5fe41ecc0a2a3757293a5291fd8561
Sylvain Lebresne <lebresne@gmail.com>
2024-11-14 09:43:12 +0100

    Allow custom SAI components discovery (#1338)
    Refactor the code that "discover" which SAI components files an SSTable
    has to allow making that code customizable. The default behavior remains
    the existing one, that is the TOC file is consulted, with a fallback to
    scanning disk if the TOC file is missing or corrupted.
    
    The way to customize this behavior is similar to what `SSTableWatcher`
    does: a new system property,
    `-Dcassandra.sai.custom_components_discovery_class` allows to load a
    specific class (that extends the `IndexCoomponentDiscovery` class)
    instead of the default.
    
    As part of the refactor to make this new customisability easier, this
    introduces a new `ComponentsBuildId` class that is the pair of the
    version and generation of a group of components, since this is
    whatultimately defines a given component "group" build.
    
    This PR is used by https://github.com/riptano/cndb/pull/11361 on the
    CNDB side, in the context of
    https://github.com/riptano/cndb/issues/10889.
    
     (Rebase of commit 54143fdc12263fb962c44b0641d6af9ac8441315)

--- src/java/org/apache/cassandra/index/sasi/SASIIndexBuilder.java
+++ src/java/org/apache/cassandra/index/sasi/SASIIndexBuilder.java
@@ -24,12 +24,14 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.db.compaction.OperationType;
 import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.index.Index;
 import org.apache.cassandra.index.SecondaryIndexBuilder;
 import org.apache.cassandra.index.sasi.conf.ColumnIndex;
 import org.apache.cassandra.index.sasi.disk.PerSSTableIndexWriter;
@@ -54,12 +56,13 @@ class SASIIndexBuilder extends SecondaryIndexBuilder
     // Keep targetDirectory for compactions, needed for `nodetool compactionstats`
     private String targetDirectory;
 
+    private final Set<Index> builtIndexes;
     private final SortedMap<SSTableReader, Map<ColumnMetadata, ColumnIndex>> sstables;
 
     private long bytesProcessed = 0;
     private final long totalBytesToProcess;
 
-    public SASIIndexBuilder(ColumnFamilyStore cfs, SortedMap<SSTableReader, Map<ColumnMetadata, ColumnIndex>> sstables)
+    public SASIIndexBuilder(ColumnFamilyStore cfs, SortedMap<SSTableReader, Map<ColumnMetadata, ColumnIndex>> sstables, Set<Index> indexes)
     {
         long totalBytesToProcess = 0;
         for (SSTableReader sstable : sstables.keySet())
@@ -67,6 +70,7 @@ class SASIIndexBuilder extends SecondaryIndexBuilder
 
         this.cfs = cfs;
         this.sstables = sstables;
+        this.builtIndexes = indexes;
         this.totalBytesToProcess = totalBytesToProcess;
     }
 
@@ -79,7 +83,7 @@ class SASIIndexBuilder extends SecondaryIndexBuilder
             SSTableReader sstable = e.getKey();
             Map<ColumnMetadata, ColumnIndex> indexes = e.getValue();
 
-            SSTableWatcher.instance.onIndexBuild(sstable);
+            SSTableWatcher.instance.onIndexBuild(sstable, builtIndexes);
             try (RandomAccessReader dataFile = sstable.openDataReader())
             {
                 PerSSTableIndexWriter indexWriter = SASIIndex.newWriter(keyValidator, sstable.descriptor, indexes, OperationType.COMPACTION);
