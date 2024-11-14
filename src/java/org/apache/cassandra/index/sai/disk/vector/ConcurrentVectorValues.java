639c6b9c3edd118364fe3df9214e2a4176615666
Michael Marshall <michael.marshall@datastax.com>
2024-11-13 21:40:21 -0600

    CNDB-11678: Use DenseIntMap for CassandraOnHeapGraph (#1418)
    ### What is the issue
    
    Fixes https://github.com/riptano/cndb/issues/11678
    
    ### What does this PR fix and why was it fixed
    
    The `NonBlockingHashMapLong` class was popping up on several profiles
    because of the `get` method. We can instead use the `DenseIntMap` from
    jvector because we know that the map keys are ordinals in the vector
    index. We technically don't need all of the features that the
    `DenseIntMap` gives us (e.g. we don't update map values after they are
    set), but given that I haven't seen the `DenseIntMap` on any of the
    flame graphs and the class is used internally in jvector in similar
    ways, I am guessing this should be a solid improvement. Further, in my
    preliminary testing, it showed a remarkable improvement on flame
    graphs.
    
    Note: I also considered a more custom class that might have had fewer
    volatile references, but ultimately, I think this code is well tested
    and we should use it until we find that it is a large enough bottleneck
    to justify the higher cognitive load associated with a custom class.
    
    ### Checklist before you submit for review
    - [ ] Make sure there is a PR in the CNDB project updating the Converged
    Cassandra version
    - [ ] Use `NoSpamLogger` for log lines that may appear frequently in the
    logs
    - [ ] Verify test results on Butler
    - [x] Test coverage for new/modified code is > 80%
    - [x] Proper code formatting
    - [x] Proper title for each commit staring with the project-issue
    number, like CNDB-1234
    - [x] Each commit has a meaningful description
    - [x] Each commit is not very long and contains related changes
    - [x] Renames, moves and reformatting are in distinct commits

--- src/java/org/apache/cassandra/index/sai/disk/vector/ConcurrentVectorValues.java
+++ src/java/org/apache/cassandra/index/sai/disk/vector/ConcurrentVectorValues.java
@@ -25,6 +25,7 @@ import java.util.function.IntUnaryOperator;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import io.github.jbellis.jvector.util.DenseIntMap;
 import io.github.jbellis.jvector.util.RamUsageEstimator;
 import io.github.jbellis.jvector.vector.ArrayVectorFloat;
 import io.github.jbellis.jvector.vector.types.VectorFloat;
@@ -34,7 +35,7 @@ import org.jctools.maps.NonBlockingHashMapLong;
 public class ConcurrentVectorValues implements RamAwareVectorValues
 {
     private final int dimensions;
-    private final NonBlockingHashMapLong<VectorFloat<?>> values = new NonBlockingHashMapLong<>();
+    private final DenseIntMap<VectorFloat<?>> values = new DenseIntMap<>(1024);
 
     public ConcurrentVectorValues(int dimensions)
     {
@@ -62,8 +63,9 @@ public class ConcurrentVectorValues implements RamAwareVectorValues
     /** return approximate bytes used by the new vector */
     public long add(int ordinal, VectorFloat<?> vector)
     {
-        values.put(ordinal, vector);
-        return RamEstimation.concurrentHashMapRamUsed(1) + oneVectorBytesUsed();
+        if (!values.compareAndPut(ordinal, null, vector))
+            throw new IllegalStateException("Vector already exists for ordinal " + ordinal);
+        return RamUsageEstimator.NUM_BYTES_OBJECT_REF + oneVectorBytesUsed();
     }
 
     @Override
@@ -83,7 +85,7 @@ public class ConcurrentVectorValues implements RamAwareVectorValues
     {
         long REF_BYTES = RamUsageEstimator.NUM_BYTES_OBJECT_REF;
         return 2 * REF_BYTES
-               + RamEstimation.concurrentHashMapRamUsed(values.size())
+               + RamEstimation.denseIntMapRamUsed(values.size())
                + values.size() * oneVectorBytesUsed();
     }
 
