c39d05534c7e839dafe6421500c5b1ad161be176
shunsaker <shunsaker@users.noreply.github.com>
2024-11-21 10:12:26 -0700

    HCD-18 add option to ignore DC name validation (#1431)
    HCD-18 add option to ignore DC name validation
    
    Added a new system property "cassandra.dc_skip_name_validation",
    which is used to bypass "ConfigurationException: Unrecognized strategy
    option {group20} passed to NetworkTopologyStrategy" messages when
    creating/altering replication strategy to a DC that doesn't exist
    
    In cassandra versions <4 and DSE 5.1 there was less strict validation
    when adding a datacenter. The nodes could be added first, or the
    keyspace replication schema could be changed to include the new DC
    first.
    
    With CASSANDRA-12681, validation was added to check that a DC exists
    before letting it be used in the replication strategy.
    
    We have customers (Netapp) that have existing automation tooling for
    adding datacenters that require this additional validation to be
    optionally disabled.

--- test/unit/org/apache/cassandra/cql3/validation/operations/CreateTest.java
+++ test/unit/org/apache/cassandra/cql3/validation/operations/CreateTest.java
@@ -29,6 +29,7 @@ import java.util.stream.IntStream;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.apache.cassandra.config.CassandraRelevantProperties;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.cql3.CQLTester;
 import org.apache.cassandra.cql3.Duration;
@@ -38,6 +39,7 @@ import org.apache.cassandra.db.memtable.SkipListMemtable;
 import org.apache.cassandra.db.memtable.TestMemtable;
 import org.apache.cassandra.db.memtable.TrieMemtable;
 import org.apache.cassandra.db.partitions.Partition;
+import org.apache.cassandra.distributed.shared.WithProperties;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.exceptions.InvalidRequestException;
 import org.apache.cassandra.exceptions.SyntaxException;
@@ -398,7 +400,7 @@ public class CreateTest extends CQLTester
     }
 
     /**
-     *  Test {@link ConfigurationException} is thrown on create keyspace with invalid DC option in replication configuration .
+     *  Test {@link ConfigurationException} is thrown on create keyspace with invalid DC option in replication configuration.
      */
     @Test
     public void testCreateKeyspaceWithNTSOnlyAcceptsConfiguredDataCenterNames() throws Throwable
@@ -412,6 +414,24 @@ public class CreateTest extends CQLTester
         // clean-up
         execute("DROP KEYSPACE IF EXISTS testABC");
         execute("DROP KEYSPACE IF EXISTS testXYZ");
+    }
+
+    /**
+     *  Test that when cassandra.dc_skip_name_validation=true nothing is thrown on create keyspace with invalid DC option in replication configuration.
+     */
+    @Test
+    public void testCreateKeyspaceWithNTSAcceptsAnyDataCenterNamesIfValidationIgnored() throws Throwable
+    {
+        try (WithProperties properties = new WithProperties().set(CassandraRelevantProperties.DATACENTER_SKIP_NAME_VALIDATION, true))
+        {
+            CassandraRelevantProperties.DATACENTER_SKIP_NAME_VALIDATION.setBoolean(true);
+            execute("CREATE KEYSPACE testABC WITH replication = { 'class' : 'NetworkTopologyStrategy', 'INVALID_DC' : 2 }");
+            execute("CREATE KEYSPACE testXYZ WITH replication={ 'class' : 'NetworkTopologyStrategy', '" + DATA_CENTER + "' : 2 , 'INVALID_DC': 1}");
+
+            // clean-up
+            execute("DROP KEYSPACE IF EXISTS testABC");
+            execute("DROP KEYSPACE IF EXISTS testXYZ");
+        }
     }
 
     /**
