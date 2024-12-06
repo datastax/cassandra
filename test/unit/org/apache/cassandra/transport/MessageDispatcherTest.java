ac2855cf6e7e10e8d9fc0ca2dc3810636522c18d
Jakub Żytka <jakub.zytka@gmail.com>
2024-12-06 17:48:40 +0100

    CNDB-10759 Make native transport stage async in regards to read and writes, which are asynchronously offloaded to the read and write stages when the NATIVE_TRANSPORT_ASYNC_READ_WRITE_ENABLED property is enabled. (#1446)
    Re-using the read and write stages allows to also reuse the related
    concurrency settings for which we already have support, and is ideal for
    Coordinator-only nodes in CNDB.
    
    Long reads or long writes negatively impact latency of the other kind of
    requests
    
    This change allows asynchronous offloading of reads and writes
    from the Native Transport Requests stage to read and write stages
    respectively.
    The change is introduced to reduce tail latencies in situations where
    heavy requests
    of one kind saturate NTR threads and make the other kind sit in the
    queue.
    
    Co-authored-by: Sergio Bossa <sergio.bossa@gmail.com>
    
     (Rebase of commit a43fb9e9011c2b2ffc6cff33724cc56222f269c2)
    
    CC5 fix MessageDispatcherTest.testAuthRateLimiterNotUsed
    fixes rebase conflict with CNDB-10759
    
     (Rebase of commit 75dc1f0be15694f768195b9d889cac47b16e6088)

--- test/unit/org/apache/cassandra/transport/MessageDispatcherTest.java
+++ test/unit/org/apache/cassandra/transport/MessageDispatcherTest.java
@@ -34,6 +34,8 @@ import org.apache.cassandra.metrics.ClientMetrics;
 import org.apache.cassandra.service.QueryState;
 import org.apache.cassandra.transport.messages.AuthResponse;
 import org.mockito.Mockito;
+import org.apache.cassandra.utils.concurrent.Future;
+import org.apache.cassandra.utils.concurrent.ImmediateFuture;
 
 public class MessageDispatcherTest
 {
@@ -46,7 +48,7 @@ public class MessageDispatcherTest
         }
 
         @Override
-        public Response execute(QueryState queryState, Dispatcher.RequestTime requestTime, boolean traceRequest)
+        public Future<Response> maybeExecuteAsync(QueryState queryState, Dispatcher.RequestTime requestTime, boolean traceRequest)
         {
             return null;
         }
@@ -105,10 +107,9 @@ public class MessageDispatcherTest
                     return connectionMock();
                 }
 
-                @Override
-                public Response execute(QueryState queryState, Dispatcher.RequestTime requestTime, boolean traceRequest)
+                public Future<Response> maybeExecuteAsync(QueryState queryState, Dispatcher.RequestTime requestTime, boolean traceRequest)
                 {
-                    return null;
+                    return ImmediateFuture.success(null);
                 }
             });
             Assert.assertEquals(requests, 1);
