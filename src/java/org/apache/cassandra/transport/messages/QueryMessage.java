2d0cd9d7c76a21ce5e35348d3859ce072f46b576
Jakub Żytka <jakub.zytka@gmail.com>
2024-12-10 14:58:37 +0100

    CNDB-12051: addtional tracing messages about query execution (#1459)
    Too coarse grained traces
    
    This PR adds a couple of new traces to give us better insight what does
    the system do during query handling
    
    - [x] Make sure there is a PR in the CNDB project updating the Converged
    Cassandra version
    - [x] Use `NoSpamLogger` for log lines that may appear frequently in the
    logs
    - [x] Verify test results on Butler
    - [x] Test coverage for new/modified code is > 80%
    - [x] Proper code formatting
    - [x] Proper title for each commit staring with the project-issue
    number, like CNDB-1234
    - [x] Each commit has a meaningful description
    - [x] Each commit is not very long and contains related changes
    - [x] Renames, moves and reformatting are in distinct commits

--- src/java/org/apache/cassandra/transport/messages/QueryMessage.java
+++ src/java/org/apache/cassandra/transport/messages/QueryMessage.java
@@ -115,6 +115,7 @@ public class QueryMessage extends Message.Request
                 traceQuery(state);
 
             long requestStartMillisTime = Clock.Global.currentTimeMillis();
+            Tracing.trace("Executing query started");
 
             QueryHandler queryHandler = ClientState.getCQLQueryHandler();
             statement = queryHandler.parse(query, state, options);
@@ -149,6 +150,10 @@ public class QueryMessage extends Message.Request
         catch (Exception ex)
         {
             return handleException(queryState, statement, ex);
+        }
+        finally
+        {
+            Tracing.trace("Executing query completed");
         }
     }
 
