544d55f9b4eb6268d77b6420a07b6c2f28bdaf9e
Zhao Yang <zhaoyangsingapore@gmail.com>
2024-12-11 14:55:46 +0800

    CNDB-11495: add REMOTE_STORAGE_HANDLER_FACTORY (#1443)
    - use `cassandra.remote_storage_handler_factory` to replace
    `cassandra.remote_storage_handler`
    
    - add `TableMetadata` to `StorageProvider#createDataDirectories`
    signature
    
    - use `enableAutoCompaction` to replace `StorageHandler#isRemote`
    
    - emit SSTaleAddingNotification in LifecycleTransaction#checkpoint
    before new sstables are made visible. Note that early open is not
    supported because it opens sstables before `trackNewWritten`
    
     (Rebase of commit f1384e70928579a6b37daa4d5ffd91b99da5a939)
    
     (Rebase of commit b02a6766cc6b43459276163f3fb50ef926a691fa)

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -488,7 +488,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     private void reloadCompactionStrategy(CompactionParams compactionParams, CompactionStrategyContainer.ReloadReason reason)
     {
         CompactionStrategyContainer previous = strategyContainer;
-        strategyContainer = strategyFactory.reload(strategyContainer, compactionParams, reason, !storageHandler.isRemote());
+        strategyContainer = strategyFactory.reload(strategyContainer, compactionParams, reason, storageHandler.enableAutoCompaction());
         if (strategyContainer != previous)
         {
             getTracker().subscribe(strategyContainer);
@@ -635,6 +635,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
             this.directories = new Directories(metadata.get());
 
         storageHandler = StorageHandler.create(this, metadata, directories, data);
+        logger.debug("Initialized storage handler with {} for {}.{}", storageHandler.getClass().getSimpleName(), keyspace.getName(), name);
 
         Collection<SSTableReader> sstables = null;
         // scan for sstables corresponding to this cf and load them
@@ -646,7 +647,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
         this.strategyContainer = strategyFactory.reload(null,
                                                         metadata.get().params.compaction,
                                                         CompactionStrategyContainer.ReloadReason.FULL,
-                                                        !storageHandler.isRemote());
+                                                        storageHandler.enableAutoCompaction());
         getTracker().subscribe(strategyContainer);
 
         if (!strategyContainer.isEnabled() || DISABLED_AUTO_COMPACTION_PROPERTY.getBoolean())
