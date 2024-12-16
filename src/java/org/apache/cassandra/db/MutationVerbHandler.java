1bf85153b92057dc4475d31feb131c16a5dde126
Joel Knighton <joel.knighton@datastax.com>
2024-12-16 07:42:15 -0600

    CNDB-11070: Limited backport of CASSANDRA-19534 (#1460)
    Unbounded queue length at the native transport stage can caused large
    backlogs of requests that the system processes, even though clients may
    no longer expect a response.
    
    This PR implements a limited backport of CNDB-11070, introducing the
    notion of a native request timeout that can shed messages with excessive
    queue times at the NTR stage as well as async read/write stages, if
    enabled. Cross-node message timeouts are also now respected earlier in
    the mutation verb handler.
    
    This is a fairly straightforward cherry-pick of
    https://github.com/datastax/cassandra/pull/1393 targeting main instead
    of cc-main-migration-release.
    
     (Rebase of commit 6f712c6cfc7175c72179fb644d3de23e1cc2c896)

--- src/java/org/apache/cassandra/db/MutationVerbHandler.java
+++ src/java/org/apache/cassandra/db/MutationVerbHandler.java
@@ -36,6 +36,7 @@ import org.apache.cassandra.sensors.RequestSensorsFactory;
 import org.apache.cassandra.sensors.RequestTracker;
 import org.apache.cassandra.sensors.Type;
 import org.apache.cassandra.tracing.Tracing;
+import org.apache.cassandra.utils.MonotonicClock;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.apache.cassandra.db.commitlog.CommitLogSegment.ENTRY_OVERHEAD_SIZE;
@@ -71,6 +72,12 @@ public class MutationVerbHandler extends AbstractMutationVerbHandler<Mutation>
         }
 
         message.payload.validateSize(MessagingService.current_version, ENTRY_OVERHEAD_SIZE);
+        if (MonotonicClock.Global.approxTime.now() > message.expiresAtNanos())
+        {
+            Tracing.trace("Discarding mutation from {} (timed out)", message.from());
+            MessagingService.instance().metrics.recordDroppedMessage(message, message.elapsedSinceCreated(NANOSECONDS), NANOSECONDS);
+            return;
+        }
 
         // Check if there were any forwarding headers in this message
         ForwardingInfo forwardTo = message.forwardTo();
