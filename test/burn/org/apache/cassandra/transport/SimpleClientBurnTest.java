77e38ca164c386cbe98742bac24693a95af62a13
dan jatnieks <jatnieks@pobox.com>
2024-12-30 16:22:12 -0800

    CNDB-12272: Fix SimpleClientPerfTest (#1477)
    SimpleClientPerfTest has been failing in CI since changes from
    CNDB-10759
    
    This change in `SimpleClientPerfTest`, updates the anonymous class
    `Message.Codec<QueryMessage>` to override the correct method, `public
    CompletableFuture<Response> maybeExecuteAsync` from `QueryMessage`,
    whose signature was changed as part of CNDB-10759.
    
    - [ ] Make sure there is a PR in the CNDB project updating the Converged
    Cassandra version
    - [ ] Use `NoSpamLogger` for log lines that may appear frequently in the
    logs
    - [ ] Verify test results on Butler
    - [ ] Test coverage for new/modified code is > 80%
    - [ ] Proper code formatting
    - [ ] Proper title for each commit staring with the project-issue
    number, like CNDB-1234
    - [ ] Each commit has a meaningful description
    - [ ] Each commit is not very long and contains related changes
    - [ ] Renames, moves and reformatting are in distinct commits
    
     (Rebase of commit 18382a9ae983b3731f04e3b9e98647b76c350f9a)

--- test/burn/org/apache/cassandra/transport/SimpleClientBurnTest.java
+++ test/burn/org/apache/cassandra/transport/SimpleClientBurnTest.java
@@ -44,6 +44,8 @@ import org.apache.cassandra.service.QueryState;
 import org.apache.cassandra.transport.messages.QueryMessage;
 import org.apache.cassandra.transport.messages.ResultMessage;
 import org.apache.cassandra.utils.AssertUtil;
+import org.apache.cassandra.utils.concurrent.Future;
+import org.apache.cassandra.utils.concurrent.ImmediateFuture;
 
 import static org.apache.cassandra.config.EncryptionOptions.TlsEncryptionPolicy.UNENCRYPTED;
 import static org.apache.cassandra.transport.BurnTestUtil.SizeCaps;
@@ -113,11 +115,12 @@ public class SimpleClientBurnTest
                 QueryMessage queryMessage = QueryMessage.codec.decode(body, version);
                 return new QueryMessage(queryMessage.query, queryMessage.options)
                 {
-                    public Message.Response executeSync(QueryState state, Dispatcher.RequestTime requestTime, boolean traceRequest)
+                    @Override
+                    public Future<Response> maybeExecuteAsync(QueryState state, Dispatcher.RequestTime requestTime, boolean traceRequest)
                     {
                         int idx = Integer.parseInt(queryMessage.query);
                         SizeCaps caps = idx % largeMessageFrequency == 0 ? largeMessageCap : smallMessageCap;
-                        return generateRows(idx, caps);
+                        return ImmediateFuture.success(generateRows(idx, caps));
                     }
                 };
             }
