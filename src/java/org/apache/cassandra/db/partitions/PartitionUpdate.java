991eee4d6118eedeb29cf51ef91fc460a8b1b95d
Daniel Jatnieks <jatnieks@pobox.com>
2025-01-06 19:01:38 -0800

    CNDB-9850 followup: Revert part of CNDB-11010 that added PartitionUpdate.accumulatedDataSize; it only worked to fix SensorsWriteTest.testMultipleRowsMutationWithClusteringKey for SkipListMemtable and may not be necessary if the default memtable becomes TrieMemtable. Revisit SensorsWriteTest later if necessary.

--- src/java/org/apache/cassandra/db/partitions/PartitionUpdate.java
+++ src/java/org/apache/cassandra/db/partitions/PartitionUpdate.java
@@ -94,14 +94,6 @@ public interface PartitionUpdate extends Partition
      */
     int operationCount();
 
-    // FIXME review
-    /**
-     * The accumulated BTree size of the data contained in this update.
-     *
-     * @return the accumulated BTree size of the data contained in this update.
-     */
-    long accumulatedDataSize();
-
     /**
      * The size of the data contained in this update.
      *
