4f7a99237bfaf356a7dc829549ccac69be1d6169
Jakub Żytka <jakub.zytka@gmail.com>
2025-01-09 11:04:17 +0100

    CNDB-12346: Fix reporting enqueue time for stage tasks (#1490)
    Enqueue start time will now be correctly measured for every API for task
    execution
    
    Enqueue start times are measured at a wrong moment for some APIs.
    
    We now measure the enqueue start time as a time of entry to each task
    execution API.
    It was fixed so that we see the correct enqueue times in metrics

--- src/java/org/apache/cassandra/net/InboundMessageHandler.java
+++ src/java/org/apache/cassandra/net/InboundMessageHandler.java
@@ -406,7 +406,7 @@ public class InboundMessageHandler extends AbstractMessageHandler
         if (state != null) state.trace("{} message received from {}", header.verb, header.from);
 
         callbacks.onDispatched(task.size(), header);
-        header.verb.stage.execute(ExecutorLocals.create(state), task);
+        header.verb.stage.execute(task, ExecutorLocals.create(state));
     }
 
     private abstract class ProcessMessage implements Runnable
