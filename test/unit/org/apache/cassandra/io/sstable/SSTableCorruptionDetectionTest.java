5262c594a2080d80796badc70a6fad1392b3200a
Branimir Lambov <branimir.lambov@datastax.com>
2025-01-13 14:21:24 +0200

    Fix test compilation

--- test/unit/org/apache/cassandra/io/sstable/SSTableCorruptionDetectionTest.java
+++ test/unit/org/apache/cassandra/io/sstable/SSTableCorruptionDetectionTest.java
@@ -190,7 +190,7 @@ public class SSTableCorruptionDetectionTest extends SSTableWriterTestBase
             finally
             {
                 if (ChunkCache.instance != null)
-                    ChunkCache.instance.invalidateFile(ssTableReader.getFilename());
+                    ChunkCache.instance.invalidateFile(ssTableReader.getDataFile());
 
                 restore(raf, corruptionPosition, backup);
             }
