de1699345c8830bf24510883e7e7555c569f2878
Andrés de la Peña <adelapena@users.noreply.github.com>
2025-01-15 20:11:24 +0000

    CNDB-12075: Allow UDFs within GROUP BY clause (#1494)

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -4216,6 +4216,11 @@ public class DatabaseDescriptor
         return conf.user_defined_functions_threads_enabled;
     }
 
+    public static void enableUserDefinedFunctionsThreads(boolean enabled)
+    {
+        conf.user_defined_functions_threads_enabled = enabled;
+    }
+
     public static long getUserDefinedFunctionWarnTimeout()
     {
         return conf.user_defined_functions_warn_timeout.toMilliseconds();
