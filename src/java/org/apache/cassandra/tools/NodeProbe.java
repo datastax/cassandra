8cee26503df84c12fb59d78c8849ae8c4570c473
Branimir Lambov <branimir.lambov@datastax.com>
2025-01-15 15:46:59 +0000

    CNDB-10907: Implement overlap diagnostics (#1453)
    Provide a new --overlap (-O) option to nodetool compactionstats that generates an overlap map showing the maximum overlaps for every shard of every level in the strategy, as well as a total for all sstables in the system (not necessarily the sum of the per-level overlaps).
    
    Change nodetool compactionstats to accept a keyspace and list of tables to restrict the --aggregate and --overlap output.
    
     (Rebase of commit 0b919f4baa0d51a897725c79893c6e566caae4c7)

--- src/java/org/apache/cassandra/tools/NodeProbe.java
+++ src/java/org/apache/cassandra/tools/NodeProbe.java
@@ -2101,6 +2101,7 @@ public class NodeProbe implements AutoCloseable
                 case "PendingTasksByTableName":
                 case "WriteAmplificationByTableName":
                 case "AggregateCompactions":
+                case "MaxOverlapsMap":
                     return JMX.newMBeanProxy(mbeanServerConn,
                             new ObjectName("org.apache.cassandra.metrics:type=Compaction,name=" + metricName),
                             CassandraMetricsRegistry.JmxGaugeMBean.class).getValue();
