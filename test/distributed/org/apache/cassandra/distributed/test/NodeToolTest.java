cdb0743c528607b669b8e2bb9589a4cd8a42326e
Branimir Lambov <branimir.lambov@datastax.com>
2025-01-15 15:46:59 +0000

    CNDB-10907: Implement overlap diagnostics (#1453)
    Provide a new --overlap (-O) option to nodetool compactionstats that generates an overlap map showing the maximum overlaps for every shard of every level in the strategy, as well as a total for all sstables in the system (not necessarily the sum of the per-level overlaps).
    
    Change nodetool compactionstats to accept a keyspace and list of tables to restrict the --aggregate and --overlap output.

--- test/distributed/org/apache/cassandra/distributed/test/NodeToolTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/NodeToolTest.java
@@ -147,4 +147,18 @@ public class NodeToolTest extends TestBaseImpl
             .success()
             .stdoutContains("GitSHA:");
     }
+
+    @Test
+    public void testCompactionStats() throws Throwable
+    {
+        NodeToolResult result = NODE.nodetoolResult("compactionstats", "--aggregate", "--overlap");
+        result.asserts().success().stdoutContains("pending tasks");
+        result.asserts().success().stdoutContains("Aggregated view");
+        result.asserts().success().stdoutContains("Max overlap map");
+
+        result = NODE.nodetoolResult("compactionstats", "--aggregate", "--overlap", "--human-readable", "system_schema", "tables");
+        result.asserts().success().stdoutContains("system_schema.tables");
+        result.asserts().success().stdoutNotContains("system.peers");
+        result.asserts().success().stdoutNotContains("system_schema.keyspaces");
+    }
 }
