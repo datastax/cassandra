de1d0a138cc96af6fe5eb6464d68ad9577835286
Michael Marshall <michael.marshall@datastax.com>
2025-01-16 18:57:37 -0500

    Revert "CNDB-12342: Avoid unnecessary looping in QueryView#build (#1491)" (#1510)
    This commit failed several CNDB tests.
    
    This reverts commit 6a1e98148bc6ce02003d2569d66e7d31c216a0f1.

--- test/unit/org/apache/cassandra/index/sai/cql/NativeIndexDDLTest.java
+++ test/unit/org/apache/cassandra/index/sai/cql/NativeIndexDDLTest.java
@@ -1035,8 +1035,6 @@ public class NativeIndexDDLTest extends SAITester
             (corruptionType != CorruptionType.REMOVED))
             return;
 
-        logger.info("CORRUPTING: " + component + ", corruption type = " + corruptionType);
-
         int rowCount = 2;
 
         // initial verification
