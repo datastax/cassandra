2d197965e1f8bd26b463f2260d34a383d258f76b
Ruslan Fomkin <ruslan.fomkin@datastax.com>
2025-02-18 18:40:30 +0100

    CNDB-13004 don't fail on too long file name for options (#1576)
    When a compaction controller config options are being written or read
    for a table with long keyspace + table names, FileSystemException
    happens.
    
    This commit fixes that such exception is caught and logged. Thus
    existing table with long name will continue to function. Tests are
    added.
    
    Also it catches other exceptions.
    
     (Rebase of commit 3e5d587db4b251f0fe473968b34cb9d3e1e5c0b8)

--- src/java/org/apache/cassandra/db/compaction/unified/AdaptiveController.java
+++ src/java/org/apache/cassandra/db/compaction/unified/AdaptiveController.java
@@ -23,6 +23,9 @@ import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
+
+import org.apache.cassandra.io.FSError;
+import org.apache.cassandra.utils.JVMStabilityInspector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -209,6 +212,15 @@ public class AdaptiveController extends Controller
         catch (ParseException e)
         {
             logger.warn("Unable to parse saved options. Using starting value instead:", e);
+        }
+        catch (FSError e)
+        {
+            logger.warn("Unable to read controller config file. Using starting value instead:", e);
+        }
+        catch (Throwable e)
+        {
+            logger.warn("Unable to read controller config file. Using starting value instead:", e);
+            JVMStabilityInspector.inspectThrowable(e);
         }
 
         if (scalingParameters == null)
