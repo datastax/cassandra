63a284db932c4f3a6a5932a638beccf5016aa03d
Szymon Miężał <szymon.miezal@datastax.com>
2025-02-20 16:17:48 +0100

    Leave comments to revisit later

--- src/java/org/apache/cassandra/io/compress/CompressedSequentialWriter.java
+++ src/java/org/apache/cassandra/io/compress/CompressedSequentialWriter.java
@@ -177,6 +177,7 @@ public class CompressedSequentialWriter extends SequentialWriter
         if (compressedLength >= maxCompressedLength)
         {
             toWrite = buffer;
+            //TODO: confirm that we do not have to change anything here
             if (uncompressedLength >= maxCompressedLength)
             {
                 compressedLength = uncompressedLength;
@@ -221,6 +222,7 @@ public class CompressedSequentialWriter extends SequentialWriter
             runPostFlush.run();
     }
 
+    //TODO: to revisit
     public CompressionMetadata open(long overrideLength)
     {
         if (overrideLength <= 0)
