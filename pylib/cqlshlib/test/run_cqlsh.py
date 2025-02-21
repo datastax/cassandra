f74e2056f008591acb0aae4ef54b85e5cd1f3a8b
dan jatnieks <jatnieks@pobox.com>
2025-02-21 06:05:31 -0800

    CNDB-11460 Fix cqlsh unicode tests in TestCqlshUnicode by handling UTF-8 characters that are split across reads. (#1561)
     (Rebase of commit 616a884c6a92617c87fad86ff503a2f9f74dc117)
    
    CNDB-11460 another fix for cqlsh unicode tests in TestCqlshUnicode (#1618)
    
    CNDB-11460 another fix for cqlsh unicode tests in TestCqlshUnicode -
    make a second read in read_until to get UTF-8 characters that are split
    across reads.
    
     (Rebase of commit bc4c089c00b4a2b2fdf8a5791c72a1872c0cc091)
    
     (Rebase of commit 19f3b57cbe30a57b0cf3c47f4a0d22704720cf94)

--- pylib/cqlshlib/test/run_cqlsh.py
+++ pylib/cqlshlib/test/run_cqlsh.py
@@ -146,6 +146,7 @@ class ProcRunner:
             self.childpty = masterfd
             self.send = self.send_tty
             self.read = self.read_tty
+            self._partial_bytes = b''
         else:
             stdin = stdout = subprocess.PIPE
             self.proc = subprocess.Popen((self.exe_path,) + tuple(self.args),
@@ -155,6 +156,7 @@ class ProcRunner:
             self.read = self.read_pipe
 
     def close(self):
+        self._partial_bytes = b''
         cqlshlog.info("Closing %r subprocess." % (self.exe_path,))
         if self.tty:
             os.close(self.childpty)
@@ -201,6 +203,7 @@ class ProcRunner:
         cqlshlog.debug("Searching for %r" % (until.pattern,))
         got = self.readbuf
         self.readbuf = ''
+        empty_reads = 0
         with timing_out(timeout):
             while True:
                 val = self.read(blksize, ptty_timeout)
@@ -209,7 +212,13 @@ class ProcRunner:
                         val = val.replace(replace_target, '')
                 cqlshlog.debug("read %r from subproc" % (val,))
                 if val == '':
-                    raise EOFError("'until' pattern %r not found" % (until.pattern,))
+                    empty_reads += 1
+                    if empty_reads > 1:
+                        raise EOFError("'until' pattern %r not found" % (until.pattern,))
+                    # Read again to allow decoding of UTF-8 characters that span multiple reads
+                    continue
+
+                empty_reads = 0
                 got += val
                 m = until.search(got)
                 if m is not None:
@@ -257,6 +266,8 @@ class CqlshRunner(ProcRunner):
         env.setdefault('TERM', 'xterm')
         env.setdefault('CQLSH_NO_BUNDLED', os.environ.get('CQLSH_NO_BUNDLED', ''))
         env.setdefault('PYTHONPATH', os.environ.get('PYTHONPATH', ''))
+        # Initialize the partial bytes buffer
+        self._partial_bytes = b''
         coverage = False
         if ('CQLSH_COVERAGE' in env.keys()):
             coverage = True
