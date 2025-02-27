datastax/cassandra

commit b0cdc37bc29fbfb4c7301523dbdc4723cde2b917

Implement synthetic columns and ORDER BY BM25 (#1434)
### What is the issue
https://github.com/riptano/cndb/issues/11725

# New functionality

ORDER BY BM25, enabled by SAI Version.EC

# Enhanced functionality

ORDER BY ANN can also used the synthetic score column so that coordinator does not need to recompute similarity for every row returned by the different replicas.  Controlled by SelectStatement.ANN_USE_SYNTHETIC_SCORE (default false)

src/java/org/apache/cassandra/cql3/SingleColumnRelation.java
@@ -21,8 +21,11 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.apache.cassandra.db.marshal.VectorType;
+import org.apache.cassandra.index.IndexRegistry;
+import org.apache.cassandra.index.sai.analyzer.AnalyzerEqOperatorSupport;
 import org.apache.cassandra.schema.ColumnMetadata;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.cql3.Term.Raw;
@@ -33,6 +36,7 @@ import org.apache.cassandra.db.marshal.CollectionType;
 import org.apache.cassandra.db.marshal.ListType;
 import org.apache.cassandra.db.marshal.MapType;
 import org.apache.cassandra.exceptions.InvalidRequestException;
+import org.apache.cassandra.service.ClientWarn;
 
 import static org.apache.cassandra.cql3.statements.RequestValidations.checkFalse;
 import static org.apache.cassandra.cql3.statements.RequestValidations.checkTrue;
@@ -191,7 +195,29 @@ public final class SingleColumnRelation extends Relation
         if (mapKey == null)
         {
             Term term = toTerm(toReceivers(columnDef), value, table.keyspace, boundNames);
-            return new SingleColumnRestriction.EQRestriction(columnDef, term);
+            // Leave the restriction as EQ if no analyzed index in backwards compatibility mode is present
+            var ebi = IndexRegistry.obtain(table).getEqBehavior(columnDef);
+            if (ebi.behavior == IndexRegistry.EqBehavior.EQ)
+                return new SingleColumnRestriction.EQRestriction(columnDef, term);
+
+            // the index is configured to transform EQ into MATCH for backwards compatibility
+            var matchIndexName = ebi.matchIndex.getIndexMetadata() == null ? "Unknown" : ebi.matchIndex.getIndexMetadata().name;
+            if (ebi.behavior == IndexRegistry.EqBehavior.MATCH)
+            {
+                ClientWarn.instance.warn(String.format(AnalyzerEqOperatorSupport.EQ_RESTRICTION_ON_ANALYZED_WARNING,
+                                                       columnDef.toString(),
+                                                       matchIndexName),
+                                         columnDef);
+                return new SingleColumnRestriction.AnalyzerMatchesRestriction(columnDef, term);
+            }
+
+            // multiple indexes support EQ, this is unsupported
+            assert ebi.behavior == IndexRegistry.EqBehavior.AMBIGUOUS;
+            var eqIndexName = ebi.eqIndex.getIndexMetadata() == null ? "Unknown" : ebi.eqIndex.getIndexMetadata().name;
+            throw invalidRequest(AnalyzerEqOperatorSupport.EQ_AMBIGUOUS_ERROR,
+                                 columnDef.toString(),
+                                 matchIndexName,
+                                 eqIndexName);
         }
         List<? extends ColumnSpecification> receivers = toReceivers(columnDef);
         Term entryKey = toTerm(Collections.singletonList(receivers.get(0)), mapKey, table.keyspace, boundNames);
@@ -331,6 +357,14 @@ public final class SingleColumnRelation extends Relation
             throw invalidRequest("ANN is only supported against DENSE FLOAT32 columns");
         Term term = toTerm(toReceivers(columnDef), value, table.keyspace, boundNames);
         return new SingleColumnRestriction.AnnRestriction(columnDef, term);
+    }
+
+    @Override
+    protected Restriction newBm25Restriction(TableMetadata table, VariableSpecifications boundNames)
+    {
+        ColumnMetadata columnDef = table.getExistingColumn(entity);
+        Term term = toTerm(toReceivers(columnDef), value, table.keyspace, boundNames);
+        return new SingleColumnRestriction.Bm25Restriction(columnDef, term);
     }
 
     @Override

src/java/org/apache/cassandra/index/IndexRegistry.java
@@ -21,6 +21,7 @@
 package org.apache.cassandra.index;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
@@ -102,12 +103,6 @@ public interface IndexRegistry
         @Override
         public void validate(PartitionUpdate update)
         {
-        }
-
-        @Override
-        public void validate(RowFilter filter)
-        {
-            // no-op since it's an empty registry
         }
     };
 
@@ -296,12 +291,6 @@ public interface IndexRegistry
         public void validate(PartitionUpdate update)
         {
         }
-
-        @Override
-        public void validate(RowFilter filter)
-        {
-            // no-op since it's an empty registry
-        }
     };
 
     default void registerIndex(Index index)
@@ -354,8 +343,6 @@ public interface IndexRegistry
      */
     void validate(PartitionUpdate update);
 
-    void validate(RowFilter filter);
-
     /**
      * Returns the {@code IndexRegistry} associated to the specified table.
      *
@@ -368,5 +355,75 @@ public interface IndexRegistry
             return NON_DAEMON;
 
         return table.isVirtual() ? EMPTY : Keyspace.openAndGetStore(table).indexManager;
+    }
+
+    enum EqBehavior
+    {
+        EQ,
+        MATCH,
+        AMBIGUOUS
+    }
+
+    class EqBehaviorIndexes
+    {
+        public EqBehavior behavior;
+        public final Index eqIndex;
+        public final Index matchIndex;
+
+        private EqBehaviorIndexes(Index eqIndex, Index matchIndex, EqBehavior behavior)
+        {
+            this.eqIndex = eqIndex;
+            this.matchIndex = matchIndex;
+            this.behavior = behavior;
+        }
+
+        public static EqBehaviorIndexes eq(Index eqIndex)
+        {
+            return new EqBehaviorIndexes(eqIndex, null, EqBehavior.EQ);
+        }
+
+        public static EqBehaviorIndexes match(Index eqAndMatchIndex)
+        {
+            return new EqBehaviorIndexes(eqAndMatchIndex, eqAndMatchIndex, EqBehavior.MATCH);
+        }
+
+        public static EqBehaviorIndexes ambiguous(Index firstEqIndex, Index secondEqIndex)
+        {
+            return new EqBehaviorIndexes(firstEqIndex, secondEqIndex, EqBehavior.AMBIGUOUS);
+        }
+    }
+
+    /**
+     * @return
+     * - AMBIGUOUS if an index supports EQ and a different one supports both EQ and ANALYZER_MATCHES
+     * - MATCHES if an index supports both EQ and ANALYZER_MATCHES
+     * - otherwise EQ
+     */
+    default EqBehaviorIndexes getEqBehavior(ColumnMetadata cm)
+    {
+        Index eqOnlyIndex = null;
+        Index bothIndex = null;
+
+        for (Index index : listIndexes())
+        {
+            boolean supportsEq = index.supportsExpression(cm, Operator.EQ);
+            boolean supportsMatches = index.supportsExpression(cm, Operator.ANALYZER_MATCHES);
+
+            if (supportsEq && supportsMatches)
+                bothIndex = index;
+            else if (supportsEq)
+                eqOnlyIndex = index;
+        }
+
+        // If we have one index supporting only EQ and another supporting both, return AMBIGUOUS
+        if (eqOnlyIndex != null && bothIndex != null)
+            return EqBehaviorIndexes.ambiguous(eqOnlyIndex, bothIndex);
+
+        // If we have an index supporting both EQ and MATCHES, return MATCHES
+        if (bothIndex != null)
+            return EqBehaviorIndexes.match(bothIndex);
+
+        // Otherwise return EQ
+        return EqBehaviorIndexes.eq(eqOnlyIndex == null ? bothIndex : eqOnlyIndex);
     }
 }

commit 3f00247be1da7d1f9c86377fc8b073992a995e0e

Fix EqBehavior bug introduced by #1434 (#1614)
### What is the issue
The `NON_DAEMON` IndexRegistry returns `true` for all calls to
`supportsExpression`. This leads to an incorrect result from the
`getEqBehavior` method where we get `MATCH` instead of `EQ` because the
index indicates that it can handle `ANALYZER_MATCHES` expressions and
`EQ` expressions.

It is an odd edge case because the javadoc for the `NON_DAEMON` object
is:

```java
    /**
     * An {@code IndexRegistry} intended for use when Cassandra is initialized in client or tool mode.
     * Contains a single stub {@code Index} which possesses no actual indexing or searching capabilities
     * but enables query validation and preparation to succeed. Useful for tools which need to prepare
     * CQL statements without instantiating the whole ColumnFamilyStore infrastructure.
     */
```

This presents a problem for the eq/match logic where we want to find a
nuanced solution to the different solutions. My proposal here is to just
make it use the EQ behavior, but that might have adverse side effects in
untested code.

### What does this PR fix and why was it fixed
The original fix used in #1434 was just to avoid the NPE we hit, but it
allowed for the wrong result in eq behavior. This fix is to say that we
should just return `EQ` in that case. It's possible this fix has
negative consequences that we haven't seen yet, but at the very least,
the CNDB tests pass with it.

src/java/org/apache/cassandra/cql3/SingleColumnRelation.java
@@ -201,23 +201,21 @@ public final class SingleColumnRelation extends Relation
                 return new SingleColumnRestriction.EQRestriction(columnDef, term);
 
             // the index is configured to transform EQ into MATCH for backwards compatibility
-            var matchIndexName = ebi.matchIndex.getIndexMetadata() == null ? "Unknown" : ebi.matchIndex.getIndexMetadata().name;
             if (ebi.behavior == IndexRegistry.EqBehavior.MATCH)
             {
                 ClientWarn.instance.warn(String.format(AnalyzerEqOperatorSupport.EQ_RESTRICTION_ON_ANALYZED_WARNING,
                                                        columnDef.toString(),
-                                                       matchIndexName),
+                                                       ebi.matchIndex.getIndexMetadata().name),
                                          columnDef);
                 return new SingleColumnRestriction.AnalyzerMatchesRestriction(columnDef, term);
             }
 
             // multiple indexes support EQ, this is unsupported
             assert ebi.behavior == IndexRegistry.EqBehavior.AMBIGUOUS;
-            var eqIndexName = ebi.eqIndex.getIndexMetadata() == null ? "Unknown" : ebi.eqIndex.getIndexMetadata().name;
             throw invalidRequest(AnalyzerEqOperatorSupport.EQ_AMBIGUOUS_ERROR,
                                  columnDef.toString(),
-                                 matchIndexName,
-                                 eqIndexName);
+                                 ebi.matchIndex.getIndexMetadata().name,
+                                 ebi.eqIndex.getIndexMetadata().name);
         }
         List<? extends ColumnSpecification> receivers = toReceivers(columnDef);
         Term entryKey = toTerm(Collections.singletonList(receivers.get(0)), mapKey, table.keyspace, boundNames);

src/java/org/apache/cassandra/index/IndexRegistry.java
@@ -408,8 +408,11 @@ public interface IndexRegistry
         {
             boolean supportsEq = index.supportsExpression(cm, Operator.EQ);
             boolean supportsMatches = index.supportsExpression(cm, Operator.ANALYZER_MATCHES);
+            // This is an edge case due to the NON_DAEMON IndexRegistry, which doesn't have index metadata and
+            // which uses regular equality by convention.
+            boolean hasIndexMetadata = index.getIndexMetadata() != null;
 
-            if (supportsEq && supportsMatches)
+            if (supportsEq && supportsMatches && hasIndexMetadata)
                 bothIndex = index;
             else if (supportsEq)
                 eqOnlyIndex = index;