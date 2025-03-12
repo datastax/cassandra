c7363f664e39b21487a577d4064ac970a3c23433
Brandon Williams <brandonwilliams@apache.org>
2025-03-12 15:02:52 -0500

    CC5: fix ReadCommandTest and a bunch of AbstractReadQueryToCQLStringTests
     (Rebase of commit 84575c424f190ade1aa1f7251a1c31e1bcd480bd)
    
     (Rebase of commit 89393d44efe5cf7cbe07136d469b5bff5f82bfeb)

--- src/java/org/apache/cassandra/db/ReadCommand.java
+++ src/java/org/apache/cassandra/db/ReadCommand.java
@@ -37,6 +37,7 @@ import org.slf4j.LoggerFactory;
 
 import io.netty.util.concurrent.FastThreadLocal;
 import org.apache.cassandra.config.*;
+import org.apache.cassandra.cql3.ColumnIdentifier;
 import org.apache.cassandra.db.filter.*;
 import org.apache.cassandra.db.transform.BasePartitions;
 import org.apache.cassandra.db.transform.BaseRows;
@@ -1009,7 +1010,7 @@ public abstract class ReadCommand extends AbstractReadQuery
     {
         StringBuilder sb = new StringBuilder();
         sb.append("SELECT ").append(columnFilter().toCQLString());
-        sb.append(" FROM ").append(metadata().keyspace).append('.').append(metadata().name);
+        sb.append(" FROM ").append(ColumnIdentifier.maybeQuote(metadata().keyspace)).append('.').append(ColumnIdentifier.maybeQuote(metadata().name));
         appendCQLWhereClause(sb);
 
         if (limits() != DataLimits.NONE)
@@ -1018,6 +1019,9 @@ public abstract class ReadCommand extends AbstractReadQuery
         ANNOptions annOptions = rowFilter().annOptions();
         if (annOptions != ANNOptions.NONE)
             sb.append(" WITH ").append(SelectOptions.ANN_OPTIONS).append(" = ").append(annOptions.toCQLString());
+
+        // ALLOW FILTERING might not be strictly necessary
+        sb.append(" ALLOW FILTERING");
 
         return sb.toString();
     }
