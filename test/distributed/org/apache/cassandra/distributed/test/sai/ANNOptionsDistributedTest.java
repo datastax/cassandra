3bb3f2a5000f3069b51b5c06b511b156018a5611
Sergio Bossa <sergio.bossa@gmail.com>
2025-03-13 10:33:46 +0000

    CNDB-12922 ANNOptions validation now checks the endpoint connection messaging version (#1624)
    This makes sure the proper version is used, as
    following CNDB-13203, each connection maintains its own version,
    separated from the one recorded in the MessagingService.
    
    `ANNOptions#validate()` was previously checking endpoint versions via
    `MessagingService#versions`: but following #13203, in order to resolve a
    race condition, each endpoint connection tracks its own
    `EndpointMessagingVersions` object, with the version negotiated during
    handshake, while `MessagingService#versions` is updated with the max
    version of each endpoint. It follows checking
    `MessagingService#versions` might not rely on the right version used for
    the connection.
    
    This PR introduces a new `MessagingService` method to check the
    connection version, and uses it inside `ANNOptions#validate()`.
    
     (Rebase of commit 3e1434a22573eecfdabbb5ee9dcde5035d042f20)

--- test/distributed/org/apache/cassandra/distributed/test/sai/ANNOptionsDistributedTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/sai/ANNOptionsDistributedTest.java
@@ -16,6 +16,8 @@
 
 package org.apache.cassandra.distributed.test.sai;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.Test;
 
 import net.bytebuddy.ByteBuddy;
@@ -118,10 +120,9 @@ public class ANNOptionsDistributedTest extends TestBaseImpl
     {
         public static void install(ClassLoader classLoader, int node)
         {
-            if (node > 1)
+            // inject randomly first or second node to make sure it works if the node is a coordinator or replica
+            if (node == ThreadLocalRandom.current().nextInt(1, 3))
             {
-                // Relies on CassandraRelevantProperties.DS_CURRENT_MESSAGING_VERSION set to DS_11 or higher
-                // set the current verson to DS 10, which does NOT support ANN options
                 new ByteBuddy().rebase(MessagingService.class)
                                .method(named("currentVersion"))
                                .intercept(MethodDelegation.to(BB.class))
