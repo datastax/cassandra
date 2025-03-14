ed49d5f22b2e40a40c5efc8b1fdd1b4fd252ac40
Sylvain Lebresne <lebresne@gmail.com>
2025-03-14 15:27:17 +0100

    Add support for a per-operation context (#1619)
    The goal of this commit is to allow low-level code, typically at the
    level of file reads (say, a custom `FileChannel` implementation), to
    obtain context on which higher level operation they correspond to. A
    typical use case may for tiered storage where `FileChannel`
    implementations may forward reads to remote storage, and where having
    context on the overall operation could help, say, take prior work done
    to set timeout, or aggregate metrics per "user-level" operations (rather
    than per `FileChannel` operation).
    
    To do this, we reuse the existing `ExecutorLocals` mechanism, and simply
    have the top-level operation setup the proper context as an
    `ExecutorLocal`, allowing it to be accessed by any low-level operations
    operating on behalf of that operation. This is, in many way, similar to
    tracing, but instead of a `TraceState` that collect what happens during
    the operation, it is a relatively flexible notion of `OperationContext`.
    
    As of this patch, this feature is fairly barebone and mostly exists for
    extensions in the following sense:
    1. only user reads (`ReadCommand` execution) currently sets up such a
       context. The code is written in such a way that adding support for
       other operations should be easy, but this is not done.
    2. the context set by reads by default has barely any information: it
       merely has a `toString()` method that can roughly tell what the
       operation itself is, and so could have use for debugging, but not
       much more. Further, that context is not read by anything in this
       patch. However, said context are created through a "factory" and
       the factory class is configurable through a system property. So
       extension can override the factory in order to create contexts
       with more information/state, and they fetch/use those context where
       they see fit.
    
     (Rebase of commit 0ddb3870fcf3437d98ba6f57f09ddfc6bea910fe)

--- src/java/org/apache/cassandra/tracing/Tracing.java
+++ src/java/org/apache/cassandra/tracing/Tracing.java
@@ -277,7 +277,7 @@ public abstract class Tracing extends ExecutorLocals.Impl
     public void set(TraceState tls)
     {
         ExecutorLocals current = ExecutorLocals.current();
-        ExecutorLocals.Impl.set(tls, current.clientWarnState, current.sensors);
+        ExecutorLocals.Impl.set(tls, current.clientWarnState, current.sensors, current.operationContext);
     }
 
     public TraceState begin(final String request, final Map<String, String> parameters)
