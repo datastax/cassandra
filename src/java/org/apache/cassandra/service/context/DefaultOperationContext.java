09cef0d054cc43a592ed13f3f988348b09e6e188
Sylvain Lebresne <lebresne@gmail.com>
2025-03-14 15:27:17 +0100

    Add support for a per-operation context (#1619)
    The goal of this commit is to allow low-level code, typically at the
    level of file reads (say, a custom `FileChannel` implementation), to
    obtain context on which higher level operation they correspond to. A
    typical use case may for tiered storage where `FileChannel`
    implementations may forward reads to remote storage, and where having
    context on the overall operation could help, say, take prior work done
    to set timeout, or aggregate metrics per "user-level" operations (rather
    than per `FileChannel` operation).
    
    To do this, we reuse the existing `ExecutorLocals` mechanism, and simply
    have the top-level operation setup the proper context as an
    `ExecutorLocal`, allowing it to be accessed by any low-level operations
    operating on behalf of that operation. This is, in many way, similar to
    tracing, but instead of a `TraceState` that collect what happens during
    the operation, it is a relatively flexible notion of `OperationContext`.
    
    As of this patch, this feature is fairly barebone and mostly exists for
    extensions in the following sense:
    1. only user reads (`ReadCommand` execution) currently sets up such a
       context. The code is written in such a way that adding support for
       other operations should be easy, but this is not done.
    2. the context set by reads by default has barely any information: it
       merely has a `toString()` method that can roughly tell what the
       operation itself is, and so could have use for debugging, but not
       much more. Further, that context is not read by anything in this
       patch. However, said context are created through a "factory" and
       the factory class is configurable through a system property. So
       extension can override the factory in order to create contexts
       with more information/state, and they fetch/use those context where
       they see fit.
    
     (Rebase of commit 0ddb3870fcf3437d98ba6f57f09ddfc6bea910fe)
    
     (Rebase of commit 0cb109bf789336d2a2aab0041a327aba87b46de3)

---
+++ src/java/org/apache/cassandra/service/context/DefaultOperationContext.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.service.context;
+
+import java.util.function.Supplier;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.ReadCommand;
+
+/**
+ * Default implementation of {@link OperationContext}.
+ * <p>
+ * This default implementation is mostly only useful for debugging as the only concrete method is provices is a
+ * {@link #toString()} method giving details on the operation the context corresponds to (though the context object
+ * also identify the operation, so it could also theoretically be used from 2 separate place in the code to decide
+ * if they execute as part of the same operation).
+ */
+public class DefaultOperationContext implements OperationContext
+{
+    private final Supplier<String> toDebugString;
+
+    private DefaultOperationContext(Supplier<String> toDebugString)
+    {
+        this.toDebugString = toDebugString;
+    }
+
+    @Override
+    public void close()
+    {
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("[%d] %s", System.identityHashCode(this), toDebugString.get());
+    }
+
+    /**
+     * Simple default implementation of {@link OperationContext.Factory} that creates {@link DefaultOperationContext}.
+     */
+    static class Factory implements OperationContext.Factory
+    {
+        @Override
+        public OperationContext forRead(ReadCommand command, ColumnFamilyStore cfs)
+        {
+            return new DefaultOperationContext(command::toCQLString);
+        }
+    }
+}
