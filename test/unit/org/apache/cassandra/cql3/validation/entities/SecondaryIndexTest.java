8287a2e6bea65d681f8e579fbc40920377f79554
ekaterinadimitrova2 <ekaterina.dimitrova@datastax.com>
2025-03-24 11:44:36 -0400

    CNDB-12620 Users should get INDEX_BUILD_IN_PROGRESS instead of INDEX NOT AVAILABLE when they cannot query because an index is building.
    We add a new reason, `RequestFailureReason.INDEX_BUILD_IN_PROGRESS`, to differentiate indexes that are currently building in error messages
    
    Co-authored-by: Sergio Bossa <sergio.bossa@gmail.com>

--- test/unit/org/apache/cassandra/cql3/validation/entities/SecondaryIndexTest.java
+++ test/unit/org/apache/cassandra/cql3/validation/entities/SecondaryIndexTest.java
@@ -51,6 +51,7 @@ import org.apache.cassandra.db.marshal.AbstractType;
 import org.apache.cassandra.db.rows.Cell;
 import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.exceptions.SyntaxException;
+import org.apache.cassandra.index.IndexBuildInProgressException;
 import org.apache.cassandra.index.IndexNotAvailableException;
 import org.apache.cassandra.index.SecondaryIndexManager;
 import org.apache.cassandra.index.StubIndex;
@@ -1192,7 +1193,7 @@ public class SecondaryIndexTest extends CQLTester
             execute("SELECT value FROM %s WHERE value = 2");
             fail();
         }
-        catch (IndexNotAvailableException e)
+        catch (IndexBuildInProgressException e)
         {
             assertTrue(true);
         }
