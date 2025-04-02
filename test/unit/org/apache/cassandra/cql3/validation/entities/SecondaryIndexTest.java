6512a8230068b4b0deb59b49f2b7c9d02d80b826
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2025-04-02 13:42:45 -0400

    DO NOT COMMIT, for testing purposes

--- test/unit/org/apache/cassandra/cql3/validation/entities/SecondaryIndexTest.java
+++ test/unit/org/apache/cassandra/cql3/validation/entities/SecondaryIndexTest.java
@@ -1183,7 +1183,7 @@ public class SecondaryIndexTest extends CQLTester
         indexName = createIndexAsync("CREATE CUSTOM INDEX ON %s (value) USING '" + ReadOnlyOnFailureIndex.class.getName() + "'");
         index = (ReadOnlyOnFailureIndex) getCurrentColumnFamilyStore().indexManager.getIndexByName(indexName);
         waitForIndexBuilds(indexName);
-        assertInvalidThrow(IndexNotAvailableException.class, "SELECT value FROM %s WHERE value = 1");
+        assertInvalidThrow(IndexBuildInProgressException.class, "SELECT value FROM %s WHERE value = 1");
         execute("INSERT INTO %s (pk, ck, value) VALUES (?, ?, ?)", 1, 1, 1);
         assertEquals(0, index.rowsInserted.size());
 
@@ -1223,7 +1223,7 @@ public class SecondaryIndexTest extends CQLTester
         waitForIndexBuilds(indexName);
         execute("INSERT INTO %s (pk, ck, value) VALUES (?, ?, ?)", 1, 1, 1);
         assertEquals(1, index.rowsInserted.size());
-        assertInvalidThrow(IndexNotAvailableException.class, "SELECT value FROM %s WHERE value = 1");
+        assertInvalidThrow(IndexBuildInProgressException.class, "SELECT value FROM %s WHERE value = 1");
 
         // Upon recovery, we can query data again
         index.reset();
