a342337b23237ce68f312d525328c9ad1a9dd345
Michael Marshall <michael.marshall@datastax.com>
2025-04-02 13:45:04 -0500

    CNDB-13417: Reduce object alloc. on brute force hybrid ann path (#1643)
    ### What is the issue
    Relates to https://github.com/riptano/cndb/issues/13417 (the issue has
    multiple PRs)
    
    ### What does this PR fix and why was it fixed
    
    In hybrid ANN search resulting in brute force row sorting, we see many
    object allocations per row materialized (so O(n) space complexity), and
    this creates memory pressure leading to reduced performance. This PR
    reduces object allocation by using jvector's `NodeQueue` data structure
    that encodes an int and a float into a long and then sorts based on the
    float. I also renamed the IntIntPairArray because I needed more
    specialized methods.
    
    - **Rename IntIntPairArray to SegmentRowIdOrdinalPairs**
    - **CNDB-13417: Reduce object alloc. on brute force hybrid ann path**
    
     (Rebase of commit 52baa033db3db4a6fcc0c363de2fca59ba84c314)

--- src/java/org/apache/cassandra/index/sai/utils/SegmentRowIdOrdinalPairs.java
+++ src/java/org/apache/cassandra/index/sai/utils/SegmentRowIdOrdinalPairs.java
@@ -23,10 +23,10 @@ import java.util.function.IntConsumer;
 import org.agrona.collections.IntIntConsumer;
 
 /**
- * A simple array of int pairs that avoids boxing. Implemented as an alternative to the
- * {@link org.agrona.collections.Int2IntHashMap} for uses that do not require lookups.
+ * A specialized data structure that stores segment row id to ordinal pairs efficiently. Implemented as an array of int
+ * pairs that avoids boxing.
  */
-public class IntIntPairArray
+public class SegmentRowIdOrdinalPairs
 {
     private final int capacity;
     private int size;
@@ -34,9 +34,9 @@ public class IntIntPairArray
 
     /**
      * Create a new IntIntPairArray with the given capacity.
-     * @param capacity
+     * @param capacity the capacity
      */
-    public IntIntPairArray(int capacity)
+    public SegmentRowIdOrdinalPairs(int capacity)
     {
         assert capacity < Integer.MAX_VALUE / 2 : "capacity is too large " + capacity;
         this.capacity = capacity;
@@ -46,16 +46,40 @@ public class IntIntPairArray
 
     /**
      * Add a pair to the array.
-     * @param x the first value
-     * @param y the second value
+     * @param segmentRowId the first value
+     * @param ordinal the second value
      */
-    public void add(int x, int y)
+    public void add(int segmentRowId, int ordinal)
     {
         if (size == capacity)
-            throw new IndexOutOfBoundsException(size);
-        array[size * 2] = x;
-        array[size * 2 + 1] = y;
+            throw new ArrayIndexOutOfBoundsException(size);
+        array[size * 2] = segmentRowId;
+        array[size * 2 + 1] = ordinal;
         size++;
+    }
+
+    /**
+     * Get the row id at the given index.
+     * @param index the index
+     * @return the row id
+     */
+    public int getSegmentRowId(int index)
+    {
+        if ( index < 0 || index >= size)
+            throw new ArrayIndexOutOfBoundsException(index);
+        return array[index * 2];
+    }
+
+    /**
+     * Get the ordinal at the given index.
+     * @param index the index
+     * @return the ordinal
+     */
+    public int getOrdinal(int index)
+    {
+        if ( index < 0 || index >= size)
+            throw new ArrayIndexOutOfBoundsException(index);
+        return array[index * 2 + 1];
     }
 
     /**
@@ -68,20 +92,31 @@ public class IntIntPairArray
     }
 
     /**
-     * Iterate over the pairs in the array, calling the consumer for each pair.
+     * Iterate over the pairs in the array, calling the consumer for each pair passing (index, x, y).
      * @param consumer the consumer to call for each pair
      */
-    public void forEachIntPair(IntIntConsumer consumer)
+    public void forEachSegmentRowIdOrdinalPair(IntIntConsumer consumer)
     {
         for (int i = 0; i < size; i++)
             consumer.accept(array[i * 2], array[i * 2 + 1]);
     }
 
     /**
+     * Iterate over the pairs in the array, calling the consumer for each pair passing (index, x, y).
+     * @param consumer the consumer to call for each pair
+     */
+    public void forEachIndexOrdinalPair(IntIntConsumer consumer)
+    {
+        for (int i = 0; i < size; i++)
+            consumer.accept(i, array[i * 2 + 1]);
+    }
+
+
+    /**
      * Calls the consumer for each right value in each pair of the array.
      * @param consumer the consumer to call for each right value
      */
-    public void forEachRightInt(IntConsumer consumer)
+    public void forEachOrdinal(IntConsumer consumer)
     {
         for (int i = 0; i < size; i++)
             consumer.accept(array[i * 2 + 1]);
