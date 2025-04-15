9a495b0626d27925daadfa9bd0ec19335b42e438
Ruslan Fomkin <ruslan.fomkin@datastax.com>
2025-04-15 16:53:32 +0200

    CNDB-13696 fix empty iterator access in BM25 search on partial SSTable (#1691)
    Executing a query with BM25 search and a condition on partial SSTable
    results in empty iterator access error. And there was no test with
    storing data in segments.
    
    The PR implements BM25 search tests with splitting data into two tables.
    This reproduced this bug, CNDB-13696, and demonstrates current confusion
    on the BM25 ordering result to be fixed by CNDB-13553.
    
    This PR adds a check for empty iterator created for a PK belonging to
    another segment. This fixes the bug of trying to get the first element
    of an empty iterator.
    
    (cherry picked from commit 167a98c35a4ccef7f70f508dd22c889d03f35685)

--- src/java/org/apache/cassandra/index/sai/disk/v1/InvertedIndexSearcher.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/InvertedIndexSearcher.java
@@ -164,6 +164,9 @@ public class InvertedIndexSearcher extends IndexSearcher
         var slices = Slices.with(indexContext.comparator(), Slice.make(primaryKey.clustering()));
         try (var rowIterator = sstable.iterator(dk, slices, columnFilter, false, SSTableReadsListener.NOOP_LISTENER))
         {
+            // primaryKey might not belong to this sstable, thus the iterator will be empty
+            if (rowIterator.isEmpty())
+                return null;
             var unfiltered = rowIterator.next();
             assert unfiltered.isRow() : unfiltered;
             Row row = (Row) unfiltered;
