fa4fdb96523b6d7230f10ca1c85289cece20c2c9
Szymon Miężał <szymon.miezal@datastax.com>
2025-04-15 13:25:24 +0200

    Port the test the verifies the encrypted sstables are different from unencrypted
    Also port a test that verifies reading data without the key doesn't work.

--- test/distributed/org/apache/cassandra/distributed/test/EncryptSSTableTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/EncryptSSTableTest.java
@@ -19,51 +19,35 @@
 package org.apache.cassandra.distributed.test;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
 import javax.crypto.NoSuchPaddingException;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.datastax.driver.core.Row;
-import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.crypto.LocalSystemKey;
 import org.apache.cassandra.crypto.TDEConfigurationProvider;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.distributed.Cluster;
 import org.apache.cassandra.distributed.api.ConsistencyLevel;
-import org.apache.cassandra.distributed.api.IInvokableInstance;
-import org.apache.cassandra.gms.FailureDetector;
-import org.apache.cassandra.gms.Gossiper;
-import org.apache.cassandra.gms.IFailureDetector;
 import org.apache.cassandra.io.sstable.Component;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
-import org.apache.cassandra.net.MessagingService;
-import org.apache.cassandra.repair.SystemDistributedKeyspace;
-import org.apache.cassandra.schema.SchemaConstants;
-import org.apache.cassandra.service.StorageProxy;
-import org.apache.cassandra.service.StorageService;
-import org.apache.cassandra.utils.ByteBufferUtil;
 
 import static org.apache.cassandra.distributed.api.ConsistencyLevel.ALL;
 import static org.apache.cassandra.distributed.api.Feature.GOSSIP;
 import static org.apache.cassandra.distributed.api.Feature.NETWORK;
 import static org.apache.cassandra.distributed.shared.AssertUtils.assertRows;
 import static org.apache.cassandra.distributed.shared.AssertUtils.row;
+import static org.apache.cassandra.distributed.shared.FutureUtils.waitOn;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.junit.Assert.assertTrue;
 
 public class EncryptSSTableTest extends TestBaseImpl
 {
@@ -137,6 +121,112 @@ public class EncryptSSTableTest extends TestBaseImpl
             assertThat(byIdRows[0][1]).isEqualTo(String.valueOf(2));
             assertThat(byIdRows[0][2]).isEqualTo(String.valueOf(2));
         }
+    }
+
+    @Test
+    public void shouldVerifyUnencryptedSSTablesDifferFromEncryptedOnes() throws Exception
+    {
+        try (Cluster cluster = builder().withNodes(1)
+                                        .withConfig(config -> config.with(GOSSIP).with(NETWORK))
+                                        .start())
+        {
+            // given tables with and without encryption
+            String keyspace = createKeyspace(cluster);
+            TestTable nonEncryptedTable1 = createTableWithSampleData(cluster, keyspace, "");
+            TestTable nonEncryptedTable2 = createTableWithSampleData(cluster, keyspace, "");
+            Path secretKey = createLocalSecretKey();
+            TestTable encryptedTable = createTableWithSampleData(cluster, keyspace, localSystemKeyEncryptionCompressionSuffix("Encryptor", secretKey.toAbsolutePath().toString()));
+
+            // then
+            // tables without encryption should have the same bytes
+            assertThat(nonEncryptedTable1.sstablePath).isNotEqualTo(nonEncryptedTable2.sstablePath);
+            assertThat(nonEncryptedTable1.partitionIndexPath).isNotEqualTo(nonEncryptedTable2.partitionIndexPath);
+            assertThat(nonEncryptedTable1.rowIndexPath).isNotEqualTo(nonEncryptedTable2.rowIndexPath);
+            assertThat(nonEncryptedTable1.sstableBytes).isEqualTo(nonEncryptedTable2.sstableBytes);
+            assertThat(nonEncryptedTable1.partitionIndexBytes).isEqualTo(nonEncryptedTable2.partitionIndexBytes);
+            assertThat(nonEncryptedTable1.rowIndexBytes).isEqualTo(nonEncryptedTable2.rowIndexBytes);
+            // table with encryption should have different bytes
+            assertThat(encryptedTable.sstablePath).isNotEqualTo(nonEncryptedTable1.sstablePath);
+            assertThat(encryptedTable.partitionIndexPath).isNotEqualTo(nonEncryptedTable1.partitionIndexPath);
+            assertThat(encryptedTable.rowIndexPath).isNotEqualTo(nonEncryptedTable1.rowIndexPath);
+            assertThat(encryptedTable.sstableBytes).isNotEqualTo(nonEncryptedTable1.sstableBytes);
+            assertThat(encryptedTable.rowIndexBytes).isNotEqualTo(nonEncryptedTable1.rowIndexBytes);
+            assertThat(encryptedTable.partitionIndexBytes).isNotEqualTo(nonEncryptedTable1.partitionIndexBytes);
+        }
+    }
+
+
+    @Test
+    public void shouldNotReadRowsFromEncryptedTableWithoutTheSecretKey() throws Exception
+    {
+        try (Cluster cluster = builder().withNodes(1)
+                                        .withConfig(config -> config.with(GOSSIP).with(NETWORK))
+                                        .start())
+        {
+            // ignore throwing an exception when closing the cluster as missing key will result in exceptions in logs
+            cluster.setUncaughtExceptionsFilter(t -> true);
+
+            // given a table with data encrypted using local key
+            String keyspace = createKeyspace(cluster);
+            Path secretKey = createLocalSecretKey();
+            String encryptedTableName = createEncryptedTable(cluster, keyspace, secretKey);
+            String nonEncryptedTableName = createTable(cluster, keyspace);
+            int numberOfRows = 10;
+            insertAndFlush(cluster, keyspace, encryptedTableName, numberOfRows);
+            insertAndFlush(cluster, keyspace, nonEncryptedTableName, numberOfRows);
+
+            // delete secret key file
+            assertTrue("secret key should be deleted", Files.deleteIfExists(secretKey));
+
+            // restart to clear in memory secret key cache
+            waitOn(cluster.get(1).shutdown());
+            cluster.get(1).startup();
+
+            // when
+            Object[][] rows = cluster. get(1).executeInternal(String.format("SELECT * FROM %s.%s", keyspace, nonEncryptedTableName));
+            Throwable throwable = catchThrowable(() -> cluster.get(0).executeInternal(String.format("SELECT * FROM %s.%s ", keyspace, encryptedTableName)));
+
+            // then it should be possible to read the table without encryption
+            assertThat(rows.length).isEqualTo(numberOfRows);
+            // then it should not be possible to read the encrypted table
+            assertThat(throwable).isInstanceOf(IndexOutOfBoundsException.class);
+        }
+    }
+
+    private TestTable createTableWithSampleData(Cluster cluster, String keyspace, String tableDefSuffix) throws IOException
+    {
+        String tableName = randomTableName();
+        String createTableCql = "CREATE TABLE %s.%s (id text, cc text, value text, PRIMARY KEY ((id), cc))" + tableDefSuffix;
+        cluster.schemaChange(String.format(createTableCql, keyspace, tableName));
+        cluster.coordinator(1).execute(String.format("INSERT INTO %s.%s (id, cc, value) VALUES ('%s', '%s', '%s')", keyspace, tableName, 0, 0, 0), ALL);
+        assertRows(cluster.coordinator(1).execute(String.format("SELECT * FROM %s.%s ", keyspace, tableName), ALL), row("0", "0", "0"));
+        // flush to make sure we have sstable
+        cluster.get(1).flush(keyspace);
+
+        List<String> sstablePaths = getPathsFor(cluster, keyspace, tableName, Component.DATA);
+        List<String> partitionIndexPaths = getPathsFor(cluster, keyspace, tableName, Component.PARTITION_INDEX);
+        List<String> rowIndexPaths = getPathsFor(cluster, keyspace, tableName, Component.ROW_INDEX);
+
+        String sstablePath = sstablePaths.get(0);
+        byte[] sstableBytes = Files.readAllBytes(Path.of(sstablePath));
+
+        String partitionIndexPath = partitionIndexPaths.get(0);
+        byte[] partitionIndexBytes = Files.readAllBytes(Path.of(partitionIndexPath));
+
+        String rowIndexPath = rowIndexPaths.get(0);
+        byte[] rowIndexBytes = Files.readAllBytes(Path.of(rowIndexPath));
+
+        return new TestTable(tableName, sstableBytes, sstablePath, partitionIndexBytes, partitionIndexPath, rowIndexBytes, rowIndexPath);
+    }
+
+    private List<String> getPathsFor(Cluster cluster, String keyspace, String tableName, Component component)
+    {
+        String componentString = component.toString();
+        return cluster.get(1).callOnInstance(() -> Keyspace.open(keyspace).getColumnFamilyStore(tableName).getLiveSSTables()
+                                             .stream()
+                                             .map(SSTableReader::getDescriptor)
+                                             .map(d -> d.filenameFor(Component.parse(componentString)))
+                                             .collect(Collectors.toList()));
     }
 
     private String createKeyspace(Cluster cluster)
@@ -217,5 +307,4 @@ public class EncryptSSTableTest extends TestBaseImpl
             this.rowIndexBytes = rowIndexBytes;
         }
     }
-
 }
