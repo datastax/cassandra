8510d0a064ae327016cb732676c560fd99db20bd
Brandon Williams <brandonwilliams@apache.org>
2025-04-17 13:55:28 -0500

    CC5: IndexStatusManagerTest accomodates INDEX_BUILD_IN_PROGRESS
     (Rebase of commit b0d250565137ccb1eda5cb651105af2cd814ba5f)
    
     (Rebase of commit ed79630dcd49efb25335006b4c1bb8bd7c2ac5a9)

---
+++ test/unit/org/apache/cassandra/io/sstable/CQLSSTableWriterSAIDaemonTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.io.sstable;
+
+import org.junit.BeforeClass;
+
+import org.apache.cassandra.SchemaLoader;
+import org.apache.cassandra.config.Config;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.db.Keyspace;
+import org.apache.cassandra.db.commitlog.CommitLog;
+import org.apache.cassandra.dht.Murmur3Partitioner;
+import org.apache.cassandra.service.StorageService;
+
+/**
+ * The test/conf/cassandra.yaml uses the ByteOrderedPartitioner, but SAI does npt support it, so
+ * in this test we use the Murmur3Partitioner.
+ */
+public class CQLSSTableWriterSAIDaemonTest extends CQLSSTableWriterSAITest
+{
+    @BeforeClass
+    public static void setup() throws Exception
+    {
+        DatabaseDescriptor.daemonInitialization(() -> {
+            Config config = DatabaseDescriptor.loadConfig();
+            config.partitioner = Murmur3Partitioner.class.getName();
+            return config;
+        });
+        CommitLog.instance.start();
+        SchemaLoader.cleanupAndLeaveDirs();
+        Keyspace.setInitialized();
+        StorageService.instance.initServer();
+    }
+}
