47f2c2a125f8c9538cb1b289ccb8ef4a12f194ff
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2025-04-23 09:42:23 -0400

    CNDB-13833: Fix QueryTimeoutTest on JDK22

--- test/unit/org/apache/cassandra/inject/Injections.java
+++ test/unit/org/apache/cassandra/inject/Injections.java
@@ -612,7 +612,7 @@ public class Injections
         public PauseBuilder(String name, long timeout)
         {
             super(String.format("pause/%s/%s", name, UUID.randomUUID()));
-            actionBuilder.actions().doAction(expr("Thread.sleep").args(timeout));
+            actionBuilder.actions().doAction(expr("Thread.sleep").args(timeout+"L"));
         }
 
         /**
