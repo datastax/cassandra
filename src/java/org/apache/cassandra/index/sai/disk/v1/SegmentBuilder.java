2c8edf9296473b18f659566090b6acdae75f36b3
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2025-05-06 21:33:41 +0200

    CNDB-13905 provide total point count to BKDWriter
    Renames confusing numRows arguments to totalPointCount in methods
    propagating the value to BKDWriter, which expects the total point
    count. Fixes to provide total point counts instead of row counts.
    
    In the most of tests the point count and the row count are the
    equivalent.
    One unused test is removed.
    
     (Rebase of commit 204044e2ca78464af375a4ae2826771dacde4cd9)
    
     (Rebase of commit b6ca993e350bc0de9eb03ff830b4bf6fcbe9a933)

--- src/java/org/apache/cassandra/index/sai/disk/v1/SegmentBuilder.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/SegmentBuilder.java
@@ -169,7 +169,7 @@ public abstract class SegmentBuilder
             try (NumericIndexWriter writer = new NumericIndexWriter(components,
                                                                     TypeUtil.fixedSizeOf(termComparator),
                                                                     maxSegmentRowId,
-                                                                    rowCount,
+                                                                    kdTreeRamBuffer.numPoints(),
                                                                     indexWriterConfig))
             {
 
