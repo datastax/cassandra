6c150e56ab89c5e08a8a142eb0dee75db842b358
Michael Marshall <michael.marshall@datastax.com>
2025-05-06 12:58:30 -0500

    CNDB-13693: Make SAI's view referenceable to simplify locking query view (#1700)
    Fixes: https://github.com/riptano/cndb/issues/13693
    
    The new design is to:
    
    1. Make sai `View` reference-able so that it holds references to the
    underlying `SSTableIndex`s. When the `view` is released the final time,
    it releases the indexes. This moves all of the complexity of grabbing
    references to sstable update time and out of the query path, which seems
    like a generally good improvement.
    2. Observe that `SSTableIndex` holds a reference to its associated
    sstable reader.
    
    Note also that we need to create the memtable index on deletions in
    order to make the index-first solution work.
    
    Also note that sstables indexes are added before they are removed on
    flush, so this change in design is safe for flush.
    
     (Rebase of commit c42a2dedb24838f6a76e42f056d4097583a3113d)
    
     (Rebase of commit fe6489e664d08bc7946ffd2ae8c8b018c8ba7b49)

--- src/java/org/apache/cassandra/index/sai/plan/StorageAttachedIndexSearcher.java
+++ src/java/org/apache/cassandra/index/sai/plan/StorageAttachedIndexSearcher.java
@@ -504,7 +504,7 @@ public class StorageAttachedIndexSearcher implements Index.Searcher
      */
     public class ScoreOrderedResultRetriever extends AbstractIterator<UnfilteredRowIterator> implements UnfilteredPartitionIterator
     {
-        private final ColumnFamilyStore.RefViewFragment view;
+        private final ColumnFamilyStore.ViewFragment view;
         private final List<AbstractBounds<PartitionPosition>> keyRanges;
         private final boolean coversFullRing;
         private final CloseableIterator<PrimaryKeyWithSortKey> scoredPrimaryKeyIterator;
@@ -531,7 +531,7 @@ public class StorageAttachedIndexSearcher implements Index.Searcher
                                             ColumnMetadata orderedColumn)
         {
             IndexContext context = controller.getOrderer().context;
-            this.view = controller.getQueryView(context).view;
+            this.view = controller.getQueryView(context).viewFragment;
             this.keyRanges = controller.dataRanges().stream().map(DataRange::keyRange).collect(Collectors.toList());
             this.coversFullRing = keyRanges.size() == 1 && RangeUtil.coversFullRing(keyRanges.get(0));
 
