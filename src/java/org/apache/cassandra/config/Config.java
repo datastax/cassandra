696847dd99403d2a7da1c3aa22d8bb635f59ab9b
Jakub Zytka <jakub.zytka@datastax.com>
2025-05-09 16:13:14 +0200

    aggregate query timeout config option

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -103,6 +103,8 @@ public class Config
 
     public volatile long range_request_timeout_in_ms = 10000L;
 
+    public volatile long aggregated_request_timeout_in_ms = 120000L;
+
     public volatile long write_request_timeout_in_ms = 2000L;
 
     public volatile long hints_request_timeout_in_ms = 2000L;
