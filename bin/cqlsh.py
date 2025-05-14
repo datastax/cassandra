c694d2d71d5d7dab59e79cf8a6e8b43df82f724c
Tiago Laureano Alves <tiago.alves@datastax.com>
2025-05-14 10:41:51 +0100

    HCD-110 Add plugin support for CQLSH (#1722)
    HCD requires custom authenticators enabled via a cqlsh plugin.
    
    Backports [CASSANDRA-16456](https://issues.apache.org/jira/browse/CASSANDRA-16456)
    to add support for `cqlsh` plugins.
    Adds `datastax_db_*-VERSION.zip` to the list of automatically loaded plugins (used by HCD only).
    
    ---------
    
    Co-authored-by: Bhouse99 <bhouse99@protonmail.com>
    Co-authored-by: Stefan Miklosovic <smiklosovic@apache.org>
    
     (Rebase of commit 25d248f5845918cf3ad1c803f40a79147dae26f3)
    
     (Rebase of commit f2a0b549583cb367e60a011abdd8e70d09e64177)

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -56,7 +56,7 @@ if cql_zip:
     sys.path.insert(0, os.path.join(cql_zip, 'cassandra-driver-' + ver))
 
 # the driver needs dependencies
-third_parties = ('pure_sasl-', 'wcwidth-', 'pyasyncore-')
+third_parties = ('pure_sasl-', 'wcwidth-', 'pyasyncore-', 'datastax_db_*-')
 
 for lib in third_parties:
     lib_zip = find_zip(lib)
