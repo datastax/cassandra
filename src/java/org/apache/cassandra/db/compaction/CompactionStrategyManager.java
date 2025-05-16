msmygit/vscassandra

STAR-410: Define compaction metrics and make them easily accessible on Fallout (#101)
* STAR-410 Refactor compaction metrics

Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

* STAR-410 Rename CompactionInfo.Holder to AbstractTableOperation and CompactionInfo to AbstractTableOperation.Progress

CompactionInfo.Holder was implemented by all table operations, not just compactions. For example index and view building
were reported as compactions. The new naming reflects more correctly the role of this class, which is to report progress or
interrupt these table operations.

CompactionInfo was a basic class capable of reporting the progress of an operation, for example how many bytes (or keys, or ranges)
have been currently processed out of a total number of bytes (or keys, or ranges).

The new names should remove the previous confusion, and allow adding further compaction progress statistics.

Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

* STAR-410 More refactoring of compaction-related code:

- Added CompactionProgress and refactored CompactionTask into CompactionOperation
- Extracted interfaces from AbstractTableOperation and its progress
  inner-class, so that they can be extended by CompactionProgress
- Added compactions in progress to the abstract strategy for a basic
  aggregation of compactions in progress, to be extended further

Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

* STAR-410 New aggregated statistics for compaction strategies added to nodetool compactionstats and compaction logger

 Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
 Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

* STAR-410 Rework compaction strategy code to remove repetition and c

src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
@@ -176,7 +176,11 @@ public class CompactionStrategyManager implements INotificationConsumer
         this.partitionSSTablesByTokenRange = partitionSSTablesByTokenRange;
         params = cfs.metadata().params.compaction;
         enabled = params.isEnabled();
-        reload(cfs.metadata().params.compaction);
+    }
+
+    CompactionLogger compactionLogger()
+    {
+        return compactionLogger;
     }
 
     /**
@@ -253,7 +257,7 @@ public class CompactionStrategyManager implements INotificationConsumer
             if (txn != null)
             {
                 logger.debug("Running automatic sstable upgrade for {}", sstable);
-                return getCompactionStrategyFor(sstable).getCompactionTask(txn, Integer.MIN_VALUE, Long.MAX_VALUE);
+                return getCompactionStrategyFor(sstable).createCompactionTask(txn, Integer.MIN_VALUE, Long.MAX_VALUE);
             }
         }
         return null;
@@ -470,6 +474,22 @@ public class CompactionStrategyManager implements INotificationConsumer
     }
 
     /**
+     * Version of the above forcing the strategy to always be reloaded. Used by tests that need to clear the state.
+     */
+    public void forceReload()
+    {
+        writeLock.lock();
+        try
+        {
+            reload(schemaCompactionParams);
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    /**
      * Checks if the disk boundaries changed and reloads the compaction strategies
      * to reflect the most up-to-date disk boundaries.
      *
@@ -506,7 +526,7 @@ public class CompactionStrategyManager implements INotificationConsumer
      * Called after changing configuration and at startup.
      * @param newCompactionParams
      */
-    private void reload(CompactionParams newCompactionParams)
+    public void reload(CompactionParams newCompactionParams)
     {
         boolean enabledWithJMX = enabled && !shouldBeEnabled();
         boolean disabledWithJMX = !enabled && shouldBeEnabled();
@@ -884,7 +904,7 @@ public class CompactionStrategyManager implements INotificationConsumer
         try
         {
             validateForCompaction(txn.originals());
-            return compactionStrategyFor(txn.originals().iterator().next()).getCompactionTask(txn, gcBefore, maxSSTableBytes);
+            return compactionStrategyFor(txn.originals().iterator().next()).createCompactionTask(txn, gcBefore, maxSSTableBytes);
         }
         finally
         {
@@ -1002,7 +1022,14 @@ public class CompactionStrategyManager implements INotificationConsumer
 
     public List<List<AbstractCompactionStrategy>> getStrategies()
     {
-        maybeReloadDiskBoundaries();
+        return getStrategies(true);
+    }
+
+    private List<List<AbstractCompactionStrategy>> getStrategies(boolean checkBoundaries)
+    {
+        if (checkBoundaries)
+            maybeReloadDiskBoundaries();
+
         readLock.lock();
         try
         {
@@ -1014,6 +1041,18 @@ public class CompactionStrategyManager implements INotificationConsumer
         {
             readLock.unlock();
         }
+    }
+
+    /**
+     * @return the statistics for the compaction strategies that have compactions in progress or pending
+     */
+    public List<CompactionStrategyStatistics> getStrategyStatistics()
+    {
+        return getStrategies(false).stream()
+                                   .flatMap(list -> list.stream())
+                                   .filter(strategy -> strategy.getTotalCompactions() > 0)
+                                   .map(AbstractCompactionStrategy::getStatistics)
+                                   .collect(Collectors.toList());
     }
 
     public void setNewLocalCompactionStrategy(CompactionParams params)

datastax/cassandra

(cherry picked from commit 29500e962943a0c582595e42f519b3e7fc1ec9d6)
(cherry picked from commit dcb9cce8d17ceb4ed8e70cfd6e6bf76bdf95b94c)
(cherry picked from commit 3d134e119bfe3d8801f7a7b433dcad60058c69c1)
(cherry picked from commit 41b49c90fee98d5de4650870c6f19874ee5eda42)
(cherry picked from commit fc0a7eb0954dbea7583525ef037d7bead66a353f)
STAR-410 Fix compile errors;
Remove unused imports;
gcBefore is a long in C* 5.0;
Use TimeUUID in place of UUID;
Use o.a.c.io.util.File instead of java.io.File;
Use ExecutorPlus instead of ExecutorService;
Use Clock.Global time methods instead of System;
Replace use of CompactionInfo with OperationProgress;
Keep targetDirectory field from C* 5.0 in AbstractTableOperation;
Keep estimagedRemainingWriteBytes from C* 5.0 in AbstractTableOperation and ActiveOperations;
Implement abstract methods replaceSSTables and removeDeadSSTables in UnifiedCompactionStrategy;
STAR-410 Fix broken tests: call CompactionStrategyManager.reloadParamsFromSchema instead of maybeReloadParamsFromSchema in ColumnFamilyStore constructor
STAR-410 Fix CompactionStatsTest

src/java/org/apache/cassandra/db/compaction/CompactionStrategyManager.java
@@ -185,8 +185,11 @@ public class CompactionStrategyManager implements INotificationConsumer
         currentBoundaries = boundariesSupplier.get();
         params = schemaCompactionParams = cfs.metadata().params.compaction;
         enabled = params.isEnabled();
-        setStrategy(schemaCompactionParams);
-        startup();
+    }
+
+    CompactionLogger compactionLogger()
+    {
+        return compactionLogger;
     }
 
     /**
@@ -262,7 +265,7 @@ public class CompactionStrategyManager implements INotificationConsumer
             if (txn != null)
             {
                 logger.debug("Running automatic sstable upgrade for {}", sstable);
-                return getCompactionStrategyFor(sstable).getCompactionTask(txn, Integer.MIN_VALUE, Long.MAX_VALUE);
+                return getCompactionStrategyFor(sstable).createCompactionTask(txn, Integer.MIN_VALUE, Long.MAX_VALUE);
             }
         }
         return null;
@@ -496,7 +499,7 @@ public class CompactionStrategyManager implements INotificationConsumer
     /**
      * @param newParams new CompactionParams set in via CQL
      */
-    private void reloadParamsFromSchema(CompactionParams newParams)
+    public void reloadParamsFromSchema(CompactionParams newParams)
     {
         logger.debug("Recreating compaction strategy for {}.{} - compaction parameters changed via CQL",
                      cfs.getKeyspaceName(), cfs.getTableName());
@@ -533,6 +536,22 @@ public class CompactionStrategyManager implements INotificationConsumer
         {
             if (!params.equals(this.params))
                 reloadParamsFromJMX(params);
+        }
+        finally
+        {
+            writeLock.unlock();
+        }
+    }
+
+    /**
+     * Version of the above forcing the strategy to always be reloaded. Used by tests that need to clear the state.
+     */
+    public void forceReload()
+    {
+        writeLock.lock();
+        try
+        {
+            reloadParamsFromSchema(schemaCompactionParams);
         }
         finally
         {
@@ -1037,7 +1056,7 @@ public class CompactionStrategyManager implements INotificationConsumer
         try
         {
             validateForCompaction(txn.originals());
-            return compactionStrategyFor(txn.originals().iterator().next()).getCompactionTask(txn, gcBefore, maxSSTableBytes);
+            return compactionStrategyFor(txn.originals().iterator().next()).createCompactionTask(txn, gcBefore, maxSSTableBytes);
         }
         finally
         {
@@ -1198,7 +1217,14 @@ public class CompactionStrategyManager implements INotificationConsumer
 
     public List<List<AbstractCompactionStrategy>> getStrategies()
     {
-        maybeReloadDiskBoundaries();
+        return getStrategies(true);
+    }
+
+    private List<List<AbstractCompactionStrategy>> getStrategies(boolean checkBoundaries)
+    {
+        if (checkBoundaries)
+            maybeReloadDiskBoundaries();
+
         readLock.lock();
         try
         {
@@ -1212,7 +1238,19 @@ public class CompactionStrategyManager implements INotificationConsumer
         }
     }
 
-    public void overrideLocalParams(CompactionParams params)
+    /**
+     * @return the statistics for the compaction strategies that have compactions in progress or pending
+     */
+    public List<CompactionStrategyStatistics> getStrategyStatistics()
+    {
+        return getStrategies(false).stream()
+                                   .flatMap(list -> list.stream())
+                                   .filter(strategy -> strategy.getTotalCompactions() > 0)
+                                   .map(AbstractCompactionStrategy::getStatistics)
+                                   .collect(Collectors.toList());
+    }
+
+    public void setNewLocalCompactionStrategy(CompactionParams params)
     {
         logger.info("Switching local compaction strategy from {} to {}", this.params, params);
         maybeReloadParamsFromJMX(params);