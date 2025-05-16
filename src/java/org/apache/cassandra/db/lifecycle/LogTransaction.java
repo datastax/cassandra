msmygit/vscassandra

STAR-1242 STAR-1243 Extend compaction pick for CNBD need (#413)
This patch allows to set the transaction UUID directly when a
compaction pick is created, which means that in CNDB we can use it as a
key for compaction tasks, making debugging of compactions much easier
(since we can grep the same UUID). Currently, we're using the name of
one of the sstables as the unique key of compaction tasks, but this is
problematic when analyzing the logs, especially after a restart.

Also, the transaction UUID was already a property of the compaction
pick, but it was nullable and volatile. It is now final, which is an
improvement, and a boolean now signals whether the pick has been
submitted or not.

Port of CNDB-2291 and of minor part from CNDB-3376

Co-authored-by: Stefania Alborghetti

src/java/org/apache/cassandra/db/lifecycle/LogTransaction.java
@@ -30,6 +30,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,7 +50,6 @@ import org.apache.cassandra.io.util.File;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.utils.Throwables;
-import org.apache.cassandra.utils.UUIDGen;
 import org.apache.cassandra.utils.concurrent.Ref;
 import org.apache.cassandra.utils.concurrent.RefCounted;
 
@@ -117,9 +117,12 @@ final class LogTransaction extends AbstractLogTransaction
     // will be recognized as GCable.
     protected static final Queue<Runnable> failedDeletions = new ConcurrentLinkedQueue<>();
 
-    LogTransaction(OperationType opType)
+    LogTransaction(OperationType opType, UUID uuid)
     {
-        this.txnFile = new LogFile(opType, UUIDGen.getTimeUUID());
+        Preconditions.checkNotNull(opType);
+        Preconditions.checkNotNull(uuid);
+
+        this.txnFile = new LogFile(opType, uuid);
         this.lock = new Object();
         this.selfRef = new Ref<>(this, new TransactionTidier(txnFile, lock));
 

datastax/cassandra

(cherry picked from commit d0265d09e0df0815e764e3fa01d2ebb056439349)
(cherry picked from commit e0e6964f52c052a0872b7fccf21e546e5f516e40)
(cherry picked from commit f7e356dd3b2011a1f62da3e7d247788e8cef7dea)
(cherry picked from commit b3fcb8d322091d571a5f494b3a725dc5b7f69d64)
# Conflicts:
#	src/java/org/apache/cassandra/db/ColumnFamilyStore.java
#	src/java/org/apache/cassandra/db/compaction/CompactionPick.java
#	src/java/org/apache/cassandra/db/compaction/DateTieredCompactionStrategy.java
#	src/java/org/apache/cassandra/db/lifecycle/LifecycleTransaction.java
#	src/java/org/apache/cassandra/db/lifecycle/LogTransaction.java

src/java/org/apache/cassandra/db/lifecycle/LogTransaction.java
@@ -26,14 +26,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import com.codahale.metrics.Counter;
 import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
-
+import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.codahale.metrics.Counter;
 import org.apache.cassandra.concurrent.ScheduledExecutors;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.db.SystemKeyspace;
@@ -51,8 +51,6 @@ import org.apache.cassandra.utils.Throwables;
 import org.apache.cassandra.utils.TimeUUID;
 import org.apache.cassandra.utils.concurrent.Ref;
 import org.apache.cassandra.utils.concurrent.RefCounted;
-
-import static org.apache.cassandra.utils.TimeUUID.Generator.nextTimeUUID;
 
 /**
  * IMPORTANT: When this object is involved in a transactional graph, and is not encapsulated in a LifecycleTransaction,
@@ -117,9 +115,12 @@ final class LogTransaction extends AbstractLogTransaction
     // will be recognized as GCable.
     protected static final Queue<Runnable> failedDeletions = new ConcurrentLinkedQueue<>();
 
-    LogTransaction(OperationType opType)
+    LogTransaction(OperationType opType, TimeUUID uuid)
     {
-        this.txnFile = new LogFile(opType, nextTimeUUID());
+        Preconditions.checkNotNull(opType);
+        Preconditions.checkNotNull(uuid);
+
+        this.txnFile = new LogFile(opType, uuid);
         this.lock = new Object();
         this.selfRef = new Ref<>(this, new TransactionTidier(txnFile, lock));
 