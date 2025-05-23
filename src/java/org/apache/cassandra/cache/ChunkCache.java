f8db20ceff25dfae066603a5ddab37e606a32528
Szymon Miężał <93650032+szymon-miezal@users.noreply.github.com>
2025-05-23 09:36:18 +0200

    DSP-23657: Introduce sstable encryption (#1669)
    ### What is the issue
    The sstable components like `*Data.db`, `*Rows.db`, `*Partitions.db`,
    `*Statistics.db` contain sensitive data that the customer may want to
    protect with encryption.
    
    ### What does this PR fix and why was it fixed
    This patch adds support for encrypting SSTable data, indexes (partition
    and row), and metadata.
    
    Data encryption is integrated via the Cassandra compression framework.
    
    Index and metadata encryption is implemented by updating their
    respective readers and writers.
    
    It is achieved by implementing a special EncryptedSequentialWriter that
    encrypts each chunk before writing, and an EncryptedChunkReader that
    decrypts the chunks when reading. To enable this, the usable space in
    each chunk is reduced by the size of the encryption metadata plus 8
    bytes used to store the CRC and actual chunk length.
    
    This design avoids the need to write and keep in memory a compressed
    offsets map for the indices, since each chunk's offset remains the same
    before and after compression.
    
    This works well for trie data, but index chunks may also include other
    data such as keys, offsets, and deletion times, which may not fit
    entirely in a chunk. To handle this, the writer supports data that
    spans chunk boundaries, splitting it as needed.
    
    The random access reader is updated to skip encryption metadata during
    reads using a new RebuffererFactory.adjustPosition() method.
    
    Encryption is enabled by altering the table to specify an encryptable
    compressor.
    
    Encryption keys are read from the local file system, under the directory
    specified by the JVM flag `cassandra.system_key_directory` (default:
    `/etc/cassandra/conf`).
    
    Note: Support for generating encryption keys will be introduced in a
    future patch.
    
    ---------
    
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

--- src/java/org/apache/cassandra/cache/ChunkCache.java
+++ src/java/org/apache/cassandra/cache/ChunkCache.java
@@ -549,7 +549,7 @@ public class ChunkCache
         public ByteBuffer buffer()
         {
             assert isReferenced() : "Already unreferenced";
-            return buffer.duplicate();
+            return buffer.duplicate().limit(bytesRead);
         }
 
         public long offset()
@@ -687,6 +687,12 @@ public class ChunkCache
         {
             long pageAlignedPos = position & alignmentMask;
             synchronousCache.invalidate(new Key(readerId, pageAlignedPos));
+        }
+
+        @Override
+        public long adjustPosition(long position)
+        {
+            return source.adjustPosition(position);
         }
 
         @Override
