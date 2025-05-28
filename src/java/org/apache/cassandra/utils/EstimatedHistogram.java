f7a70ff16221d76f2752e6fc94efb8000315bed3
Jakub Żytka <jakub.zytka@gmail.com>
2025-05-28 12:41:08 +0200

    CNDB-13617: EstimatedHistogram uses C* bucket boundaries regardless of the DSE-compatibility flag (#1725)
    ### What is the issue
    When using `use_dse_compatible_histogram_boundaries` flag we by mistake
    use DSE DecayingEstimatedHistogramReservoir bucket boundaries for
    `EstimatedHistogram`. This may lead to unexpected and unhandled
    histogram overflow for extremely large partitions.
    
    ### What does this PR fix and why was it fixed
    This fix makes `EstimatedHistogram` use, by default, the same bucket
    boundaries as in upstream Cassandra and DSE `EstimatedHistogram`

--- src/java/org/apache/cassandra/utils/EstimatedHistogram.java
+++ src/java/org/apache/cassandra/utils/EstimatedHistogram.java
@@ -36,7 +36,6 @@ import org.apache.cassandra.metrics.DecayingEstimatedHistogramReservoir;
 public class EstimatedHistogram
 {
     public static final EstimatedHistogramSerializer serializer = new EstimatedHistogramSerializer();
-    public static final boolean USE_DSE_COMPATIBLE_HISTOGRAM_BOUNDARIES = CassandraRelevantProperties.USE_DSE_COMPATIBLE_HISTOGRAM_BOUNDARIES.getBoolean();
 
     /**
      * The series of values to which the counts in `buckets` correspond:
@@ -91,10 +90,7 @@ public class EstimatedHistogram
 
     public static long[] newOffsets(int size, boolean considerZeroes)
     {
-        if (USE_DSE_COMPATIBLE_HISTOGRAM_BOUNDARIES)
-            return DecayingEstimatedHistogramReservoir.newDseOffsets(size, considerZeroes);
-        else
-            return newCassandraOffsets(size, considerZeroes);
+        return newCassandraOffsets(size, considerZeroes);
     }
 
     public static long[] newCassandraOffsets(int size, boolean considerZeroes)
