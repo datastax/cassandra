6070503c21cacaf45360076b4a5d08ad6746cbde
Sylvain Lebresne <lebresne@gmail.com>
2025-05-28 16:32:05 +0200

    CNDB-14123: allows intercepting issues opening flushed sstables (#1742)
    For https://github.com/riptano/cndb/issues/14123, we want to be able to
    catch issues happening during the opening of just-flushed sstables to
    use shallow sstables. This commit enable this by adding a method to
    `StorageHandler` that is called on such issue, and allow to provide a
    "replacement" `SSTableReader` instance.
    
     (Rebase of commit 2171e5bf69b02735c3428114f7326e3b9fce1389)

--- test/unit/org/apache/cassandra/io/sstable/SSTableRewriterTest.java
+++ test/unit/org/apache/cassandra/io/sstable/SSTableRewriterTest.java
@@ -984,7 +984,7 @@ public class SSTableRewriterTest extends SSTableWriterTestBase
 
                     writer.append(builder.build().unfilteredIterator());
                 }
-                result.addAll(writer.finish(true));
+                result.addAll(writer.finish(true, null));
             }
         }
         return result;
