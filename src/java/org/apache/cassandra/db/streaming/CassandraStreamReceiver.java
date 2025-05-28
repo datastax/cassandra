78b98f2741f0f7a4be9b4de6dac0859fca912d8c
Sylvain Lebresne <lebresne@gmail.com>
2025-05-28 16:32:05 +0200

    CNDB-14123: allows intercepting issues opening flushed sstables (#1742)
    For https://github.com/riptano/cndb/issues/14123, we want to be able to
    catch issues happening during the opening of just-flushed sstables to
    use shallow sstables. This commit enable this by adding a method to
    `StorageHandler` that is called on such issue, and allow to provide a
    "replacement" `SSTableReader` instance.
    
     (Rebase of commit 2171e5bf69b02735c3428114f7326e3b9fce1389)

--- src/java/org/apache/cassandra/db/streaming/CassandraStreamReceiver.java
+++ src/java/org/apache/cassandra/db/streaming/CassandraStreamReceiver.java
@@ -108,7 +108,7 @@ public class CassandraStreamReceiver implements StreamReceiver
         SSTableMultiWriter sstable = file.getSSTable();
         try
         {
-            finished = sstable.finish(true);
+            finished = sstable.finish(true, cfs.getStorageHandler());
         }
         catch (Throwable t)
         {
