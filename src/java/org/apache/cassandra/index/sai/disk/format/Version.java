2bf88ccded64aabd808f7b92b49ff0837bc84000
Michael Marshall <mmarshall@apache.org>
2025-05-30 15:13:09 -0500

    CNDB-14301: couple jvector file format and SAI version

--- src/java/org/apache/cassandra/index/sai/disk/format/Version.java
+++ src/java/org/apache/cassandra/index/sai/disk/format/Version.java
@@ -52,14 +52,14 @@ public class Version implements Comparable<Version>
     public static final Version AA = new Version("aa", V1OnDiskFormat.instance, Version::aaFileNameFormat);
     // Stargazer
     public static final Version BA = new Version("ba", V2OnDiskFormat.instance, (c, i, g) -> stargazerFileNameFormat(c, i, g, "ba"));
-    // Converged Cassandra with JVector
+    // Converged Cassandra with JVector with file format version 2
     public static final Version CA = new Version("ca", V3OnDiskFormat.instance, (c, i, g) -> stargazerFileNameFormat(c, i, g, "ca"));
     // NOTE: use DB to prevent collisions with upstream file formats
     // Encode trie entries using their AbstractType to ensure trie entries are sorted for range queries and are prefix free.
     public static final Version DB = new Version("db", V4OnDiskFormat.instance, (c, i, g) -> stargazerFileNameFormat(c, i, g, "db"));
     // revamps vector postings lists to cause fewer reads from disk
     public static final Version DC = new Version("dc", V5OnDiskFormat.instance, (c, i, g) -> stargazerFileNameFormat(c, i, g, "dc"));
-    // histograms in index metadata
+    // histograms in index metadata; bump jvector file format version to 4
     public static final Version EB = new Version("eb", V6OnDiskFormat.instance, (c, i, g) -> stargazerFileNameFormat(c, i, g, "eb"));
     // term frequencies index component (support for BM25)
     public static final Version EC = new Version("ec", V7OnDiskFormat.instance, (c, i, g) -> stargazerFileNameFormat(c, i, g, "ec"));
