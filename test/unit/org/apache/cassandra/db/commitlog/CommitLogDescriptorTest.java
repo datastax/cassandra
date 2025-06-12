fdc3c0eafd3debbd4eb45c62751d3fda73b0dda8
mck <mck@apache.org>
2025-06-12 09:50:37 +0200

    HCD-136 – DSE 6.8 to CC4 internode transport compatibility
    CC4 can only communicate with DSE 6.8 on messaging version VERSION_3014 (messaging/protocol version 11) and VERSION_3014 (messaging/protocol version 11).
    Manully setting the CC4 node's `MessagingService.current_version` achieves this, done using the jvm flag `-Dds.current_messaging_version=11`.  Using VERSION_30 (messaging/protocol version 10) is also accepted, but hits the column filter bug on system tables `select *` queries during startup.
    
    This commits automates the DSE 6.8 detection and negotiation when the maxMessagingVersion in the handshake is above 256, replying with a max accept of VERSION_3014. The ds.current_messaging_version flag is kept, as when it is used everything behaves as it previously did.  This is important for CNDB, as it hasn't dropped support for bdp/cndb-6.8 (a customised version of DSE 6.8).  The flag and fields related to it are marked deprecated and can be removed in CC5.
    
    Logging in OutboundConnectionInitiator is improved with an introduced ClosedOutboundConnectionException that extends ClosedChannelException. ClosedOutboundConnectionException adds a message including the requestMessagingVersion, settings.framing and settings.encryption variables.
    
    In StartupClusterConnectivityChecker,  don't send PING_REQ messages to any DSE 6.x/C* 3.x peers. They are not required (only speed up startup times), and isn't understood anyway by those peers.
    
    Fix commit log versions, as they were being incorrectly written with filenames using messaging versions not corresponding to commit log versions.  Introduce CommitLogDescriptor.currentVersion() to translate messaging versions to commit log versions. This is important to honour custom ds.current_messaging_version values.
    
     (Rebase of commit 6e8b4ce3904e9bb914889835be79398a45d9c93f)

--- test/unit/org/apache/cassandra/db/commitlog/CommitLogDescriptorTest.java
+++ test/unit/org/apache/cassandra/db/commitlog/CommitLogDescriptorTest.java
@@ -349,4 +349,32 @@ public class CommitLogDescriptorTest
         CommitLogDescriptor descriptor = new CommitLogDescriptor(680, 1, null, null);
         Assert.assertEquals(MessagingService.VERSION_DSE_68, descriptor.getMessagingVersion());
     }
+
+    @Test
+    public void testMessagingToCommitLogVersionSymmetry()
+    {
+        for (MessagingService.Version messagingVersion : MessagingService.Version.values())
+        {
+            int commitLogVersion = CommitLogDescriptor.currentVersion(messagingVersion.value);
+
+            // special case for MessagingService.VERSION_3014
+            int expectedMessagingVersion = messagingVersion.value;
+            if (MessagingService.Version.VERSION_3014 == messagingVersion)
+                expectedMessagingVersion = MessagingService.VERSION_30;
+
+            Assert.assertEquals(expectedMessagingVersion, CommitLogDescriptor.getMessagingVersion(commitLogVersion));
+        }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testMessagingToCommitLogVersionFailure()
+    {
+        CommitLogDescriptor.currentVersion(1);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testCommitLogToMessagingVersionFailure()
+    {
+        CommitLogDescriptor.getMessagingVersion(1);
+    }
 }
