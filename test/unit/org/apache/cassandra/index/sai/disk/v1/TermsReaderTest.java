21e452729c72398c9e352696c30941a8df9478ad
ekaterinadimitrova2 <ekaterina.dimitrova@datastax.com>
2025-07-11 09:29:52 -0400

    CNDB-13192: Bump the default `cassandra.sai.latest.version` from `DC` to `EC` so we support BM25 by default.  (#1737)
    We do not need to update JVECTOR_VERSION post CNDB-14301
    
     (Rebase of commit e1c587fb68fddfe7412231b853a626d64b336ebe)

--- test/unit/org/apache/cassandra/index/sai/disk/v1/TermsReaderTest.java
+++ test/unit/org/apache/cassandra/index/sai/disk/v1/TermsReaderTest.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
@@ -31,6 +32,7 @@ import org.apache.cassandra.db.marshal.UTF8Type;
 import org.apache.cassandra.index.sai.IndexContext;
 import org.apache.cassandra.index.sai.QueryContext;
 import org.apache.cassandra.index.sai.SAITester;
+import org.apache.cassandra.index.sai.SAIUtil;
 import org.apache.cassandra.index.sai.disk.MemtableTermsIterator;
 import org.apache.cassandra.index.sai.disk.PostingList;
 import org.apache.cassandra.index.sai.disk.TermsIterator;
@@ -56,6 +58,8 @@ public class TermsReaderTest extends SaiRandomizedTest
 {
     public static final ByteComparable.Version VERSION = TypeUtil.BYTE_COMPARABLE_VERSION;
 
+    private final Version version;
+
     @ParametersFactory()
     public static Collection<Object[]> data()
     {
@@ -65,7 +69,11 @@ public class TermsReaderTest extends SaiRandomizedTest
         return Version.ALL.stream().map(v -> new Object[]{v}).collect(Collectors.toList());
     }
 
-    private final Version version;
+    @Before
+    public void setCurrentSAIVersion()
+    {
+        SAIUtil.setCurrentVersion(version);
+    }
 
     public TermsReaderTest(Version version)
     {
@@ -232,6 +240,8 @@ public class TermsReaderTest extends SaiRandomizedTest
 
     private List<InvertedIndexBuilder.TermsEnum> buildTermsEnum(Version version, int terms, int postings)
     {
-        return buildStringTermsEnum(version, terms, postings, () -> randomSimpleString(4, 10), () -> nextInt(0, Integer.MAX_VALUE));
+        // We use terms * postings * 2 as the upper bound on row ids used in the postings list to allow for a somewhat
+        // sparse mapping but not one that is too sparse.
+        return buildStringTermsEnum(version, terms, postings, () -> randomSimpleString(4, 10), () -> nextInt(0, terms * postings * 2));
     }
 }
