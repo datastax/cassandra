56920b87be9de16af985add31f565b99d5633232
Szymon Miężał <93650032+szymon-miezal@users.noreply.github.com>
2025-07-23 10:28:17 +0200

    DSP-23657: Introduce sstable encryption (v2) (#1795)
    The sstable components like `*Data.db`, `*Rows.db`, `*Partitions.db`,
    `*Statistics.db` contain sensitive data that the customer may want to
    protect with encryption.
    
    This patch adds support for encrypting SSTable data, indexes (partition
    and row), and metadata.
    
    Data encryption is integrated via the Cassandra compression framework.
    
    Index and metadata encryption is implemented by updating their
    respective readers and writers.
    
    It is achieved by implementing a special EncryptedSequentialWriter that
    encrypts each chunk before writing, and an EncryptedChunkReader that
    decrypts the chunks when reading. To enable this, the usable space in
    each chunk is reduced by the size of the encryption metadata plus 8
    bytes used to store the CRC and actual chunk length.
    
    This design avoids the need to write and keep in memory a compressed
    offsets map for the indices, since each chunk's offset remains the same
    before and after compression.
    
    This works well for trie data, but index chunks may also include other
    data such as keys, offsets, and deletion times, which may not fit
    entirely in a chunk. To handle this, the writer supports data that
    spans chunk boundaries, splitting it as needed.
    
    The random access reader is updated to skip encryption metadata during
    reads using a new RebuffererFactory.adjustPosition() method.
    
    Encryption is enabled by altering the table to specify an encryptable
    compressor.
    
    Encryption keys are read from the local file system, under the directory
    specified by the JVM flag `cassandra.system_key_directory` (default:
    `/etc/cassandra/conf`).
    
    Note: Support for generating encryption keys will be introduced in a
    future patch.
    
    Previously reviewed and merged under
    https://github.com/datastax/cassandra/pull/1669.
    
    As a follow-up which addresses earlier CNDB regression this patch also
    uses a different file handle depending on write/read time.
    It is required by CNDB as the file writer has to access the file on
    local disk (the file wans't uploaded yet)
    and the reader need to access it via remote storage (as it does not have
    it at hand locally).
    
    Note: This patch does not introduce encryption for SAI indexes.
    
    ---------
    
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
     (Rebase of commit ab9fb4c1441860a932e8c4892722a934b9961164)

--- src/java/org/apache/cassandra/io/util/RandomAccessReader.java
+++ src/java/org/apache/cassandra/io/util/RandomAccessReader.java
@@ -69,6 +69,7 @@ public class RandomAccessReader extends RebufferingInputStream implements FileDa
 
     private void reBufferAt(long position)
     {
+        position = rebufferer.adjustPosition(position);
         bufferHolder.release();
         if (position == length())
         {
