096741fa303f2651ba0c3ef73a0b083a8509daa1
Christophe Bornet <cbornet@hotmail.com>
2025-07-28 11:18:55 +0200

    CNDB-14577: Compact all SSTables of a level shard if their number reaches a limit (#1873)
    CNDB-14577: [UCS by default does not compact many small non-overlapping
    sstables with very few
    rows](https://github.com/riptano/cndb/issues/14577)
    
    This PR limits the number of SSTables for a given compaction level shard
    by executing a major compaction of the shard instead of the regular
    compaction of overlapping SSTables if the number of SSTables reaches a
    threshold.
    
    The threshold is controlled by the `max_sstables_per_shard_factor`
    setting:
    ```md
      `max_sstables_per_shard_factor` Limits the number of SSTables per shard. If the number of sstables in a shard
      exceeds this factor times the shard compaction threshold, a major compaction of the shard will be triggered.
      Some conditions like slow writes can lead to SSTables being very small, and never overlap with enough other SSTables
      to be compacted.
      So this setting is useful to prevent the number of SSTables in a shard from growing too large, which can cause
      problems due to the per-sstable overhead. Also these small SSTables may still have overlaps even if under the
      compaction threshold (eg. due to write replicas) and never compacting them wastes storage space.
      The default value is 10.
    ```
    
    ---------
    
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

--- test/unit/org/apache/cassandra/db/compaction/unified/AdaptiveControllerTest.java
+++ test/unit/org/apache/cassandra/db/compaction/unified/AdaptiveControllerTest.java
@@ -87,6 +87,7 @@ public class AdaptiveControllerTest extends ControllerTest
                                       Controller.DEFAULT_OVERLAP_INCLUSION_METHOD,
                                       true,
                                       false,
+                                      Controller.DEFAULT_MAX_SSTABLES_PER_SHARD_FACTOR,
                                       interval,
                                       minW,
                                       maxW,
