53619254835faaa19e3e116e80a995e8dddc3128
Andrés de la Peña <a.penya.garcia@gmail.com>
2025-07-31 15:23:26 +0100

    CNDB-15001: Fix comment

--- src/java/org/apache/cassandra/cql3/Ordering.java
+++ src/java/org/apache/cassandra/cql3/Ordering.java
@@ -96,7 +96,7 @@ public class Ordering
      */
     public static class Ann implements Expression
     {
-        // TODO: remove this when we no longer need to downgrade to replicas on versilic class LargePartitionon lower than ED SAI that don't
+        // TODO: remove this when we no longer need to downgrade to replicas on version lower than ED SAI that don't
         //  know about synthetic columns, and the related code in
         //  - StatementRestrictions.addOrderingRestrictions
         //  - StorageAttachedIndexSearcher.PrimaryKeyIterator constructor
