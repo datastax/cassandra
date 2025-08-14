078866e7a0e8cc29c3c0c5cfd4b2443edad1bc26
Andrés de la Peña <a.penya.garcia@gmail.com>
2025-08-14 12:14:12 +0100

    CNDB-14997: Fix column selection on SAI-ordered queries
    When SAI's TopKProcessor created PK-sorted in-memory row iterators,
    it wasn't preserving the actual column information from the base row iterator.
    Instead, it was returning the columns of the base table,
    leading to incorrect column selection behavior.
    
    The fix consists on copying the column selection of the original iterator
    into PartitionInfo, so the PK-sorted iterator can return that same column
    selection.

--- src/java/org/apache/cassandra/index/sai/utils/PartitionInfo.java
+++ src/java/org/apache/cassandra/index/sai/utils/PartitionInfo.java
@@ -24,6 +24,7 @@ import javax.annotation.Nullable;
 
 import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.db.DeletionTime;
+import org.apache.cassandra.db.RegularAndStaticColumns;
 import org.apache.cassandra.db.rows.BaseRowIterator;
 import org.apache.cassandra.db.rows.EncodingStats;
 import org.apache.cassandra.db.rows.Row;
@@ -34,6 +35,7 @@ public class PartitionInfo
 {
     public final DecoratedKey key;
     public final Row staticRow;
+    public final RegularAndStaticColumns columns;
 
     // present if it's unfiltered partition iterator
     @Nullable
@@ -43,30 +45,36 @@ public class PartitionInfo
     @Nullable
     public final EncodingStats encodingStats;
 
-    public PartitionInfo(DecoratedKey key, Row staticRow)
+    private PartitionInfo(DecoratedKey key,
+                          Row staticRow,
+                          RegularAndStaticColumns columns,
+                          @Nullable DeletionTime partitionDeletion,
+                          @Nullable EncodingStats encodingStats)
     {
         this.key = key;
         this.staticRow = staticRow;
-        this.partitionDeletion = null;
-        this.encodingStats = null;
-    }
-
-    public PartitionInfo(DecoratedKey key, Row staticRow, DeletionTime partitionDeletion, EncodingStats encodingStats)
-    {
-        this.key = key;
-        this.staticRow = staticRow;
-
+        this.columns = columns;
         this.partitionDeletion = partitionDeletion;
         this.encodingStats = encodingStats;
     }
 
     public static <U extends Unfiltered, R extends BaseRowIterator<U>> PartitionInfo create(R baseRowIterator)
     {
-        return baseRowIterator instanceof UnfilteredRowIterator
-               ? new PartitionInfo(baseRowIterator.partitionKey(), baseRowIterator.staticRow(),
-                                   ((UnfilteredRowIterator) baseRowIterator).partitionLevelDeletion(),
-                                   ((UnfilteredRowIterator) baseRowIterator).stats())
-               : new PartitionInfo(baseRowIterator.partitionKey(), baseRowIterator.staticRow());
+        // only unfiltered row iterators have a partition deletion time and encoding stats
+        DeletionTime partitionDeletion = null;
+        EncodingStats encodingStats = null;
+        if (baseRowIterator instanceof UnfilteredRowIterator)
+        {
+            UnfilteredRowIterator unfilteredRowIterator = (UnfilteredRowIterator) baseRowIterator;
+            partitionDeletion = unfilteredRowIterator.partitionLevelDeletion();
+            encodingStats = unfilteredRowIterator.stats();
+        }
+
+        return new PartitionInfo(baseRowIterator.partitionKey(),
+                                 baseRowIterator.staticRow(),
+                                 baseRowIterator.columns(),
+                                 partitionDeletion,
+                                 encodingStats);
     }
 
     @Override
@@ -75,13 +83,16 @@ public class PartitionInfo
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PartitionInfo that = (PartitionInfo) o;
-        return Objects.equals(key, that.key) && Objects.equals(staticRow, that.staticRow)
-               && Objects.equals(partitionDeletion, that.partitionDeletion) && Objects.equals(encodingStats, that.encodingStats);
+        return Objects.equals(key, that.key)
+               && Objects.equals(staticRow, that.staticRow)
+               && Objects.equals(columns, that.columns)
+               && Objects.equals(partitionDeletion, that.partitionDeletion)
+               && Objects.equals(encodingStats, that.encodingStats);
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hash(key, staticRow, partitionDeletion, encodingStats);
+        return Objects.hash(key, staticRow, columns, partitionDeletion, encodingStats);
     }
 }
