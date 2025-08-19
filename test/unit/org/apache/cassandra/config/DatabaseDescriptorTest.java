1b3fda5acc4784b5f6f7f62ce0a954cfe5a7d5a2
Daniel Jatnieks <jatnieks@pobox.com>
2025-08-18 17:59:22 -0700

    Fix tests affected by GuardrailsOptions.enforceDefaults

--- test/unit/org/apache/cassandra/config/DatabaseDescriptorTest.java
+++ test/unit/org/apache/cassandra/config/DatabaseDescriptorTest.java
@@ -338,7 +338,7 @@ public class DatabaseDescriptorTest
             fail("Should have received a IllegalArgumentException batch_size_warn_threshold = -1");
         }
         catch (IllegalArgumentException ignored) { }
-        Assert.assertEquals(5120, DatabaseDescriptor.getBatchSizeWarnThreshold());
+        Assert.assertEquals(65536, DatabaseDescriptor.getBatchSizeWarnThreshold());
 
         try
         {
