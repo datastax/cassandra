5f6c1c33ac7dd076d63558b7fac39b8dc170012b
Daniel Jatnieks <jatnieks@pobox.com>
2025-08-27 11:58:40 -0500

    Add bloomFilterOffHeapMemoryUsedAwareCFSGaugeProvider and use with BloomFilterMetrics.bloomFilterOffHeapMemoryUsed
     (Rebase of commit c37db981128a9aa5b53605e01497fa821dde7391)

--- src/java/org/apache/cassandra/io/sstable/filter/BloomFilterMetrics.java
+++ src/java/org/apache/cassandra/io/sstable/filter/BloomFilterMetrics.java
@@ -65,10 +65,7 @@ public class BloomFilterMetrics<R extends SSTableReaderWithFilter> extends Abstr
     /**
      * Off heap memory used by bloom filter
      */
-    public final GaugeProvider<Long> bloomFilterOffHeapMemoryUsed = newGaugeProvider("BloomFilterOffHeapMemoryUsed",
-                                                                                     // FIXME: CNDB-15213
-                                                                                     //   Seems to correspond to TableMetrics.bloomFilterOffHeapMemoryUsed.getLong() in main
-                                                                                     //   where we want TableMetrics.inFlightBloomFilterOffHeapMemoryUsed.get() as the initial value;
+    public final GaugeProvider<Long> bloomFilterOffHeapMemoryUsed = bloomFilterOffHeapMemoryUsedAwareCFSGaugeProvider("BloomFilterOffHeapMemoryUsed",
                                                                                      0L,
                                                                                      SSTableReaderWithFilter::getFilterOffHeapSize,
                                                                                      Long::sum);
