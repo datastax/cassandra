4d6ffa6d00c1d9b0556eb65b1e125ba391dfccd4
Andrés de la Peña <a.penya.garcia@gmail.com>
2025-08-28 13:52:43 +0100

    CNDB-15155: Segregate SAI's query metrics per query type
    Add separate SAI query metrics for filtering, top-k, single-partition and range queries.
    The new metrics are identical to the previous ones, but they are only applied to the
    relevant queries. The previous query metrics remain unaltered.

--- src/java/org/apache/cassandra/index/sai/metrics/TableQueryMetrics.java
+++ src/java/org/apache/cassandra/index/sai/metrics/TableQueryMetrics.java
@@ -17,66 +17,201 @@
  */
 package org.apache.cassandra.index.sai.metrics;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Timer;
+import org.apache.cassandra.db.ReadCommand;
 import org.apache.cassandra.index.sai.QueryContext;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.tracing.Tracing;
 
 import static org.apache.cassandra.metrics.CassandraMetricsRegistry.Metrics;
 
-public class TableQueryMetrics extends AbstractMetrics
+/**
+ * Table query metrics for different types of query. The metrics for each type of query are divided into two groups:
+ * <ul>
+ * <li>Per table counters ({@link PerTable}).</li>
+ * <li>Per query timers and histograms ({@link PerQuery}).</li>
+ * </ul>
+ */
+public class TableQueryMetrics
 {
-    public static final String TABLE_QUERY_METRIC_TYPE = "TableQueryMetrics";
+    /** Per table metrics for all types of queries (counters). */
+    private final List<PerTable> perTableMetrics = new ArrayList<>();
 
-    public final PerQueryMetrics perQueryMetrics;
-
-    public final Counter totalQueryTimeouts;
-    public final Counter totalPartitionReads;
-    public final Counter totalRowsFiltered;
-    public final Counter totalQueriesCompleted;
-
-    public final Counter sortThenFilterQueriesCompleted;
-    public final Counter filterThenSortQueriesCompleted;
+    /** Per query metrics for all types of queries (timers and histograms). */
+    private final List<PerQuery> perQueryMetrics = new ArrayList<>();
 
     public TableQueryMetrics(TableMetadata table)
     {
-        super(table.keyspace, table.name, TABLE_QUERY_METRIC_TYPE);
-
-        perQueryMetrics = new PerQueryMetrics(table);
-
-        totalPartitionReads = Metrics.counter(createMetricName("TotalPartitionReads"));
-        totalRowsFiltered = Metrics.counter(createMetricName("TotalRowsFiltered"));
-        totalQueriesCompleted = Metrics.counter(createMetricName("TotalQueriesCompleted"));
-        totalQueryTimeouts = Metrics.counter(createMetricName("TotalQueryTimeouts"));
-
-        sortThenFilterQueriesCompleted = Metrics.counter(createMetricName("SortThenFilterQueriesCompleted"));
-        filterThenSortQueriesCompleted = Metrics.counter(createMetricName("FilterThenSortQueriesCompleted"));
+        addMetrics(table, "", cmd -> true);
+        addMetrics(table, "Filter", cmd -> !cmd.isTopK()); // queries that are filtering only
+        addMetrics(table, "TopK", ReadCommand::isTopK); // queries that are top-k only
+        addMetrics(table, "SinglePartition", ReadCommand::isSinglePartition); // single-partition queries
+        addMetrics(table, "Range", cmd -> !cmd.isSinglePartition()); // range queries
     }
 
-    public void record(QueryContext queryContext)
+    private void addMetrics(TableMetadata table, String type, Predicate<ReadCommand> filter)
     {
-        if (queryContext.queryTimeouts() > 0)
-        {
-            assert queryContext.queryTimeouts() == 1;
-
-            totalQueryTimeouts.inc();
-        }
-
-        perQueryMetrics.record(queryContext);
+        perTableMetrics.add(new PerTable(table, type, filter));
+        perQueryMetrics.add(new PerQuery(table, type, filter));
     }
 
+    /**
+     * Records metrics for a single query.
+     *
+     * @param context the stats relevant to the execution of a single query
+     * @param command the query command
+     */
+    public void record(QueryContext context, ReadCommand command)
+    {
+        Snapshot snapshot = new Snapshot(context);
+        perTableMetrics.forEach(m -> m.record(snapshot, command));
+        perQueryMetrics.forEach(m -> m.record(snapshot, command));
+    }
+
+    /**
+     * Releases all the resources used by these metrics.
+     */
     public void release()
     {
-        super.release();
-        perQueryMetrics.release();
+        perTableMetrics.forEach(PerTable::release);
+        perQueryMetrics.forEach(PerQuery::release);
     }
 
-    public class PerQueryMetrics extends AbstractMetrics
+    private static String pluralize(long count, String root, String plural)
     {
+        return count == 1 ? String.format("1 %s", root) : String.format("%d %s%s", count, root, plural);
+    }
+
+    /**
+     * Family of metrics for a specific type of query.
+     */
+    public abstract static class AbstractQueryMetrics extends AbstractMetrics
+    {
+        private static final Pattern PATTERN = Pattern.compile("Query");
+
+        private final Predicate<ReadCommand> filter;
+
+        private AbstractQueryMetrics(String keyspace, String table, String scope, String type, Predicate<ReadCommand> filter)
+        {
+            super(keyspace, table, makeName(scope, type));
+            this.filter = filter;
+        }
+
+        protected final void record(Snapshot snapshot, ReadCommand command)
+        {
+            if (filter.test(command))
+                record(snapshot);
+        }
+
+        protected abstract void record(Snapshot snapshot);
+
+        public static String makeName(String scope, String type)
+        {
+            return PATTERN.matcher(scope).replaceFirst(type + "Query");
+        }
+    }
+
+    /**
+     * Per table metrics for a specific type of query. These metrics are always counters.
+     */
+    public static class PerTable extends AbstractQueryMetrics
+    {
+        public static final String METRIC_TYPE = "TableQueryMetrics";
+
+        public final Counter totalQueryTimeouts;
+        public final Counter totalPartitionReads;
+        public final Counter totalRowsFiltered;
+        public final Counter totalQueriesCompleted;
+
+        public final Counter sortThenFilterQueriesCompleted;
+        public final Counter filterThenSortQueriesCompleted;
+
+        /**
+         * @param table the table to measure metrics for
+         * @param type an identifier for the type of query which metrics are being recorded for
+         * @param filter a predicate that determines whether a given query should be recorded
+         */
+        public PerTable(TableMetadata table, String type, Predicate<ReadCommand> filter)
+        {
+            super(table.keyspace, table.name, METRIC_TYPE, type, filter);
+
+            totalPartitionReads = Metrics.counter(createMetricName("TotalPartitionReads"));
+            totalRowsFiltered = Metrics.counter(createMetricName("TotalRowsFiltered"));
+            totalQueriesCompleted = Metrics.counter(createMetricName("TotalQueriesCompleted"));
+            totalQueryTimeouts = Metrics.counter(createMetricName("TotalQueryTimeouts"));
+
+            sortThenFilterQueriesCompleted = Metrics.counter(createMetricName("SortThenFilterQueriesCompleted"));
+            filterThenSortQueriesCompleted = Metrics.counter(createMetricName("FilterThenSortQueriesCompleted"));
+        }
+
+        @Override
+        public void record(Snapshot snapshot)
+        {
+            if (snapshot.queryTimeouts > 0)
+            {
+                assert snapshot.queryTimeouts == 1;
+                totalQueryTimeouts.inc();
+            }
+
+            totalQueriesCompleted.inc();
+            totalPartitionReads.inc(snapshot.partitionsRead);
+            totalRowsFiltered.inc(snapshot.rowsFiltered);
+
+            if (snapshot.filterSortOrder == QueryContext.FilterSortOrder.SCAN_THEN_FILTER)
+                sortThenFilterQueriesCompleted.inc();
+            else if (snapshot.filterSortOrder == QueryContext.FilterSortOrder.SEARCH_THEN_ORDER)
+                filterThenSortQueriesCompleted.inc();
+
+            if (Tracing.isTracing())
+            {
+                final long queryLatencyMicros = TimeUnit.NANOSECONDS.toMicros(snapshot.totalQueryTimeNs);
+
+                if (snapshot.filterSortOrder == QueryContext.FilterSortOrder.SEARCH_THEN_ORDER)
+                {
+                    Tracing.trace("Index query accessed memtable indexes, {}, and {}, selected {} before ranking, " +
+                                  "post-filtered {} in {}, and took {} microseconds.",
+                                  pluralize(snapshot.sstablesHit, "SSTable index", "es"),
+                                  pluralize(snapshot.segmentsHit, "segment", "s"),
+                                  pluralize(snapshot.rowsPreFiltered, "row", "s"),
+                                  pluralize(snapshot.rowsFiltered, "row", "s"),
+                                  pluralize(snapshot.partitionsRead, "partition", "s"),
+                                  queryLatencyMicros);
+                }
+                else
+                {
+                    Tracing.trace("Index query accessed memtable indexes, {}, and {}, post-filtered {} in {}, " +
+                                  "and took {} microseconds.",
+                                  pluralize(snapshot.sstablesHit, "SSTable index", "es"),
+                                  pluralize(snapshot.segmentsHit, "segment", "s"),
+                                  pluralize(snapshot.rowsFiltered, "row", "s"),
+                                  pluralize(snapshot.partitionsRead, "partition", "s"),
+                                  queryLatencyMicros);
+                }
+            }
+        }
+
+        @Override
+        public void release()
+        {
+            super.release();
+        }
+    }
+
+    /**
+     * Per query metrics for a specific type of query. These metrics are always timers and histograms.
+     */
+    public static class PerQuery extends AbstractQueryMetrics
+    {
+        public static final String METRIC_TYPE = "PerQuery";
+
         public final Timer queryLatency;
 
         /**
@@ -111,9 +246,9 @@ public class TableQueryMetrics extends AbstractMetrics
          */
         public final Timer annGraphSearchLatency;
 
-        public PerQueryMetrics(TableMetadata table)
+        public PerQuery(TableMetadata table, String type, Predicate<ReadCommand> filter)
         {
-            super(table.keyspace, table.name, "PerQuery");
+            super(table.keyspace, table.name, METRIC_TYPE, type, filter);
 
             queryLatency = Metrics.timer(createMetricName("QueryLatency"));
 
@@ -121,7 +256,6 @@ public class TableQueryMetrics extends AbstractMetrics
             segmentsHit = Metrics.histogram(createMetricName("IndexSegmentsHit"), false);
 
             kdTreePostingsSkips = Metrics.histogram(createMetricName("KDTreePostingsSkips"), true);
-
             kdTreePostingsNumPostings = Metrics.histogram(createMetricName("KDTreePostingsNumPostings"), false);
             kdTreePostingsDecodes = Metrics.histogram(createMetricName("KDTreePostingsDecodes"), false);
 
@@ -137,92 +271,93 @@ public class TableQueryMetrics extends AbstractMetrics
             annGraphSearchLatency = Metrics.timer(createMetricName("ANNGraphSearchLatency"));
         }
 
-        private void recordStringIndexCacheMetrics(QueryContext events)
+        @Override
+        public void record(Snapshot snapshot)
         {
-            postingsSkips.update(events.triePostingsSkips());
-            postingsDecodes.update(events.triePostingsDecodes());
-        }
+            queryLatency.update(snapshot.totalQueryTimeNs, TimeUnit.NANOSECONDS);
+            sstablesHit.update(snapshot.sstablesHit);
+            segmentsHit.update(snapshot.segmentsHit);
+            partitionReads.update(snapshot.partitionsRead);
+            rowsFiltered.update(snapshot.rowsFiltered);
 
-        private void recordNumericIndexCacheMetrics(QueryContext events)
-        {
-            kdTreePostingsNumPostings.update(events.bkdPostingListsHit());
-
-            kdTreePostingsSkips.update(events.bkdPostingsSkips());
-            kdTreePostingsDecodes.update(events.bkdPostingsDecodes());
-        }
-
-        private void recordVectorIndexMetrics(QueryContext queryContext)
-        {
-            annGraphSearchLatency.update(queryContext.annGraphSearchLatency(), TimeUnit.NANOSECONDS);
-        }
-
-        public void record(QueryContext queryContext)
-        {
-            final long totalQueryTimeNs = queryContext.totalQueryTimeNs();
-            queryLatency.update(totalQueryTimeNs, TimeUnit.NANOSECONDS);
-            final long queryLatencyMicros = TimeUnit.NANOSECONDS.toMicros(totalQueryTimeNs);
-
-            final long ssTablesHit = queryContext.sstablesHit();
-            final long segmentsHit = queryContext.segmentsHit();
-            final long partitionsRead = queryContext.partitionsRead();
-            final long rowsFiltered = queryContext.rowsFiltered();
-            final long rowsPreFiltered = queryContext.rowsFiltered();
-
-            sstablesHit.update(ssTablesHit);
-            this.segmentsHit.update(segmentsHit);
-
-            partitionReads.update(partitionsRead);
-            totalPartitionReads.inc(partitionsRead);
-
-            this.rowsFiltered.update(rowsFiltered);
-            totalRowsFiltered.inc(rowsFiltered);
-
-            if (queryContext.filterSortOrder() == QueryContext.FilterSortOrder.SCAN_THEN_FILTER)
-                sortThenFilterQueriesCompleted.inc();
-            else if (queryContext.filterSortOrder() == QueryContext.FilterSortOrder.SEARCH_THEN_ORDER)
-                filterThenSortQueriesCompleted.inc();
-
-            if (Tracing.isTracing())
+            // Record string index cache metrics.
+            if (snapshot.trieSegmentsHit > 0)
             {
-                if (queryContext.filterSortOrder() == QueryContext.FilterSortOrder.SEARCH_THEN_ORDER)
-                {
-                    Tracing.trace("Index query accessed memtable indexes, {}, and {}, selected {} before ranking, post-filtered {} in {}, and took {} microseconds.",
-                                  pluralize(ssTablesHit, "SSTable index", "es"),
-                                  pluralize(segmentsHit, "segment", "s"),
-                                  pluralize(rowsPreFiltered, "row", "s"),
-                                  pluralize(rowsFiltered, "row", "s"),
-                                  pluralize(partitionsRead, "partition", "s"),
-                                  queryLatencyMicros);
-                }
-                else
-                {
-                    Tracing.trace("Index query accessed memtable indexes, {}, and {}, post-filtered {} in {}, and took {} microseconds.",
-                                  pluralize(ssTablesHit, "SSTable index", "es"),
-                                  pluralize(segmentsHit, "segment", "s"),
-                                  pluralize(rowsFiltered, "row", "s"),
-                                  pluralize(partitionsRead, "partition", "s"),
-                                  queryLatencyMicros);
-                }
+                postingsSkips.update(snapshot.triePostingsSkips);
+                postingsDecodes.update(snapshot.triePostingsDecodes);
             }
 
-            if (queryContext.trieSegmentsHit() > 0)
-                recordStringIndexCacheMetrics(queryContext);
-            if (queryContext.bkdSegmentsHit() > 0)
-                recordNumericIndexCacheMetrics(queryContext);
+            // Record numeric index cache metrics.
+            if (snapshot.bkdSegmentsHit > 0)
+            {
+                kdTreePostingsNumPostings.update(snapshot.bkdPostingListsHit);
+                kdTreePostingsSkips.update(snapshot.bkdPostingsSkips);
+                kdTreePostingsDecodes.update(snapshot.bkdPostingsDecodes);
+            }
+
+            // Record vector index metrics.
             // If ann brute forced the whole search, this is 0. We don't measure brute force latency. Maybe we should?
             // At the very least, we collect brute force comparison metrics, which should give a reasonable indicator
             // of work done.
-            if (queryContext.annGraphSearchLatency() > 0)
-                recordVectorIndexMetrics(queryContext);
+            if (snapshot.annGraphSearchLatency > 0)
+            {
+                annGraphSearchLatency.update(snapshot.annGraphSearchLatency, TimeUnit.NANOSECONDS);
+            }
 
-            shadowedKeysScannedHistogram.update(queryContext.getShadowedPrimaryKeyCount());
-
-            totalQueriesCompleted.inc();
+            shadowedKeysScannedHistogram.update(snapshot.shadowedPrimaryKeyCount);
         }
     }
 
-    private String pluralize(long count, String root, String plural)
+    /**
+     * A snapshot of all relevant metrics in a {@link QueryContext} at a specific point in time.
+     * This class memoizes the values of those metrics so that we can record them later in the
+     * {@link AbstractQueryMetrics#record(Snapshot)} method of as many {@link AbstractQueryMetrics}
+     * instances as needed, without calculating the same values once and again.
+     */
+    public static class Snapshot
     {
-        return count == 1 ? String.format("1 %s", root) : String.format("%d %s%s", count, root, plural);
+        private final long totalQueryTimeNs;
+        private final long sstablesHit;
+        private final long segmentsHit;
+        private final long partitionsRead;
+        private final long rowsFiltered;
+        private final long rowsPreFiltered;
+        private final long trieSegmentsHit;
+        private final long bkdPostingListsHit;
+        private final long bkdSegmentsHit;
+        private final long bkdPostingsSkips;
+        private final long bkdPostingsDecodes;
+        private final long triePostingsSkips;
+        private final long triePostingsDecodes;
+        private final long queryTimeouts;
+        private final long annGraphSearchLatency;
+        private final long shadowedPrimaryKeyCount;
+        private final QueryContext.FilterSortOrder filterSortOrder;
+
+        /**
+         * Creates a snapshot of all long-valued metrics from the given QueryContext.
+         *
+         * @param context the QueryContext to snapshot
+         */
+        public Snapshot(QueryContext context)
+        {
+            totalQueryTimeNs = context.totalQueryTimeNs();
+            sstablesHit = context.sstablesHit();
+            segmentsHit = context.segmentsHit();
+            partitionsRead = context.partitionsRead();
+            rowsFiltered = context.rowsFiltered();
+            rowsPreFiltered = context.rowsPreFiltered();
+            trieSegmentsHit = context.trieSegmentsHit();
+            bkdPostingListsHit = context.bkdPostingListsHit();
+            bkdSegmentsHit = context.bkdSegmentsHit();
+            bkdPostingsSkips = context.bkdPostingsSkips();
+            bkdPostingsDecodes = context.bkdPostingsDecodes();
+            triePostingsSkips = context.triePostingsSkips();
+            triePostingsDecodes = context.triePostingsDecodes();
+            queryTimeouts = context.queryTimeouts();
+            annGraphSearchLatency = context.annGraphSearchLatency();
+            shadowedPrimaryKeyCount = context.getShadowedPrimaryKeyCount();
+            filterSortOrder = context.filterSortOrder();
+        }
     }
 }
