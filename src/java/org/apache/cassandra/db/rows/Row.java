9c8a85354ec1f7c571bc336b0146f37882aa7286
Andrés de la Peña <adelapena@users.noreply.github.com>
2025-09-24 12:45:29 +0100

    CNDB-15482: CNDB-15435: Only count live data size in bytes-based paging (#2010)
    Only count live data size when we are using bytes-sized limits. This
    prevents issues with paging when row purging or other unexpected
    transformation changes the size of the read rows.
    
    This fixes  DBPE-16935 and DBPE-17751.
    
    Rebase notes:
     * nowInSec and minDeletionTime are not longs instead of ints (ref CASSANDRA-14227)
     * need to disable new read size thresholds, since we are testing with abnormally large responses
     * can be upstreamed as part of CASSANDRA-11745
    
     (Rebase of commit bfd09f45d66744a4899fb32ae4a1d72aa72fa8aa)

--- src/java/org/apache/cassandra/db/rows/Row.java
+++ src/java/org/apache/cassandra/db/rows/Row.java
@@ -126,7 +126,7 @@ public interface Row extends Unfiltered, Iterable<ColumnData>, IMeasurableMemory
 
     /**
      * Whether the row has some live information (i.e. it's not just deletion informations).
-     * 
+     *
      * @param nowInSec the current time to decide what is deleted and what isn't
      * @param enforceStrictLiveness whether the row should be purged if there is no PK liveness info,
      *                              normally retrieved from {@link TableMetadata#enforceStrictLiveness()}
@@ -326,12 +326,12 @@ public interface Row extends Unfiltered, Iterable<ColumnData>, IMeasurableMemory
     public int dataSize();
 
     /**
-     * Returns the original data size in bytes of this row as it was returned by {@link #dataSize()} before purging it
-     * from all deletion info with {@link #purge}.
+     * Returns the size of the data hold by this row that is live at {@code nowInSec}.
      *
-     * @return the original data size of this row in bytes before purging
+     * @param nowInSec the query timestamp in seconds
+     * @return the size of the data hold by this row that is live at {@code nowInSec}.
      */
-    int dataSizeBeforePurge();
+    int liveDataSize(long nowInSec);
 
     public long unsharedHeapSizeExcludingData();
 
