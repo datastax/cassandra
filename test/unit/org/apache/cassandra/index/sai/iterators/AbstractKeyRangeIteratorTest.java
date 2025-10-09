060d3a18135ecbb22f95e81be1b1f81eea8614a5
Piotr Kołaczkowski <pkolaczk@datastax.com>
2025-10-09 17:37:31 +0200

    CNDB-15666: CNDB-15570: Fix handling mixed key types in SAI iterators CNDB-15683: Fix incorrect results when querying mixed AA and EC indexes
    This commit fixes multiple issues with KeyRangeIterator implementations
    occasionally skipping or emitting duplicate keys when working on
    a mix of primary keys with empty / non-empty clusterings.
    This situation is possible while scanning tables with static columns
    or when some indexes are partition-aware (e.g. version AA) and
    others have been updated to a row-aware version (e.g. DC or EC).
    Due to those bugs, users could get incorrect results from SAI queries,
    e.g. results containing duplicated rows, duplicated partitions or
    even missing rows.
    
    The commit introduces extensive randomized property-based tests for
    KeyRangeUnionIterator and KeyIntersectionIterator. Previously,
    the tests did not test for keys with mixed empty/non-empty clusterings.
    
    Changes in KeyRangeUnionIterator:
    
    KeyRangeUnionIterator merges streams of primary keys in such a way that
    duplicates are removed. Unfortunately it does not properly account
    for the fact that if a key with an empty clustering meets a key
    with a non-empty clustering and the same partition key, we must
    always return the key with an empty clustering. A key with an empty
    clustering will always fetch the rows matched by any specific row
    key for the same partition, but the reverse is not true.
    
    The iterator implementation has been modified to always pick the
    key that matches more rows - a key with empty clustering wins
    over a key with non-empty clustering. Additionally, once a key
    with an empty clustering is emitted, no more keys in that partition
    are emitted.
    
    Changes in KeyRangeIntersectionIterator:
    
    Due to a very similar problem like in KeyRangeUnionIterator,
    KeyRangeIntersectionIterator could return either too few or
    too many keys, when keys with empty clusterings and keys
    with non-empty clusterings were present in the input key streams.
    
    In particular consider 2 input streams A and B with the following
    keys:
    
    A:
    0: (1, Clustering.EMPTY)
    
    B:
    0: (1, 1)
    1: (1, 2)
    
    Key A.0 matches the whole partition 1. Therefore, the correct result
    of intersection are both keys of stream B. Unfortunately, the algorithm
    before this patch would advance both A and B iterators when emitting
    the first matching key. At the beginning of the second step,
    the iterator A would be already exhausted and no more keys would
    be produced. Finally key B.1 would be missing from the results.
    
    This patch fixes it by introducing two changes to the intersection
    algorithm:
    
    1. A key with non-empty clustering wins over a key with
    empty clustering and same partition.
    
    2. The selected highest key is not consumed while searching
    for the highest matching key, but that happens only after the
    search loop finds a match. Then we have more information
    which iterators would be moved to the next item. Iterators positioned
    at a key with an empty clustering can be advanced only after
    we run out of keys with non-empty clustering in the same partition
    or if there are no other keys with non-empty clustering.
    
    This patch also fixes another issue where we could return
    a less-specific key matching a full partition instead of a key
    matching one row:
    
    A:
    0: (1, Clustering.EMPTY)
    
    B:
    0: (1, 1)
    
    In that case the iterator returned a key with empty clustering,
    which would result in fetching and postfiltering many unnecessary rows.
    
    CNDB-15683: Fix incorrect results when querying mixed AA and EC indexes (#2066)
    
    When row-aware and non-row-aware indexes are mixed, we now check
    the clustering index filter for all the keys that have clustering
    information, i.e. keys coming from the row-aware
    indexes. Earlier that check was accidentally disabled
    if at least one non-row-aware index was used by the query.
    That could cause retrieving rows that do not match
    the clustering condition of the query.
    
    Rebase notes:
     - includes CNDB-15683
    
     (Rebase of commit b25cba18d823ab1cfaf1f5ee3baa6f1a6e37e6b8)

--- test/unit/org/apache/cassandra/index/sai/iterators/AbstractKeyRangeIteratorTest.java
+++ test/unit/org/apache/cassandra/index/sai/iterators/AbstractKeyRangeIteratorTest.java
@@ -17,15 +17,34 @@
  */
 package org.apache.cassandra.index.sai.iterators;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
 
 import org.junit.Assert;
 
+import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.db.ClusteringComparator;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.marshal.LongType;
+import org.apache.cassandra.dht.Murmur3Partitioner;
+import org.apache.cassandra.index.sai.disk.format.Version;
 import org.apache.cassandra.index.sai.utils.PrimaryKey;
 import org.apache.cassandra.index.sai.utils.SaiRandomizedTest;
 import org.apache.cassandra.utils.Pair;
+
+import static org.apache.cassandra.io.util.FileUtils.closeQuietly;
 
 public class AbstractKeyRangeIteratorTest extends SaiRandomizedTest
 {
@@ -159,4 +178,465 @@ public class AbstractKeyRangeIteratorTest extends SaiRandomizedTest
                 throw new AssertionError();
         }
     }
+
+
+    private static final PrimaryKey.Factory TEST_PRIMARY_KEY_FACTORY = Version.current().onDiskFormat()
+                                                                              .newPrimaryKeyFactory(new ClusteringComparator(LongType.instance));
+
+    /**
+     * Generates a random list of primary keys with the given average number of partitions and rows per partition.
+     * Partition keys and clusterings are generated in such a way that when combining two such lists generated with
+     * same parameters (but different random), there is a high chance both sets would contain many common keys, as well
+     * as each would contain some keys not present in the other set.
+     *
+     * @return list of primary keys in (token, clustering) order.
+     */
+    static List<PrimaryKey> randomPrimaryKeys(int avgPartitions, int avgRowsPerPartition)
+    {
+        List<PrimaryKey> keys = new ArrayList<>((int)(avgPartitions * avgRowsPerPartition * 1.5));
+
+        for (int p = 0; p < avgPartitions * 2; p++)
+        {
+            if (randomBoolean())   // skip 50% of partitions
+                continue;
+
+            if (randomBoolean())
+            {
+                keys.add(makeKey(p, null)); // add partition key only
+            }
+            else
+            {
+                for (int r = 0; r < avgRowsPerPartition * 2; r++)
+                {
+                    if (randomBoolean())   // skip 50% of rows
+                        keys.add(makeKey(p, (long) r));
+                }
+            }
+        }
+
+        // We must sort the keys to recover proper token order
+        Collections.sort(keys);
+        return keys;
+    }
+
+
+    /**
+      * Helper to create PrimaryKey with/without clustering.
+      * Pass null clustering to create a key with Clustering.EMPTY.
+      */
+    static PrimaryKey makeKey(long partitionKey, @Nullable Long clustering)
+    {
+        ByteBuffer pkValue = LongType.instance.getSerializer().serialize(partitionKey);
+        ByteBuffer clusteringValue = LongType.instance.getSerializer().serialize(clustering);
+        DecoratedKey pk = Murmur3Partitioner.instance.decorateKey(pkValue);
+        Clustering<ByteBuffer> c = clustering == null ? Clustering.EMPTY : Clustering.make(clusteringValue);
+        return TEST_PRIMARY_KEY_FACTORY.create(pk, c);
+    }
+
+    /**
+     * Convenience method for comparing arrays of PrimaryKey; we don't use assertEquals to compare arrays
+     * because its output is one huge line of text that is hard to read when the test fails.
+     */
+    void assertKeysEqual(List<PrimaryKey> expected, List<PrimaryKey> result)
+    {
+        int matchesUntil = 0;
+        try
+        {
+            for (int i = 0; i < expected.size() && i < result.size(); i++)
+            {
+                PrimaryKey e = expected.get(i);
+                PrimaryKey r = result.get(i);
+                assertEquals(e, r);
+                matchesUntil = i;
+            }
+
+            if (result.size() < expected.size())
+                throw new AssertionError("Missing " + (expected.size() - result.size()) + " key(s) at the end");
+            if (result.size() > expected.size())
+                throw new AssertionError("Got extra keys at the end: " + result.get(expected.size()));
+        }
+        catch (AssertionError e)
+        {
+            // Print out all the keys that matched properly before the failure to help debugging
+            for (int i = 0; i < matchesUntil; i++)
+                System.err.println("Keys match correctly: " + expected.get(i));
+
+            throw e;
+        }
+    }
+
+    /**
+     * Checks if the given keys are in increasing order and contain no duplicates.
+     */
+    static void assertIncreasing(Collection<PrimaryKey> keys)
+    {
+        PrimaryKey lastPrimaryKey = null;
+        DecoratedKey lastPartitionKey = null;
+        Clustering<?> lastClustering = Clustering.EMPTY;
+        for (PrimaryKey key : keys)
+        {
+            if (key.hasEmptyClustering() && key.partitionKey().equals(lastPartitionKey))
+                throw new AssertionError("A primary key with empty clustering follows a key in the same partition:\n" + key + "\nafter:\n" + lastPrimaryKey);
+
+            if (!key.hasEmptyClustering() && lastClustering.isEmpty() && key.partitionKey().equals(lastPartitionKey))
+                throw new AssertionError("A primary key with non-empty clustering follows a key with empty clustering in the same partition:\n" + key + "\nafter:\n" + lastPrimaryKey);
+
+            if (Objects.equals(key, lastPrimaryKey))
+                throw new AssertionError("Duplicate key:\n" + key + " = " + lastPrimaryKey);
+
+            if (lastPrimaryKey != null && key.compareTo(lastPrimaryKey) < 0)
+                throw new AssertionError("Out of order key:\n" + key + " < " + lastPrimaryKey);
+
+            lastPrimaryKey = key;
+            lastPartitionKey = key.partitionKey();
+            lastClustering = key.clustering();
+        }
+    }
+
+    /**
+     * Helper class to quickly find if a key exists in the set or not.
+     * We cannot just use a hashmap for that, because keys with no clustering match full partitions.
+     */
+    static class PrimaryKeySet
+    {
+        Set<DecoratedKey> partitions = new HashSet<>();
+        Set<Pair<DecoratedKey, Clustering<?>>> rows = new HashSet<>();
+
+        public PrimaryKeySet(Collection<PrimaryKey> keys)
+        {
+            for (PrimaryKey pk : keys)
+            {
+                if (pk.hasEmptyClustering())
+                    partitions.add(pk.partitionKey());
+                else
+                    rows.add(Pair.create(pk.partitionKey(), pk.clustering()));
+            }
+        }
+
+        public boolean contains(PrimaryKey key)
+        {
+            return partitions.contains(key.partitionKey()) ||
+                   rows.contains(Pair.create(key.partitionKey(), key.clustering()));
+        }
+    }
+
+
+    static class PrimaryKeyListIterator extends KeyRangeIterator
+    {
+        private final List<PrimaryKey> keys;
+        private int currentIdx = 0;
+
+        private PrimaryKeyListIterator(List<PrimaryKey> keys)
+        {
+            super(keys.isEmpty() ? null : keys.get(0), keys.isEmpty() ? null : keys.get(keys.size() - 1), keys.size());
+            this.keys = new ArrayList<>(keys);
+
+        }
+
+        public static PrimaryKeyListIterator create(PrimaryKey... keys)
+        {
+            List<PrimaryKey> list = Arrays.asList(keys);
+            Collections.sort(list);
+            return new PrimaryKeyListIterator(list);
+        }
+
+        public static PrimaryKeyListIterator create(List<PrimaryKey> keys)
+        {
+            Collections.sort(keys);
+            return new PrimaryKeyListIterator(keys);
+        }
+
+        @Override
+        protected PrimaryKey computeNext()
+        {
+            if (currentIdx >= keys.size())
+                return endOfData();
+
+            return keys.get(currentIdx++);
+        }
+
+        @Override
+        protected void performSkipTo(PrimaryKey nextToken)
+        {
+            while (currentIdx < keys.size() && keys.get(currentIdx).compareTo(nextToken) < 0)
+                currentIdx++;
+        }
+
+        @Override
+        public void close()
+        {}
+    }
+
+
+    /**
+     * Prints each key in a separate line to help debugging.
+     * Useful because those printed keys are very long.
+     */
+    void printKeys(Collection<PrimaryKey> keys)
+    {
+        for (PrimaryKey key : keys)
+            System.err.println(key);
+    }
+
+    /**
+     * Generates all permutations of array of integers from 0 to n - 1.
+     * E.g. for n = 3, generates: [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 1, 0], [2, 0, 1]]
+     */
+    static List<int[]> permutations(int n) {
+        int[] indices = new int[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+
+        List<int[]> result = new ArrayList<>();
+        generatePermutations(indices, 0, result);
+        return result;
+    }
+
+    // Recursive function to find all possible permutations
+    private static void generatePermutations(int[] arr, int idx, List<int[]> res) {
+        if (idx == arr.length)
+        {
+            res.add(Arrays.copyOf(arr, arr.length));
+            return;
+        }
+
+        for (int i = idx; i < arr.length; i++) {
+            int temp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = temp;
+
+            generatePermutations(arr, idx + 1, res);
+
+            temp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+
+    /**
+     * Performs a merge operation on the primary key lists and validates the result.
+     * If the validation fails, it will try to first minimize the input lists needed to crash the operation
+     * or fail the validation. If the operation succeeds, returns normally, otherwise throws the final exception.
+     *
+     * @param inputs some arbitrary lists of primary keys
+     * @param merge operation under test that merges multiple lists into one
+     * @param validator validation function that checks if the result of the operation is correct, expected to throw if not
+     */
+    void testMerge(List<List<PrimaryKey>> inputs,
+                   Function<List<List<PrimaryKey>>, List<PrimaryKey>> merge,
+                   BiConsumer<List<List<PrimaryKey>>, List<PrimaryKey>> validator) throws Throwable
+    {
+        List<PrimaryKey> result = null;       // last result we obtained from operation (may be valid)
+        List<PrimaryKey> failedResult = null; // last result that failed validation
+        Throwable exception = null;    // last exception we got from operation or validation
+
+        try
+        {
+            result = merge.apply(inputs);
+            validator.accept(inputs, result);
+            return;  // test passes, nothing to do
+        }
+        catch (Throwable e)
+        {
+            failedResult = result;
+            exception = e;
+        }
+
+        // Run the test with smaller inputs until the test doesn't fail anymore
+        // or we reach the max number of attempts
+        int attempt = 0;
+
+        while (attempt < 10 && inputs.stream().anyMatch(l -> !l.isEmpty()))
+        {
+            // make a copy of each input with some keys removed
+            boolean removed; // tracks if we actually removed something
+            List<List<PrimaryKey>> minimizedInputs;
+            do
+            {
+                minimizedInputs = new ArrayList<>();
+                removed = false;
+                int totalKeys = inputs.stream().mapToInt(List::size).sum();
+
+                for (List<PrimaryKey> input : inputs)
+                {
+                    ArrayList<PrimaryKey> minimized = new ArrayList<>();
+                    minimizedInputs.add(minimized);
+
+                    // We want to remove a constant fraction of keys (~10%) to make sure we converge quickly,
+                    // but we must be carefult when the number of keys gets small, so we don't end up leaving all keys
+                    // unmodified.
+                    for (PrimaryKey key : input)
+                        if (nextInt(Math.min(10, totalKeys)) != 0)
+                            minimized.add(key);
+
+                    removed |= minimized.size() < input.size();
+                }
+            } while (!removed);
+
+            try
+            {
+                result = null;  // must clean result in case operation.apply fails in the next line;
+                                // we don't want to keep a result from a previous run
+                result = merge.apply(minimizedInputs);
+                validator.accept(minimizedInputs, result);
+                attempt++;
+            }
+            catch (Throwable e)
+            {
+                // if we're still failing, then it's a success! we managed to get a smaller input
+                attempt = 0;
+                inputs = minimizedInputs;
+                failedResult = result;
+                exception = e;
+            }
+        }
+
+        System.err.println("Validation failed");
+        for (int i = 0; i < inputs.size(); i++)
+        {
+            System.err.println("\nInput " + i + ':');
+            printKeys(inputs.get(i));
+        }
+
+
+        if (failedResult != null)
+        {
+            System.err.println("\nResult:");
+            printKeys(failedResult);
+        }
+
+        throw exception;
+    }
+
+    /**
+     * Tests skipping support of the given merge operation.
+     * Works by comparing the results obtained from calling skipTo on the result merge iterator directly,
+     * with the results obtained by first materializing the full merge result and then applying skipping to
+     * the list (which is easy and obviously correct).
+     * <p>
+     * This does not test the correctness of merge operation itself.
+     * It only checks if skipping works correctly.
+     * <p>
+     * If the validation fails, it will try to first minimize the input lists in the same way as {@link #testMerge}.
+     *
+     * @param inputs some arbitrary lists of primary keys
+     * @param skips the list of positions to skip to
+     * @param mergeOperation the merge operation under test, e.g. intersection or union
+     * @throws Throwable when the merge operation or validation of results fails
+     */
+    void testSkipping(List<List<PrimaryKey>> inputs,
+                             List<Skip> skips,
+                             Function<List<List<PrimaryKey>>, KeyRangeIterator> mergeOperation) throws Throwable
+    {
+        int sizeLimit = inputs.stream().mapToInt(List::size).sum() + 10;
+
+        // The test and validation code looks very alike, but the test code performs skipping *directly*
+        // on the KeyRangeIterator, while the validation logic first materializes the merge
+        // result to an in-memory list and then applies skipping on the list.
+        try
+        {
+            testMerge(inputs,
+                      inp -> {
+                          KeyRangeIterator iterator = mergeOperation.apply(inp);
+                          return collectKeysSkipping(iterator, skips);
+                      },
+                      (inp, result) -> {
+                          KeyRangeIterator iterator = mergeOperation.apply(inp);
+                          List<PrimaryKey> merged = collectKeys(iterator, sizeLimit);
+                          List<PrimaryKey> expected = collectKeysSkipping(merged, skips);
+                          assertKeysEqual(expected, result);
+                      });
+        }
+        catch (Throwable e)
+        {
+            // Skipping informaion is not printed by testMerge, so print it here to help debugging:
+            System.err.println("\nSkipping operations:");
+            for (Skip skip : skips)
+                System.err.println(skip);
+            throw e;
+        }
+    }
+
+    /**
+     * Generates a random list of skip operations to perform on the given keys.
+     */
+    static List<Skip> randomSkips(List<PrimaryKey> keys)
+    {
+        List<Integer> skipPositions = new ArrayList<>();
+        for (int i = 0; i < keys.size(); i++)
+            skipPositions.add(nextInt(keys.size()));
+        Collections.sort(skipPositions);
+
+        List<Skip> skips = new ArrayList<>();
+        for (int pos : skipPositions)
+            skips.add(new Skip(keys.get(pos), nextInt(1, 5)));
+
+        return skips;
+    }
+
+    /**
+     * Iterates the given iterator and collects all keys into an array.
+     */
+    static List<PrimaryKey> collectKeys(KeyRangeIterator iterator, int sizeLimit)
+    {
+        try
+        {
+            List<PrimaryKey> result = new ArrayList<>();
+            while (iterator.hasNext() && result.size() < sizeLimit)
+                result.add(iterator.next());
+            return result;
+        }
+        finally
+        {
+            closeQuietly(iterator);
+        }
+    }
+
+    /**
+     * Iterates the given iterator, skipping to the given keys and collecting a chunk of keys after each skip.
+     */
+    static List<PrimaryKey> collectKeysSkipping(KeyRangeIterator iterator, List<Skip> skips)
+    {
+        try
+        {
+            List<PrimaryKey> result = new ArrayList<>();
+            for (Skip skip : skips)
+            {
+                iterator.skipTo(skip.target);
+                for (int i = 0; i < skip.chunkSize && iterator.hasNext(); i++)
+                    result.add(iterator.next());
+            }
+            return result;
+        }
+        finally
+        {
+            closeQuietly(iterator);
+        }
+    }
+
+    static List<PrimaryKey> collectKeysSkipping(List<PrimaryKey> keys, List<Skip> skips)
+    {
+        return collectKeysSkipping(PrimaryKeyListIterator.create(keys), skips);
+    }
+
+    static class Skip
+    {
+        public final PrimaryKey target;
+        public final int chunkSize;
+
+        Skip(PrimaryKey skipToKey, int chunkSize)
+        {
+            this.target = skipToKey;
+            this.chunkSize = chunkSize;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Skip: { " + "target: " + target + ", chunkSize: " + chunkSize + " }";
+        }
+    }
+
 }
