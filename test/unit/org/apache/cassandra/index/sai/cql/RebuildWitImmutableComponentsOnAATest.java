e8e718a9dd2339ab5f287a69b1f96c5d6a42ff0e
Sylvain Lebresne <lebresne@gmail.com>
2025-10-29 17:25:14 +0100

    CNDB-15898: CNDB-15745: Supports immutable components for SAI AA version (#2079)
    Updates the file formatting methods for SAI `Version.AA` to handle the
    generation used by immutable components, enabling the option to use
    immutable components with version AA.
    
    This also add a new `cassandra.sai.immutable_components.min_version`
    system property that allows to defines the minimum verison starting from
    which the existing `cassandra.sai.immutable_components` option will
    apply. As of this patch, this defaults to `ca`, because it is the
    behavior before this patch, but it can change to `aa` to allow using
    immutable components with `Version.AA`.
    
    Fixes https://github.com/riptano/cndb/issues/15745.
    
     (Rebase of commit 432b589eeca23cc66ad1e40ae9ffdfe33048b910)

---
+++ test/unit/org/apache/cassandra/index/sai/cql/RebuildWitImmutableComponentsOnAATest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.cql;
+
+import org.junit.After;
+import org.junit.Before;
+
+import org.apache.cassandra.index.sai.SAIUtil;
+import org.apache.cassandra.index.sai.disk.format.Version;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.IMMUTABLE_SAI_COMPONENTS_MIN_VERSION;
+
+public class RebuildWitImmutableComponentsOnAATest extends RebuildWithImmutableComponentsTest
+{
+    private String defaultImmutableMinVersionSetting;
+
+    @Before
+    public void setupAAVersion()
+    {
+        SAIUtil.setCurrentVersion(Version.AA);
+        defaultImmutableMinVersionSetting = IMMUTABLE_SAI_COMPONENTS_MIN_VERSION.getString();
+        IMMUTABLE_SAI_COMPONENTS_MIN_VERSION.setString("aa");
+    }
+
+    @After
+    public void restoreVersionSettings()
+    {
+        SAIUtil.setCurrentVersion(Version.CURRENT);
+        if (defaultImmutableMinVersionSetting != null)
+            IMMUTABLE_SAI_COMPONENTS_MIN_VERSION.setString(defaultImmutableMinVersionSetting);
+    }
+}
