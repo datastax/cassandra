8445e705bb712f419377ca1eda75a69d7074bfaf
Andrés de la Peña <a.penya.garcia@gmail.com>
2025-10-30 11:59:35 +0000

    Remove unused import

--- src/java/org/apache/cassandra/db/monitoring/MonitoringTask.java
+++ src/java/org/apache/cassandra/db/monitoring/MonitoringTask.java
@@ -36,7 +36,6 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.concurrent.ScheduledExecutors;
 import org.apache.cassandra.config.CassandraRelevantProperties;
-import org.apache.cassandra.config.Config;
 import org.apache.cassandra.utils.NoSpamLogger;
 
 import static java.lang.System.getProperty;
