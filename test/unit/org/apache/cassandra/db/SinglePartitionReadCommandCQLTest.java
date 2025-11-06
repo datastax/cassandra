ff5785e6a86002946927a24a52551b7bc04bf5f4
Andrés de la Peña <adelapena@users.noreply.github.com>
2025-11-06 16:45:33 +0000

    CNDB-16020: CNDB-15760: Fix AbstractReadQuery.toCQLString (#2083)
    Fix AbstractReadQuery.toCQLString to produce output that is as close to valid CQL as possible.
    The fixes here mainly come from CASSANDRA-16510, and there is also some removal of code duplication.
    
     (Rebase of commit 283f8ec906585ba48fefabd1ee6f9fb7b7cd5c70)

--- test/unit/org/apache/cassandra/db/SinglePartitionReadCommandCQLTest.java
+++ test/unit/org/apache/cassandra/db/SinglePartitionReadCommandCQLTest.java
@@ -60,10 +60,77 @@ public class SinglePartitionReadCommandCQLTest extends ReadCommandCQLTester<Sing
         assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c >= 0 AND v = 1 ALLOW FILTERING", "SELECT * FROM %s WHERE k = 0 AND c >= 0 AND v = 1 ALLOW FILTERING");
         assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c <= 0 AND v = 1 ALLOW FILTERING", "SELECT * FROM %s WHERE k = 0 AND c <= 0 AND v = 1 ALLOW FILTERING");
 
+        // test clustering-based ORDER BY
+        createTable("CREATE TABLE %s (k int, c1 int, c2 int, PRIMARY KEY (k, c1, c2))");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 ORDER BY c1",
+                          "SELECT * FROM %s WHERE k = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 ORDER BY c1 ASC",
+                          "SELECT * FROM %s WHERE k = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 ORDER BY c1 ASC, c2 ASC",
+                          "SELECT * FROM %s WHERE k = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 ORDER BY c1 DESC",
+                          "SELECT * FROM %s WHERE k = 0 ORDER BY c1 DESC, c2 DESC ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 ORDER BY c1 DESC, c2 DESC",
+                          "SELECT * FROM %s WHERE k = 0 ORDER BY c1 DESC, c2 DESC ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1",
+                          "SELECT * FROM %s WHERE k = 0 AND c1 > 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1 ASC",
+                          "SELECT * FROM %s WHERE k = 0 AND c1 > 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1 ASC, c2 ASC",
+                          "SELECT * FROM %s WHERE k = 0 AND c1 > 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1 DESC",
+                          "SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1 DESC, c2 DESC ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1 DESC, c2 DESC",
+                          "SELECT * FROM %s WHERE k = 0 AND c1 > 0 ORDER BY c1 DESC, c2 DESC ALLOW FILTERING");
+
+        // test with multi-column partition key (without index to test SinglePartitionReadCommand directly)
+        createTable("CREATE TABLE %s (k1 int, k2 int, c int, v int, PRIMARY KEY ((k1, k2), c))");
+        assertToCQLString("SELECT * FROM %s WHERE k1 = 1 AND k2 = 2",
+                          "SELECT * FROM %s WHERE k1 = 1 AND k2 = 2 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM %s WHERE k1 = 1 AND k2 = 2 AND c = 3",
+                          "SELECT * FROM %s WHERE k1 = 1 AND k2 = 2 AND c = 3 ALLOW FILTERING");
+
         // test with multi-column partition key (without index to test SinglePartitionReadCommand directly)
         createTable("CREATE TABLE %s (k1 int, k2 int, c int, v int, PRIMARY KEY ((k1, k2), c))");
         assertToCQLString("SELECT * FROM %s WHERE k1 = 1 AND k2 = 2", "SELECT * FROM %s WHERE k1 = 1 AND k2 = 2 ALLOW FILTERING");
         assertToCQLString("SELECT * FROM %s WHERE k1 = 1 AND k2 = 2 AND c = 3", "SELECT * FROM %s WHERE k1 = 1 AND k2 = 2 AND c = 3 ALLOW FILTERING");
+
+        // test literals
+        createTable("CREATE TABLE %s (k text, c text, m map<text, text>, PRIMARY KEY (k, c))");
+        assertToCQLString("SELECT m['key'] FROM %s WHERE k = 'k' AND m['key'] = 'value' ALLOW FILTERING",
+                          "SELECT m FROM %s WHERE k = 'k' AND m['key'] = 'value' ALLOW FILTERING");
+        assertToCQLString("SELECT m['key'] FROM %s WHERE k = 'k' AND c = 'c' AND m['key'] = 'value' ALLOW FILTERING",
+                          "SELECT m FROM %s WHERE k = 'k' AND c = 'c' AND m['key'] = 'value' ALLOW FILTERING");
+        assertToCQLString("SELECT m['key'] FROM %s WHERE k = 'k' AND c > 'c' AND m['key'] > 'value' ALLOW FILTERING",
+                          "SELECT m FROM %s WHERE k = 'k' AND c > 'c' AND m['key'] > 'value' ALLOW FILTERING");
+        assertToCQLString("SELECT m['key'] FROM %s WHERE k = 'k' AND c < 'c' AND m['key'] < 'value' ALLOW FILTERING",
+                          "SELECT m FROM %s WHERE k = 'k' AND c < 'c' AND m['key'] < 'value' ALLOW FILTERING");
+        assertToCQLString("SELECT m['key'] FROM %s WHERE k = 'k' AND c >= 'c' AND m['key'] >= 'value' ALLOW FILTERING",
+                          "SELECT m FROM %s WHERE k = 'k' AND c >= 'c' AND m['key'] >= 'value' ALLOW FILTERING");
+        assertToCQLString("SELECT m['key'] FROM %s WHERE k = 'k' AND c <= 'c' AND m['key'] <= 'value' ALLOW FILTERING",
+                          "SELECT m FROM %s WHERE k = 'k' AND c <= 'c' AND m['key'] <= 'value' ALLOW FILTERING");
+
+        // test with quoted identifiers
+        createKeyspace("CREATE KEYSPACE \"K\" WITH replication={ 'class' : 'SimpleStrategy', 'replication_factor' : 1 }");
+        createTable("CREATE TABLE \"K\".\"T\" (\"K\" int, \"C\" int, \"S\" int static, \"V\" int, PRIMARY KEY(\"K\", \"C\"))");
+        assertToCQLString("SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0",
+                          "SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT \"K\" FROM \"K\".\"T\" WHERE \"K\" = 0",
+                          "SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT \"S\" FROM \"K\".\"T\" WHERE \"K\" = 0",
+                          "SELECT \"S\" FROM \"K\".\"T\" WHERE \"K\" = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT \"V\" FROM \"K\".\"T\" WHERE \"K\" = 0",
+                          "SELECT \"V\" FROM \"K\".\"T\" WHERE \"K\" = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT \"K\", \"C\", \"S\", \"V\" FROM \"K\".\"T\" WHERE \"K\" = 0",
+                          "SELECT \"S\", \"V\" FROM \"K\".\"T\" WHERE \"K\" = 0 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 AND \"C\" = 1",
+                          "SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 AND \"C\" = 1 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 AND \"C\" > 1 AND \"C\" <= 2",
+                          "SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 AND \"C\" > 1 AND \"C\" <= 2 ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 ORDER BY \"C\" DESC",
+                          "SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 ORDER BY \"C\" DESC ALLOW FILTERING");
+        assertToCQLString("SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 AND \"C\" = 1 ORDER BY \"C\" DESC",
+                          "SELECT * FROM \"K\".\"T\" WHERE \"K\" = 0 AND \"C\" = 1 ORDER BY \"C\" DESC ALLOW FILTERING");
     }
 
     @Override
