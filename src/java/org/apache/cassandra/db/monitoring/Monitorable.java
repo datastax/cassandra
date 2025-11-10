3cc0b63c4aec62a1a0bec0f56c3fb0f380777b06
Andrés de la Peña <adelapena@users.noreply.github.com>
2025-11-10 11:29:33 +0000

    CNDB-16023: CNDB-15260: Add SAI-specific execution info to slow query logger (#2044)
    Adds the metrics in IndexContext and the tree view of the query's Plan
    to the log messages produced for slow SAI queries.
    
    There is a new system property named cassandra.sai.slow_query_log.execution_info_enabled
    to toggle this feature, which is enabled by default. This property can be changed
    dynamically at any time, without a restart.
    
    When SAI-specific information is added to the slow query logger, the log messages for
    slow queries can take up almost 10x the (uncompressed) disk space. In the worst of the
    worst cases, with all queries being permanently slower than 500ms, and at least 50
    different queries, reporting every 5 seconds (the default), the slow query logger can
    produce around 120MB of text per day and instance. With the detailed logging, it would
    be 1.14GB per day.
    
     (Rebase of commit 0077e1da30549d7960c18ea1b7ce8269a360b89a)

--- src/java/org/apache/cassandra/db/monitoring/Monitorable.java
+++ src/java/org/apache/cassandra/db/monitoring/Monitorable.java
@@ -18,6 +18,8 @@
 
 package org.apache.cassandra.db.monitoring;
 
+import java.util.function.Supplier;
+
 public interface Monitorable
 {
     String name();
@@ -33,4 +35,41 @@ public interface Monitorable
 
     boolean abort();
     boolean complete();
+
+    /**
+     * Returns the specific {@link ExecutionInfo} for this monitorable operation.
+     *
+     * @return the execution info for this operation
+     */
+    default ExecutionInfo executionInfo()
+    {
+        return ExecutionInfo.EMPTY;
+    }
+
+    /**
+     * Specific execution details for a monitorable operation.
+     * </p>
+     * {@link Monitorable} implementations should use this interface to hold and provide additional information about
+     * the execution of the operation. This information will be logged when the operation is reported as slow.
+     */
+    interface ExecutionInfo
+    {
+        /**
+         * An empty no-op implementation.
+         */
+        ExecutionInfo EMPTY = unique -> "";
+
+        /**
+         * A supplier for the empty implementation.
+         */
+        Supplier<ExecutionInfo> EMPTY_SUPPLIER = () -> EMPTY;
+
+        /**
+         * Returns a string representation of this execution info, suitable for logging.
+         *
+         * @param unique whether the execution info is for a single operation or an aggregation of operations
+         * @return a log-suitable string representation of this execution info
+         */
+        String toLogString(boolean unique);
+    }
 }
