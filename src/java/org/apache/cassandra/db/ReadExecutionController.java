ea6d75e87cf70c23dfac59066ddf7b1f533414c5
Michael Marshall <michael.marshall@datastax.com>
2025-11-13 08:46:00 -0600

    CNDB-15971: Only init ReadExecutionController's histogram when tracing on (#2118)
    ### What is the issue
    Fixes: https://github.com/riptano/cndb/issues/15971
    CNDB PR: https://github.com/riptano/cndb/pull/15986
    
    ### What does this PR fix and why was it fixed
    In #800, we added a histogram to the `ReadExecutionController` to track
    some metric, but it is only observed when tracing is enabled. I propose
    that instead of creating one and tracking for every query, we only do
    that when tracing is enabled.
    
    Perhaps we could remove the logic entirely, but this solution will
    likely take care of all performance concerns related to unnecessary
    allocations and unnecessary tracking when updating the histogram.

--- src/java/org/apache/cassandra/db/ReadExecutionController.java
+++ src/java/org/apache/cassandra/db/ReadExecutionController.java
@@ -54,7 +54,7 @@ public class ReadExecutionController implements AutoCloseable
     private final RepairedDataInfo repairedDataInfo;
     private int oldestUnrepairedTombstone = Integer.MAX_VALUE;
 
-    public final Histogram sstablesScannedPerRowRead;
+    private final Histogram sstablesScannedPerRowRead;
 
     ReadExecutionController(ReadCommand command,
                             OpOrder.Group baseOp,
@@ -74,7 +74,10 @@ public class ReadExecutionController implements AutoCloseable
         this.command = command;
         this.createdAtNanos = createdAtNanos;
 
-        this.sstablesScannedPerRowRead = new Histogram(new DecayingEstimatedHistogramReservoir(true));
+        // This is expensive to create, and since this is on the query hot path, we must only make it when we need it.
+        this.sstablesScannedPerRowRead = Tracing.isTracing()
+                ? new Histogram(new DecayingEstimatedHistogramReservoir(true))
+                : null;
 
         if (trackRepairedStatus)
         {
@@ -227,7 +230,7 @@ public class ReadExecutionController implements AutoCloseable
         if (createdAtNanos != NO_SAMPLING)
             addSample();
 
-        if (Tracing.traceSinglePartitions())
+        if (sstablesScannedPerRowRead != null)
         {
             var sstablesHistogram = sstablesScannedPerRowRead.getSnapshot();
             Tracing.trace("Scanned {} rows; average {} sstables scanned per row with stdev {} and max {}",
@@ -271,6 +274,9 @@ public class ReadExecutionController implements AutoCloseable
 
     public void updateSstablesIteratedPerRow(int mergedSSTablesIterated)
     {
-        sstablesScannedPerRowRead.update(mergedSSTablesIterated);
+        if (sstablesScannedPerRowRead != null)
+        {
+            sstablesScannedPerRowRead.update(mergedSSTablesIterated);
+        }
     }
 }
