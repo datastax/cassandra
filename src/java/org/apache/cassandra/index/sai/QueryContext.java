1c0c4587d280185c79c4b21c4c3ca989797b87ff
Piotr Kołaczkowski <pkolaczk@datastax.com>
2025-11-17 13:53:23 +0100

    CNDB-15508: Query planner metrics
    This commit adds new metrics related to the operation of SAI query
    planner. The metrics should help checking if the query planner makes
    proper decisions by correlating them with the other metrics,
    e.g. the metrics of the actual query execution.
    
    Per-query metrics (histograms):
    - `RowsToReturnEstimated`: the estimated number of rows to be returned
      by the query
    - `RowsToFetchEstimated`: the estimated number of rows the query
      is going to fetch from storage
    - `KeysToIterateEstimated`: the estimated number of primary keys to
      read from the indexes when executing the query to completion
    - `CostEstimated`: the abstract cost of query execution
    - `LogSelectivityEstimated`: minus decimal logarithm of query
      selectivity,  before applying the query LIMIT (0 means the query
      selects all rows, 5 means it selects 10^(-5) = 0.00001 subset
      of rows)
    - `IndexReferencesInQuery`: the number of index references in the
      unoptimized query execution plan (the same index may
      be referenced multiple times and counts separately)
    - `IndexReferencesInPlan`: the number of index references in the
      optimized query execution plan (the same index may
      be referenced multiple times and counts separately)
    
    Per-table:
    - `TotalRowsToReturnEstimated`: the sum of all estimates of returned
      rows from all completed queries
    - `TotalRowsToFetchEstimated`: the sum of all estimates of fetched
      rows from all completed queries
    - `TotalKeysToIterateEstimated`: the sum of all estimates of iterated
      primary keys from all completed queries
    - `TotalCostEstimated`: counts the sum of all cost estimates from
      all completed queries

--- src/java/org/apache/cassandra/index/sai/QueryContext.java
+++ src/java/org/apache/cassandra/index/sai/QueryContext.java
@@ -19,11 +19,15 @@
 package org.apache.cassandra.index.sai;
 
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import org.apache.cassandra.config.CassandraRelevantProperties;
 import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.index.sai.plan.Plan;
 import org.apache.cassandra.index.sai.utils.AbortedOperationException;
 import org.apache.cassandra.utils.MonotonicClock;
 
@@ -88,9 +92,7 @@ public class QueryContext
 
     private float annRerankFloor = 0.0f; // only called from single-threaded setup code
 
-    // Determines the order of using indexes for filtering and sorting.
-    // Null means the query execution order hasn't been decided yet.
-    private FilterSortOrder filterSortOrder = null;
+    private PlanInfo queryPlanInfo;
 
     @VisibleForTesting
     public QueryContext()
@@ -219,12 +221,6 @@ public class QueryContext
         annGraphSearchLatency += val;
     }
 
-    public void setFilterSortOrder(FilterSortOrder filterSortOrder)
-    {
-        checkThreadOwnership();
-        this.filterSortOrder = filterSortOrder;
-    }
-
     public void checkpoint()
     {
         checkThreadOwnership();
@@ -250,17 +246,10 @@ public class QueryContext
             annRerankFloor = max(annRerankFloor, observedFloor);
     }
 
-    /**
-     * Determines the order of filtering and sorting operations.
-     * Currently used only by vector search.
-     */
-    public enum FilterSortOrder
+    public void recordQueryPlan(Plan.RowsIteration originalPlan, Plan.RowsIteration optimizedPlan)
     {
-        /** First get the matching keys from the non-vector indexes, then use vector index to return the top K by similarity order */
-        SEARCH_THEN_ORDER,
-
-        /** First get the candidates in ANN order from the vector index, then fetch the rows and filter them until we find K matching the predicates */
-        SCAN_THEN_FILTER
+        if (CassandraRelevantProperties.SAI_QUERY_PLAN_METRICS_ENABLED.getBoolean())
+            this.queryPlanInfo = new PlanInfo(originalPlan, optimizedPlan);
     }
 
     public Snapshot snapshot()
@@ -311,7 +300,9 @@ public class QueryContext
         public final long triePostingsDecodes;
         public final long queryTimeouts;
         public final long annGraphSearchLatency;
-        public final FilterSortOrder filterSortOrder;
+
+        @Nullable
+        public final PlanInfo queryPlanInfo;
 
         /**
          * Creates a snapshot of all the metrics in the given {@link QueryContext}.
@@ -339,7 +330,38 @@ public class QueryContext
             triePostingsDecodes = context.triePostingsDecodes;
             queryTimeouts = context.queryTimeouts;
             annGraphSearchLatency = context.annGraphSearchLatency;
-            filterSortOrder = context.filterSortOrder;
+            queryPlanInfo = context.queryPlanInfo;
+        }
+    }
+
+    /**
+     * Captures relevant information about a query plan, both original and optimized.
+     */
+    public static class PlanInfo
+    {
+        public final boolean searchExecutedBeforeOrder;
+        public final boolean filterExecutedAfterOrderedScan;
+
+        public final long costEstimated;
+        public final long rowsToReturnEstimated;
+        public final long rowsToFetchEstimated;
+        public final long keysToIterateEstimated;
+        public final int logSelectivityEstimated;
+
+        public final int indexReferencesInQuery;
+        public final int indexReferencesInPlan;
+
+        public PlanInfo(@Nonnull Plan.RowsIteration originalPlan, @Nonnull Plan.RowsIteration optimizedPlan)
+        {
+            this.costEstimated = Math.round(optimizedPlan.fullCost());
+            this.rowsToReturnEstimated = Math.round(optimizedPlan.expectedRows());
+            this.rowsToFetchEstimated = Math.round(optimizedPlan.estimatedRowsToFetch());
+            this.keysToIterateEstimated = Math.round(optimizedPlan.estimatedKeysToIterate());
+            this.logSelectivityEstimated = Math.min(20, (int) Math.floor(-Math.log10(optimizedPlan.selectivity())));
+            this.indexReferencesInQuery = originalPlan.referencedIndexCount();
+            this.indexReferencesInPlan = optimizedPlan.referencedIndexCount();
+            this.searchExecutedBeforeOrder = optimizedPlan.isSearchThenOrderHybrid();
+            this.filterExecutedAfterOrderedScan = optimizedPlan.isOrderedScanThenFilterHybrid();
         }
     }
 }
