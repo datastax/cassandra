25ecbb8948880e288c90ab9153057883f5c6c7a1
Zhao Yang <zhaoyangsingapore@gmail.com>
2025-11-18 12:12:06 +0800

    CNDB-16173: CNDB-15621: expose approximateTimeUUID in ULIDBasedSSTableId (#2113)
    expose TimeUUID from ULIDBasedSSTableId
    
     (Rebase of commit 78550b79e91fe3f16bae61c2e77ab2cb97f037c7)

--- src/java/org/apache/cassandra/io/sstable/ULIDBasedSSTableId.java
+++ src/java/org/apache/cassandra/io/sstable/ULIDBasedSSTableId.java
@@ -58,6 +58,14 @@ public final class ULIDBasedSSTableId implements SSTableId<ULIDBasedSSTableId>
         return new TimeUUID(rawTimestamp, ulid.getLeastSignificantBits());
     }
 
+    /**
+     * @return approximated {@link TimeUUID} based on ulid
+     */
+    public TimeUUID getApproximateTimeUUID()
+    {
+        return approximateTimeUUID;
+    }
+
     @Override
     public ByteBuffer asBytes()
     {
