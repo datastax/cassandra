23c2479383e636dc554dda27cf48a2bab57829d8
Piotr Kołaczkowski <pkolaczk@datastax.com>
2026-01-07 18:07:01 +0100

    CNDB-16363: Improve matched rows estimation accuracy for memory indexes
    When a memory index contains very few rows and is split into
    many shards, we can expect a lot of variance in the number of rows
    between the shards. Hence, if we took only one
    shard to estimate the number of matched rows, and
    extrapolate that on all shards to compute the estimated matching rows
    from the whole index, we risk making a huge estimation error.
    
    This commit changes the algorithm to take as many shards as needed
    to collect enough returned or indexed rows. For very
    tiny datasets is it's likely to use all shards for estimation.
    For big datasets, one shard will likely be enough, speeding up
    estimation.
    
    This change also allows to remove one estimation method.
    We no longer need to manually choose between the estimation
    from the first shard and from all shards.
    
    Additionally, the accuracy of estimating of NOT_EQ rows has been
    improved by letting the planner know the union generated by NOT_EQ
    is disjoint so the result set cardinality is the sum of cardinalities
    of the subplans.

--- src/java/org/apache/cassandra/index/sai/memory/MemtableIndex.java
+++ src/java/org/apache/cassandra/index/sai/memory/MemtableIndex.java
@@ -84,18 +84,7 @@ public interface MemtableIndex extends MemtableOrdering
      * @param keyRange   the key range to search within
      * @return an approximate number of the matching rows
      */
-    long estimateMatchingRowsCountUsingFirstShard(Expression expression, AbstractBounds<PartitionPosition> keyRange);
-
-    /**
-     * Estimates the number of rows that would be returned by this index given the predicate.
-     * It estimates from all relevant shards individually.
-     * Note that this is not a guarantee of the number of rows that will actually be returned.
-     *
-     * @param expression predicate to match
-     * @param keyRange   the key range to search within
-     * @return an estimated number of the matching rows
-     */
-    long estimateMatchingRowsCountUsingAllShards(Expression expression, AbstractBounds<PartitionPosition> keyRange);
+    long estimateMatchingRowsCount(Expression expression, AbstractBounds<PartitionPosition> keyRange);
 
     Iterator<Pair<ByteComparable.Preencoded, List<MemoryIndex.PkWithFrequency>>> iterator(DecoratedKey min, DecoratedKey max);
 
