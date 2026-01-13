5bcce1f47ae230a1d8c571d1d4ed08893bd178be
Scott Fines <scott.fines@ibm.com>
2026-01-13 13:43:07 -0600

    CNDB-16145: Split VectorSiftSmallTest into multiple components to avoid ant timeouts, and add explicit junit timeouts.

---
+++ test/unit/org/apache/cassandra/index/sai/cql/NVQEnabledVectorSiftSmallTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.cql;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import org.apache.cassandra.index.sai.SAIUtil;
+import org.apache.cassandra.index.sai.disk.format.Version;
+import org.apache.cassandra.index.sai.disk.vector.NVQUtil;
+
+@RunWith(Parameterized.class)
+public class NVQEnabledVectorSiftSmallTest extends VectorSiftSmallTest
+{
+    @Parameterized.Parameter
+    public Version version;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data()
+    {
+        // See Version file for explanation of changes associated with each version
+        return Version.ALL.stream()
+                          .filter(v -> v.onOrAfter(Version.JVECTOR_EARLIEST))
+               .filter(NVQUtil::versionSupportsNVQ)
+               .map(v -> new Object[]{v})
+               .collect(Collectors.toList());
+    }
+
+    @Before
+    public void setCurrentVersion() throws Throwable
+    {
+        SAIUtil.setCurrentVersion(version);
+    }
+
+    @Before
+    public void setEnableNVQ()
+    {
+        SAIUtil.setEnableNVQ(true);
+    }
+}
+
