msmygit/vscassandra

STAR-745: Recreate BF on fp chance change and limit the total memory usage (#177)
* STAR-745: Add BloomFilter global memory limit

Added a memory limiter which is a global instance (static field in BloomFilter) which monitors
the total memory used by all created (and deserialized) Bloom filters. When the limit is reached
we return a dummy always-true filter with a log error message.

* STAR-745: Recreate Bloom filter on startup

In certain situations BF will be recreated on startup (for example, if FP chance changed
more than the defined tolerance).

Reviewed by: Daniel Jatnieks <jatnieks@pobox.com>

Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>

src/java/org/apache/cassandra/utils/BloomFilterSerializer.java
@@ -20,40 +20,61 @@ package org.apache.cassandra.utils;
 import java.io.DataInputStream;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.cassandra.db.TypeSizes;
 import org.apache.cassandra.io.util.DataOutputPlus;
 import org.apache.cassandra.utils.obs.IBitSet;
+import org.apache.cassandra.utils.obs.MemoryLimiter;
 import org.apache.cassandra.utils.obs.OffHeapBitSet;
+
+import static org.apache.cassandra.utils.FilterFactory.AlwaysPresent;
 
 public final class BloomFilterSerializer
 {
-    private BloomFilterSerializer()
+    private final static Logger logger = LoggerFactory.getLogger(BloomFilterSerializer.class);
+
+    private final MemoryLimiter memoryLimiter;
+
+    public BloomFilterSerializer(MemoryLimiter memoryLimiter)
     {
+        this.memoryLimiter = memoryLimiter;
     }
 
-    public static void serialize(BloomFilter bf, DataOutputPlus out) throws IOException
+    public void serialize(BloomFilter bf, DataOutputPlus out) throws IOException
     {
         out.writeInt(bf.hashCount);
         bf.bitset.serialize(out);
     }
 
     @SuppressWarnings("resource")
-    public static BloomFilter deserialize(DataInputStream in, boolean oldBfFormat) throws IOException
+    public IFilter deserialize(DataInputStream in, boolean oldBfFormat) throws IOException
     {
         int hashes = in.readInt();
-        IBitSet bs = OffHeapBitSet.deserialize(in, oldBfFormat);
-
+        IBitSet bs;
+        try
+        {
+            bs = OffHeapBitSet.deserialize(in, oldBfFormat, memoryLimiter);
+        }
+        catch (MemoryLimiter.ReachedMemoryLimitException | OutOfMemoryError e)
+        {
+            logger.error("Failed to create Bloom filter during deserialization: ({}) - " +
+                         "continuing but this will have severe performance implications, consider increasing FP chance or" +
+                         "lowering number of sstables through compaction", e.getMessage());
+            return AlwaysPresent;
+        }
         return new BloomFilter(hashes, bs);
     }
 
     /**
      * Calculates a serialized size of the given Bloom Filter
-     * @param bf Bloom filter to calculate serialized size
-     * @see org.apache.cassandra.io.ISerializer#serialize(Object, org.apache.cassandra.io.util.DataOutputPlus)
      *
+     * @param bf Bloom filter to calculate serialized size
      * @return serialized size of the given bloom filter
+     * @see org.apache.cassandra.io.ISerializer#serialize(Object, org.apache.cassandra.io.util.DataOutputPlus)
      */
-    public static long serializedSize(BloomFilter bf)
+    public long serializedSize(BloomFilter bf)
     {
         int size = TypeSizes.sizeof(bf.hashCount); // hash count
         size += bf.bitset.serializedSize();

datastax/cassandra

(cherry picked from commit c1a2595e556cb325fe915febd6c57b77955a09d8)
(cherry picked from commit a75b01a98120636d051f53301f82b4fff08c9fb1)
(cherry picked from commit de52f5c03f011cce5db1a878289347d1eab24d0e)
(cherry picked from commit 1a0ae2c651390788d967a8491241c5e1edae16c3)
(cherry picked from commit 191305d43da0f0bb5314b1b22937d21a6ef5ef28)
(cherry picked from commit 140d446587c142b38e86f740052733fcd4e202ce)
(cherry picked from commit ed14633364dea4b413fadc55ec395527d05ebe08)
(cherry picked from commit 82e681662803a48d25367ff4d55491d247fb5958)
STAR-745 Fix compile issues and adapt memory limiter use in BloomFilterSerializer.
(cherry picked from commit 69d15452abcbb47e28b3edda31e7f1497894154c)
 (Rebase of commit 1a6550efc9c9894e9a7659c407ad368baeb1ad49)

src/java/org/apache/cassandra/utils/BloomFilterSerializer.java
@@ -19,15 +19,20 @@ package org.apache.cassandra.utils;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.cassandra.db.TypeSizes;
 import org.apache.cassandra.io.IGenericSerializer;
 import org.apache.cassandra.io.util.DataInputPlus.DataInputStreamPlus;
 import org.apache.cassandra.io.util.DataOutputStreamPlus;
 import org.apache.cassandra.utils.obs.IBitSet;
+import org.apache.cassandra.utils.obs.MemoryLimiter;
 import org.apache.cassandra.utils.obs.OffHeapBitSet;
 
 public final class BloomFilterSerializer implements IGenericSerializer<BloomFilter, DataInputStreamPlus, DataOutputStreamPlus>
 {
+    private final static Logger logger = LoggerFactory.getLogger(BloomFilterSerializer.class);
     public final static BloomFilterSerializer newFormatInstance = new BloomFilterSerializer(false);
     public final static BloomFilterSerializer oldFormatInstance = new BloomFilterSerializer(true);
 
@@ -72,8 +77,21 @@ public final class BloomFilterSerializer implements IGenericSerializer<BloomFilt
     @Override
     public BloomFilter deserialize(DataInputStreamPlus in) throws IOException
     {
+        return deserialize(in, BloomFilter.memoryLimiter);
+    }
+
+    public BloomFilter deserialize(DataInputStreamPlus in, MemoryLimiter memoryLimiter) throws IOException
+    {
         int hashes = in.readInt();
-        IBitSet bs = OffHeapBitSet.deserialize(in, oldFormat);
+        IBitSet bs;
+        try
+        {
+            bs = OffHeapBitSet.deserialize(in, oldFormat, memoryLimiter);
+        }
+        catch (MemoryLimiter.ReachedMemoryLimitException | OutOfMemoryError e)
+        {
+            throw new RuntimeException("Out of native memory occured, You can avoid it by increasing the system ram space or by increasing bloom_filter_fp_chance.");
+        }
 
         return new BloomFilter(hashes, bs);
     }