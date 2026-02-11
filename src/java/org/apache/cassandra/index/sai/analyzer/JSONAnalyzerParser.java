msmygit/vscassandra

STAR-123 custom lucene analyzer and some language unit tests (#192)
added more language tests

added brazilian

cql test passes

added support for setting a lucene analyzer

cql json test passes

fixed up some things

cleanup

added query analyzer

cleanup; added constants

added exception handling in unit test

added bad options unit tests

added char filter

removed comments and extra code

added illegal arg ex to LuceneAnalyzer#hasNext

added stop word support; prior to removal

reworked, no more stop words

added lowercase filter test

added ngram filter test

added simplepattern test; snowball off

added czech and porter

fixed alloc

removed commented out code

removed extra code

fixed minor issues

maybe fixex setMinMax

cleanup

reverted

reverted to a new byte[] per tokenized term

cleanup

cleanup

fixed sasi test

fixed unit test bug

cleanup

refactored for npe

addressed review comments

fixed npe bug

fixed a couple of bugs

removed json_ from options names; applied sonar comments

fixed sonar comments

fixed unit test bug

changed exception thrown

get -> create

fixed minor issue

src/java/org/apache/cassandra/index/sai/analyzer/JSONAnalyzerParser.java
@@ -1,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.analyzer;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import org.apache.cassandra.cql3.Json;
+import org.apache.cassandra.exceptions.InvalidRequestException;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+
+public class JSONAnalyzerParser
+{
+    // unsupported because these filters open external files such as stop words
+    public static final Set<String> unsupportedFilters =
+    Sets.newHashSet("synonymgraph", // same as synonym
+                    "synonym", // replaces words, loads external file, could be implemented
+                    "commongrams", // loads external file terms, search feature
+                    "stop", // stop words remove terms which doens't make sense for a database index
+                    "snowballporter"); // bug in reflection instantiation
+
+    public static Analyzer parse(String json) throws IOException
+    {
+        List<Map<String,String>> list = (List<Map<String,String>>) Json.decodeJson(json);
+        CustomAnalyzer.Builder builder = CustomAnalyzer.builder();
+        for (int x = 0; x < list.size(); x++)
+        {
+            final Map<String,String> map = list.get(x);
+
+            // remove from the map to avoid passing as parameters to the TokenFilterFactory
+            final String tokenizer = map.remove("tokenizer");
+            if (tokenizer != null)
+            {
+                builder.withTokenizer(tokenizer, map);
+            }
+            final String filter = map.remove("filter");
+            if (filter != null)
+            {
+                if (unsupportedFilters.contains(filter))
+                {
+                    throw new InvalidRequestException("filter=" + filter + " is unsupported.");
+                }
+                builder.addTokenFilter(filter, map);
+            }
+
+            final String charfilter = map.remove("charfilter");
+            if (charfilter != null)
+            {
+                builder.addCharFilter(charfilter, map);
+            }
+        }
+        return builder.build();
+    }
+}

src/java/org/apache/cassandra/index/sai/memory/TrieMemoryIndex.java
@@ -66,7 +66,7 @@ public class TrieMemoryIndex extends MemoryIndex
     private final MemtableTrie<PrimaryKeys> data;
     private final ClusteringComparator clusteringComparator;
     private final PrimaryKeysReducer primaryKeysReducer;
-    private final AbstractAnalyzer analyzer;
+    private final AbstractAnalyzer.AnalyzerFactory analyzerFactory;
     private final AbstractType<?> validator;
     private final boolean isLiteral;
     private final Object writeLock = new Object();
@@ -88,7 +88,7 @@ public class TrieMemoryIndex extends MemoryIndex
         this.clusteringComparator = columnContext.clusteringComparator();
         this.primaryKeysReducer = new PrimaryKeysReducer();
         // MemoryIndex is per-core, so analyzer should be thread-safe..
-        this.analyzer = columnContext.getAnalyzer();
+        this.analyzerFactory = columnContext.getAnalyzerFactory();
         this.validator = columnContext.getValidator();
         this.isLiteral = TypeUtil.isLiteral(validator);
     }
@@ -98,39 +98,48 @@ public class TrieMemoryIndex extends MemoryIndex
     {
         synchronized (writeLock)
         {
-            AbstractAnalyzer analyzer = columnContext.getAnalyzer();
-            value = TypeUtil.encode(value, validator);
-            analyzer.reset(value.duplicate());
-            final PrimaryKey primaryKey = PrimaryKey.of(key, clustering);
-            final long initialSizeOnHeap = data.sizeOnHeap();
-            final long initialSizeOffHeap = data.sizeOffHeap();
-            final long reducerHeapSize = primaryKeysReducer.heapAllocations();
-
-
-            while (analyzer.hasNext())
+            AbstractAnalyzer analyzer = analyzerFactory.create();
+            try
             {
-                final ByteBuffer term = analyzer.next();
-                setMinMaxTerm(term);
+                value = TypeUtil.encode(value, validator);
+                analyzer.reset(value.duplicate());
+                final PrimaryKey primaryKey = PrimaryKey.of(key, clustering);
+                final long initialSizeOnHeap = data.sizeOnHeap();
+                final long initialSizeOffHeap = data.sizeOffHeap();
+                final long reducerHeapSize = primaryKeysReducer.heapAllocations();
 
-                final ByteComparable encodedTerm = encode(term);
-                try
+                while (analyzer.hasNext())
                 {
-                    if (term.limit() <= MAX_RECURSIVE_KEY_LENGTH)
+                    final ByteBuffer term = analyzer.next();
+
+                    setMinMaxTerm(term.duplicate());
+
+                    final ByteComparable encodedTerm = encode(term.duplicate());
+
+                    try
                     {
-                        data.putRecursive(encodedTerm, primaryKey, primaryKeysReducer);
+                        if (term.limit() <= MAX_RECURSIVE_KEY_LENGTH)
+                        {
+                            data.putRecursive(encodedTerm, primaryKey, primaryKeysReducer);
+                        }
+                        else
+                        {
+                            data.apply(Trie.singleton(encodedTerm, primaryKey), primaryKeysReducer);
+                        }
                     }
-                    else
+                    catch (MemtableTrie.SpaceExhaustedException e)
                     {
-                        data.apply(Trie.singleton(encodedTerm, primaryKey), primaryKeysReducer);
+                        //TODO Handle this properly
+                        throw new RuntimeException(e);
                     }
                 }
-                catch (MemtableTrie.SpaceExhaustedException e)
-                {
-                    //TODO Handle this properly
-                    throw new RuntimeException(e);
-                }
+
+                return (data.sizeOnHeap() - initialSizeOnHeap) + (data.sizeOffHeap() - initialSizeOffHeap) + (primaryKeysReducer.heapAllocations() - reducerHeapSize);
             }
-            return (data.sizeOnHeap() - initialSizeOnHeap) + (data.sizeOffHeap() - initialSizeOffHeap) + (primaryKeysReducer.heapAllocations() - reducerHeapSize);
+            finally
+            {
+                analyzer.end();
+            }
         }
     }
 

datastax/cassandra

(cherry picked from commit 3227a57ab49a94dd8bb80bf48c429667c969eb52)
(cherry picked from commit add6b8d8e626d5ab4a31bdda6fcb75eed4890438)
(cherry picked from commit 1c60b2d462190ca65ef88fcfc5b0c4e56315befe)
(cherry picked from commit 4d3912f4e5150eb0d917b689f6aefe59099339b7)
(cherry picked from commit 5afe63d6b0dc2ac37ab4f1ee8278c2ccff5ccb95)
(cherry picked from commit d6c1172cd8fb2c6ddceb1dd915f364ebd69874e4)
(cherry picked from commit 4b7b55702ba90c9f7b0b29870e84c6dc89a6670f)
STAR-123 Fix rebase compile errors and replace lucene-analysis-common with lucene-analyzers-common needed by CC code
 (Rebase of commit cf85206e5251d2459f5316605e0a49c10cf44a68)

src/java/org/apache/cassandra/index/sai/analyzer/JSONAnalyzerParser.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.index.sai.analyzer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import org.apache.cassandra.exceptions.InvalidRequestException;
+import org.apache.cassandra.utils.JsonUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+
+public class JSONAnalyzerParser
+{
+    // unsupported because these filters open external files such as stop words
+    public static final Set<String> unsupportedFilters =
+    Sets.newHashSet("synonymgraph", // same as synonym
+                    "synonym", // replaces words, loads external file, could be implemented
+                    "commongrams", // loads external file terms, search feature
+                    "stop", // stop words remove terms which doens't make sense for a database index
+                    "snowballporter"); // bug in reflection instantiation
+
+    public static Analyzer parse(String json) throws IOException
+    {
+        List<Map<String,String>> list = (List<Map<String,String>>) JsonUtils.decodeJson(json);
+        CustomAnalyzer.Builder builder = CustomAnalyzer.builder();
+        for (int x = 0; x < list.size(); x++)
+        {
+            final Map<String,String> map = list.get(x);
+
+            // remove from the map to avoid passing as parameters to the TokenFilterFactory
+            final String tokenizer = map.remove("tokenizer");
+            if (tokenizer != null)
+            {
+                builder.withTokenizer(tokenizer, map);
+            }
+            final String filter = map.remove("filter");
+            if (filter != null)
+            {
+                if (unsupportedFilters.contains(filter))
+                {
+                    throw new InvalidRequestException("filter=" + filter + " is unsupported.");
+                }
+                builder.addTokenFilter(filter, map);
+            }
+
+            final String charfilter = map.remove("charfilter");
+            if (charfilter != null)
+            {
+                builder.addCharFilter(charfilter, map);
+            }
+        }
+        return builder.build();
+    }
+}

src/java/org/apache/cassandra/index/sai/memory/TrieMemoryIndex.java
@@ -66,7 +66,7 @@ public class TrieMemoryIndex extends MemoryIndex
     private final InMemoryTrie<PrimaryKeys> data;
     private final ClusteringComparator clusteringComparator;
     private final PrimaryKeysReducer primaryKeysReducer;
-    private final AbstractAnalyzer analyzer;
+    private final AbstractAnalyzer.AnalyzerFactory analyzerFactory;
     private final AbstractType<?> validator;
     private final boolean isLiteral;
     private final Object writeLock = new Object();
@@ -88,7 +88,7 @@ public class TrieMemoryIndex extends MemoryIndex
         this.clusteringComparator = columnContext.clusteringComparator();
         this.primaryKeysReducer = new PrimaryKeysReducer();
         // MemoryIndex is per-core, so analyzer should be thread-safe..
-        this.analyzer = columnContext.getAnalyzer();
+        this.analyzerFactory = columnContext.getAnalyzerFactory();
         this.validator = columnContext.getValidator();
         this.isLiteral = TypeUtil.isLiteral(validator);
     }
@@ -98,39 +98,48 @@ public class TrieMemoryIndex extends MemoryIndex
     {
         synchronized (writeLock)
         {
-            AbstractAnalyzer analyzer = columnContext.getAnalyzer();
-            value = TypeUtil.asIndexBytes(value, validator);
-            analyzer.reset(value.duplicate());
-            final PrimaryKey primaryKey = PrimaryKey.of(key, clustering);
-            final long initialSizeOnHeap = data.sizeOnHeap();
-            final long initialSizeOffHeap = data.sizeOffHeap();
-            final long reducerHeapSize = primaryKeysReducer.heapAllocations();
-
-
-            while (analyzer.hasNext())
+            AbstractAnalyzer analyzer = analyzerFactory.create();
+            try
             {
-                final ByteBuffer term = analyzer.next();
-                setMinMaxTerm(term);
+                value = TypeUtil.asIndexBytes(value, validator);
+                analyzer.reset(value.duplicate());
+                final PrimaryKey primaryKey = PrimaryKey.of(key, clustering);
+                final long initialSizeOnHeap = data.sizeOnHeap();
+                final long initialSizeOffHeap = data.sizeOffHeap();
+                final long reducerHeapSize = primaryKeysReducer.heapAllocations();
 
-                final ByteComparable encodedTerm = encode(term);
-                try
+                while (analyzer.hasNext())
                 {
-                    if (term.limit() <= MAX_RECURSIVE_KEY_LENGTH)
+                    final ByteBuffer term = analyzer.next();
+
+                    setMinMaxTerm(term.duplicate());
+
+                    final ByteComparable encodedTerm = encode(term.duplicate());
+
+                    try
                     {
-                        data.putRecursive(encodedTerm, primaryKey, primaryKeysReducer);
+                        if (term.limit() <= MAX_RECURSIVE_KEY_LENGTH)
+                        {
+                            data.putRecursive(encodedTerm, primaryKey, primaryKeysReducer);
+                        }
+                        else
+                        {
+                            data.apply(Trie.singleton(encodedTerm, primaryKey), primaryKeysReducer);
+                        }
                     }
-                    else
+                    catch (InMemoryTrie.SpaceExhaustedException e)
                     {
-                        data.apply(Trie.singleton(encodedTerm, primaryKey), primaryKeysReducer);
+                        //TODO Handle this properly
+                        throw new RuntimeException(e);
                     }
                 }
-                catch (InMemoryTrie.SpaceExhaustedException e)
-                {
-                    //TODO Handle this properly
-                    throw new RuntimeException(e);
-                }
+
+                return (data.sizeOnHeap() - initialSizeOnHeap) + (data.sizeOffHeap() - initialSizeOffHeap) + (primaryKeysReducer.heapAllocations() - reducerHeapSize);
             }
-            return (data.sizeOnHeap() - initialSizeOnHeap) + (data.sizeOffHeap() - initialSizeOffHeap) + (primaryKeysReducer.heapAllocations() - reducerHeapSize);
+            finally
+            {
+                analyzer.end();
+            }
         }
     }
 