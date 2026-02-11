msmygit/vscassandra

STAR-1150 (#362)
Add clearCache to Roles and AuthenticatedUser;
Return affected roles from IAuthorizer.revokeAllOn;
Add IRoleManager.getRoleMemberOf

src/java/org/apache/cassandra/auth/CassandraRoleManager.java
@@ -24,6 +24,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
@@ -118,6 +119,7 @@ public class CassandraRoleManager implements IRoleManager
     }
 
     private SelectStatement loadRoleStatement;
+    private SelectStatement loadRoleMembersStatement;
 
     private final Set<Option> supportedOptions;
     private final Set<Option> alterableOptions;
@@ -140,6 +142,10 @@ public class CassandraRoleManager implements IRoleManager
         loadRoleStatement = (SelectStatement) prepare("SELECT * from %s.%s WHERE role = ?",
                                                       SchemaConstants.AUTH_KEYSPACE_NAME,
                                                       AuthKeyspace.ROLES);
+
+        loadRoleMembersStatement = (SelectStatement) prepare("SELECT member FROM %s.%s WHERE role = ?",
+                                                      SchemaConstants.AUTH_KEYSPACE_NAME,
+                                                      AuthKeyspace.ROLE_MEMBERS);
         scheduleSetupTask(() -> {
             setupDefaultRole();
             return null;
@@ -255,6 +261,19 @@ public class CassandraRoleManager implements IRoleManager
         return collectRoles(getRole(grantee.getRoleName()),
                             true,
                             filter())
+               .collect(Collectors.toSet());
+    }
+
+    public Set<RoleResource> getMembersOf(RoleResource role)
+    {
+        // Get the membership list of the given role
+        QueryOptions options = QueryOptions.forInternalCalls(consistencyForRole(role.getRoleName()),
+                                                             Collections.singletonList(ByteBufferUtil.bytes(role.getRoleName())));
+        ResultMessage.Rows rows = select(loadRoleMembersStatement, options);
+        UntypedResultSet resultSet = UntypedResultSet.create(rows.result);
+
+        return StreamSupport.stream(resultSet.spliterator(), false)
+               .map(row -> RoleResource.role(row.getString("member")))
                .collect(Collectors.toSet());
     }
 

datastax/cassandra

(cherry picked from commit bc83cd92b6836e862e5aa98c18f785efcbc2558d)
(cherry picked from commit d13899e33219e1b4f7c0fa9b35a59c1818f34afb)
(cherry picked from commit 49646a9e27869b94e0ee8fbe3609439f0b55e3b4)
(cherry picked from commit a0f37668478922846bc47840df0187903fc2b520)
(cherry picked from commit af0b6233ed4febc3189f2ff7782e0b1e0a4fa259)
 (Rebase of commit b65d0b848102b0b25cec8acdbb23d9d63ea9b814)

src/java/org/apache/cassandra/auth/CassandraRoleManager.java
@@ -26,6 +26,7 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
@@ -129,6 +130,7 @@ public class CassandraRoleManager implements IRoleManager
 
     private SelectStatement loadRoleStatement;
     private SelectStatement loadIdentityStatement;
+    private SelectStatement loadRoleMembersStatement;
 
     private final Set<Option> supportedOptions;
     private final Set<Option> alterableOptions;
@@ -148,6 +150,9 @@ public class CassandraRoleManager implements IRoleManager
     {
         loadRoleStatement();
         loadIdentityStatement();
+        loadRoleMembersStatement = (SelectStatement) prepare("SELECT member FROM %s.%s WHERE role = ?",
+                                                      SchemaConstants.AUTH_KEYSPACE_NAME,
+                                                      AuthKeyspace.ROLE_MEMBERS);
         scheduleSetupTask(() -> {
             setupDefaultRole();
             return null;
@@ -345,6 +350,19 @@ public class CassandraRoleManager implements IRoleManager
                             true,
                             filter(),
                             this::getRole)
+               .collect(Collectors.toSet());
+    }
+
+    public Set<RoleResource> getMembersOf(RoleResource role)
+    {
+        // Get the membership list of the given role
+        QueryOptions options = QueryOptions.forInternalCalls(consistencyForRoleRead(role.getRoleName()),
+                                                             Collections.singletonList(ByteBufferUtil.bytes(role.getRoleName())));
+        ResultMessage.Rows rows = select(loadRoleMembersStatement, options);
+        UntypedResultSet resultSet = UntypedResultSet.create(rows.result);
+
+        return StreamSupport.stream(resultSet.spliterator(), false)
+               .map(row -> RoleResource.role(row.getString("member")))
                .collect(Collectors.toSet());
     }
 