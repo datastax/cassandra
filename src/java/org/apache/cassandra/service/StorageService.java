544a9fbbb830bc4e1fcce8f32b24387ff248ae78
mck <mck@apache.org>
2026-02-13 09:34:00 +0100

    STAR-891 Add pluggable disk/commit log error handlers and shutdown hooks (#271)
    (cherry picked from commit c6cd4ee6673f9b43f7de7630e7669a873f4bf01c)
    (cherry picked from commit fb5c241b5bf22ae4683152b82a04605f925ef108)
    (cherry picked from commit f7f8e0a3adfcc09dcf365a6d452d9bbdbe6c76a9)
    (cherry picked from commit 0ff7339d9da4a64994c9d92ff89832463ffbcf55)
    (cherry picked from commit e7ca94c40ddd904ca50bb1f734a815b6a7ec620e)
    
    STAR-891 Fix conflict in DiskFailurePolicyTest with CASSANDRA-18294
    
    (cherry picked from commit 07cd5c9dfdcb44141d7b70a475864b01fad02fc7)
    
     (Rebase of commit 3ac50e1aa906b4b00c9376c7ffd042ad9c365b4f)
    
     (Rebase of commit a2340fa25152d43d76227e844bfc0723f1400fe4)

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -337,7 +337,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public volatile VersionedValue.VersionedValueFactory valueFactory = new VersionedValue.VersionedValueFactory(TokenMetadataProvider.instance.getTokenMetadata().partitioner);
 
-    private Thread drainOnShutdown = null;
     private volatile boolean isShutdown = false;
     private final List<Runnable> preShutdownHooks = new ArrayList<>();
     private final List<Runnable> postShutdownHooks = new ArrayList<>();
@@ -991,7 +990,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         }
 
         // daemon threads, like our executors', continue to run while shutdown hooks are invoked
-        drainOnShutdown = NamedThreadFactory.createThread(new WrappedRunnable()
+        Thread drainOnShutdown = NamedThreadFactory.createThread(new WrappedRunnable()
         {
             @Override
             public void runMayThrow() throws InterruptedException, ExecutionException, IOException
@@ -1012,8 +1011,9 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
                 }
             }
         }, "StorageServiceShutdownHook");
-        Runtime.getRuntime().addShutdownHook(drainOnShutdown);
+
         registerMBeans();
+        JVMStabilityInspector.registerShutdownHook(drainOnShutdown, this::onShutdownHookRemoved);
 
         replacing = isReplacing();
 
@@ -1118,12 +1118,9 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     /**
      * In the event of forceful termination we need to remove the shutdown hook to prevent hanging (OOM for instance)
      */
-    public void removeShutdownHook()
+    public void onShutdownHookRemoved()
     {
         PathUtils.clearOnExitThreads();
-
-        if (drainOnShutdown != null)
-            Runtime.getRuntime().removeShutdownHook(drainOnShutdown);
     }
 
     private boolean shouldBootstrap()
@@ -7126,15 +7123,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
             logger.info("Disabling rejection of requests on tokens outside owned ranges");
 
         DatabaseDescriptor.setRejectOutOfTokenRangeRequests(enabled);
-    }
-
-    @VisibleForTesting
-    public void shutdownServer()
-    {
-        if (drainOnShutdown != null)
-        {
-            Runtime.getRuntime().removeShutdownHook(drainOnShutdown);
-        }
     }
 
     @Override
