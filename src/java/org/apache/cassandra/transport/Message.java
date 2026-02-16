msmygit/vscassandra

STAR-1249 Relax access to setTracingId in Message (#417)

src/java/org/apache/cassandra/transport/Message.java
@@ -277,7 +277,7 @@ public abstract class Message
                 throw new IllegalArgumentException();
         }
 
-        Message setTracingId(UUID tracingId)
+        public Message setTracingId(UUID tracingId)
         {
             this.tracingId = tracingId;
             return this;

datastax/cassandra

(cherry picked from commit bbd4a1b07fe731e98c12a00c6c4d06cc34bc82ee)
(cherry picked from commit 2362c83b7f4dddbbc8000af0982bbaeeb0dc1175)
(cherry picked from commit 6227c5bbbad4d88fc9c2c71a8d7138967ac3e1e3)
(cherry picked from commit 3c8b7c8883212bd6a8b0083867692a4a8c1d54ab)
(cherry picked from commit 282c66b0ef475734d411bd52d6c8f011082cd4d8)
 (Rebase of commit 5c8f2f093296b8438e59b92e3217c6ed15e867f8)

src/java/org/apache/cassandra/transport/Message.java
@@ -303,7 +303,7 @@ public abstract class Message
                 throw new IllegalArgumentException();
         }
 
-        Message setTracingId(TimeUUID tracingId)
+        public Message setTracingId(TimeUUID tracingId)
         {
             this.tracingId = tracingId;
             return this;