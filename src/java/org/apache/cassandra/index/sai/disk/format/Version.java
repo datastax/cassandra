f8d26ab835dbd6d9d92553add46a11f441cae7b6
Ruslan Fomkin <ruslan.fomkin@ibm.com>
2026-02-18 15:12:15 +0100

    CNDB-16762 use the index name constant and validate it (#2232)
    ### What is the issue
    
    Index name length is controlled through INDEX_NAME_LENGTH constant,
    however, the generated index name uses calculateIndexNameAllowedLength,
    which will give different result if components are changing. Also
    calculateIndexNameAllowedLength uses a particular longest component,
    which might become not longest.
    
    ### What does this PR fix and why was it fixed
    
    Fixes calculateGeneratedIndexNameMaxLength to use the index length
    constant and validates the constant to fit the maximum file name length
    even with the longest component name representation.

--- src/java/org/apache/cassandra/index/sai/disk/format/Version.java
+++ src/java/org/apache/cassandra/index/sai/disk/format/Version.java
@@ -23,8 +23,8 @@ import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Objects;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
 import org.slf4j.Logger;
@@ -130,7 +130,10 @@ public class Version implements Comparable<Version>
      * Calculates the maximum allowed length for SAI index names to ensure generated filenames
      * do not exceed the system's filename length limit (defined in {@link SchemaConstants#FILENAME_LENGTH}).
      * This accounts for all additional components in the filename.
+     * It is only used to validate that {@link SchemaConstants#INDEX_NAME_LENGTH}
+     * is not bigger than the actual acceptable length.
      */
+    @VisibleForTesting
     public static int calculateIndexNameAllowedLength(String keyspace)
     {
         int addedLength = getAddedLengthFromDescriptorAndVersion(keyspace);
@@ -153,7 +156,7 @@ public class Version implements Comparable<Version>
         int addedLength = SAI_DESCRIPTOR.length()
                           + versionNameLength
                           + generationLength
-                          + IndexComponentType.PRIMARY_KEY_BLOCK_OFFSETS.representation.length()
+                          + calculateMaxComponentRepresentationLength()
                           + SAI_SEPARATOR.length() * 3
                           + EXTENSION.length();
 
@@ -166,6 +169,14 @@ public class Version implements Comparable<Version>
                        + tableIdLength
                        + separatorLength * 3;
         return addedLength;
+    }
+
+    private static int calculateMaxComponentRepresentationLength()
+    {
+        int maxLength = 0;
+        for (IndexComponentType component : IndexComponentType.values())
+            maxLength = Math.max(maxLength, component.representation.length());
+        return maxLength;
     }
 
     @Override
