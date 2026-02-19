3e26204a75f67977603d4536ed3e44d6673867f8
Mahesh Rajamani <99678631+maheshrajamani@users.noreply.github.com>
2026-02-19 16:11:25 -0500

    CNDB-10919: Add writeRequests counter to TableMetrics (#2235)
    ### What is the issue
    Add writeRequests counter to TableMetrics for tracking table-level write
    operations
    
    ### What does this PR fix and why was it fixed
    Fixes https://github.com/riptano/cndb/issues/10919 partially by adding
    the write request metric to cassandra.
    
     (Rebase of commit c68843d2947eb2083a2797c646ec631d4d02e333)

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -1710,6 +1710,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
 
     {
         long start = nanoTime();
+        metric.writeRequests.inc();
         OpOrder.Group opGroup = context.getGroup();
         CommitLogPosition commitLogPosition = context.getPosition();
         try
