e3ce1d510ee5c0274396890bb23eb0d8f1636a6a
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2026-02-27 13:51:53 -0500

    CNDB-15670: Counter metrics at the table level are automatically updated at the keyspace level

--- src/java/org/apache/cassandra/cql3/statements/ModificationStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/ModificationStatement.java
@@ -747,7 +747,6 @@ public abstract class ModificationStatement implements CQLStatement.SingleKeyspa
         {
             ColumnFamilyStore cfs = Keyspace.openAndGetStore(metadata);
             cfs.metric.deleteRequests.inc();
-            cfs.keyspace.metric.deleteRequests.inc();
         }
 
         if (hasSlices())
