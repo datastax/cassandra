700d8650a9d665152c803b8a2cfe9220a12a8052
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2026-02-27 10:27:00 +0100

    Move reduced expected disk size from fa to ha in the test

--- test/unit/org/apache/cassandra/index/sai/functional/SaiDiskSizeTest.java
+++ test/unit/org/apache/cassandra/index/sai/functional/SaiDiskSizeTest.java
@@ -90,6 +90,11 @@ public class SaiDiskSizeTest extends SAITester
                                       new Object[]{ v, 29648, "pk, v_int", 100 });
                                   case "fa":
                                       return Stream.of(
+                                      new Object[]{ v, 67378, "pk", 1 },
+                                      new Object[]{ v, 59271, "pk, v_int", 2 },
+                                      new Object[]{ v, 29648, "pk, v_int", 100 });
+                                  case "ha":
+                                      return Stream.of(
                                       new Object[]{ v, 17514, "pk", 1 },
                                       new Object[]{ v, 19707, "pk, v_int", 2 },
                                       new Object[]{ v, 16750, "pk, v_int", 100 });
