msmygit/vscassandra

STAR-1128: Let provide explicit mutation serializer (#355)

src/java/org/apache/cassandra/db/partitions/PartitionUpdate.java
@@ -25,12 +25,14 @@ import java.util.List;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import net.openhft.chronicle.core.util.ThrowingFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.db.*;
 import org.apache.cassandra.db.filter.ColumnFilter;
 import org.apache.cassandra.db.rows.*;
+import org.apache.cassandra.exceptions.UnknownTableException;
 import org.apache.cassandra.index.IndexRegistry;
 import org.apache.cassandra.io.util.*;
 import org.apache.cassandra.net.MessagingService;
@@ -59,7 +61,8 @@ public class PartitionUpdate extends AbstractBTreePartition
 {
     protected static final Logger logger = LoggerFactory.getLogger(PartitionUpdate.class);
 
-    public static final PartitionUpdateSerializer serializer = new PartitionUpdateSerializer();
+    @SuppressWarnings("Convert2MethodRef")
+    public static final PartitionUpdateSerializer serializer = new PartitionUpdateSerializer(tableId -> SchemaManager.instance.getExistingTableMetadata(tableId));
 
     private final BTreePartitionData holder;
     private final DeletionInfo deletionInfo;
@@ -624,6 +627,13 @@ public class PartitionUpdate extends AbstractBTreePartition
 
     public static class PartitionUpdateSerializer
     {
+        private final ThrowingFunction<? super TableId, ? extends TableMetadata, ? extends UnknownTableException> tableMetadataResolver;
+
+        public PartitionUpdateSerializer(ThrowingFunction<? super TableId, ? extends TableMetadata, ? extends UnknownTableException> tableMetadataResolver)
+        {
+            this.tableMetadataResolver = tableMetadataResolver;
+        }
+
         public void serialize(PartitionUpdate update, DataOutputPlus out, int version) throws IOException
         {
             Preconditions.checkArgument(version != MessagingService.VERSION_DSE_68,
@@ -639,7 +649,7 @@ public class PartitionUpdate extends AbstractBTreePartition
 
         public PartitionUpdate deserialize(DataInputPlus in, int version, DeserializationHelper.Flag flag) throws IOException
         {
-            TableMetadata metadata = SchemaManager.instance.getExistingTableMetadata(TableId.deserialize(in));
+            TableMetadata metadata = tableMetadataResolver.apply(TableId.deserialize(in));
             if (version == MessagingService.VERSION_DSE_68)
             {
                 // ignore maxTimestamp

datastax/cassandra

(cherry picked from commit b4ccc1d4a270d41afc6f7129f374eb1261bbae4b)
(cherry picked from commit 7b7b0779a32b295ba5b419393c0ed5c4462f088d)
(cherry picked from commit c2ea26b06e011ecdbedaa2b3d34699455b64ed84)
(cherry picked from commit a900a0d8fe0e085327d0273a1417a6f1cd874464)
(cherry picked from commit 7e25f181af34ae63fa24381b005cb77ecbe09722)
# Conflicts:
#	src/java/org/apache/cassandra/db/Mutation.java
#	src/java/org/apache/cassandra/db/partitions/PartitionUpdate.java
 (Rebase of commit 069bb00c85bcb6b942f145424b44e054fcccaa22)

src/java/org/apache/cassandra/db/partitions/PartitionUpdate.java
@@ -33,6 +33,7 @@ import com.google.common.primitives.Ints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.openhft.chronicle.core.util.ThrowingFunction;
 import org.apache.cassandra.db.Clustering;
 import org.apache.cassandra.db.Columns;
 import org.apache.cassandra.db.CounterMutation;
@@ -62,6 +63,7 @@ import org.apache.cassandra.db.rows.Unfiltered;
 import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 import org.apache.cassandra.db.rows.UnfilteredRowIteratorSerializer;
 import org.apache.cassandra.db.rows.UnfilteredRowIterators;
+import org.apache.cassandra.exceptions.UnknownTableException;
 import org.apache.cassandra.index.IndexRegistry;
 import org.apache.cassandra.io.util.DataInputBuffer;
 import org.apache.cassandra.io.util.DataInputPlus;
@@ -97,7 +99,8 @@ public class PartitionUpdate extends AbstractBTreePartition
 {
     protected static final Logger logger = LoggerFactory.getLogger(PartitionUpdate.class);
 
-    public static final PartitionUpdateSerializer serializer = new PartitionUpdateSerializer();
+    @SuppressWarnings("Convert2MethodRef")
+    public static final PartitionUpdateSerializer serializer = new PartitionUpdateSerializer(tableId -> Schema.instance.getExistingTableMetadata(tableId));
 
     private final BTreePartitionData holder;
     private final DeletionInfo deletionInfo;
@@ -735,6 +738,13 @@ public class PartitionUpdate extends AbstractBTreePartition
 
     public static class PartitionUpdateSerializer
     {
+        private final ThrowingFunction<? super TableId, ? extends TableMetadata, ? extends UnknownTableException> tableMetadataResolver;
+
+        public PartitionUpdateSerializer(ThrowingFunction<? super TableId, ? extends TableMetadata, ? extends UnknownTableException> tableMetadataResolver)
+        {
+            this.tableMetadataResolver = tableMetadataResolver;
+        }
+
         public void serialize(PartitionUpdate update, DataOutputPlus out, int version) throws IOException
         {
             Preconditions.checkArgument(version != MessagingService.VERSION_DSE_68,
@@ -750,7 +760,7 @@ public class PartitionUpdate extends AbstractBTreePartition
 
         public PartitionUpdate deserialize(DataInputPlus in, int version, DeserializationHelper.Flag flag) throws IOException
         {
-            TableMetadata metadata = Schema.instance.getExistingTableMetadata(TableId.deserialize(in));
+            TableMetadata metadata = tableMetadataResolver.apply(TableId.deserialize(in));
             if (version == MessagingService.VERSION_DSE_68)
             {
                 // ignore maxTimestamp