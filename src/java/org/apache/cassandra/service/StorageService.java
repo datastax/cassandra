msmygit/vscassandra

STAR-986: Fix compilation issues in DataDaemon class (#296)
* STAR-986 Add ColumnFamilyStore.getStorageHandler method for access to the storage handler
* STAR-986 Port DatabaseDescriptor.enableMemtableAndCommitLog flag.
* STAR-986 Ports ability for StorageService drain to stop mutation stages after compactions and flushing due to mutations still being generated and install a barrier in each keyspace preventing any more mutations after that.

(cherry picked from commit fdece72ae69c72bd35e11f2fe9c5e4ceda0fe6ca)

src/java/org/apache/cassandra/service/StorageService.java
@@ -119,6 +119,7 @@ import org.apache.cassandra.tracing.TraceKeyspace;
 import org.apache.cassandra.transport.ClientResourceLimits;
 import org.apache.cassandra.transport.ProtocolVersion;
 import org.apache.cassandra.utils.*;
+import org.apache.cassandra.utils.concurrent.OpOrder;
 import org.apache.cassandra.utils.logging.LoggingSupportFactory;
 import org.apache.cassandra.utils.progress.ProgressEvent;
 import org.apache.cassandra.utils.progress.ProgressEventType;
@@ -4975,19 +4976,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
             }
 
             if (!isFinalShutdown)
-                setMode(Mode.DRAINING, "clearing mutation stage", false);
-            viewMutationStage.shutdown();
-            counterMutationStage.shutdown();
-            mutationStage.shutdown();
-
-            // FIXME? should these *really* take up to one hour?
-            viewMutationStage.awaitTermination(3600, TimeUnit.SECONDS);
-            counterMutationStage.awaitTermination(3600, TimeUnit.SECONDS);
-            mutationStage.awaitTermination(3600, TimeUnit.SECONDS);
-
-            StorageProxy.instance.verifyNoHintsInProgress();
-
-            if (!isFinalShutdown)
                 setMode(Mode.DRAINING, "flushing column families", false);
 
             // we don't want to start any new compactions while we are draining
@@ -5038,6 +5026,31 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
                     flushes.add(cfs.forceFlush(ColumnFamilyStore.FlushReason.SHUTDOWN));
             }
             FBUtilities.waitOnFutures(flushes);
+
+            // Now that client requests, messaging service and compactions are shutdown, there shouldn't be any more
+            // mutations so let's wait for any pending mutations and then clear the stages. Note that the compaction
+            // manager can generated mutations, for example because of the view builder or the sstable_activity updates
+            // in the SSTableReader.GlobalTidy, so we do this step quite late, but before shutting down the CL
+            if (!isFinalShutdown)
+                setMode(Mode.DRAINING, "stopping mutations", false);
+
+            List<OpOrder.Barrier> barriers = StreamSupport.stream(Keyspace.all().spliterator(), false)
+                                                          .map(ks -> ks.stopMutations())
+                                                          .collect(Collectors.toList());
+            barriers.forEach(OpOrder.Barrier::await); // we could parallelize this...
+
+            if (!isFinalShutdown)
+                setMode(Mode.DRAINING, "clearing mutation stages", false);
+
+            viewMutationStage.shutdown();
+            counterMutationStage.shutdown();
+            mutationStage.shutdown();
+            // FIXME? should these *really* take up to one hour?
+            viewMutationStage.awaitTermination(3600, TimeUnit.SECONDS);
+            counterMutationStage.awaitTermination(3600, TimeUnit.SECONDS);
+            mutationStage.awaitTermination(3600, TimeUnit.SECONDS);
+
+            StorageProxy.instance.verifyNoHintsInProgress();
 
             HintsService.instance.shutdownBlocking();
 

datastax/cassandra

(cherry picked from commit 9fe96bf5acdaf902c91d5974e517543009e6103b)
(cherry picked from commit 93828e5136b9a330885031194e74c61ef331a904)
(cherry picked from commit f60906948be1340d8151229e79f9e8d1b67eda98)
(cherry picked from commit f7824d83132f128e2c36fe2a24f683e667e1dffa)
 (Rebase of commit 2d674376ba4a052338d7ad3f97910d0eea9e6b4f)

src/java/org/apache/cassandra/service/StorageService.java
@@ -175,9 +175,9 @@ import org.apache.cassandra.locator.ReplicaCollection.Builder.Conflict;
 import org.apache.cassandra.locator.Replicas;
 import org.apache.cassandra.locator.SystemReplicas;
 import org.apache.cassandra.locator.TokenMetadata;
+import org.apache.cassandra.locator.TokenMetadataProvider;
 import org.apache.cassandra.metrics.Sampler;
 import org.apache.cassandra.metrics.SamplingManager;
-import org.apache.cassandra.locator.TokenMetadataProvider;
 import org.apache.cassandra.metrics.StorageMetrics;
 import org.apache.cassandra.net.AsyncOneResponse;
 import org.apache.cassandra.net.Message;
@@ -227,6 +227,7 @@ import org.apache.cassandra.utils.WrappedRunnable;
 import org.apache.cassandra.utils.concurrent.Future;
 import org.apache.cassandra.utils.concurrent.FutureCombiner;
 import org.apache.cassandra.utils.concurrent.ImmediateFuture;
+import org.apache.cassandra.utils.concurrent.OpOrder;
 import org.apache.cassandra.utils.concurrent.UncheckedInterruptedException;
 import org.apache.cassandra.utils.logging.LoggingSupportFactory;
 import org.apache.cassandra.utils.progress.ProgressEvent;
@@ -5870,13 +5871,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
             }
 
             if (!isFinalShutdown)
-                setMode(Mode.DRAINING, "clearing mutation stage", false);
-            Stage.shutdownAndAwaitMutatingExecutors(false,
-                                                    DRAIN_EXECUTOR_TIMEOUT_MS.getInt(), TimeUnit.MILLISECONDS);
-
-            StorageProxy.instance.verifyNoHintsInProgress();
-
-            if (!isFinalShutdown)
                 setMode(Mode.DRAINING, "flushing column families", false);
 
             // we don't want to start any new compactions while we are draining
@@ -5928,7 +5922,28 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
             }
             FBUtilities.waitOnFutures(flushes);
 
+            // Now that client requests, messaging service and compactions are shutdown, there shouldn't be any more
+            // mutations so let's wait for any pending mutations and then clear the stages. Note that the compaction
+            // manager can generated mutations, for example because of the view builder or the sstable_activity updates
+            // in the SSTableReader.GlobalTidy, so we do this step quite late, but before shutting down the CL
+            if (!isFinalShutdown)
+                setMode(Mode.DRAINING, "stopping mutations", false);
+
+            List<OpOrder.Barrier> barriers = StreamSupport.stream(Keyspace.all().spliterator(), false)
+                                                          .map(Keyspace::stopMutations)
+                                                          .collect(Collectors.toList());
+            barriers.forEach(OpOrder.Barrier::await); // we could parallelize this...
+
+            if (!isFinalShutdown)
+                setMode(Mode.DRAINING, "clearing mutation stage", false);
+            Stage.shutdownAndAwaitMutatingExecutors(false,
+                                                    DRAIN_EXECUTOR_TIMEOUT_MS.getInt(), TimeUnit.MILLISECONDS);
+
+            StorageProxy.instance.verifyNoHintsInProgress();
+
+
             SnapshotManager.shutdownAndWait(1L, MINUTES);
+
             HintsService.instance.shutdownBlocking();
 
             // Interrupt ongoing compactions and shutdown CM to prevent further compactions.