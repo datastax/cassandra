945d44f6d0411cc8f51f5bd87732f3d65b55dcce
Scott Fines <scott.fines@ibm.com>
2026-03-19 14:36:52 -0500

    Removing TrieMemtale metrics if Table metrics are disabled

--- src/java/org/apache/cassandra/metrics/TableMetrics.java
+++ src/java/org/apache/cassandra/metrics/TableMetrics.java
@@ -1481,7 +1481,7 @@ public class TableMetrics
         {
             LatencyMetrics[] parents = Stream.of(Optional.of(keyspace), global).filter(Optional::isPresent)
                                              .map(Optional::get).toArray(LatencyMetrics[]::new);
-            LatencyMetrics innerMetrics = new LatencyMetrics(factory, namePrefix, parents);
+            LatencyMetrics innerMetrics = new LatencyMetrics(factory, namePrefix, metricsRegistry, parents);
             metric = new TableLatencyMetrics.IndividualTableLatencyMetrics(innerMetrics);
         }
         else
