msmygit/vscassandra

STAR 1174 (#375)
* STAR-1174: use PartitionPosition interface instead of concrete DecoratedKey where appropriate
* STAR-1174: sstable resolution added to Directories

src/java/org/apache/cassandra/db/Directories.java
@@ -32,6 +32,7 @@ import java.nio.file.Path;
 
 import javax.annotation.Nullable;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
@@ -322,6 +323,12 @@ public class Directories
         return null;
     }
 
+    /**
+     * This method looks for the file name passed in and resolves it into a descriptor
+     * if the file exists.
+     *
+     * @return a descriptor for the file name passed in
+     */
     public Descriptor find(String filename)
     {
         for (File dir : dataPaths)
@@ -331,6 +338,20 @@ public class Directories
                 return Descriptor.fromFilename(file);
         }
         return null;
+    }
+
+    /**
+     * This method resolves the filename against the specified directory number, whether
+     * the file exists or not.
+     *
+     * @return a descriptor for the passed in filename
+     */
+    public Descriptor resolve(String filename, int dirNumber)
+    {
+        Preconditions.checkArgument(dirNumber < dataPaths.length, "Invalid dir number: " + dirNumber);
+        File dir = dataPaths[dirNumber];
+        File path = dir.resolve(filename);
+        return Descriptor.fromFilename(path);
     }
 
     /**

datastax/cassandra

(cherry picked from commit 2c18c004ee82a808469ad22e85b32c0819d0682c)
(cherry picked from commit c4aca4fb5e722b66863bd7b03f8a75088e5f3d4e)
(cherry picked from commit 3a0228226fe65310a70b67a2a5be3331a1f86647)
(cherry picked from commit 98d61fb7e54f1634729470d2830adaaf26bf2304)
(cherry picked from commit 5a4e121dac05f2671aaa94dabdfc6d52ebaf364b)
 (Rebase of commit f0953196df9a234a315bd33e0c3d981ec20a7da3)

src/java/org/apache/cassandra/db/Directories.java
@@ -45,8 +45,10 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
@@ -55,7 +57,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.beust.jcommander.internal.Nullable;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.db.lifecycle.LifecycleTransaction;
 import org.apache.cassandra.io.FSDiskFullWriteError;
@@ -342,6 +343,12 @@ public class Directories
         return null;
     }
 
+    /**
+     * This method looks for the file name passed in and resolves it into a descriptor
+     * if the file exists.
+     *
+     * @return a descriptor for the file name passed in
+     */
     public Descriptor find(String filename)
     {
         for (File dir : dataPaths)
@@ -351,6 +358,20 @@ public class Directories
                 return Descriptor.fromFileWithComponent(file, false).left;
         }
         return null;
+    }
+
+    /**
+     * This method resolves the filename against the specified directory number, whether
+     * the file exists or not.
+     *
+     * @return a descriptor for the passed in filename
+     */
+    public Descriptor resolve(String filename, int dirNumber)
+    {
+        Preconditions.checkArgument(dirNumber < dataPaths.length, "Invalid dir number: " + dirNumber);
+        File dir = dataPaths[dirNumber];
+        File file = dir.resolve(filename);
+        return Descriptor.fromFile(file);
     }
 
     /**