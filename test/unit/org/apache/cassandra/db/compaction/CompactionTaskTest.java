msmygit/vscassandra

STAR-1210 Port CNDB-1338 Pass to CompactionObserver#onCompleted if the task finished without exceptions (#388)
* Port CNDB-1338 Pass to CompactionObserver#onCompleted if the task finished without exceptions

Co-authored-by: Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
Co-authored-by: Ruslan Fomkin <Ruslan.Fomkin@gmail.com>

test/unit/org/apache/cassandra/db/compaction/CompactionTaskTest.java
@@ -279,9 +279,22 @@ public class CompactionTaskTest
 
         verify(operationObserver, times(1)).onOperationStart(tableOpCaptor.capture());
         verify(compObserver, times(1)).onInProgress(compactionCaptor.capture());
-        verify(compObserver, times(1)).onCompleted(eq(txn.opId()));
+        verify(compObserver, times(1)).onCompleted(eq(txn.opId()), eq(true));
     }
 
+    @Test
+    public void testFailCompactionTask()
+    {
+        Set<SSTableReader> sstables = generateData(2, 2);
+        LifecycleTransaction txn = cfs.getTracker().tryModify(sstables, OperationType.COMPACTION);
+        AbstractCompactionTask task = new CompactionTask(cfs, txn, 0, false, mockStrategy);
+        AbstractCompactionTask taskMock = Mockito.spy(task);
+        CompactionObserver compObserver = Mockito.mock(CompactionObserver.class);
+        taskMock.addObserver(compObserver);
+        Mockito.doThrow(new RuntimeException("Test throw")).when(taskMock).executeInternal();
+        Assert.assertThrows(RuntimeException.class, () ->  taskMock.execute());
+        Mockito.verify(compObserver, times(1)).onCompleted(any(UUID.class), eq(false));
+    }
 
     private Set<SSTableReader> generateData(int numSSTables, int numKeys)
     {

datastax/cassandra

(cherry picked from commit 24e65ac318c31235ace38017410f24aed4219bae)
(cherry picked from commit 8e1f7634e1b125301ab74e7bde34db19f828c383)
(cherry picked from commit 61c89cc033642fdb8b3761f25c619cd19b6be553)
(cherry picked from commit 5fb6587f9bd8f65bdc66f64ecfe65e809854c7db)
(cherry picked from commit f9d5132245d77015f32460270e92fb6a8b7337ed)
 (Rebase of commit a82d8c8f15c6115cdfdf918bc68afd22cbd78b9b)

test/unit/org/apache/cassandra/db/compaction/CompactionTaskTest.java
@@ -513,9 +513,22 @@ public class CompactionTaskTest
 
         verify(operationObserver, times(1)).onOperationStart(tableOpCaptor.capture());
         verify(compObserver, times(1)).onInProgress(compactionCaptor.capture());
-        verify(compObserver, times(1)).onCompleted(eq(txn.opId()));
+        verify(compObserver, times(1)).onCompleted(eq(txn.opId()), eq(true));
     }
 
+    @Test
+    public void testFailCompactionTask()
+    {
+        Set<SSTableReader> sstables = generateData(2, 2);
+        LifecycleTransaction txn = cfs.getTracker().tryModify(sstables, OperationType.COMPACTION);
+        AbstractCompactionTask task = new CompactionTask(cfs, txn, 0, false, mockStrategy);
+        AbstractCompactionTask taskMock = Mockito.spy(task);
+        CompactionObserver compObserver = Mockito.mock(CompactionObserver.class);
+        taskMock.addObserver(compObserver);
+        Mockito.doThrow(new RuntimeException("Test throw")).when(taskMock).executeInternal();
+        Assert.assertThrows(RuntimeException.class, () ->  taskMock.execute());
+        Mockito.verify(compObserver, times(1)).onCompleted(any(TimeUUID.class), eq(false));
+    }
 
     private Set<SSTableReader> generateData(int numSSTables, int numKeys)
     {