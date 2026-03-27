msmygit/vscassandra

STAR-1221 Port CNDB-293 Adapt compaction-related code for scheduling tasks coming from etcd (#394)
Co-authored-by: Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>

src/java/org/apache/cassandra/db/compaction/CompactionPick.java
@@ -36,39 +36,40 @@ import com.google.common.collect.ImmutableSet;
  * Also, it contains other useful parameters such as a score that was assigned to this candidate (the read hotness or level
  * score depending on the strategy) and the level, if applicable.
  **/
-class CompactionPick
+public class CompactionPick
 {
     final static CompactionPick EMPTY = create(-1, Collections.emptyList(), 0);
 
     /** The key to the parent compaction aggregate, e.g. a level number or tier avg size, -1 if no parent */
-    final long parent;
+    private final long parent;
 
     /** The sstables to be compacted */
-    final ImmutableSet<CompactionSSTable> sstables;
+    private final ImmutableSet<CompactionSSTable> sstables;
 
     /** Only expired sstables */
-    final ImmutableSet<CompactionSSTable> expired;
+    private final ImmutableSet<CompactionSSTable> expired;
 
     /** The sum of all the sstable hotness scores */
-    final double hotness;
+    private final double hotness;
 
     /** The average size in bytes for the sstables in this compaction */
-    final long avgSizeInBytes;
+    private final long avgSizeInBytes;
 
     /** The total size on disk for the sstables in this compaction */
-    final long totSizeInBytes;
+    private final long totSizeInBytes;
 
     /** The unique compaction id, this is only available when a compaction is submitted */
     @Nullable
-    volatile UUID id;
+    private volatile UUID id;
 
     /** The compaction progress, this is only available when compaction actually starts and will be null as long as
      * the candidate is still pending execution, also some tasks cannot report a progress at all, e.g. {@link SingleSSTableLCSTask}.
      * */
-    @Nullable volatile CompactionProgress progress;
+    @Nullable 
+    private volatile CompactionProgress progress;
 
     /** Set to true when the compaction has completed */
-    volatile boolean completed;
+    private volatile boolean completed;
 
     private CompactionPick(long parent,
                            Collection<? extends CompactionSSTable> compacting,
@@ -88,7 +89,7 @@ class CompactionPick
     /**
      * Create a pending compaction candidate calculating hotness and avg size.
      */
-    static CompactionPick create(long parent, Collection<? extends CompactionSSTable> sstables, Collection<? extends CompactionSSTable> expired)
+    public static CompactionPick create(long parent, Collection<? extends CompactionSSTable> sstables, Collection<? extends CompactionSSTable> expired)
     {
         Collection<CompactionSSTable> nonExpiring = sstables.stream().filter(sstable -> !expired.contains(sstable)).collect(Collectors.toList());
         return create(parent,
@@ -136,6 +137,42 @@ class CompactionPick
     public long avgSizeInBytes()
     {
         return avgSizeInBytes;
+    }
+
+    public long totSizeInBytes()
+    {
+        return totSizeInBytes;
+    }
+
+    public long parent()
+    {
+        return parent;
+    }
+
+    public ImmutableSet<CompactionSSTable> ssstables()
+    {
+        return sstables;
+    }
+
+    public ImmutableSet<CompactionSSTable> expired()
+    {
+        return expired;
+    }
+
+    @Nullable
+    public UUID id()
+    {
+        return id;
+    }
+    
+    public CompactionProgress progress()
+    {
+        return progress;
+    }
+    
+    public boolean completed()
+    {
+        return completed;
     }
 
     void setSubmitted(UUID id)

datastax/cassandra

(cherry picked from commit 85a339748ddaea0e9e9e668c668ebdc75bb88ac8)
(cherry picked from commit b73913e70d45da33dc2407859c06785f09b76f39)
(cherry picked from commit f728a92ce9d8edd0723c5291c99995f7aa426688)
(cherry picked from commit 28156c3d9676801e6f14e4b0723b9c3d1e35f0df)
(cherry picked from commit 09306bb6ca62a79ecd32c6aaaeb450e683692a95)
 (Rebase of commit 37991b8ac9a22cef7ed07e07ddd19005ab5067ee)

src/java/org/apache/cassandra/db/compaction/CompactionPick.java
@@ -36,39 +36,40 @@ import org.apache.cassandra.utils.TimeUUID;
  * Also, it contains other useful parameters such as a score that was assigned to this candidate (the read hotness or level
  * score depending on the strategy) and the level, if applicable.
  **/
-class CompactionPick
+public class CompactionPick
 {
     final static CompactionPick EMPTY = create(-1, Collections.emptyList(), 0);
 
     /** The key to the parent compaction aggregate, e.g. a level number or tier avg size, -1 if no parent */
-    final long parent;
+    private final long parent;
 
     /** The sstables to be compacted */
-    final ImmutableSet<CompactionSSTable> sstables;
+    private final ImmutableSet<CompactionSSTable> sstables;
 
     /** Only expired sstables */
-    final ImmutableSet<CompactionSSTable> expired;
+    private final ImmutableSet<CompactionSSTable> expired;
 
     /** The sum of all the sstable hotness scores */
-    final double hotness;
+    private final double hotness;
 
     /** The average size in bytes for the sstables in this compaction */
-    final long avgSizeInBytes;
+    private final long avgSizeInBytes;
 
     /** The total size on disk for the sstables in this compaction */
-    final long totSizeInBytes;
+    private final long totSizeInBytes;
 
     /** The unique compaction id, this is only available when a compaction is submitted */
     @Nullable
-    volatile TimeUUID id;
+    private volatile TimeUUID id;
 
     /** The compaction progress, this is only available when compaction actually starts and will be null as long as
      * the candidate is still pending execution, also some tasks cannot report a progress at all, e.g. {@link SingleSSTableLCSTask}.
      * */
-    @Nullable volatile CompactionProgress progress;
+    @Nullable 
+    private volatile CompactionProgress progress;
 
     /** Set to true when the compaction has completed */
-    volatile boolean completed;
+    private volatile boolean completed;
 
     private CompactionPick(long parent,
                            Collection<? extends CompactionSSTable> compacting,
@@ -88,7 +89,7 @@ class CompactionPick
     /**
      * Create a pending compaction candidate calculating hotness and avg size.
      */
-    static CompactionPick create(long parent, Collection<? extends CompactionSSTable> sstables, Collection<? extends CompactionSSTable> expired)
+    public static CompactionPick create(long parent, Collection<? extends CompactionSSTable> sstables, Collection<? extends CompactionSSTable> expired)
     {
         Collection<CompactionSSTable> nonExpiring = sstables.stream().filter(sstable -> !expired.contains(sstable)).collect(Collectors.toList());
         return create(parent,
@@ -136,6 +137,42 @@ class CompactionPick
     public long avgSizeInBytes()
     {
         return avgSizeInBytes;
+    }
+
+    public long totSizeInBytes()
+    {
+        return totSizeInBytes;
+    }
+
+    public long parent()
+    {
+        return parent;
+    }
+
+    public ImmutableSet<CompactionSSTable> ssstables()
+    {
+        return sstables;
+    }
+
+    public ImmutableSet<CompactionSSTable> expired()
+    {
+        return expired;
+    }
+
+    @Nullable
+    public TimeUUID id()
+    {
+        return id;
+    }
+    
+    public CompactionProgress progress()
+    {
+        return progress;
+    }
+    
+    public boolean completed()
+    {
+        return completed;
     }
 
     void setSubmitted(TimeUUID id)