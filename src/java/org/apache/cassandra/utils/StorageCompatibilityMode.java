283cbaf62ae32c084c228850d340221bc212faa4
Brandon Williams <brandonwilliams@apache.org>
2026-03-30 14:33:19 -0500

    Rename CC_4 SCM to HCD_1

--- src/java/org/apache/cassandra/utils/StorageCompatibilityMode.java
+++ src/java/org/apache/cassandra/utils/StorageCompatibilityMode.java
@@ -44,7 +44,7 @@ public enum StorageCompatibilityMode
     /**
      * Same as {@link #CASSANDRA_4}, but allows the use of BTI format in {@link #validateSstableFormat}.
      */
-    CC_4(4),
+    HCD_1(4),
 
     /**
      * Use the storage formats of the current version, but disabling features that are not compatible with any
@@ -95,7 +95,7 @@ public enum StorageCompatibilityMode
 
     /**
      * Returns the messaging version to use for on-disk storage formats (commit log, hints, batch logs).
-     * When a compatibility mode is set (e.g., CC_4), this ensures that on-disk formats are written
+     * When a compatibility mode is set (e.g., HCD_1), this ensures that on-disk formats are written
      * in a way that older versions can read.
      *
      * @return the messaging version appropriate for storage serialization
@@ -106,7 +106,7 @@ public enum StorageCompatibilityMode
         switch (this)
         {
             case CASSANDRA_4:
-            case CC_4:
+            case HCD_1:
                 version = MessagingService.VERSION_40;
                 break;
             case UPGRADING:
