3033f696f3c79de729ddd9eb7a9c55b8c29ad23f
Piotr Kołaczkowski <pkolaczk@gmail.com>
2026-04-16 10:25:37 +0200

    Revert "CNDB-16243: Improve ChunkCache Key in order to keep O(1) access time (#2156)" (#2320)
    This reverts commit 7c75761d347e7db8a1510500183841bf750eddbb.
    
    Revert needed because the hashCode function introduced by CNDB-16243
    turned out to cause an excessive number of hash collisions in 
    AFS functional tests for SAI. See CNDB-17275. 
    
    To be fixed properly in CNDB-17444.

--- src/java/org/apache/cassandra/cache/ChunkCache.java
+++ src/java/org/apache/cassandra/cache/ChunkCache.java
@@ -297,12 +297,12 @@ public class ChunkCache
         synchronousCache.invalidateAll(Iterables.filter(cache.asMap().keySet(), x -> (x.readerId & mask) == fileId));
     }
 
-    static class Key implements Comparable<Key>
+    static class Key
     {
         final long readerId;
         final long position;
 
-        Key(long readerId, long position)
+        private Key(long readerId, long position)
         {
             super();
             this.position = position;
@@ -312,15 +312,11 @@ public class ChunkCache
         @Override
         public int hashCode()
         {
-            // Mix readerId and position into a single long using a large prime multiplier
-            // This constant is a mixing constant derived from the Golden Ratio
-            long mixed = (Long.rotateLeft(readerId, 16) + Long.rotateLeft(position, 16)) * 0x9E3779B97F4A7C15L;
-
-            // Spread the bits (XOR-shift) to ensure high bits affect low bits
-            mixed ^= (mixed >>> 32);
-            mixed ^= (mixed >>> 16);
-
-            return (int) mixed;
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + Long.hashCode(readerId);
+            result = prime * result + Long.hashCode(position);
+            return result;
         }
 
         @Override
@@ -328,23 +324,12 @@ public class ChunkCache
         {
             if (this == obj)
                 return true;
-            if (obj == null || getClass() != obj.getClass())
+            if (obj == null)
                 return false;
 
             Key other = (Key) obj;
             return (position == other.position)
                    && readerId == other.readerId;
-        }
-
-        @Override
-        public int compareTo(Key other) {
-            // Compare readerId first
-            int cmp = Long.compare(this.readerId, other.readerId);
-            if (cmp != 0) {
-                return cmp;
-            }
-            // Then compare position
-            return Long.compare(this.position, other.position);
         }
     }
 
