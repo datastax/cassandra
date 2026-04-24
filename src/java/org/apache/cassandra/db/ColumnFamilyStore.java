c012eab56d32ea6b8f90ab5d3582bc19ac18c2f4
Zhao Yang <zhaoyangsingapore@gmail.com>
2026-04-24 12:09:32 +0800

    CNDB-17573-main: expose lazy bloom filter metrics
    0. Expose approximate BF memory size
    
    1. Add bloom filter state metrics:
       - LoadedBloomFilter: SSTables with a real bloom filter loaded
       - LazyBloomFilter: SSTables still waiting for lazy bloom filter loading
       - PassThroughBloomFilter: SSTables using pass-through filtering after lazy loading could not complete or due to BF memory limit
    
    2. Add lazy loading criteria metrics:
       - LazyBloomFilterByRequestRate: num of lazy bloom filters waiting because the request rate threshold is not met
       - LazyBloomFilterByRequestCount: num of lazy bloom filters waiting because the request count threshold is not met
    
    3. Add hit metrics for pass-through bloom filter states:
       - LazyBloomFilterHits: reads against lazy bloom
      filters waiting to load
       - PassThroughBloomFilterHits: reads against SSTables with no bloom filter - AlwaysPresent
    
    Fixes #17573

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -3610,6 +3610,21 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
         return bloomFilterTracker.getRecentTrueNegativeRate();
     }
 
+    public long getLazyBloomFilterHitCount()
+    {
+        return bloomFilterTracker.getLazyBloomFilterHitCount();
+    }
+
+    public long getLoadedBloomFilterHitCount()
+    {
+        return bloomFilterTracker.getLoadedBloomFilterHitCount();
+    }
+
+    public long getPassThroughBloomFilterHitCount()
+    {
+        return bloomFilterTracker.getPassThroughBloomFilterHitCount();
+    }
+
     public long getReadRequests()
     {
         return metric == null ? 0 : metric.readRequests.getCount();
