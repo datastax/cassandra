09bf3ba0bd91fc3313478b809cc3051ac8716108
bereng <6973517+bereng@users.noreply.github.com>
2026-05-04 17:12:01 +0200

    HCD-339 CQLSH Describe command failure (#2386)
    The Descrive cqlsh command would fail under some specific scenarios.
    
    ### What is the issue
    On HCD-190 python was updated to later releases. That implied changing
    the code in a number of places but it was missed on an error condition.
    
    ### What does this PR fix and why was it fixed
    The correct method is being called now instead.
    
    Co-authored-by: Berenguer Blasi <bereng@li-a620ff4c-2b81-11b2-a85c-8583e47274fb.ibm.com>

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -1446,7 +1446,7 @@ class Shell(cmd.Cmd):
                     self.describe_element(result)
 
             except CQL_ERRORS as err:
-                err_msg = ensure_text(err.message if hasattr(err, 'message') else str(err))
+                err_msg = maybe_ensure_text(err.message if hasattr(err, 'message') else str(err))
                 self.printerr(err_msg.partition("message=")[2].strip('"'))
             except Exception:
                 import traceback
