5f3d65c43bda9d14c0aa83bb080147b31d4a1102
ekaterinadimitrova2 <ekaterina.dimitrova@datastax.com>
2026-06-02 07:52:02 -0400

    CNDB-18142 CNDB-17871: Tie FusedPQ feature to FA index version, deprecate cassandra.sai.vector.enable_fused (#2440)
    Vector features should be tied to index version if we want them enabled
    always by default when we enable FA version.
    
    Tie FusedPQ feature to FA index version, deprecate
    cassandra.sai.vector.enable_fused
    
    This patch was already committed to the `cndb-main-release-202604`
    branch.
    
    This forward merges https://github.com/datastax/cassandra/commit/c9efbac93222313e0192f451c6c6458794abee02
    
     (Rebase of commit 16c90d9be4477d60540c9da8858e45d89e17858a)

--- src/java/org/apache/cassandra/index/sai/disk/vector/JVectorVersionUtil.java
+++ src/java/org/apache/cassandra/index/sai/disk/vector/JVectorVersionUtil.java
@@ -22,11 +22,9 @@ import org.apache.cassandra.index.sai.disk.format.Version;
 public class JVectorVersionUtil
 {
     /**
-     * Whether to fuse quantized vectors into the graph when writing indexes, assuming all other conditions are met.
-     * Variables are volatile to allow for changing in unit tests. They are only accessed on flush, so their access
-     * is infrequent.
+     * Variables are volatile to allow for changing in unit tests. They are only accessed on flush and compaction,
+     * so their access is infrequent.
      */
-    public static volatile boolean ENABLE_FUSED = CassandraRelevantProperties.SAI_VECTOR_ENABLE_FUSED.getBoolean();
     public static volatile boolean ENABLE_NVQ = CassandraRelevantProperties.SAI_VECTOR_ENABLE_NVQ.getBoolean();
     public static final int NUM_SUB_VECTORS = CassandraRelevantProperties.SAI_VECTOR_NVQ_NUM_SUB_VECTORS.getInt();
 
@@ -52,12 +50,17 @@ public class JVectorVersionUtil
      * Decide whether to attempt to write the quantized vectors as fused parts of the graph. Note that this method
      * does not take into account whether the graph has enough information to build a quantization, as that depends on
      * external factors.
+     * <p>
+     * FusedPQ is automatically enabled for all indexes using version FA or later (jvector file format version 6+).
+     * The deprecated ENABLE_FUSED property is ignored for these versions.
+     *
      * @param version the SAI on disk format to use when writing to disk
      * @return true if conditions are met, false otherwise
      */
     public static boolean shouldWriteFused(Version version)
     {
-        return ENABLE_FUSED && versionSupportsFused(version);
+        // For FA version and later, FusedPQ is always enabled (tied to the version)
+        return versionSupportsFused(version);
     }
 
     public static boolean versionSupportsFused(Version version)
