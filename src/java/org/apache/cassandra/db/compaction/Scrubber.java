0fc9e68eb39f76a037ab9ebc6d0e0ed5de6a727c
Berenguer Blasi <berenguer.blasi@ibm.com>
2026-06-08 13:18:29 +0200

    DSP-25176: Support early open failures better Log error and let operations continue when an early open attempt fails. Change online scrub to not use early open as it is unsafe.

--- src/java/org/apache/cassandra/db/compaction/Scrubber.java
+++ src/java/org/apache/cassandra/db/compaction/Scrubber.java
@@ -215,7 +215,9 @@ public class Scrubber implements Closeable
     {
         List<SSTableReader> finished = new ArrayList<>();
         outputHandler.output(String.format("Scrubbing %s (%s)", sstable, FBUtilities.prettyPrintMemory(dataFile.length())));
-        try (SSTableRewriter writer = SSTableRewriter.construct(realm, transaction, false, sstable.maxDataAge);
+        // Do not use early opening with scrub as it is unsafe (scrub may move rows to a different table that is not
+        // written until the whole operation completes).
+        try (SSTableRewriter writer = SSTableRewriter.constructWithoutEarlyOpening(transaction, false, sstable.maxDataAge);
              Refs<SSTableReader> refs = Refs.ref(Collections.singleton(sstable)))
         {
             assert !indexAvailable() || indexIterator.dataPosition() == sstable.getDataFileSliceDescriptor().dataStart : indexIterator.dataPosition();
