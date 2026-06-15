e940a153c4a6c7c457b07be87d5e1759244c1b53
Ruslan Fomkin <ruslan.fomkin@ibm.com>
2026-06-15 14:32:21 +0200

    CNDB-18062 test row aware primary key map with static clustering (#2444)
    Static clustering is a special case, and, thus, important to exercise in
    tests. Row aware primary key map API is not exercises in the case of
    static clustering as it was discovered during work on CNDB-15608.
    
    Adds unit tests on row aware primary key map API with static clustering.
    
    Moves parameterizing on row aware versions into CQL tester.

--- test/unit/org/apache/cassandra/index/sai/SAITester.java
+++ test/unit/org/apache/cassandra/index/sai/SAITester.java
@@ -1284,5 +1284,21 @@ public class SAITester extends CQLTester
         {
             SAIUtil.setCurrentVersion(version);
         }
+
+        /**
+         * {@link CQLTester} parameterized for {@link Version#ALL}, which are row-aware.
+         */
+        public abstract static class RowAware extends Versioned
+        {
+            @Parameterized.Parameters(name = "{0}")
+            public static Collection<Object[]> data()
+            {
+                // See Version file for explanation of changes associated with each version
+                return Version.ALL.stream()
+                                  .filter(v -> v.onDiskFormat().indexFeatureSet().isRowAware())
+                                  .map(v -> new Object[]{ v })
+                                  .collect(Collectors.toList());
+            }
+        }
     }
 }
