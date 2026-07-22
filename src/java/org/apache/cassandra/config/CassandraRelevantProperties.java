40de7786e4106cf18d19a20fe775a479dbda8c9c
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2026-07-22 16:50:14 +0200

    Set SAI default format to GA to run CNDB's cross version tests

--- src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
+++ src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
@@ -421,7 +421,7 @@ public enum CassandraRelevantProperties
     SKIP_INDEXES_ON_FULL_PRIMARY_KEYS("cassandra.index.skip_on_full_primary_keys", "true"),
 
     /** The current version of the SAI on-disk index format. */
-    SAI_CURRENT_VERSION("cassandra.sai.latest.version", "ec"),
+    SAI_CURRENT_VERSION("cassandra.sai.latest.version", "ga"),
 
     /** The class to use for selecting the current version of the SAI on-disk index format on a per-keyspace basis. */
     SAI_VERSION_SELECTOR_CLASS("cassandra.sai.version.selector.class", ""),
