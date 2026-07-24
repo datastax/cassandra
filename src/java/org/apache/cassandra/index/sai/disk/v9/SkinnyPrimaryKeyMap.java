2da8f934280f29ac945fcd0f7d163a9fbde9989b
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2026-07-24 11:44:41 +0200

    Suppress warning on unused private field

--- src/java/org/apache/cassandra/index/sai/disk/v9/SkinnyPrimaryKeyMap.java
+++ src/java/org/apache/cassandra/index/sai/disk/v9/SkinnyPrimaryKeyMap.java
@@ -77,6 +77,7 @@ public class SkinnyPrimaryKeyMap implements PrimaryKeyMap
     public static class Factory implements PrimaryKeyMap.Factory
     {
         // The class member is needed to avoid memory leaks and to be addressed by CNDB-17902
+        @SuppressWarnings("unused")
         private final IndexComponents.ForRead perSSTableComponents;
         protected final SSTableId<?> sstableId;
         protected final boolean hasStaticColumns;
