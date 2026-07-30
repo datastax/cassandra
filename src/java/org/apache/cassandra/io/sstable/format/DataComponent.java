daf60098c0bc98b9380d19baef682ddf593b80cb
blambov <branimir.lambov@ibm.com>
2026-07-30 10:47:55 +0300

    DSP-25176: Fix encryption
    Fix encrypted data files to use the compressed writers and readers
    Fix encrypted index files to use the encryption settings of the data
    file
    Make it obvious that flush compression does not apply to encrypted tables
    Test reading encrypted SSTables

--- src/java/org/apache/cassandra/io/sstable/format/DataComponent.java
+++ src/java/org/apache/cassandra/io/sstable/format/DataComponent.java
@@ -24,8 +24,6 @@ import org.apache.cassandra.config.CassandraRelevantProperties;
 import org.apache.cassandra.config.Config.FlushCompression;
 import org.apache.cassandra.db.compaction.OperationType;
 import org.apache.cassandra.io.compress.CompressedSequentialWriter;
-import org.apache.cassandra.io.compress.EncryptedSequentialWriter;
-import org.apache.cassandra.io.compress.Encryptor;
 import org.apache.cassandra.io.compress.ICompressor;
 import org.apache.cassandra.io.sstable.Component;
 import org.apache.cassandra.io.sstable.Descriptor;
@@ -68,25 +66,13 @@ public class DataComponent
         if (metadata.params.compression.isEnabled())
         {
             final CompressionParams compressionParams = buildCompressionParams(metadata, operationType, flushCompression);
-            final ICompressor compressor = compressionParams.getSstableCompressor();
-
-            // Check if this is encryption-only (no actual compression)
-            if (compressor instanceof Encryptor)
-            {
-                return new EncryptedSequentialWriter(descriptor.fileFor(Components.DATA),
-                                                     options,
-                                                     compressor);
-            }
-            else
-            {
-                return new CompressedSequentialWriter(descriptor.fileFor(Components.DATA),
-                                                      descriptor.fileFor(Components.COMPRESSION_INFO),
-                                                      descriptor.fileFor(digestComponent),
+            return new CompressedSequentialWriter(descriptor.fileFor(Components.DATA),
+                                                  descriptor.fileFor(Components.COMPRESSION_INFO),
+                                                  descriptor.fileFor(digestComponent),
                                                       checksumType,
-                                                      options,
-                                                      compressionParams,
-                                                      metadataCollector);
-            }
+                                                  options,
+                                                  compressionParams,
+                                                  metadataCollector);
         }
         else
         {
@@ -110,6 +96,9 @@ public class DataComponent
 
         if (null != compressor && operationType == OperationType.FLUSH)
         {
+            if (compressor.encryptionOnly() != null)
+                return compressionParams; // FlushCompression cannot disable encryption
+
             // When we are flushing out of the memtable throughput of the compressor is critical as flushes,
             // especially of large tables, can queue up and potentially block writes.
             // This optimization allows us to fall back to a faster compressor if a particular
