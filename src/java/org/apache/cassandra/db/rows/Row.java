6cc2f5914098a9ff97f2a976fb76628bc55d2290
Andrés de la Peña <a.penya.garcia@gmail.com>
2026-08-13 11:11:44 +0100

    CNDB-18512: Add metrics about fetched/returned cells to the slow query logger

--- src/java/org/apache/cassandra/db/rows/Row.java
+++ src/java/org/apache/cassandra/db/rows/Row.java
@@ -179,6 +179,11 @@ public interface Row extends Unfiltered, Iterable<ColumnData>, IMeasurableMemory
     public Iterable<Cell<?>> cells();
 
     /**
+     * @return the number of cells in this row.
+     */
+    public int cellsCount();
+
+    /**
      * A collection of the ColumnData representation of this row, for columns with some data (possibly not live) present
      * <p>
      * The data is returned in column order.
