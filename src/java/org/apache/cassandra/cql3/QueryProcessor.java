msmygit/vscassandra

STAR-1112 Add QueryInterceptor to QueryProcessor (#393)

src/java/org/apache/cassandra/cql3/QueryProcessor.java
@@ -93,6 +93,8 @@ public class QueryProcessor implements QueryHandler
 
     private static final AtomicInteger lastMinuteEvictionsCount = new AtomicInteger(0);
 
+    private final List<QueryInterceptor> interceptors = new ArrayList<>();
+
     static
     {
         preparedStatements = Caffeine.newBuilder()
@@ -244,11 +246,34 @@ public class QueryProcessor implements QueryHandler
         statement.authorize(clientState);
         statement.validate(queryState);
 
+        for (QueryInterceptor interceptor: interceptors)
+        {
+            ResultMessage result = interceptor.interceptStatement(statement, queryState, options, queryStartNanoTime);
+
+            if (result != null)
+                return result;
+        }
+
         ResultMessage result = options.getConsistency() == ConsistencyLevel.NODE_LOCAL
                              ? processNodeLocalStatement(statement, queryState, options)
                              : statement.execute(queryState, options, queryStartNanoTime);
 
         return result == null ? new ResultMessage.Void() : result;
+    }
+
+    /**
+     * Register a new {@link QueryInterceptor}
+     * @param interceptor the {@link QueryInterceptor} to register
+     */
+    public void registerInterceptor(QueryInterceptor interceptor)
+    {
+        interceptors.add(Objects.requireNonNull(interceptor));
+    }
+
+    @VisibleForTesting
+    public void clearInterceptors()
+    {
+        interceptors.clear();
     }
 
     private ResultMessage processNodeLocalStatement(CQLStatement statement, QueryState queryState, QueryOptions options)

datastax/cassandra

(cherry picked from commit c7d3220580c919c4f91a9641265237bbffbb8003)
(cherry picked from commit af9298fc46f35a83bd24bd3eb6a45e2f271e6b6f)
(cherry picked from commit a161b726585b135f74c8b55310ae759b498e14e9)
(cherry picked from commit b655559a4039dbf7cf3dcaffbdc16d2acf0bc787)
(cherry picked from commit de3061081fcbb37d903e8768c01e1eea18b4297b)
 (Rebase of commit 2fde1d8f945a5a1c95fd7e5642c0fb9d023dde4e)
 (Rebase of commit 2cbb80570cca41269bc41f0eb550f51399bcf4f0)

src/java/org/apache/cassandra/cql3/QueryProcessor.java
@@ -18,7 +18,13 @@
 package org.apache.cassandra.cql3;
 
 import java.nio.ByteBuffer;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
@@ -129,6 +135,8 @@ public class QueryProcessor implements QueryHandler
     public static long PREPARED_STATEMENT_CACHE_SIZE_BYTES = capacityToBytes(DatabaseDescriptor.getPreparedStatementsCacheSizeMiB());
 
     private static final AtomicInteger lastMinuteEvictionsCount = new AtomicInteger(0);
+
+    private final List<QueryInterceptor> interceptors = new ArrayList<>();
 
     static
     {
@@ -293,11 +301,34 @@ public class QueryProcessor implements QueryHandler
         statement.authorize(clientState);
         statement.validate(clientState);
 
+        for (QueryInterceptor interceptor: interceptors)
+        {
+            ResultMessage result = interceptor.interceptStatement(statement, queryState, options, requestTime);
+
+            if (result != null)
+                return result;
+        }
+
         ResultMessage result = options.getConsistency() == ConsistencyLevel.NODE_LOCAL
                              ? processNodeLocalStatement(statement, queryState, options)
                              : statement.execute(queryState, options, requestTime);
 
         return result == null ? new ResultMessage.Void() : result;
+    }
+
+    /**
+     * Register a new {@link QueryInterceptor}
+     * @param interceptor the {@link QueryInterceptor} to register
+     */
+    public void registerInterceptor(QueryInterceptor interceptor)
+    {
+        interceptors.add(Objects.requireNonNull(interceptor));
+    }
+
+    @VisibleForTesting
+    public void clearInterceptors()
+    {
+        interceptors.clear();
     }
 
     private ResultMessage processNodeLocalStatement(CQLStatement statement, QueryState queryState, QueryOptions options)