msmygit/vscassandra

STAR-1226 Allow mutation of where clause relations (#397)

test/unit/org/apache/cassandra/cql3/WhereClauseMutationTest.java
@@ -1,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.cql3;
+
+import java.util.List;
+import javax.annotation.Nullable;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.datastax.driver.core.Row;
+import org.apache.cassandra.cql3.statements.SelectStatement;
+import org.apache.cassandra.service.QueryState;
+import org.apache.cassandra.transport.messages.ResultMessage;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The purpose of the test is to show the mutation of {@link WhereClause} relations
+ * by a {@link QueryInterceptor}.
+ *
+ * The test adds 2 rows with the keys of "original" and "mutated".
+ *
+ * Without the {@link QueryInterceptor} when "original" is select the value associated
+ * with the "original" row is returned.
+ *
+ * A {@link QueryInterceptor} is then added that changes the value of the primary key
+ * relation from "original" to "mutated" so when "original" is selected the value for
+ * the "mutated" row is returned.
+ */
+public class WhereClauseMutationTest extends CQLTester
+{
+    @BeforeClass
+    public static void setup() throws Throwable
+    {
+        requireNetwork();
+    }
+
+    @After
+    public void cleanInterceptors() throws Throwable
+    {
+        QueryProcessor.instance.clearInterceptors();
+    }
+
+    @Test
+    public void mutationTest() throws Throwable
+    {
+        createTable("create table %s (pk text, ck text, v text, primary key (pk, ck))");
+        execute("insert into %s (pk, ck, v) values ('original', 'clustering', 'value1')");
+        execute("insert into %s (pk, ck, v) values ('mutated', 'clustering', 'value2')");
+
+        assertRowsIgnoringOrder(execute("select * from %s where pk = 'original' and ck = 'clustering'"), row("original", "clustering", "value1"));
+
+        QueryProcessor.instance.registerInterceptor(new QueryInterceptor()
+        {
+            @Nullable
+            @Override
+            public ResultMessage interceptStatement(CQLStatement statement, QueryState queryState, QueryOptions options, long queryStartNanoTime)
+            {
+                if (statement instanceof SelectStatement)
+                {
+                    SelectStatement selectStatement = (SelectStatement)statement;
+                    // We only want to attempt mutation on our table
+                    if (selectStatement.table.keyspace.equals(keyspace()) && selectStatement.table.name.equals(currentTable()))
+                    {
+                        SelectStatement.RawStatement rawStatement = (SelectStatement.RawStatement)QueryProcessor.parseStatement(selectStatement.getRawCQLStatement());
+
+                        // Mutate the SelectStatement with a new WhereClause with the relations mutated
+                        rawStatement = new SelectStatement.RawStatement(new QualifiedName(rawStatement.keyspace(),
+                                                                                          rawStatement.name()),
+                                                                        rawStatement.parameters,
+                                                                        rawStatement.selectClause,
+                                                                        rawStatement.whereClause.mutateRelations(r -> mutateRelation(r)),
+                                                                        rawStatement.limit,
+                                                                        rawStatement.perPartitionLimit);
+
+                        selectStatement = rawStatement.prepare(queryState.getClientState());
+                        return selectStatement.execute(queryState, options, queryStartNanoTime);
+                    }
+                }
+                return null;
+            }
+        });
+
+        List<Row> rows = executeNet("select * from %s where pk = 'original' and ck = 'clustering'").all();
+        assertEquals(1, rows.size());
+        assertEquals("value2", rows.get(0).getString(2));
+    }
+
+    private Relation mutateRelation(Relation original)
+    {
+        // Only perform the mutation on single column relations
+        if (original instanceof SingleColumnRelation)
+        {
+            SingleColumnRelation singleColumnRelation = (SingleColumnRelation)original;
+            // Make sure we are only changing the primary key column
+            if (singleColumnRelation.getEntity().toCQLString().equals("pk"))
+                // Return a new SingleColumnRelation with the primary key column
+                // value changed to "mutated"
+                return new SingleColumnRelation(singleColumnRelation.getEntity(),
+                                                singleColumnRelation.operator(),
+                                                Constants.Literal.string("mutated"));
+        }
+        return original;
+    }
+}

datastax/cassandra

(cherry picked from commit a303a08b59702ecaf0c68d1bef5184a23a48b04c)
(cherry picked from commit 83881ad8fe36601cafeca7d7e879a683163eaaf1)
(cherry picked from commit 3da8b623eece6ecc0e9723eb5a8f9a5e648f1ee2)
(cherry picked from commit a34ff14cb957c812dca0bd51ef844696b37ad502)
(cherry picked from commit 52f26ee20b682abe0c37d266bfedbdf8aa6d0a2b)
 (Rebase of commit e29ef64fd3ca9fbe21a93205b1a01b9954b952a9)
 (Rebase of commit c1c2a8539ae310b2d44b5d9a654df85da8131bc7)

test/unit/org/apache/cassandra/cql3/WhereClauseMutationTest.java
@@ -1,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.cql3;
+
+import java.util.List;
+import javax.annotation.Nullable;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.datastax.driver.core.Row;
+import org.apache.cassandra.cql3.statements.SelectStatement;
+import org.apache.cassandra.service.QueryState;
+import org.apache.cassandra.transport.Dispatcher;
+import org.apache.cassandra.transport.messages.ResultMessage;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The purpose of the test is to show the mutation of {@link WhereClause} relations
+ * by a {@link QueryInterceptor}.
+ *
+ * The test adds 2 rows with the keys of "original" and "mutated".
+ *
+ * Without the {@link QueryInterceptor} when "original" is select the value associated
+ * with the "original" row is returned.
+ *
+ * A {@link QueryInterceptor} is then added that changes the value of the primary key
+ * relation from "original" to "mutated" so when "original" is selected the value for
+ * the "mutated" row is returned.
+ */
+public class WhereClauseMutationTest extends CQLTester
+{
+    @BeforeClass
+    public static void setup() throws Throwable
+    {
+        requireNetwork();
+    }
+
+    @After
+    public void cleanInterceptors() throws Throwable
+    {
+        QueryProcessor.instance.clearInterceptors();
+    }
+
+    @Test
+    public void mutationTest() throws Throwable
+    {
+        createTable("create table %s (pk text, ck text, v text, primary key (pk, ck))");
+        execute("insert into %s (pk, ck, v) values ('original', 'clustering', 'value1')");
+        execute("insert into %s (pk, ck, v) values ('mutated', 'clustering', 'value2')");
+
+        assertRowsIgnoringOrder(execute("select * from %s where pk = 'original' and ck = 'clustering'"), row("original", "clustering", "value1"));
+
+        QueryProcessor.instance.registerInterceptor(new QueryInterceptor()
+        {
+            @Nullable
+            @Override
+            public ResultMessage interceptStatement(CQLStatement statement, QueryState queryState, QueryOptions options, Dispatcher.RequestTime requestTime)
+            {
+                if (statement instanceof SelectStatement)
+                {
+                    SelectStatement selectStatement = (SelectStatement)statement;
+                    // We only want to attempt mutation on our table
+                    if (selectStatement.table.keyspace.equals(keyspace()) && selectStatement.table.name.equals(currentTable()))
+                    {
+                        SelectStatement.RawStatement rawStatement = (SelectStatement.RawStatement)QueryProcessor.parseStatement(selectStatement.getRawCQLStatement());
+
+                        // Mutate the SelectStatement with a new WhereClause with the relations mutated
+                        rawStatement = new SelectStatement.RawStatement(new QualifiedName(rawStatement.keyspace(),
+                                                                                          rawStatement.name()),
+                                                                        rawStatement.parameters,
+                                                                        rawStatement.selectClause,
+                                                                        rawStatement.whereClause.mutateRelations(r -> mutateRelation(r)),
+                                                                        rawStatement.limit,
+                                                                        rawStatement.perPartitionLimit);
+
+                        selectStatement = rawStatement.prepare(queryState.getClientState());
+                        return selectStatement.execute(queryState, options, requestTime);
+                    }
+                }
+                return null;
+            }
+        });
+
+        List<Row> rows = executeNet("select * from %s where pk = 'original' and ck = 'clustering'").all();
+        assertEquals(1, rows.size());
+        assertEquals("value2", rows.get(0).getString(2));
+    }
+
+    private Relation mutateRelation(Relation original)
+    {
+        // Only perform the mutation on single column relations
+        if (original instanceof SingleColumnRelation)
+        {
+            SingleColumnRelation singleColumnRelation = (SingleColumnRelation)original;
+            // Make sure we are only changing the primary key column
+            if (singleColumnRelation.getEntity().toCQLString().equals("pk"))
+                // Return a new SingleColumnRelation with the primary key column
+                // value changed to "mutated"
+                return new SingleColumnRelation(singleColumnRelation.getEntity(),
+                                                singleColumnRelation.operator(),
+                                                Constants.Literal.string("mutated"));
+        }
+        return original;
+    }
+}