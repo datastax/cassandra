msmygit/vscassandra

STAR 993: Add abstractions to run repair across regions (#311)
STAR-993: add result to RepairRunnable
STAR-993: Add push repair to Repair
STAR-993: make repair prepare message timeout configurable
STAR-993: add repair progress reporter to hide SystemDistributedKeyspace
STAR-993: add parent repair session listener
STAR-993: add isOfflineService to repair option
STAR-993: Add API to check sstable global tidy reference
(cherry picked from commit 8bd82db908d3b3fe64d0439607e30f7109d79ce5)

test/unit/org/apache/cassandra/repair/NeighborsAndRangesTest.java
@@ -19,7 +19,9 @@
 package org.apache.cassandra.repair;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Lists;
@@ -27,12 +29,51 @@ import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.apache.cassandra.dht.Murmur3Partitioner;
 import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.repair.messages.RepairOption;
+import org.apache.cassandra.utils.FBUtilities;
 
 import static org.apache.cassandra.repair.RepairRunnable.NeighborsAndRanges;
+import static org.apache.cassandra.repair.RepairRunnable.createNeighbordAndRangesForOfflineService;
+import static org.apache.cassandra.repair.messages.RepairOption.parse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NeighborsAndRangesTest extends AbstractRepairTest
 {
+    @Test
+    public void testCreateNeighbordAndRangesForOfflineService()
+    {
+        Map<String, String> options = new HashMap<>();
+
+        // no hosts
+        assertThatThrownBy(() -> create(options)).hasMessageContaining("There should be at least 1 host");
+        options.put(RepairOption.HOSTS_KEY, FBUtilities.getJustBroadcastAddress().getHostAddress());
+
+        // no ranges
+        assertThatThrownBy(() -> create(options)).hasMessageContaining("Token ranges must be specified");
+        options.put(RepairOption.RANGES_KEY, "0:10,11:20,21:30");
+
+        // no neighor, because the only host is local
+        assertThatThrownBy(() -> create(options)).hasMessageContaining("There should be at least 1 neighbor");
+
+        // with proper neighbors
+        options.put(RepairOption.HOSTS_KEY, "127.0.99.1,127.0.99.2," + FBUtilities.getJustBroadcastAddress().getHostAddress());
+        RepairRunnable.NeighborsAndRanges neighborsAndRanges = create(options);
+
+        assertThat(neighborsAndRanges.shouldExcludeDeadParticipants).isFalse();
+        assertThat(neighborsAndRanges.participants).hasSize(2); // excluded local host
+        assertThat(neighborsAndRanges.commonRanges).hasSize(1); // all in one common range
+        assertThat(neighborsAndRanges.filterCommonRanges("ks", new String[] { "cf"})).isSameAs(neighborsAndRanges.commonRanges);
+    }
+
+    private static RepairRunnable.NeighborsAndRanges create(Map<String, String> options)
+    {
+        RepairOption option = parse(options, Murmur3Partitioner.instance);
+        return createNeighbordAndRangesForOfflineService(option);
+    }
+
     /**
      * For non-forced repairs, common ranges should be passed through as-is
      */

datastax/cassandra

(cherry picked from commit e857671e58b21b81630c4c77b5ed0682ab98ab31)
(cherry picked from commit d82c8ee4c9e75338a1f7fc3b718f5ef8b2a637fc)
(cherry picked from commit 79a7bbad45deab6434bebb648182c611b2b8e9e8)
(cherry picked from commit fa7971b5dd4af2525fc7cc8d3fcd0d92491efa06)
(cherry picked from commit 487fb581368cb112d61724032a35cf0a31f07aff)
 (Rebase of commit 5bf5500b47da33e337df4e9e7a2699e79e55c42d)
 (Rebase of commit 59c9683d19671be27d98eac2fe058231984b135f)

test/unit/org/apache/cassandra/repair/NeighborsAndRangesTest.java
@@ -19,7 +19,9 @@
 package org.apache.cassandra.repair;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Lists;
@@ -27,12 +29,51 @@ import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.apache.cassandra.dht.Murmur3Partitioner;
 import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.repair.messages.RepairOption;
+import org.apache.cassandra.utils.FBUtilities;
 
 import static org.apache.cassandra.repair.RepairCoordinator.NeighborsAndRanges;
+import static org.apache.cassandra.repair.RepairCoordinator.createNeighbordAndRangesForOfflineService;
+import static org.apache.cassandra.repair.messages.RepairOption.parse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NeighborsAndRangesTest extends AbstractRepairTest
 {
+    @Test
+    public void testCreateNeighbordAndRangesForOfflineService()
+    {
+        Map<String, String> options = new HashMap<>();
+
+        // no hosts
+        assertThatThrownBy(() -> create(options)).hasMessageContaining("There should be at least 1 host");
+        options.put(RepairOption.HOSTS_KEY, FBUtilities.getJustBroadcastAddress().getHostAddress());
+
+        // no ranges
+        assertThatThrownBy(() -> create(options)).hasMessageContaining("Token ranges must be specified");
+        options.put(RepairOption.RANGES_KEY, "0:10,11:20,21:30");
+
+        // no neighor, because the only host is local
+        assertThatThrownBy(() -> create(options)).hasMessageContaining("There should be at least 1 neighbor");
+
+        // with proper neighbors
+        options.put(RepairOption.HOSTS_KEY, "127.0.99.1,127.0.99.2," + FBUtilities.getJustBroadcastAddress().getHostAddress());
+        NeighborsAndRanges neighborsAndRanges = create(options);
+
+        assertThat(neighborsAndRanges.shouldExcludeDeadParticipants).isFalse();
+        assertThat(neighborsAndRanges.participants).hasSize(2); // excluded local host
+        assertThat(neighborsAndRanges.commonRanges).hasSize(1); // all in one common range
+        assertThat(neighborsAndRanges.filterCommonRanges("ks", new String[] { "cf"})).isSameAs(neighborsAndRanges.commonRanges);
+    }
+
+    private static NeighborsAndRanges create(Map<String, String> options)
+    {
+        RepairOption option = parse(options, Murmur3Partitioner.instance);
+        return createNeighbordAndRangesForOfflineService(option);
+    }
+
     /**
      * For non-forced repairs, common ranges should be passed through as-is
      */