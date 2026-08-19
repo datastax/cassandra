msmygit/vscassandra

STAR-1489 (#524)
* STAR-1489 make verb decorator efficient

* STAR-1489 Add Mutator callbacks invoked when locally applying mutations.

* STAR-1489 Allow to override attributes in create/alter keyspace.

* STAR-1489 Added tests.

Co-authored-by: Jake Luciani <jake@datastax.com>

src/java/org/apache/cassandra/service/StorageProxy.java
@@ -226,6 +226,7 @@ public class StorageProxy implements StorageProxyMBean
         {
             assert mutation instanceof Mutation;
             sendToHintedReplicas((Mutation) mutation, targets, responseHandler, localDataCenter, Stage.MUTATION);
+            mutator.onAppliedMutation(mutation);
         };
 
         /*
@@ -842,7 +843,7 @@ public class StorageProxy implements StorageProxyMBean
             {
                 try
                 {
-                    PaxosState.commit(message.payload);
+                    PaxosState.commit(message.payload, p -> mutator.onAppliedProposal(p));
                     if (responseHandler != null)
                         responseHandler.onResponse(null);
                 }
@@ -1795,6 +1796,7 @@ public class StorageProxy implements StorageProxyMBean
 
                 Mutation result = ((CounterMutation) mutation).applyCounterMutation();
                 responseHandler.onResponse(null);
+                mutator.onAppliedCounter(result);
                 sendToHintedReplicas(result, replicaPlan, responseHandler, localDataCenter, Stage.COUNTER_MUTATION);
             }
         };

test/unit/org/apache/cassandra/service/PaxosStateTest.java
@@ -19,6 +19,7 @@ package org.apache.cassandra.service;
 
 import java.nio.ByteBuffer;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.google.common.collect.Iterables;
 
@@ -71,22 +72,27 @@ public class PaxosStateTest
         // CFS should be empty initially
         assertNoDataPresent(cfs, Util.dk(key));
 
+        AtomicBoolean committed = new AtomicBoolean();
+
         // Commit the proposal & verify the data is present
         Commit beforeTruncate = newProposal(0, update);
-        PaxosState.commit(beforeTruncate);
+        PaxosState.commit(beforeTruncate, ignored -> committed.set(true));
         assertDataPresent(cfs, Util.dk(key), "val", value);
+        assertTrue(committed.getAndSet(false));
 
         // Truncate then attempt to commit again, mutation should
         // be ignored as the proposal predates the truncation
         cfs.truncateBlocking();
-        PaxosState.commit(beforeTruncate);
+        PaxosState.commit(beforeTruncate, ignored -> committed.set(true));
         assertNoDataPresent(cfs, Util.dk(key));
+        assertFalse(committed.getAndSet(false));
 
         // Now try again with a ballot created after the truncation
         long timestamp = Nodes.local().getTruncatedAt(update.metadata().id) + 1;
         Commit afterTruncate = newProposal(timestamp, update);
-        PaxosState.commit(afterTruncate);
+        PaxosState.commit(afterTruncate, ignored -> committed.set(true));
         assertDataPresent(cfs, Util.dk(key), "val", value);
+        assertTrue(committed.getAndSet(false));
     }
 
     private Commit newProposal(long ballotMillis, PartitionUpdate update)

datastax/cassandra

(cherry picked from commit 8fc3807cbd0ab0de2d662d9ec9d40f71ad01dc0d)
(cherry picked from commit 0f8abbcf7cde4fccdd17f5377a532b778494bc58)
(cherry picked from commit 7211eb627a202e8a30179bc1b75cec52eb2d0431)
(cherry picked from commit 5201992db8841d699627f592c440842fdb966046)
 (Rebase of commit 4891f83fb162f6c80d3ee80e84df55138c298079)
 (Rebase of commit c3c8a18c50ab83bfac7231d47487974762982f5c)

src/java/org/apache/cassandra/service/StorageProxy.java
@@ -262,6 +262,7 @@ public class StorageProxy implements StorageProxyMBean
         {
             assert mutation instanceof Mutation;
             sendToHintedReplicas((Mutation) mutation, targets, responseHandler, localDataCenter, Stage.MUTATION, requestTime);
+            mutator.onAppliedMutation(mutation);
         };
 
         /*
@@ -921,7 +922,7 @@ public class StorageProxy implements StorageProxyMBean
             {
                 try
                 {
-                    PaxosState.commitDirect(message.payload);
+                    PaxosState.commitDirect(message.payload, p -> mutator.onAppliedProposal(p));
                     if (responseHandler != null)
                         responseHandler.onResponse(null);
                 }
@@ -1959,6 +1960,7 @@ public class StorageProxy implements StorageProxyMBean
 
                 Mutation result = ((CounterMutation) mutation).applyCounterMutation();
                 responseHandler.onResponse(null);
+                mutator.onAppliedCounter(result);
                 sendToHintedReplicas(result, replicaPlan, responseHandler, localDataCenter, Stage.COUNTER_MUTATION, requestTime);
             }
         };

test/unit/org/apache/cassandra/service/PaxosStateTest.java
@@ -18,6 +18,7 @@
 package org.apache.cassandra.service;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.google.common.collect.Iterables;
 
@@ -85,22 +86,27 @@ public class PaxosStateTest
         // CFS should be empty initially
         assertNoDataPresent(cfs, Util.dk(key));
 
+        AtomicBoolean committed = new AtomicBoolean();
+
         // Commit the proposal & verify the data is present
         Commit beforeTruncate = newProposal(0, update);
-        PaxosState.commitDirect(beforeTruncate);
+        PaxosState.commitDirect(beforeTruncate, ignored -> committed.set(true));
         assertDataPresent(cfs, Util.dk(key), "val", value);
+        assertTrue(committed.getAndSet(false));
 
         // Truncate then attempt to commit again, mutation should
         // be ignored as the proposal predates the truncation
         cfs.truncateBlocking();
-        PaxosState.commitDirect(beforeTruncate);
+        PaxosState.commitDirect(beforeTruncate, ignored -> committed.set(true));
         assertNoDataPresent(cfs, Util.dk(key));
+        assertFalse(committed.getAndSet(false));
 
         // Now try again with a ballot created after the truncation
         long timestamp = SystemKeyspace.getTruncatedAt(update.metadata().id) + 1;
         Commit afterTruncate = newProposal(timestamp, update);
-        PaxosState.commitDirect(afterTruncate);
+        PaxosState.commitDirect(afterTruncate, ignored -> committed.set(true));
         assertDataPresent(cfs, Util.dk(key), "val", value);
+        assertTrue(committed.getAndSet(false));
     }
 
     private Commit newProposal(long ballotMicros, PartitionUpdate update)