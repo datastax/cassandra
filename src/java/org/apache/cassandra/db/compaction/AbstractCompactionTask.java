55430ef952f1af3ccc6e208add85a4fb70e7df7b
Berenguer Blasi <berenguer.blasi@ibm.com>
2026-09-09 14:37:46 +0200

    HCD-565 RepairFinishedCompactionTask leak
    RepairFinishedCompactionTask weren't being cleaned up correctly as the
    cleanup mechanics were calling the wrong life cycle management methods.
    
    HCD-565 Split into a compaction task and a non one
    
    Branimir's Runnable removal from CompactionTask
    
    review minors
    
    Missing blank lines

--- src/java/org/apache/cassandra/db/compaction/AbstractCompactionTask.java
+++ src/java/org/apache/cassandra/db/compaction/AbstractCompactionTask.java
@@ -35,12 +35,11 @@ import org.apache.cassandra.db.lifecycle.ILifecycleTransaction;
 import org.apache.cassandra.io.FSDiskFullWriteError;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.cassandra.utils.Throwables;
-import org.apache.cassandra.utils.WrappedRunnable;
 
 import static com.google.common.base.Throwables.propagate;
 
 
-public abstract class AbstractCompactionTask extends WrappedRunnable
+public abstract class AbstractCompactionTask
 {
     protected static final Logger logger = LoggerFactory.getLogger(AbstractCompactionTask.class);
 
@@ -140,6 +139,8 @@ public abstract class AbstractCompactionTask extends WrappedRunnable
             }
         }
     }
+
+    protected abstract void runMayThrow() throws Exception;
 
     /**
      * Executes the task after setting a new observer, normally the observer is the
@@ -256,7 +257,14 @@ public abstract class AbstractCompactionTask extends WrappedRunnable
 
     protected void executeInternal()
     {
-        run();
+        try
+        {
+            runMayThrow();
+        }
+        catch (Exception e)
+        {
+            throw propagate(e);
+        }
     }
 
     // TODO Eventually these three setters should be passed in to the constructor.
